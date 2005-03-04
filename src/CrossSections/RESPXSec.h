82aa3bb607c5c9ebb0ce842eda53bf7809dbe91e
costas <costas.andreopoulos@stfc.ac.uk>
2005-03-04 10:28:48 +0000

    Added the RESPXSec alg. which computes RES dxsec/dt where t is either Q2,W This is model independent cross section and it merely performs an 1-D integration of the double differential cross section d2xsec/dQ2dW. Updated LinkDef and added some comments to DISPXSec.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@9 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/CrossSections/RESPXSec.h
+++ src/CrossSections/RESPXSec.h
@@ -1,47 +1,53 @@
 //____________________________________________________________________________
 /*!
 
-\class    genie::DISPXSec
+\class    genie::RESPXSec
 
-\brief    Computes single differential DIS cross section.
+\brief    Computes single differential RES cross section.
 
           It can be configured to compute either
-            \li dxsec / dx  where \c x is the Bjorken scaling variable, or
-            \li dxsec / dy  where \c y is the Inelasticity
+            \li dxsec / dQ2  where \c Q2 is the momentum transfer, or
+            \li dxsec / dW   where \c W is the hadronic invariant mass
 
           This is a model-independent algorithm. It merely integrates the
           specified double differential cross section.
-
+            
           Is a concrete implementation of the XSecAlgorithmI interface.
             
 \author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
           CCLRC, Rutherford Appleton Laboratory
 
-\created  July 01, 2004
+\created  March 03, 2005
 
 */
 //____________________________________________________________________________
 
-#ifndef _DIS_PXSEC_H_
-#define _DIS_PXSEC_H_
+#ifndef _RES_PXSEC_H_
+#define _RES_PXSEC_H_
 
 #include "Base/XSecAlgorithmI.h"
 
 namespace genie {
 
-class DISPXSec : public XSecAlgorithmI {
+class IntegratorI;
+
+class RESPXSec : public XSecAlgorithmI {
 
 public:
 
-  DISPXSec();
-  DISPXSec(const char * param_set);
-  virtual ~DISPXSec();
+  RESPXSec();
+  RESPXSec(const char * param_set);
+  virtual ~RESPXSec();
 
   //-- XSecAlgorithmI interface implementation
 
   double XSec (const Interaction * interaction) const;
+
+public:
+
+  const XSecAlgorithmI * DoubleDifferentialXSecAlg (void) const;
+  const IntegratorI *    Integrator                (void) const;
 };
 
 }       // genie namespace
-
-#endif  // _DIS_PXSEC_H_
+#endif  // _RES_PXSEC_H_
