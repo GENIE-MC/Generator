e2219c7694715fc877a37bb9aee07bd487830368
costas <costas.andreopoulos@stfc.ac.uk>
2005-04-28 10:39:27 +0000

    Spline can now be constructed from floats /as well as from doubles/
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@68 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Numerical/Spline.cxx
+++ src/Numerical/Spline.cxx
@@ -17,7 +17,14 @@
 */
 //____________________________________________________________________________
 
-#include "Spline.h"
+#include <TNtuple.h>
+#include <TTree.h>
+#include <TSQLServer.h>
+#include <TGraph.h>
+#include <TSpline.h>
+
+#include "Messenger/Messenger.h"
+#include "Numerical/Spline.h"
 
 using namespace genie;
 
@@ -29,34 +36,68 @@ Spline::Spline()
 //___________________________________________________________________________
 Spline::Spline(const char * filename)
 {
-  LoadFromFile(filename);
+  LOG("Spline", pDEBUG) << "Constructing spline from data in text file";
+
+  this->InitSpline();
+  this->LoadFromFile(filename);
 }
 //___________________________________________________________________________
-Spline::Spline(
-          TNtuple & ntuple, const char * var_string, const char * constraint)
+Spline::Spline(TNtuple * ntuple, const char * var, const char * constraint)
 {
-  LoadFromNtuple(ntuple, var_string, constraint);
+  LOG("Spline", pDEBUG) << "Constructing spline from data in a TNtuple";
+
+  this->InitSpline();
+  this->LoadFromNtuple(ntuple, var, constraint);
 }
 //___________________________________________________________________________
-Spline::Spline(
-              TTree & tree, const char * var_string, const char * constraint)
+Spline::Spline(TTree * tree, const char * var, const char * constraint)
 {
-  LoadFromTTree(tree, var_string, constraint);
+  LOG("Spline", pDEBUG) << "Constructing spline from data in a TTree";
+
+  this->InitSpline();
+  this->LoadFromTree(tree, var, constraint);
 }
 //___________________________________________________________________________
 Spline::Spline(TSQLServer * db, const char * query)
 {
-  LoadFromSQLDBase(db, query);
+  LOG("Spline", pDEBUG) << "Constructing spline from data in a MySQL server";
+
+  this->InitSpline();
+  this->LoadFromDBase(db, query);
 }
 //___________________________________________________________________________
-Spline::Spline(int n_entries, double x[], double y[])
+Spline::Spline(int nentries, double x[], double y[])
 {
-  BuildSpline(n_entries, x, y);
+  LOG("Spline", pDEBUG)
+                 << "Constructing spline from the arrays passed to the ctor";
+
+  this->InitSpline();
+  this->BuildSpline(nentries, x, y);
+}
+//___________________________________________________________________________
+Spline::Spline(int nentries, float x[], float y[])
+{
+  LOG("Spline", pDEBUG)
+                 << "Constructing spline from the arrays passed to the ctor";
+
+  double * dblx = new double[nentries];
+  double * dbly = new double[nentries];
+
+  for(int i = 0; i < nentries; i++) {
+     dblx[i] = double( x[i] );
+     dbly[i] = double( y[i] );
+  }
+    
+  this->InitSpline();
+  this->BuildSpline(nentries, dblx, dbly);
+
+  delete [] x;
+  delete [] y;
 }
 //___________________________________________________________________________
 Spline::Spline(const Spline & spline)
 {
-
+  LOG("Spline", pDEBUG) << "Copy constructor";
 }
 //___________________________________________________________________________
 Spline::Spline(const TSpline3 & spline)
@@ -71,36 +112,41 @@ Spline::~Spline()
 //___________________________________________________________________________
 void Spline::LoadFromFile(const char * filename)
 {
+  LOG("Spline", pDEBUG) << "Retrieving data from file: " << filename;
 
 }
 //___________________________________________________________________________
 void Spline::LoadFromNtuple(
-          TNtuple & ntuple, const char * var_string, const char * constraint)
+                     TNtuple * nt, const char * var, const char * constraint)
 {
-  ntuple.Draw(var_string,"","GOFF"); // select all entries - graphics off
+  LOG("Spline", pDEBUG) << "Retrieving data from ntuple: " << nt->GetName();
 
-  int n_entries     = ntuple.GetSelectedRows();
-  double * x_values = ntuple.GetV1();
-  double * y_values = ntuple.GetV2();
+  nt->Draw(var,"","GOFF"); // select all entries - graphics off
 
-  BuildSpline(n_entries, x_values, y_values);
+  int      n = nt->GetSelectedRows();
+  double * x = nt->GetV1();
+  double * y = nt->GetV2();
+
+  this->BuildSpline(n, x, y);
 }
 //___________________________________________________________________________
-void Spline::LoadFromTTree(
-              TTree & tree, const char * var_string, const char * constraint)
+void Spline::LoadFromTree(
+              TTree * tree, const char * var_string, const char * constraint)
 {
-  tree.Draw(var_string,"","GOFF"); // select all entries - graphics off
+  LOG("Spline", pDEBUG) << "Retrieving data from tree: " << tree->GetName();
 
-  int n_entries     = tree.GetSelectedRows();
-  double * x_values = tree.GetV1();
-  double * y_values = tree.GetV2();
+  tree->Draw(var_string,"","GOFF"); // select all entries - graphics off
 
-  BuildSpline(n_entries, x_values, y_values);
+  int      n = tree->GetSelectedRows();
+  double * x = tree->GetV1();
+  double * y = tree->GetV2();
+
+  this->BuildSpline(n, x, y);
 }
 //___________________________________________________________________________
-void Spline::LoadFromSQLDBase(TSQLServer * db,  const char * query)
+void Spline::LoadFromDBase(TSQLServer * db,  const char * query)
 {
-
+  LOG("Spline", pDEBUG) << "Retrieving data from data-base: ";
 }
 //___________________________________________________________________________
 bool Spline::IsWithinValidRange(double x) const
@@ -114,32 +160,63 @@ double Spline::Evaluate(double x) const
 {
   double y = -1;
   
-  if( IsWithinValidRange(x) ) return y = fInterpolator->Eval(x);
+  if( this->IsWithinValidRange(x) ) return y = fInterpolator->Eval(x);
   
   return y;
 }
 //___________________________________________________________________________
+TGraph * Spline::GetAsTGraph(int np, bool scale_with_x) const
+{
+  double xmin = fValidityRange.first;
+  double xmax = fValidityRange.second;
+
+  if(np < 2) np = 2;
+
+  double dx = (xmax - xmin) / (np-1);
+
+  double * x = new double[np];
+  double * y = new double[np];
+
+  for(int i=0; i<np; i++) {
+
+      x[i]  = xmin + i*dx;
+      y[i] = this->Evaluate( x[i] );
+
+      if (scale_with_x) y[i] /= x[i];
+  }
+
+  TGraph * graph = new TGraph(np, x, y);
+
+  return graph;  
+}
+//___________________________________________________________________________
 void Spline::InitSpline(void)
 {
+  LOG("Spline", pDEBUG) << "Initializing spline...";
+  
   fValidityRange.first  = 0.0;
   fValidityRange.second = 0.0;
   
-  if(fInterpolator) delete fInterpolator;
+  fInterpolator = 0;
   
-  fInterpolator         = 0;
+  LOG("Spline", pDEBUG) << "...done initializing spline";
 }
 //___________________________________________________________________________
-void Spline::BuildSpline(int n_entries, double x[], double y[])
+void Spline::BuildSpline(int nentries, double x[], double y[])
 {
-  double x_min = x[ TMath::LocMin(n_entries, x) ]; // minimum x in spline
-  double x_max = x[ TMath::LocMax(n_entries, x) ]; // maximum x in spline
+  LOG("Spline", pDEBUG) << "Building spline...";
 
-  fValidityRange.first  = x_min;
-  fValidityRange.second = x_max;
+  double xmin = x[ TMath::LocMin(nentries, x) ]; // minimum x in spline
+  double xmax = x[ TMath::LocMax(nentries, x) ]; // maximum x in spline
+
+  fValidityRange.first  = xmin;
+  fValidityRange.second = xmax;
 
   if(fInterpolator) delete fInterpolator;
 
-  fInterpolator = new TSpline3("spl3", x, y, n_entries, "0", x_min, x_max);
+  fInterpolator = new TSpline3("spl3", x, y, nentries, "0", xmin, xmax);
+
+  LOG("Spline", pDEBUG) << "...done building spline";  
 }
 //___________________________________________________________________________
 
--- src/Numerical/Spline.h
+++ src/Numerical/Spline.h
@@ -22,10 +22,11 @@
 
 #include <map>
 
-#include <TSpline.h>
-#include <TNtuple.h>
-#include <TTree.h>
-#include <TSQLServer.h>
+class TNtuple;
+class TTree;
+class TSQLServer;
+class TGraph;
+class TSpline3;
 
 using std::pair;
 
@@ -37,26 +38,30 @@ public:
 
   Spline();
   Spline(const char * filename);
-  Spline(TNtuple & ntuple, const char * xy, const char * constraint = 0);
-  Spline(TTree & tree,     const char * xy, const char * constraint = 0);
+  Spline(TNtuple * ntuple, const char * xy, const char * constraint = 0);
+  Spline(TTree * tree,     const char * xy, const char * constraint = 0);
   Spline(TSQLServer * db, const char * query);
   Spline(int nentries, double x[], double y[]);
+  Spline(int nentries, float  x[], float  y[]);
   Spline(const Spline & spline);
   Spline(const TSpline3 & spline);
   virtual ~Spline();
+  
+  void LoadFromFile   (const char * filename);
+  void LoadFromNtuple (TNtuple * nt, const char * xy, const char * constraint = 0);
+  void LoadFromTree   (TTree *   tr, const char * xy, const char * constraint = 0);
+  void LoadFromDBase  (TSQLServer * db,  const char * query);
 
-  void LoadFromFile     (const char * filename);
-  void LoadFromNtuple   (TNtuple & ntuple, const char * xy, const char * constraint = 0);
-  void LoadFromTTree    (TTree & tree,     const char * xy, const char * constraint = 0);
-  void LoadFromSQLDBase (TSQLServer * db,  const char * query);
-    
   bool   IsWithinValidRange (double x) const;
   double Evaluate           (double x) const;
+  
+  TGraph *   GetAsTGraph  (int npoints = 100, bool scale_with_x = false) const;
+  TSpline3 * GetAsTSpline (void) const { return fInterpolator; }
 
 private:
 
-  void InitSpline(void);
-  void BuildSpline(int nentries, double x[], double y[]);
+  void InitSpline       (void);
+  void BuildSpline      (int nentries, double x[], double y[]);
   
   TSpline3 *            fInterpolator;
   pair<double, double>  fValidityRange; // (x_min, x_max)
