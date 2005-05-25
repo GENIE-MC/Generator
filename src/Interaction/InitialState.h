2f902e9f87ef3dcbb95e3a4cd8a879ed7cc258a4
costas <costas.andreopoulos@stfc.ac.uk>
2005-05-25 13:45:24 +0000

    Added more constructors to build init state from (Z,A,nu_pdg) and (target_pdg,nu_pdg) / Made Copy() private / Added AsString() for creating keys / Added private method Create() that is used by all constructors to actually build the InitialState
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@92 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Interaction/InitialState.h
+++ src/Interaction/InitialState.h
@@ -17,6 +17,7 @@
 #define _INITIAL_STATE_H_
 
 #include <iostream>
+#include <string>
 
 #include <TParticlePDG.h>
 #include <TLorentzVector.h>
@@ -25,6 +26,7 @@
 #include "Interaction/Target.h"
 
 using std::ostream;
+using std::string;
 
 namespace genie {
 
@@ -33,38 +35,39 @@ class InitialState {
 public:
 
   InitialState();
+  InitialState(int tgt_pdgc, int probe_pdgc);
+  InitialState(int Z, int A, int probe_pdgc);
   InitialState(const Target & tgt, int probe_pdgc);
   InitialState(const InitialState & initial_state);
   virtual ~InitialState();
 
-  const Target & GetTarget       (void) const { return *fTarget;    }
-  int            GetProbePDGCode (void) const { return  fProbePdgC; }
-  Target *       GetTargetPtr    (void) const { return  fTarget;    }
-  TParticlePDG * GetProbe        (void) const;
-  
+  const Target &   GetTarget       (void) const { return *fTarget;    }
+  int              GetProbePDGCode (void) const { return  fProbePdgC; }
+  Target *         GetTargetPtr    (void) const { return  fTarget;    }
+  TParticlePDG *   GetProbe        (void) const;
   TLorentzVector * GetTargetP4 (RefFrame_t ref_frame = kRfLab) const;
   TLorentzVector * GetProbeP4  (RefFrame_t ref_frame = kRfStruckNucAtRest) const;
   double           GetProbeE   (RefFrame_t ref_frame) const;
    
-  void SetProbePDGCode (int pdg_code) { fProbePdgC = pdg_code; }
-
+  void SetProbePDGCode (int pdg_code);
   void SetTargetP4     (const TLorentzVector & P4); // in LAB-frame
   void SetProbeP4      (const TLorentzVector & P4); // in LAB-frame
 
-  void Copy  (const InitialState & init_state);
-  void Print (ostream & stream) const;
+  string AsString (void) const;
+  void   Print    (ostream & stream) const;
   
   friend ostream & operator << (ostream& stream, const InitialState & init_state);
    
 private:
 
-  void Initialize(void);
+  void Initialize (void);
+  void Copy       (const InitialState & init_state);
+  void Create     (int Z, int A, int probe_pdgc);
 
   int              fProbePdgC; // probe PDG code
   Target *         fTarget;    // nuclear target
   TLorentzVector * fProbeP4;   // probe 4-momentum in LAB-frame
   TLorentzVector * fTargetP4;  // nuclear target 4-momentum in LAB-frame
-
 };
 
 }      // namespace
