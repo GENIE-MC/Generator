0524ea3a00a7653aa5598c92be78caa91ddf02b0
costas <costas.andreopoulos@stfc.ac.uk>
2005-07-02 03:16:41 +0000

    Added place holders for keeping the selected interaction cross section (at the given energy) and the differential cross section (for the selected kinematics). These should be set by the InteractionSelectors and the KinematicalSelectors.
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@154 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Interaction/Interaction.h
+++ src/Interaction/Interaction.h
@@ -42,21 +42,34 @@ public:
   Interaction(const Interaction & interaction);
   virtual ~Interaction();
 
+  void Copy  (const Interaction & interaction);
+  void Reset (void);
+
+  //! get read-only interaction information
   const InitialState &     GetInitialState     (void) const { return *fInitialState;     }
   const ProcessInfo &      GetProcessInfo      (void) const { return *fProcInfo;         }
   const ScatteringParams & GetScatteringParams (void) const { return *fScatteringParams; }
   const XclsTag &          GetExclusiveTag     (void) const { return *fExclusiveTag;     }
 
+  //! get read/write interaction information
   ScatteringParams *       GetScatParamsPtr    (void) const { return fScatteringParams;  }
   InitialState *           GetInitialStatePtr  (void) const { return fInitialState;      }
   
+  //! methods to set/reset and examine whether an XclsTag object has been attached
   void   SetExclusiveTag (const XclsTag & xcls_tag);
   void   ResetExclusive  (void);
   bool   IsExclusive     (void) const { return (fExclusiveTag != 0); }
-  double CrossSection    (void) const { return fXSec; }
 
+  //! get final state primary lepton / uniquely determined from the inputs
   TParticlePDG * GetFSPrimaryLepton (void) const; // tmp
   
+  //! setting/getting cross sections set during event generation
+  void   SetXSec     (double xsec) { fXSec  = xsec; } // to be set when selecting interaction
+  void   SetDiffXSec (double xsec) { fdXSec = xsec; } // to be set when selecting kinematics
+  double XSec        (void) const  { return fXSec;  }
+  double DiffXSec    (void) const  { return fdXSec; }
+  
+  //! printing itself and the interaction string code
   string AsString (void)             const;
   void   Print    (ostream & stream) const;
 
@@ -64,11 +77,17 @@ public:
   
 private:
 
+  void Init (void);
+
+  //! initial state, process info, scattering parameters and exclusive information
   InitialState *     fInitialState;     ///< Initial State info
   ProcessInfo *      fProcInfo;         ///< Process info (scattering, weak current,...)
   ScatteringParams * fScatteringParams; ///< Scattering parameters
   XclsTag *          fExclusiveTag;     ///< Additional info for exclusive channels
-  double             fXSec;             ///< XSec for the process as set during event generation
+
+  //! cross section for this interaction
+  double fXSec;    ///< xsec for the given energy
+  double fdXSec;   ///< diff. xsec for the given kinematical parameters
 };
 
 }      // genie namespace
