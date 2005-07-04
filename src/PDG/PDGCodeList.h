349ce964a447a3b43e8772d2f457c1ea0f77e080
costas <costas.andreopoulos@stfc.ac.uk>
2005-07-04 07:45:05 +0000

    Changing PDGCodeList to inherit from vector<int> rather than list<int> as I need random access. Protect PDGCodeList so that no duplicate pdg-codes can be present.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@155 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/PDG/PDGCodeList.h
+++ src/PDG/PDGCodeList.h
@@ -16,15 +16,15 @@
 #ifndef _PDG_CODE_LIST_H_
 #define _PDG_CODE_LIST_H_
 
-#include <list>
+#include <vector>
 #include <ostream>
 
-using std::list;
+using std::vector;
 using std::ostream;
 
 namespace genie {
 
-class PDGCodeList : public list<int> {
+class PDGCodeList : public vector<int> {
 
 public :
 
@@ -32,20 +32,22 @@ public :
   PDGCodeList(size_type n);
   ~PDGCodeList();
 
-  //-- override all list<int> insertion methods to explicitly check for
-  //   PDG code validity
+  //-- override the vector<int> insertion methods to explicitly check for
+  //   PDG code validity and that no PDG code is listed more than once
 
-  void push_front (int pdg_code);
   void push_back  (int pdg_code);
+  void insert     (iterator pos, size_type n, const int& x);
 
-  //-- print list
+  //-- print 
   
   void Print(ostream & stream) const;
   friend ostream & operator << (ostream & stream, const PDGCodeList & list);
 
 private:
 
-  bool ExistsInPDGLibrary(int pdg_code);
+  bool CheckPDGCode        (int pdg_code);
+  bool ExistsInPDGLibrary  (int pdg_code);
+  bool ExistsInPDGCodeList (int pdg_code);
 };
 
 }      // genie namespace
