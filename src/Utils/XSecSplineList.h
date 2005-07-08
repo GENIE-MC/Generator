49a895468ac5bbc37cacd5d06d10390df8150871
costas <costas.andreopoulos@stfc.ac.uk>
2005-07-08 08:52:25 +0000

    Fix incorrect parsing problem when loading XSecSplineList from XML file.
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@163 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Utils/XSecSplineList.h
+++ src/Utils/XSecSplineList.h
@@ -38,38 +38,33 @@ public:
   static XSecSplineList * Instance();
 
   //-- query the existence, access or create a spline
-
   bool           SplineExists (const XSecAlgorithmI * alg, const Interaction * i) const;
   const Spline * GetSpline    (const XSecAlgorithmI * alg, const Interaction * i) const;
   void           CreateSpline (const XSecAlgorithmI * alg, const Interaction * i,
                                    int nknots = -1, double Emin = -1, double Emax = -1);
   //-- set XSecSplineList options
-
   void   SetLogE   (bool   on); ///< set opt to build splines as f(E) or as f(logE)
   void   SetNKnots (int    nk); ///< set default number of knots for building the spline
   void   SetMinE   (double Ev); ///< set default minimum energy for xsec splines
   void   SetMaxE   (double Ev); ///< set default maximum energy for xsec splines
-  
-  //-- read XSecSplineList options
 
+  //-- read XSecSplineList options
   bool   UseLogE     (void) const { return fUseLogE;     }
   int    NKnots      (void) const { return fNKnots;      }
   double Emin        (void) const { return fEmin;        }
   double Emax        (void) const { return fEmax;        }
 
   //-- save to / load from XML file
-
   void               SaveAsXml   (string filename) const;
   XmlParserStatus_t  LoadFromXml (string filename, bool keep = false);
-  
+
   //-- print available splines
-  
   void   Print (ostream & stream) const;
   friend ostream & operator << (ostream & stream, const XSecSplineList & xsl);
-  
+
 private:
 
-  XSecSplineList(); 
+  XSecSplineList();
   XSecSplineList(const XSecSplineList & spline_list);
   virtual ~XSecSplineList();
 
@@ -82,9 +77,9 @@ private:
   double fEmin;
   double fEmax;
   double fEExtrap;
-  
+
   map<string, Spline *> fSplineMap; ///< xsec_alg_name/param_set/interaction -> Spline
-  
+
   struct Cleaner {
       void DummyMethodAndSilentCompiler() { }
       ~Cleaner() {
@@ -94,7 +89,6 @@ private:
          }
       }
   };
-
   friend struct Cleaner;
 };
 
