6ea115efcfffbca5770c68b3c2bd1e3752f6c1f5
costas <costas.andreopoulos@stfc.ac.uk>
2005-07-18 15:08:22 +0000

    Added protection against y=1 that hits a log(1-y). Removed some methods in favor of Algorithm's SubAlg() and SubAlgWithDefault()
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@169 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/InverseMuonDecay/BardinIMDRadCorXSec.cxx
+++ src/InverseMuonDecay/BardinIMDRadCorXSec.cxx
@@ -17,12 +17,12 @@
           (for futher details, see the documentation of the Bardin-Dokuchaeva
           model diffential IMD cross section algorithms and the Bardin paper,
           cited below).
-          
+
           BardinIMDRadCorXSec is a concrete implementation of the
           XSecAlgorithmI interface. \n
 
 \ref      D.Yu.Bardin and V.A.Dokuchaeva, Nucl.Phys.B287:839 (1987)
-          
+
 \author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
           CCLRC, Rutherford Appleton Laboratory
 
@@ -33,7 +33,6 @@
 
 #include <iostream>
 
-#include "Algorithm/AlgFactory.h"
 #include "Conventions/Constants.h"
 #include "Conventions/RefFrame.h"
 #include "InverseMuonDecay/BardinIMDRadCorXSec.h"
@@ -66,84 +65,39 @@ BardinIMDRadCorXSec::~BardinIMDRadCorXSec()
 //____________________________________________________________________________
 double BardinIMDRadCorXSec::XSec(const Interaction * interaction) const
 {
-  const XSecAlgorithmI * pxsec      = this->DiffXSec();
-  const IntegratorI *    integrator = this->Integrator();
+  //-- get a differential IMD cross section sub-algorithm
+  const XSecAlgorithmI * pxsec =
+           dynamic_cast<const XSecAlgorithmI *> (this->SubAlg(
+                          "partial-xsec-alg-name", "partial-xsec-param-set"));
 
+  //-- get a 1-D nuerical integrator or set default
+  const IntegratorI * integrator =
+          dynamic_cast<const IntegratorI *> (this->SubAlgWithDefault(
+                                 "integrator-name","","genie::Simpson1D",""));
   const int    nsteps  = 201;
   const double min     = 0;
   const double max     = 0.999;
   const double step    = (max-min)/(nsteps-1);
 
   UnifGrid grid;
-
   grid.AddDimension(nsteps, min, max);
 
   FunctionMap fmap(grid);
 
-  //-- all kinematical cuts (energy threshold, physical y range) are applied
-  //   within the differential cross section algorithm - returns 0 if kinematic
-  //   params are not valid.
-  
+  //-- all kinematical cuts (energy threshold, physical y range) are
+  //   applied within the differential cross section algorithm - returns 0
+  //   if kinematic params are not valid.
+
   for(int i = 0; i < nsteps; i++) {
-
-    double y  = min + i * step;
-
+    double y = min + i * step;
     interaction->GetScatParamsPtr()->Set("y",y);
-    
-    double dsig_dy  = pxsec->XSec(interaction);
-
+    double dsig_dy = pxsec->XSec(interaction);
     fmap.AddPoint(dsig_dy, i);
   }
-
   double sig = integrator->Integrate(fmap);
 
   LOG("InverseMuDecay", pDEBUG) << "*** xsec[IMD] = " << sig;
 
   return sig;
-}
-//____________________________________________________________________________
-const XSecAlgorithmI * BardinIMDRadCorXSec::DiffXSec(void) const
-{
- LOG("InverseMuDecay", pINFO)
-                           << "Getting requested differential xsec algorithm";
-
- assert(fConfig->Exists("partial-xsec-alg-name") &&
-                                  fConfig->Exists("partial-xsec-param-set"));
-
- string alg_name  = fConfig->GetString("partial-xsec-alg-name");
- string param_set = fConfig->GetString("partial-xsec-param-set");
-
- //-- Get the requested cross section calculator from the AlgFactory
-
- AlgFactory * algf = AlgFactory::Instance();
-
- const Algorithm * alg_base = algf->GetAlgorithm(alg_name, param_set);
-
- const XSecAlgorithmI * xs = dynamic_cast<const XSecAlgorithmI *> (alg_base);
-
- assert(xs);
-
- return xs;
-}
-//____________________________________________________________________________
-const IntegratorI * BardinIMDRadCorXSec::Integrator(void) const
-{
- LOG("InverseMuDecay", pINFO) << "Getting requested integrator algorithm";
-
- // read integrator name from config or set default
- string integrator_name = ( fConfig->Exists("integrator-name") ) ?
-                  fConfig->GetString("integrator-name") : "genie::Simpson1D";
- 
- // get integrator algorithm
-
- AlgFactory * algf = AlgFactory::Instance();
-
- const Algorithm * alg_base = algf->GetAlgorithm(integrator_name);
-
- const IntegratorI * intgr = dynamic_cast<const IntegratorI *> (alg_base);
-
- assert(intgr);
-
- return intgr;
 }
 //____________________________________________________________________________
