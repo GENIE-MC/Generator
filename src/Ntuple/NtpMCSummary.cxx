3258da299ada9318d865536ff731ba450a19d235
costas <costas.andreopoulos@stfc.ac.uk>
2005-07-18 15:06:23 +0000

    Ongoing work with the standard output ntuple. Added NtpGHepAnalyzer for extracting the summary information by analyzing the GHEP record.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@168 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Ntuple/NtpMCSummary.cxx
+++ src/Ntuple/NtpMCSummary.cxx
@@ -18,6 +18,7 @@
 #include "EventGeneration/EventRecord.h"
 #include "Interaction/Interaction.h"
 #include "Ntuple/NtpMCSummary.h"
+#include "Ntuple/NtpGHepAnalyzer.h"
 
 using namespace genie;
 
@@ -60,56 +61,70 @@ void NtpMCSummary::PrintToStream(ostream & stream) const
 //____________________________________________________________________________
 void NtpMCSummary::Copy(const EventRecord & evrec)
 {
-  const Interaction * interaction = evrec.GetInteraction();
+  NtpGHepAnalyzer ghep_analyzer;
 
-  const InitialState &     init_state = interaction->GetInitialState();
-  const ProcessInfo &      proc_info  = interaction->GetProcessInfo();
-  const ScatteringParams & scp        = interaction->GetScatteringParams();
-  const Target &           tgt        = init_state.GetTarget();
+  ghep_analyzer.AnalyzeEventRecord(evrec);
 
-  TLorentzVector * p4lprobe = init_state.GetProbeP4(kRfLab);
-  TLorentzVector * p4probe  = init_state.GetProbeP4(kRfStruckNucAtRest);
+  this->probe     = ghep_analyzer.ProbePdgCode();
+  this->fsl       = ghep_analyzer.FslPdgCode();
+  this->tgt       = ghep_analyzer.TgtPdgCode();
+  this->nucl      = ghep_analyzer.HitNuclPdgCode();
+  this->iqrk      = ghep_analyzer.HitQuarkPdgCode();
+  this->fqrk      = ghep_analyzer.OutgQuarkPdgCode();
+  this->res       = ghep_analyzer.ResPdgCode();
+  this->ch        = ghep_analyzer.CharmHadPdgCode();
+  this->Z         = ghep_analyzer.NuclTgtZ();
+  this->A         = ghep_analyzer.NuclTgtA();
+  this->N         = ghep_analyzer.NuclTgtN();
+  this->scat      = ghep_analyzer.ScatType();
+  this->proc      = ghep_analyzer.ProcType();
+  this->x         = ghep_analyzer.KineX();
+  this->y         = ghep_analyzer.KineY();
+  this->z         = ghep_analyzer.FragmZ();
+  this->Q2        = ghep_analyzer.KineQ2();
+  this->W         = ghep_analyzer.KineW();
+  this->xsec      = ghep_analyzer.XSec();
+  this->dxsec     = ghep_analyzer.dXSec();
+  this->emfrac    = ghep_analyzer.EmFrac();
 
-  this->probe     = init_state.GetProbePDGCode();
-  this->fsl       = 0;
-  this->tgt       = tgt.PDGCode();
-  this->nucl      = tgt.StruckNucleonPDGCode();
-  this->iqrk      = 0;
-  this->fqrk      = 0;
-  this->Z         = tgt.Z();
-  this->A         = tgt.A();
-  this->N         = tgt.N();  
-  this->scat      = proc_info.ScatteringTypeId();
-  this->proc      = proc_info.InteractionTypeId();
-  this->x         = (scp.Exists("x"))  ? scp.GetDouble("x")  : 0.;
-  this->y         = (scp.Exists("y"))  ? scp.GetDouble("y")  : 0.;
-  this->z         = 0;
-  this->Q2        = (scp.Exists("Q2")) ? scp.GetDouble("Q2") : 0.;
-  this->W         = (scp.Exists("W"))  ? scp.GetDouble("W")  : 0.;
-  this->xsec      = 0;
-  this->p4p[0]    = p4probe->Px();
-  this->p4p[1]    = p4probe->Py();
-  this->p4p[2]    = p4probe->Pz();
-  this->p4p[3]    = p4probe->Energy();
-  this->p4nucl[0] = tgt.StruckNucleonP4()->Px();
-  this->p4nucl[1] = tgt.StruckNucleonP4()->Py();
-  this->p4nucl[2] = tgt.StruckNucleonP4()->Pz();
-  this->p4nucl[3] = tgt.StruckNucleonP4()->Energy();
-  this->p4fsl[0]  =  0;
-  this->p4fsl[1]  =  0;
-  this->p4fsl[2]  =  0;
-  this->p4fsl[3]  =  0;
-  this->p4fsl2[0] =  0;
-  this->p4fsl2[1] =  0;
-  this->p4fsl2[2] =  0;
-  this->p4fsl2[3] =  0;
-  this->p4fsh[0]  =  0;
-  this->p4fsh[1]  =  0;
-  this->p4fsh[2]  =  0;
-  this->p4fsh[3]  =  0;
+  this->v[0]      = ghep_analyzer.Vtx().X();
+  this->v[1]      = ghep_analyzer.Vtx().Y();
+  this->v[2]      = ghep_analyzer.Vtx().Z();
+  this->v[3]      = ghep_analyzer.Vtx().T();
 
-  delete p4lprobe;
-  delete p4probe;
+  this->p4p[0]    = ghep_analyzer.Probe4P().Px();
+  this->p4p[1]    = ghep_analyzer.Probe4P().Py();
+  this->p4p[2]    = ghep_analyzer.Probe4P().Pz();
+  this->p4p[3]    = ghep_analyzer.Probe4P().E();
+
+  this->p4nucl[0] = ghep_analyzer.HitNucl4P().Px();
+  this->p4nucl[1] = ghep_analyzer.HitNucl4P().Py();
+  this->p4nucl[2] = ghep_analyzer.HitNucl4P().Pz();
+  this->p4nucl[3] = ghep_analyzer.HitNucl4P().E();
+
+  this->p4fsl[0]  = ghep_analyzer.Fsl4P().Px();
+  this->p4fsl[1]  = ghep_analyzer.Fsl4P().Py();
+  this->p4fsl[2]  = ghep_analyzer.Fsl4P().Pz();
+  this->p4fsl[3]  = ghep_analyzer.Fsl4P().E();
+
+  this->p4fsh[0]  = ghep_analyzer.HadShw4P().Px();
+  this->p4fsh[1]  = ghep_analyzer.HadShw4P().Py();
+  this->p4fsh[2]  = ghep_analyzer.HadShw4P().Pz();
+  this->p4fsh[3]  = ghep_analyzer.HadShw4P().E();
+
+  this->p4fsl2[0] = 0;
+  this->p4fsl2[1] = 0;
+  this->p4fsl2[2] = 0;
+  this->p4fsl2[3] = 0;
+
+  this->np   = ghep_analyzer.NumProton();
+  this->nn   = ghep_analyzer.NumNeutron();
+  this->npi0 = ghep_analyzer.NumPi0();
+  this->npip = ghep_analyzer.NumPiPlus();
+  this->npim = ghep_analyzer.NumPiMinus();
+  this->nK0  = ghep_analyzer.NumK0();
+  this->nKp  = ghep_analyzer.NumKPlus();
+  this->nKm  = ghep_analyzer.NumKMinus();
 }
 //____________________________________________________________________________
 void NtpMCSummary::Copy(const NtpMCSummary & mcs)
@@ -120,6 +135,8 @@ void NtpMCSummary::Copy(const NtpMCSummary & mcs)
   this->nucl      =  mcs.nucl;
   this->iqrk      =  mcs.iqrk;
   this->fqrk      =  mcs.fqrk;
+  this->res       =  mcs.res;
+  this->ch        =  mcs.ch;
   this->Z         =  mcs.Z;
   this->A         =  mcs.A;
   this->N         =  mcs.N;
@@ -130,7 +147,9 @@ void NtpMCSummary::Copy(const NtpMCSummary & mcs)
   this->z         =  mcs.z;
   this->Q2        =  mcs.Q2;
   this->W         =  mcs.W;
-  this->xsec      =  mcs.xsec;  
+  this->xsec      =  mcs.xsec;
+  this->dxsec     =  mcs.dxsec;
+  this->emfrac    =  mcs.emfrac;
   this->p4p[0]    =  mcs.p4p[0];
   this->p4p[1]    =  mcs.p4p[1];
   this->p4p[2]    =  mcs.p4p[2];
@@ -151,6 +170,14 @@ void NtpMCSummary::Copy(const NtpMCSummary & mcs)
   this->p4fsh[1]  =  mcs.p4fsh[1];
   this->p4fsh[2]  =  mcs.p4fsh[2];
   this->p4fsh[3]  =  mcs.p4fsh[3];
+  this->np        =  mcs.np;
+  this->nn        =  mcs.nn;
+  this->npi0      =  mcs.npi0;
+  this->npip      =  mcs.npip;
+  this->npim      =  mcs.npim;
+  this->nK0       =  mcs.nK0;
+  this->nKp       =  mcs.nKp;
+  this->nKm       =  mcs.nKm;
 }
 //____________________________________________________________________________
 void NtpMCSummary::Init(void)
@@ -161,6 +188,8 @@ void NtpMCSummary::Init(void)
   this->nucl      =  0;
   this->iqrk      =  0;
   this->fqrk      =  0;
+  this->res       =  0;
+  this->ch        =  0;
   this->Z         =  0;
   this->A         =  0;
   this->N         =  0;
@@ -171,7 +200,9 @@ void NtpMCSummary::Init(void)
   this->z         =  0.;
   this->Q2        =  0.;
   this->W         =  0.;
-  this->xsec      =  0.;  
+  this->xsec      =  0.;
+  this->dxsec     =  0.;
+  this->emfrac    =  0.;
   this->p4p[0]    =  0.;
   this->p4p[1]    =  0.;
   this->p4p[2]    =  0.;
@@ -192,5 +223,13 @@ void NtpMCSummary::Init(void)
   this->p4fsh[1]  =  0.;
   this->p4fsh[2]  =  0.;
   this->p4fsh[3]  =  0.;
+  this->np        =  0;
+  this->nn        =  0;
+  this->npi0      =  0;
+  this->npip      =  0;
+  this->npim      =  0;
+  this->nK0       =  0;
+  this->nKp       =  0;
+  this->nKm       =  0;
 }
 //____________________________________________________________________________
--- src/Ntuple/NtpMCSummary.h
+++ src/Ntuple/NtpMCSummary.h
@@ -47,32 +47,45 @@ public :
   // rule-breaking field data members not prefaced by "f" and mostly lowercase
   // with in -some cases- absurdely abbreviated & cryptic names for fast typing
   // from the ROOT command line
+  int          probe;     ///< Probe (v,e...) PDG code
+  int          fsl;       ///< Final state primary lepton PDG code
+  int          tgt;       ///< Nuclear target PDG code
+  int          nucl;      ///< Struck nucleon PDG code
+  int          iqrk;      ///< Struck quark PDG code
+  int          fqrk;      ///< Outgoing quark PDG code
+  int          res;       ///< Resonance PDG code
+  int          ch;        ///< Charm hadron PDG code
+  int          Z;         ///< Nuclear target: Z
+  int          A;         ///< Nuclear target: A
+  int          N;         ///< Nuclear target: N
+  int          scat;      ///< Scattering type (QEL, DIS, RES, COH, ...)
+  int          proc;      ///< Process type (E/M, CC, NC, ...)
+  float        x;         ///< Bjorken x
+  float        y;         ///< Inelasticity
+  float        z;         ///< Fragmentation z
+  float        Q2;        ///< Momentum transfer (>0 = -q2)
+  float        W;         ///< Hadronic invariant mass
+  float        xsec;      ///< Cross section at this energy
+  float        dxsec;     ///< Cross section for the selected kinematics
+  float        emfrac;    ///< E/M fraction
+  float        v[4];      ///< Interaction vertex (x,y,z,t)
+  float        p4p[4];    ///< Probe 4-P (px,py,pz,E) in LAB
+  float        p4nucl[4]; ///< Struck nucleon 4-P (px,py,py,E) in LAB
+  float        p4fsl[4];  ///< Final state primary lepton 4-P (px,py,py,E) in LAB
+  float        p4fsl2[4]; ///<
+  float        p4fsh[4];  ///< Hadronic system 4-P (px,py,py,E) in LAB
+  unsigned int np;        ///< Number of generated protons
+  unsigned int nn;        ///< Number of generated neutrons
+  unsigned int npi0;      ///< Number of generated pi^0
+  unsigned int npip;      ///< Number of generated pi^+
+  unsigned int npim;      ///< Number of generated pi^-
+  unsigned int nK0;       ///< Number of generated K^0
+  unsigned int nKp;       ///< Number of generated K^+
+  unsigned int nKm;       ///< Number of generated K^-
 
-  int    probe;     ///< Probe (v,e...) PDG Code
-  int    fsl;       ///< Final state primary lepton
-  int    tgt;       ///< Nuclear target PDG Code
-  int    nucl;      ///< Struck nucleon PDG Code
-  int    iqrk;      ///< Struck quark PDG Code
-  int    fqrk;      ///< Outgoing quark PDG Code
-  int    Z;         ///< Nuclear target: Z
-  int    A;         ///< Nuclear target: A
-  int    N;         ///< Nuclear target: N    
-  int    scat;      ///< Scattering type (QEL, DIS, RES, COH, ...)
-  int    proc;      ///< Process type (E/M, CC, NC, ...)
-  float  x;         ///< Bjorken x
-  float  y;         ///< Inelasticity
-  float  z;         ///< 
-  float  Q2;        ///< Momentum transfer (>0 = -q2)
-  float  W;         ///< Hadronic invariant mass  
-  float  xsec;      ///< Cross section for this interaction
-  float  v[4];      ///< Interaction vertex (x,y,z,t)
-  float  p4p[4];    ///< Probe 4-momentum (px,py,pz,E) in LAB
-  float  p4nucl[4]; ///< Struck nucleon 4-momentum (px,py,py,E) in LAB
-  float  p4fsl[4];  ///< Final state primary lepton 4-momentum (px,py,py,E) in LAB
-  float  p4fsl2[4]; ///< 
-  float  p4fsh[4];  ///< Hadronic system 4-momentum (px,py,py,E) in LAB
+private:
 
-  ClassDef(NtpMCSummary, 1)    
+ClassDef(NtpMCSummary, 1)
 };
 
 }      // genie namespace
