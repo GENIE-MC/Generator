e579a1ca1882b3284a4caf4902da1ef18ea7aafc
costas <costas.andreopoulos@stfc.ac.uk>
2005-07-18 15:08:55 +0000

    Added a bit for the IMD cross section
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@170 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Nuclear/NuclearTargetXSec.cxx
+++ src/Nuclear/NuclearTargetXSec.cxx
@@ -7,7 +7,7 @@
           nucleon cross sections.
 
           Is a concrete implementation of the XSecAlgorithmI interface.
-          
+
 \author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
           CCLRC, Rutherford Appleton Laboratory
 
@@ -45,60 +45,65 @@ NuclearTargetXSec::~NuclearTargetXSec()
 //____________________________________________________________________________
 double NuclearTargetXSec::XSec(const Interaction * interaction) const
 {
-  //--- get free nucleon cross section calculator
-  
+  double xsec = 0;
+
+  //--- get particle (nucleon, electron) cross section calculator
   const Algorithm * xsec_alg_base = this->SubAlg(
-                "free-nucleon-xsec-alg-name", "free-nucleon-xsec-param-set");
-  const XSecAlgorithmI * free_nucleon_xsec_alg =
+              "free-particle-xsec-alg-name", "free-particle-xsec-param-set");
+  const XSecAlgorithmI * free_particle_xsec_alg =
                          dynamic_cast<const XSecAlgorithmI *>(xsec_alg_base);
 
-  //--- get a cloned interaction
-  Interaction ci(*interaction);
-  
-  //--- get number of proton/neutrons/nucleons
-
+  //--- get number of proton/neutrons/nucleons/e
   const Target & tgt = interaction->GetInitialState().GetTarget();
-
   int Z = tgt.Z();
   int N = tgt.N();
   int A = tgt.A();
 
   //--- check whether a struck nucleon was set
-
   int  pdgc = tgt.StruckNucleonPDGCode();
   bool struck_nuc_set = pdg::IsProton(pdgc) || pdg::IsNeutron(pdgc);
 
-  //--- compute the cross section
+  //--- get the process type
+  const ProcessInfo & proc_info = interaction->GetProcessInfo();
 
-  double xsec = 0;
+  //--- handle IMD:
+  if(proc_info.IsInverseMuDecay()) {
+    double xsec_e = free_particle_xsec_alg->XSec(interaction);
+    xsec = Z*xsec_e;
+    return xsec;
+  }
+
+  //--- handle QEL,DIS,RES,COH:
+
+  //--- get a cloned interaction
+  Interaction ci(*interaction);
 
   if(struck_nuc_set) {
-
     // get cross section for a free struck nucleon (p or n) and multiply
-    // with the number of p or n in the target    
+    // with the number of p or n in the target
     LOG("NuclXSec", pDEBUG)
          << "Computing xsec as a weighted average over nucl.target's "
                             << (pdg::IsProton(pdgc) ? "protons" : "neutrons");
 
     int nnuc = pdg::IsProton(pdgc) ? Z : N;
-    double xsec_nuc = free_nucleon_xsec_alg->XSec(&ci);
+    double xsec_nuc = free_particle_xsec_alg->XSec(&ci);
     xsec = nnuc * xsec_nuc;
 
-  } else {    
+  } else {
     // get cross section for free nucleons (p,n) compute a weighted average
     LOG("NuclXSec", pDEBUG)
        << "Computing xsec as a weighted average over nucl.target's nucleons";
-    
+
     Target * ctgt = ci.GetInitialStatePtr()->GetTargetPtr();
-    
+
     ctgt->SetStruckNucleonPDGCode(kPdgProton);
-    double xsec_p = free_nucleon_xsec_alg->XSec(&ci);
-    
+    double xsec_p = free_particle_xsec_alg->XSec(&ci);
+
     ctgt->SetStruckNucleonPDGCode(kPdgNeutron);
-    double xsec_n = free_nucleon_xsec_alg->XSec(&ci);
-    
+    double xsec_n = free_particle_xsec_alg->XSec(&ci);
+
     xsec = (Z*xsec_p + N*xsec_n) / A;
-  }  
+  }
   LOG("NuclXSec", pDEBUG)
            << "xsec for a nucl.target with (Z,A) = ("
                                          << Z << ", " << A << ") = " << xsec;
