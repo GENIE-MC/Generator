ad5ac6ecd35c4ba88d7e5e56d6895d2d542d7427
costas <costas.andreopoulos@stfc.ac.uk>
2005-08-17 17:16:23 +0000

    Committing the EVGDrivers package split by the reorganized EventGeneration and GMCJob packages
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@188 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/EVGDrivers/LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace genie;
+
+#pragma link C++ class genie::GENIE;
+#pragma link C++ class genie::GMCJob;
+#pragma link C++ class genie::GPool;
+
+#endif
