ea764b4630ff1dad79d654b84907c3b584932f0e
costas <costas.andreopoulos@stfc.ac.uk>
2005-08-22 18:01:22 +0000

    Copied from the XML package which is about to get removed
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@203 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Conventions/XmlParserStatus.h
@@ -0,0 +1,51 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::XmlParserStatus
+
+\brief    Encapsulates an XML document parsing status
+
+\author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+          CCLRC, Rutherford Appleton Laboratory
+
+\created  May 4, 2004
+
+*/
+//_______________________________________________________________________________
+
+#ifndef _XML_PARSER_STATUS_H_
+#define _XML_PARSER_STATUS_H_
+
+namespace genie {
+
+typedef enum EXmlParseStatus {
+
+  kXmlUndefined   = -1,
+  kXmlOK          =  0,
+  kXmlNotParsed   =  1,
+  kXmlEmpty       =  2,
+  kXmlInvalidRoot =  3         
+
+} XmlParserStatus_t;
+
+
+class XmlParserStatus {
+
+  public:
+
+     static char * AsString(XmlParserStatus_t status) {
+
+       switch(status) {
+         case kXmlUndefined:   return "Undefined state";                       break;
+         case kXmlOK:          return "XML document succesfully parsed";       break;
+         case kXmlNotParsed:   return "XML document parsing failed";           break;
+         case kXmlEmpty:       return "XML document is empty";                 break;
+         case kXmlInvalidRoot: return "XML document has invalid root element"; break;
+         default:              return "unrecognized XMLParseStatus_t enum";    break;
+       }
+     }
+};
+
+}       // namespace
+#endif  // _XML_PARSER_STATUS_H_
+
