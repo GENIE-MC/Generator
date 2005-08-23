b872c899a0c6431aba76d5455e40e2ee211eb04a
costas <costas.andreopoulos@stfc.ac.uk>
2005-08-23 18:30:51 +0000

    Added code for loading/serving Fermi momenta tables and locating the Fermi momentum for specific a (target,nucleon) within an input table.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@213 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Nuclear/FermiMomentumTablePool.h
@@ -0,0 +1,66 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::FermiMomentumTablePool
+
+\brief    Singleton class to load & serve tables of Fermi momentum constants
+
+\author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+          CCLRC, Rutherford Appleton Laboratory
+
+\created  August 18, 2005
+
+*/
+//____________________________________________________________________________
+
+#ifndef _FERMI_MOMENTUM_TABLE_POOL_H_
+#define _FERMI_MOMENTUM_TABLE_POOL_H_
+
+#include <map>
+#include <string>
+
+#include "Conventions/XmlParserStatus.h"
+
+using std::map;
+using std::string;
+
+namespace genie {
+
+class FermiMomentumTable;
+
+class FermiMomentumTablePool
+{
+public:
+
+  static FermiMomentumTablePool * Instance(void);
+
+  const FermiMomentumTable * GetTable(string name);
+
+private:
+
+  FermiMomentumTablePool();
+  FermiMomentumTablePool(const FermiMomentumTablePool & fmc);
+  virtual ~FermiMomentumTablePool();
+
+  bool LoadTables (void);
+  XmlParserStatus_t ParseXMLTables (string filename);
+
+  static FermiMomentumTablePool * fInstance;
+
+  map<string, FermiMomentumTable *> fKFSets; // kf tables
+
+  struct Cleaner {
+      void DummyMethodAndSilentCompiler() { }
+      ~Cleaner() {
+         if (FermiMomentumTablePool::fInstance !=0) {
+            delete FermiMomentumTablePool::fInstance;
+            FermiMomentumTablePool::fInstance = 0;
+         }
+      }
+  };
+  friend struct Cleaner;
+};
+
+}      // genie namespace
+
+#endif // _FERMI_MOMENTUM_TABLE_POOL_H_
