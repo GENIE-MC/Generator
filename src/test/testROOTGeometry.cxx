dbecd26c85be9303847c6b17d4ca3ba9153b26a3
anselmo <anselmo>
2005-08-23 14:45:07 +0000

    added Materials
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@210 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/test/testROOTGeometry.cxx
+++ src/test/testROOTGeometry.cxx
@@ -18,19 +18,16 @@ using namespace genie;
 int main(int argc, char ** argv)
 {
 
-  ROOTGeomAnalyzer* root_analyzer = new ROOTGeomAnalyzer();
-  
-  root_analyzer->Load("$GENIE/src/test/TestGeometry.root");
+  ROOTGeomAnalyzer* root_analyzer = new ROOTGeomAnalyzer("$GENIE/src/test/TestGeometry.root");
   //root_analyzer->Load("/eth/store_6/home_6/amerega/TEST/vgm.2.03/examples/E01/N03/bin/Linux-g++/Geometry.root");
 
-  //root_analyzer->SetVtxMaterial("Lead");
-
-   
+  
   TLorentzVector* x= new TLorentzVector(0,0,0,0);
   TLorentzVector* p= new TLorentzVector(1,0,0,1);
-
+  
   root_analyzer->ComputePathLengths(*x,*p);
   //root_analyzer->test();
+
   
   return 0;
 }
