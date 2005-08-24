c3c0d40590f4c22b45cf2fe0f0faa3eec0e13b54
costas <costas.andreopoulos@stfc.ac.uk>
2005-08-24 15:13:52 +0000

    Moved the Print(ostream&) method to the GHepRecord base class as this is what gets passed to the event record visitors. EventRecord's Print(..) was reduced to calling GHepRecord::Print(..)
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@219 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/EVGCore/EventRecord.cxx
+++ src/EVGCore/EventRecord.cxx
@@ -16,20 +16,12 @@
 */
 //____________________________________________________________________________
 
-#include <iomanip>
-
 #include "EVGCore/EventRecord.h"
 #include "EVGCore/EventRecordVisitorI.h"
 #include "GHEP/GHepStatus.h"
 #include "GHEP/GHepParticle.h"
 
 using namespace genie;
-
-using std::endl;
-using std::setw;
-using std::setprecision;
-using std::setfill;
-using std::ios;
 
 ClassImp(EventRecord)
 
@@ -38,7 +30,6 @@ namespace genie {
  ostream & operator<< (ostream& stream, const EventRecord & event_record)
  {
    event_record.Print(stream);
-
    return stream;
  }
 }
@@ -75,105 +66,6 @@ void EventRecord::AcceptVisitor(EventRecordVisitorI * visitor)
 //___________________________________________________________________________
 void EventRecord::Print(ostream & stream) const
 {
-  stream << "\n\n |";
-  stream << setfill('-') << setw(104) << "|";
-
-  stream << "\n |";
-  stream << setfill(' ') << setw(6)  << "Idx | "
-         << setfill(' ') << setw(11) << "Name | "
-         << setfill(' ') << setw(6)  << "Ist | "
-         << setfill(' ') << setw(13) << "PDG | "
-         << setfill(' ') << setw(12) << "Mother  | "
-         << setfill(' ') << setw(12) << "Daughter  | "
-         << setfill(' ') << setw(9)  << "Px | "
-         << setfill(' ') << setw(9)  << "Py | "
-         << setfill(' ') << setw(9)  << "Pz | "
-         << setfill(' ') << setw(9)  << "E  | "
-         << setfill(' ') << setw(9)  << "m  | ";
-
-  stream << "\n |";
-  stream << setfill('-') << setw(104) << "|";
-
-  GHepParticle * p = 0;
-
-  TObjArrayIter piter(this);
-
-  unsigned int idx = 0;
-
-  double sum_E  = 0;
-  double sum_px = 0;
-  double sum_py = 0;
-  double sum_pz = 0;
-
-  while( (p = (GHepParticle *) piter.Next()) ) {
-
-     stream << "\n |";
-     stream << setfill(' ') << setw(3)  << idx++               << " | ";
-     stream << setfill(' ') << setw(8)  << p->Name()           << " | ";
-     stream << setfill(' ') << setw(3)  << p->Status()         << " | ";
-     stream << setfill(' ') << setw(10) << p->PdgCode()        << " | ";
-     stream << setfill(' ') << setw(3)  << p->FirstMother()    << " | ";
-     stream << setfill(' ') << setw(3)  << p->LastMother()     << " | ";
-     stream << setfill(' ') << setw(3)  << p->FirstDaughter()  << " | ";
-     stream << setfill(' ') << setw(3)  << p->LastDaughter()   << " | ";
-     stream << setiosflags(ios::fixed) << setprecision(3);
-     stream << setfill(' ') << setw(6)  << p->Px()             << " | ";
-     stream << setfill(' ') << setw(6)  << p->Py()             << " | ";
-     stream << setfill(' ') << setw(6)  << p->Pz()             << " | ";
-     stream << setfill(' ') << setw(6)  << p->E()              << " | ";
-
-     if( p->IsOnMassShell() )
-        stream << setfill(' ') << setw(6)  << p->Mass()        << " | ";
-     else
-        stream << setfill('*') << setw(6)  << p->Mass()        << " | " << p->GetP4()->M();
-
-     // compute P4Final - P4Initial
-     //
-     // Take into account real particles and fake (generator-specific)
-     // particles (rootino, bindino, ...) used to record non-fake physics.
-     // Ignore initial & final state ions (if any).
-
-     if( p->IsParticle() || p->IsFake() ) {
-
-       if(p->Status() == kIStStableFinalState) {
-
-          sum_E  += p->E();
-          sum_px += p->Px();
-          sum_py += p->Py();
-          sum_pz += p->Pz();
-       }
-       else if(p->Status() == kIStInitialState || p->Status() == kIstNucleonTarget) {
-
-          sum_E  -= p->E();
-          sum_px -= p->Px();
-          sum_py -= p->Py();
-          sum_pz -= p->Pz();
-       }
-     }// !nucleus
-
-  } // loop over particles
-
-  stream << "\n |";
-  stream << setfill('-') << setw(104) << "|";
-
-  // Print SUMS
-
-  stream << "\n |";
-  stream << setfill(' ') << setw(6)  << "    | "
-         << setfill(' ') << setw(11) << "Fin-Init:| "
-         << setfill(' ') << setw(6)  << "    | "
-         << setfill(' ') << setw(13) << "    | "
-         << setfill(' ') << setw(12) << "        | "
-         << setfill(' ') << setw(12) << "          | ";
-  stream << setiosflags(ios::fixed) << setprecision(3);
-  stream << setfill(' ') << setw(6)  << sum_px  << " | ";
-  stream << setfill(' ') << setw(6)  << sum_py  << " | ";
-  stream << setfill(' ') << setw(6)  << sum_pz  << " | ";
-  stream << setfill(' ') << setw(6)  << sum_E   << " | ";
-  stream << setfill(' ') << setw(9)  << "   | ";
-
-  stream << "\n |";
-  stream << setfill('-') << setw(104) << "|";
-  stream << "\n";
+  GHepRecord::Print(stream);
 }
 //___________________________________________________________________________
