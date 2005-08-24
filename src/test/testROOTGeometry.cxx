a697324ff7aada974be82fd176a26c6afc4af4a7
costas <costas.andreopoulos@stfc.ac.uk>
2005-08-24 18:46:58 +0000

    Added option to specify the ROOT geometry file from the command line argument and to print the computed path lengths.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@231 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/test/testROOTGeometry.cxx
+++ src/test/testROOTGeometry.cxx
@@ -3,6 +3,13 @@
 
 \program testROOTGeometry
 
+\brief   Tests the ROOT Geometry Analyzer.
+
+\syntax  testROOTGeometry -f /path/to/root/geometry/file.root
+
+         If no file is specified, $GENIE/src/test/TestGeometry.root is used
+         as default.
+
 \author  Anselmo Meregaglia <anselmo.meregaglia@cern.ch>
          ETH Zurich
 
@@ -10,24 +17,42 @@
 */
 //____________________________________________________________________________
 
-#include "Geo/ROOTGeomAnalyzer.h"
+#include <string>
+
 #include <TLorentzVector.h>
+
+#include "Geo/ROOTGeomAnalyzer.h"
+#include "Geo/PathLengthList.h"
+#include "Messenger/Messenger.h"
+
+using std::string;
 
 using namespace genie;
 
 int main(int argc, char ** argv)
 {
+  // get filename from the command line argument (following -f)
 
-  ROOTGeomAnalyzer* root_analyzer = new ROOTGeomAnalyzer("$GENIE/src/test/TestGeometry.root");
-  //root_analyzer->Load("/eth/store_6/home_6/amerega/TEST/vgm.2.03/examples/E01/N03/bin/Linux-g++/Geometry.root");
+  string filename = "$GENIE/src/test/TestGeometry.root"; // default
+  for(int iarg = 0; iarg < argc-1; iarg++) {
+     string argument(argv[iarg]);
+     if( argument.compare("-f") == 0 ) filename = string(argv[++iarg]);
+  }
 
-  
+  LOG("Test",pINFO) << "Starting ROOTGeomAnalyzer with geometry from: " << filename;
+
+  ROOTGeomAnalyzer* root_analyzer = new ROOTGeomAnalyzer(filename);
+ 
+  LOG("Test",pINFO) << "Computing path lengths";
+
   TLorentzVector* x= new TLorentzVector(0,0,0,0);
   TLorentzVector* p= new TLorentzVector(1,0,0,1);
   
-  root_analyzer->ComputePathLengths(*x,*p);
-  //root_analyzer->test();
+  const PathLengthList & pl = root_analyzer->ComputePathLengths(*x,*p);
 
+  LOG("Test",pINFO) << "Printing computed path lengths:";
+  LOG("Test",pINFO) << pl;
   
   return 0;
 }
+
