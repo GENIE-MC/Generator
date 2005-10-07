6780bd7c0efa5e9b458420d7ae3590dd2b4bf482
costas <costas.andreopoulos@stfc.ac.uk>
2005-10-07 16:24:47 +0000

    Specify which environmental variables to store when taking snapshot of your environment
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@387 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Conventions/Constants.h
+++ src/Conventions/Constants.h
@@ -218,6 +218,23 @@ static const unsigned int kMaxEVGThreadExceptions = 5;
 // setting the $GSEED env. var. or by using RandomGen::SetSeed(int)
 static const unsigned int kDefaultRandSeed = 65539;
 
+// Names of environmental variables to keep track of when saving
+// the job configuration in TFolders along with the output event tree
+static const char * kMCEnv[] =
+{
+  "GENIE", "GEVGL", "GSPLOAD", "GSPSAVE", "GMSGCONF",
+  "ROOTSYS", "CERNLIB", "PYTHIA6", "NEUGEN3PATH",
+  "LIBXML2_INC", "LIBXML2_LIB", "LOG4CPP_INC", "LOG4CPP_LIB",
+  "CLHEP_INC", "CLHEP_LIB", "GEANT4_INC", "GEANT4_LIB",
+  "GPROFILER_LIB", "DOXYGEN",
+  "GOPT_ENABLE_NUVALIDATOR", "GOPT_ENABLE_NEUGEN",
+  "GOPT_ENABLE_GEANT_INTERFACE",
+  "GOPT_ENABLE_FLUX_DRIVERS", "GOPT_ENABLE_GEOM_DRIVERS",
+  "GOPT_ENABLE_PROFILER", "GOPT_ENABLE_DOXYGEN_DOC"
+  "PATH", "LD_LIBRARY_PATH", 0
+};
+
+
 } // namespace constants
 } // namespace genie
 
