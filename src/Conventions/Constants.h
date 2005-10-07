d4837dad3a71d113c088ad3002f668972b1a01fd
costas <costas.andreopoulos@stfc.ac.uk>
2005-10-07 17:00:45 +0000

    decouple physics and control constants
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@388 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Conventions/Constants.h
+++ src/Conventions/Constants.h
@@ -3,7 +3,7 @@
 
 \namespace genie::constants
 
-\brief     Constants
+\brief     Physics constants
 
 \author    Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
            CCLRC, Rutherford Appleton Laboratory
@@ -200,39 +200,6 @@ static const double kNucRo = 1.2E-15*m;
 //----- Misc hard limits, cuts
 
 static const double kMinQ2Limit = 1e-6;  // GeV^2
-
-//----- Misc non-physics model constans
-
-// maximum allowed number of iterations in rejection MC method
-// before selecting a valid number
-static const unsigned int kRjMaxIterations = 1000;
-
-// maximum allowed depth when GENIE is running in recursive mode
-static const unsigned int kRecursiveModeMaxDepth = 20;
-
-// maximum allowed number of EVGThreadExceptions that is allowed
-// to be caught by EventGenerator at a single event generation thread
-static const unsigned int kMaxEVGThreadExceptions = 5;
-
-// Default random number generator seed number. It can be overriden
-// setting the $GSEED env. var. or by using RandomGen::SetSeed(int)
-static const unsigned int kDefaultRandSeed = 65539;
-
-// Names of environmental variables to keep track of when saving
-// the job configuration in TFolders along with the output event tree
-static const char * kMCEnv[] =
-{
-  "GENIE", "GEVGL", "GSPLOAD", "GSPSAVE", "GMSGCONF",
-  "ROOTSYS", "CERNLIB", "PYTHIA6", "NEUGEN3PATH",
-  "LIBXML2_INC", "LIBXML2_LIB", "LOG4CPP_INC", "LOG4CPP_LIB",
-  "CLHEP_INC", "CLHEP_LIB", "GEANT4_INC", "GEANT4_LIB",
-  "GPROFILER_LIB", "DOXYGEN",
-  "GOPT_ENABLE_NUVALIDATOR", "GOPT_ENABLE_NEUGEN",
-  "GOPT_ENABLE_GEANT_INTERFACE",
-  "GOPT_ENABLE_FLUX_DRIVERS", "GOPT_ENABLE_GEOM_DRIVERS",
-  "GOPT_ENABLE_PROFILER", "GOPT_ENABLE_DOXYGEN_DOC"
-  "PATH", "LD_LIBRARY_PATH", 0
-};
 
 
 } // namespace constants
