dc221efc194a04313ac629c7ccf3cb251840448f
costas <costas.andreopoulos@stfc.ac.uk>
2005-10-28 09:20:46 +0000

    Is no longer necessary to extract the interaction summary from the GHEP record in order to print it. Control the GHEP print level using $GHEPPRINTLEVEL.
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@453 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/test/testReadGNtuple.cxx
+++ src/test/testReadGNtuple.cxx
@@ -138,11 +138,9 @@ void readNtpEventRecord(TTree * tree)
 
     NtpMCRecHeader rec_header = mcrec->hdr;
     EventRecord &  event      = *(mcrec->event);
-    Interaction & interaction = *(event.GetInteraction());
 
     LOG("Main", pINFO) << rec_header;
     LOG("Main", pINFO) << event;
-    LOG("Main", pINFO) << interaction;
   }
 }
 //___________________________________________________________________
