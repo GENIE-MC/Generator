7f6615c778b8d46b29956073254fb53da5a1eb05
costas <costas.andreopoulos@stfc.ac.uk>
2005-10-29 12:56:49 +0000

    added nes test program
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@470 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/test/testInteraction.cxx
@@ -0,0 +1,101 @@
+//____________________________________________________________________________
+/*!
+
+\program testInteraction
+
+\brief   small test program used for testing / debugging the Interaction and 
+         its aggregate objects (InitialState, ProcessInfo, Kinematics, XclsTag)
+
+\author  Costas Andreopoulos <C.V.Andreopoulos@@rl.ac.uk>
+         CCLRC, Rutherford Appleton Laboratory
+
+\created May 4, 2004
+*/
+//____________________________________________________________________________
+
+#include "Conventions/Constants.h"
+#include "Interaction/Interaction.h"
+#include "Messenger/Messenger.h"
+#include "PDG/PDGCodes.h"
+
+using namespace genie;
+using namespace genie::constants;
+
+int main(int argc, char ** argv)
+{
+  //-- build an initial state
+  Target         Fe(26,56);
+  int            numu(kPdgNuMu); 
+  TLorentzVector pnu(0,0,8,8);
+  InitialState   init_state(Fe, numu);
+
+  init_state.SetProbeP4(pnu);
+
+  //-- build process info
+  ProcessInfo proc(kScDeepInelastic, kIntWeakCC);
+
+  //-- create an interaction & print it
+  Interaction interaction(init_state, proc);
+
+  LOG("test", pINFO) << "Printing an interaction object";
+  LOG("test", pINFO) << interaction;
+
+  //-- set struck nucleon & quark info in the initial state's Target
+  //   note: using methods ending in Ptr -> they return a 'writable'
+  //         object that can be modified
+
+  TLorentzVector pnucl(0,0,0,kNucleonMass);
+
+  interaction.GetInitialStatePtr()->GetTargetPtr()->SetStruckNucleonP4(pnucl);
+  interaction.GetInitialStatePtr()->GetTargetPtr()->SetStruckNucleonPDGCode(kPdgProton);
+  interaction.GetInitialStatePtr()->GetTargetPtr()->SetStruckQuarkPDGCode(kPdgUQuark);
+
+  //-- get a 'read-only' InitialState and print it (check that struck nucleon
+  //   and quark were set) 
+  //   note: using the methods not ending in Ptr to get a 'read-only' object
+
+  const InitialState & cinit = interaction.GetInitialState();
+
+  LOG("test", pINFO) << "Printing initial state after setting struck nucl/qrk";
+  LOG("test", pINFO) << "\n" << cinit;
+
+  //-- take just the Target from the initial state and print it
+
+  LOG("test", pINFO) << "Printing target after setting struck nucl/qrk";
+  const Target & ctgt = interaction.GetInitialState().GetTarget();
+  LOG("test", pINFO) << "\n" <<ctgt;
+
+  //-- change the struck nucleon
+  //-- instead of using the long syntax above, get a writable Target object first
+
+  Target * wtgt = interaction.GetInitialStatePtr()->GetTargetPtr();
+  wtgt->SetStruckNucleonPDGCode(kPdgProton);
+
+  LOG("test", pINFO) << "Printing target after changing struck nucl";
+  LOG("test", pINFO) << "\n" << *wtgt;
+
+  //-- take the Kinematics object and set some
+
+  Kinematics * wkine = interaction.GetKinematicsPtr();
+ 
+  wkine->Setx(0.1781);
+  wkine->Sety(0.6892);
+  wkine->SetQ2(3.2218);
+
+  LOG("test", pINFO) << "Printing kinematics after setting x,y,Q2";
+  LOG("test", pINFO) << "\n" <<*wkine;
+
+  //-- modify some & add a new
+  wkine->Setx(0.2);
+  wkine->Sety(0.2);
+  wkine->SetW(1.9219);
+
+  LOG("test", pINFO) << "Printing kinematics after modifying x,y and adding W";
+  LOG("test", pINFO) << "\n" << *wkine;
+
+  //-- see that the interaction was updated
+
+  LOG("test", pINFO) << "Printing the interaction object after all changes";
+  LOG("Main", pINFO) << interaction;
+}
+
