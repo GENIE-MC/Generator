a8fdd4b77168d775cb4c5246a5dea969a054d4eb
costas <costas.andreopoulos@stfc.ac.uk>
2005-11-23 17:05:22 +0000

    Add utility method for converting from string to genie::units. Is used for setting the geometry units from a command line argument string
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@493 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Utils/UnitUtils.h
@@ -0,0 +1,31 @@
+//____________________________________________________________________________
+/*!
+
+\namespace  genie::utils::unit
+
+\brief      Simple unit system utilities
+
+\author     Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+            CCLRC, Rutherford Appleton Laboratory
+
+\created    May 06, 2004
+
+*/
+//____________________________________________________________________________
+
+#ifndef _UNIT_UTILS_H_
+#define _UNIT_UTILS_H_
+
+namespace genie {
+namespace utils {
+
+namespace units {
+
+  double UnitFromString(string u);
+
+} // namespace units
+} // namespace utils
+} // namespace genie
+
+#endif // _UNIT_UTILS_H_
+
