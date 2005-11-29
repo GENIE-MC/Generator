81d4f789be5bdaf657353b4fa7993aa99e8de2ff
costas <costas.andreopoulos@stfc.ac.uk>
2005-11-29 15:38:34 +0000

    extended the template
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@517 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/EVGModules/Intranuke.h
+++ src/EVGModules/Intranuke.h
@@ -24,8 +24,22 @@
 #include "EVGModules/InukeInt.h"
 
 class TLorentzVector;
+class TVector3;
 
 namespace genie {
+
+// Cummulative interaction probabilities for pi+Fe in 50 MeV bins
+// Data from NeuGEN's Intranuke
+static const int kPNDataPoints = 12;
+static const double kPElastic[kPNDataPoints] = {
+       .97,.94,.93,.92,.91,.90,.90,.90,.90,.90,.90,.90
+};
+static const double kPInelastic[kPNDataPoints] = {
+       .51,.53,.55,.56,.56,.57,.57,.57,.57,.57,.57,.57
+};
+static const double kPAbsorption[kPNDataPoints] = {
+       .13,.18,.23,.23,.18,.13,.10,.08,.07,.06,.05,.05
+};
 
 class Target;
 
@@ -41,11 +55,13 @@ public :
 
 private:
 
+  TVector3       Hadronic3P   (GHepRecord * event_rec) const;
   TLorentzVector StepParticle (const TLorentzVector & x4,
                                const TLorentzVector & p4, double step) const;
   double         MeanFreePath (const Target & target,
                                const TLorentzVector & p4, int pdgc) const;
-  InukeInt_t     PionFate     (void) const;
+  InukeInt_t     PionFate     (const Target & target,
+                               const TLorentzVector & p4, int pdgc) const;
 };
 
 }      // genie namespace
