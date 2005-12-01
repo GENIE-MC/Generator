3376c6057d357775a279af79004d7971c6f99405
costas <costas.andreopoulos@stfc.ac.uk>
2005-12-01 14:54:52 +0000

    added more command line arguments
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@527 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/test/testMCJobDriver.cxx
+++ src/test/testMCJobDriver.cxx
@@ -6,6 +6,46 @@
 \brief   Simple program to drive the GMCJobDriver and generate events for an
          input neutrino flux and an input detector geometry
 
+         Syntax :
+           testMCJobDriver -f filename [-u units] [-r run] [-n nevents] [-s]
+                          [-m filename] [-d dx,dy,dz] [-b x,y,z] [-t size]
+
+         Options :
+           -f  a ROOT file containing a ROOT/GEANT geometry description
+           -u  geometry length units [default: meter]
+           -n  number of events [default: 10]
+           -r  run number [default: 0]
+           -m  XML file with max path lengths for geometry (if not set then
+               the max path lengths for the input geometry will be computed
+               at the mc job driver configuration and it might take a while
+               for complex ROOT/GEANT geometries)
+           -s  if set turns on using xsec splines (if set splines will be
+               loaded from the $GSPLOAD XML file and all needed but missing
+               splines will be computed)
+           -d  beam direction (dx,dy,dz) [default: 1,0,0 - along x]
+           -b  beam "spot" (x,y,z) - defines a surface perpendicular to beam
+               direction where flux neutrinos will be generated  (in m)
+               [default: -10m, 0m, 0m]
+           -t  radius of the area, on the surface perpendicular to the beam
+               direction, in which flux neutrinos would be generated (in m)
+               [default: 5 m]
+
+         Notes:
+           - An argument in []'s is optional
+           - The defaults for the flux driver match the example detector setup
+             in the $GENIE/src/test/data directory
+
+         Example :
+           testMCJobDriver -f ~/mysim/inputs/geometry.root -u cm -r 101 -n 1000
+                           -s -m ~/mysim/inputs/maxpl.xml -d 0,0,1 -b 0,0,-100
+                           -t 0.20
+
+         Also see $GENIE/src/stdapp/gEvGen.cxx for a list of environmental
+         variables that can affect the program behaviour (eg which event
+         generators to load, how to specify the xsec splines XML file,...)
+         For more options for the used flux driver and more flux drivers see
+         $GENIE/src/FluxDrivers/
+
 \author  Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
          CCLRC, Rutherford Appleton Laboratory
 
@@ -13,7 +53,9 @@
 */
 //____________________________________________________________________________
 
+#include <cassert>
 #include <string>
+#include <vector>
 
 #include <TSystem.h>
 #include <TFile.h>
@@ -33,23 +75,42 @@
 #include "PDG/PDGCodes.h"
 #include "Utils/XSecSplineList.h"
 #include "Utils/UnitUtils.h"
+#include "Utils/PrintUtils.h"
+#include "Utils/StringUtils.h"
 #include "Utils/CmdLineArgParserUtils.h"
 #include "Utils/CmdLineArgParserException.h"
 
 using std::string;
+using std::vector;
 
 using namespace genie;
 using namespace genie::flux;
 using namespace genie::geometry;
 
-void GetCommandLineArgs(int argc, char ** argv);
+//prototypes:
+void            PrintSyntax        (void);
+void            GetCommandLineArgs (int argc, char ** argv);
+GFluxI *        GetFluxDriver      (void);
+GeomAnalyzerI * GetGeometryDriver  (void);
 
-//command line options
-bool   gOptBuildSplines; // spline building option
-int    gOptNevents;      // number of events to generate
-Long_t gOptRunNu;        // run number
-string gOptRootGeom;     // detector geometry ROOT file
-string gOptGeomUnits;    // detector geometry units
+//command line options:
+Long_t   gOptRunNu;         // run number
+bool     gOptBuildSplines;  // spline building option
+int      gOptNevents;       // number of events to generate
+string   gOptRootGeom;      // detector geometry ROOT file
+string   gOptGeomUnits;     // detector geometry units
+string   gOptExtMaxPlXml;   // external max path lengths XML file
+TVector3 gOptBeamDirection; // beam direction
+TVector3 gOptBeamSpot;      // beam spot
+double   gOptBeamRt;        // beam transverse size (radius)
+
+// defaults for optional command line arguments
+Long_t   kDefOptRunNu     = 0;
+string   kDefOptGeomUnits = "m";
+int      kDefOptNevents   = 10;
+double   kDefOptBeamRt    = 5;
+TVector3 kDefOptBeamDirection(1,0,0);
+TVector3 kDefOptBeamSpot(-10,0,0);
 
 //___________________________________________________________________
 int main(int argc, char ** argv)
@@ -58,48 +119,21 @@ int main(int argc, char ** argv)
   GetCommandLineArgs(argc, argv);
 
   //-- Create/configure a flux driver
-  LOG("Main", pINFO)
-            << "Creating/configuring the GCylindTH1Flux flux driver";
-
-  GCylindTH1Flux * flux = new GCylindTH1Flux;
-
-  TF1 * f1 = new TF1("f1","1./x+2.",0.5,5.0);
-  TH1D * spectrum1 = new TH1D("spectrum1","numu spectrum", 20,0.5,5);
-  spectrum1->FillRandom("f1",10000);
-
-  TVector3 direction(1,0,0);
-  TVector3 beam_spot(-10,0,0);
-
-  flux -> SetNuDirection      (direction);
-  flux -> SetBeamSpot         (beam_spot);
-  flux -> SetTransverseRadius (8.0);
-  flux -> AddEnergySpectrum   (kPdgNuMu, spectrum1);
+  GFluxI * flux = GetFluxDriver();
 
   //-- Create/configure a geometry driver
-  LOG("Main", pINFO) << "Creating/configuring the ROOT geom. driver";
+  GeomAnalyzerI * geom = GetGeometryDriver();
 
-  ROOTGeomAnalyzer * geom = new ROOTGeomAnalyzer(gOptRootGeom);
-  geom->SetUnits(genie::utils::units::UnitFromString(gOptGeomUnits));
-
-  //-- Create the GENIE MC-job driver
+  //-- Create/configure the GENIE MC-job driver
   LOG("Main", pINFO) << "Creating the GENIE MC Job driver";
-
   GMCJDriver mcj;
 
-  //-- Load the the flux and the geometry drivers to the MC driver
+  LOG("Main", pINFO) << "Loading flux & geometry drivers";
+  mcj.UseFluxDriver(flux);
+  mcj.UseGeomAnalyzer(geom);
+  mcj.UseMaxPathLengths(gOptExtMaxPlXml);
 
-  LOG("Main", pINFO)
-         << "Loading flux & geometry drivers to GENIE MC Job driver";
-
-  GFluxI *        fluxb = dynamic_cast<GFluxI *>       (flux);
-  GeomAnalyzerI * geomb = dynamic_cast<GeomAnalyzerI *>(geom);
-
-  mcj.UseFluxDriver  (fluxb);
-  mcj.UseGeomAnalyzer(geomb);
-
-  //-- Configure the GENIE MC driver
   LOG("Main", pINFO) << "Configuring the GENIE MC Job driver";
-
   mcj.Configure();
 
   //-- If this job uses cross section splines, build all splines that
@@ -114,17 +148,18 @@ int main(int argc, char ** argv)
   //-- Create an MC Job Monitor
   GMCJMonitor mcjmonitor(gOptRunNu);
 
-  //-- Start generating events
-
+  //-- Event loop
   int i=0;
   while (i<gOptNevents) {
 
+     // generate next event
      EventRecord * event = mcj.GenerateEvent();
 
+     // update job monitor
      mcjmonitor.Update(i,event);
 
+     // print event & add it at the output event tree
      if(event) {
-
        LOG("Main", pINFO) << "\n\n***EVENT NU: " << i;
        LOG("Main", pINFO) << *event;
 
@@ -138,10 +173,10 @@ int main(int argc, char ** argv)
      }
   }
 
-  //-- save the generated MC events
+  //-- Save the generated MC events
   ntpw.Save();
 
-  delete f1;
+  //-- Clean up and exit
   delete flux;
   delete geom;
 
@@ -150,14 +185,48 @@ int main(int argc, char ** argv)
   return 0;
 }
 //___________________________________________________________________
+GFluxI * GetFluxDriver(void)
+{
+// Create/configure a flux driver
+
+  LOG("Main", pINFO)
+            << "Creating/configuring the GCylindTH1Flux flux driver";
+
+  GCylindTH1Flux * flux = new GCylindTH1Flux;
+
+  TF1 * f1 = new TF1("f1","1./x+2.",0.5,5.0);
+  TH1D * spectrum1 = new TH1D("spectrum1","numu spectrum", 20,0.5,5);
+  spectrum1->FillRandom("f1",10000);
+
+  flux -> SetNuDirection      (gOptBeamDirection);
+  flux -> SetBeamSpot         (gOptBeamSpot);
+  flux -> SetTransverseRadius (gOptBeamRt);
+  flux -> AddEnergySpectrum   (kPdgNuMu, spectrum1);
+
+  GFluxI * fluxb = dynamic_cast<GFluxI *> (flux);
+
+  delete f1;
+
+  return fluxb;
+}
+//___________________________________________________________________
+GeomAnalyzerI * GetGeometryDriver(void)
+{
+// Create/configure a geometry driver
+
+  LOG("Main", pINFO) << "Creating/configuring the ROOT geom. driver";
+
+  ROOTGeomAnalyzer * geom = new ROOTGeomAnalyzer(gOptRootGeom);
+  geom->SetUnits(genie::utils::units::UnitFromString(gOptGeomUnits));
+
+  GeomAnalyzerI * geomb = dynamic_cast<GeomAnalyzerI *> (geom);
+
+  return geomb;
+}
+//___________________________________________________________________
 void GetCommandLineArgs(int argc, char ** argv)
 {
-  // default options
-  string kDefOptRootGeom  = string(gSystem->Getenv("GENIE")) +
-                            "/src/test/data/GeometryLArPbBox.root";
-  string kDefOptGeomUnits = "m";
-  int    kDefOptNevents   = 10;
-  Long_t kDefOptRunNu     = 0;
+// Parse the command line arguments
 
   //geometry file:
   try {
@@ -166,8 +235,9 @@ void GetCommandLineArgs(int argc, char ** argv)
               genie::utils::clap::CmdLineArgAsString(argc,argv,'f');
   } catch(exceptions::CmdLineArgParserException e) {
     if(!e.ArgumentFound()) {
-      LOG("Main", pNOTICE) << "Using default geometry file";
-      gOptRootGeom = kDefOptRootGeom;
+      LOG("Main", pFATAL) << "No geometry file was specified - Exiting";
+      PrintSyntax();
+      exit(1);
     }
   }
   //geometry units:
@@ -179,6 +249,18 @@ void GetCommandLineArgs(int argc, char ** argv)
     if(!e.ArgumentFound()) {
       LOG("Main", pNOTICE) << "Using default geometry units";
       gOptGeomUnits = kDefOptGeomUnits;
+    }
+  }
+  //external, maximum path lengths:
+  try {
+    LOG("Main", pINFO) << "Getting XML file with max path length list";
+    gOptExtMaxPlXml =
+              genie::utils::clap::CmdLineArgAsString(argc,argv,'m');
+  } catch(exceptions::CmdLineArgParserException e) {
+    if(!e.ArgumentFound()) {
+      LOG("Main", pFATAL)
+        << "No input max path lengths. They will be computed at jon init";
+     gOptExtMaxPlXml = "";
     }
   }
   //number of events:
@@ -199,14 +281,73 @@ void GetCommandLineArgs(int argc, char ** argv)
     gOptRunNu = genie::utils::clap::CmdLineArgAsInt(argc,argv,'r');
   } catch(exceptions::CmdLineArgParserException e) {
     if(!e.ArgumentFound()) {
-      LOG("Main", pNOTICE) << "Unspecified run number - Using default";
+      LOG("Main", pINFO) << "Unspecified run number - Using default";
       gOptRunNu = kDefOptRunNu;
     }
   }
-
   //spline building option:
   gOptBuildSplines =
                 genie::utils::clap::CmdLineArgAsBool(argc,argv,'s');
+
+  //beam direction:
+  string direction = "";
+  try {
+    LOG("Main", pINFO) << "Reading neutrino beam direction";
+    direction = genie::utils::clap::CmdLineArgAsString(argc,argv,'d');
+
+    // split the comma separated list
+    vector<string> dirv = utils::str::Split(direction, ",");
+    assert(dirv.size() == 3);
+    double dx = atof( dirv[0].c_str() );
+    double dy = atof( dirv[1].c_str() );
+    double dz = atof( dirv[2].c_str() );
+    // can not pass negative numbers in command line argument as the -
+    // symbol marks option names - pass negative number by a trailing m
+    dx *= ( (dirv[0].find("m") != string::npos) ? -1. : 1);
+    dy *= ( (dirv[1].find("m") != string::npos) ? -1. : 1);
+    dz *= ( (dirv[2].find("m") != string::npos) ? -1. : 1);
+    gOptBeamDirection.SetXYZ(dx,dy,dz);
+  } catch(exceptions::CmdLineArgParserException e) {
+    if(!e.ArgumentFound()) {
+      LOG("Main", pINFO) << "No input direction - Using default";
+      gOptBeamDirection = kDefOptBeamDirection;
+    }
+  }
+
+  //beam spot:
+  string bspot = "";
+  try {
+    LOG("Main", pINFO) << "Reading neutrino beam spot";
+    bspot = genie::utils::clap::CmdLineArgAsString(argc,argv,'b');
+
+    // split the comma separated list
+    vector<string> bsv = utils::str::Split(bspot, ",");
+    assert(bsv.size() == 3);
+    double x = atof( bsv[0].c_str() );
+    double y = atof( bsv[1].c_str() );
+    double z = atof( bsv[2].c_str() );
+    x *= ( (bsv[0].find("m") != string::npos) ? -1. : 1);
+    y *= ( (bsv[1].find("m") != string::npos) ? -1. : 1);
+    z *= ( (bsv[2].find("m") != string::npos) ? -1. : 1);
+    gOptBeamSpot.SetXYZ(x,y,z);
+  } catch(exceptions::CmdLineArgParserException e) {
+    if(!e.ArgumentFound()) {
+      LOG("Main", pINFO) << "No input beam spot - Using default";
+      gOptBeamSpot = kDefOptBeamSpot;
+    }
+  }
+
+  //beam size:
+  try {
+    LOG("Main", pINFO) << "Reading beam size";
+    gOptBeamRt = genie::utils::clap::CmdLineArgAsDouble(argc,argv,'t');
+  } catch(exceptions::CmdLineArgParserException e) {
+    if(!e.ArgumentFound()) {
+      LOG("Main", pINFO) << "No input beam size - Using default";
+      gOptBeamRt = kDefOptBeamRt;
+    }
+  }
+  gOptBeamRt = TMath::Abs(gOptBeamRt); // must be positive
 
   LOG("Main", pINFO) << "Command line options - Summary:";
   LOG("Main", pINFO) << "run number:           " << gOptRunNu;
@@ -214,6 +355,21 @@ void GetCommandLineArgs(int argc, char ** argv)
   LOG("Main", pINFO) << "number of events:     " << gOptNevents;
   LOG("Main", pINFO) << "detector geom. file:  " << gOptRootGeom;
   LOG("Main", pINFO) << "detector geom. units: " << gOptGeomUnits;
+  LOG("Main", pINFO) << "external max pl-list: " << gOptExtMaxPlXml;
+  LOG("Main", pINFO) << "beam size:            " << gOptBeamRt;
+  LOG("Main", pINFO) << "beam direction:       "
+                     << utils::print::Vec3AsString(&gOptBeamDirection);
+  LOG("Main", pINFO) << "beam spot:            "
+                     << utils::print::Vec3AsString(&gOptBeamSpot);
 }
 //___________________________________________________________________
+void PrintSyntax(void)
+{
+  LOG("Main", pNOTICE)
+    << "\n\n" << "Syntax:" << "\n"
+    << "   testMCJobDriver -f filename [-u units] [-r run] [-n nevents]\n"
+    << "                   [-s] [-m filename] [-d dx,dy,dz] [-b x,y,z] [-t size]";
+}
+//____________________________________________________________________________
+
 
