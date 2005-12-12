fe9529f4a7ed256ae62330daa0bd03cd8400e518
costas <costas.andreopoulos@stfc.ac.uk>
2005-12-12 16:38:08 +0000

    when no input quark is specified, use some PDF set to select one.
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@539 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Fragmentation/PythiaHadronization.cxx
+++ src/Fragmentation/PythiaHadronization.cxx
@@ -6,7 +6,7 @@
 \brief    Provides access to the PYTHIA hadronization models.
 
           Is a concrete implementation of the HadronizationModelI interface.
-          
+
 \author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
           CCLRC, Rutherford Appleton Laboratory
 
@@ -20,8 +20,11 @@
 #include "Fragmentation/PythiaHadronization.h"
 #include "Messenger/Messenger.h"
 #include "Numerical/RandomGen.h"
+#include "PDF/PDF.h"
+#include "PDF/PDFModelI.h"
 #include "PDG/PDGCodes.h"
 #include "PDG/PDGUtils.h"
+#include "Utils/KineUtils.h"
 
 using namespace genie;
 
@@ -33,28 +36,30 @@ extern "C" void py2ent_(int *,  int *, int *, double *);
 PythiaHadronization::PythiaHadronization() :
 HadronizationModelI("genie::PythiaHadronization")
 {
-  fPythia = new TPythia6();  
+  fPythia = new TPythia6();
 }
 //____________________________________________________________________________
 PythiaHadronization::PythiaHadronization(string config) :
 HadronizationModelI("genie::PythiaHadronization", config)
 {
-  fPythia = new TPythia6();  
+  fPythia = new TPythia6();
 }
 //____________________________________________________________________________
-PythiaHadronization::~PythiaHadronization() 
-{ 
+PythiaHadronization::~PythiaHadronization()
+{
 
 }
 //____________________________________________________________________________
 void PythiaHadronization::Initialize(void) const
 {
 
-}  
+}
 //____________________________________________________________________________
 TClonesArray * PythiaHadronization::Hadronize(
                                         const Interaction * interaction) const
 {
+  LOG("PythiaHad", pINFO) << "Running PYTHIA hadronizer";
+
   const Kinematics & kinematics = interaction->GetKinematics();
   double W = kinematics.W();
 
@@ -64,99 +69,87 @@ TClonesArray * PythiaHadronization::Hadronize(
   int hit_quark   = init_state.GetTarget().StruckQuarkPDGCode();
   int diquark     = 0;
 
+  LOG("PythiaHad", pINFO)
+         << "Struck quark pdgc = " << hit_quark
+                   << ", nucleon pdgc = " << hit_nucleon << " at W = " << W;
+
+  bool isp = pdg::IsProton(hit_nucleon);
+  bool isn = pdg::IsNeutron(hit_nucleon);
+  if( !(isp||isn) ) {
+    LOG("PythiaHad", pERROR) << "Can not handle nucleon: " << hit_nucleon;
+    exit(1);
+  }
+
   //-- check for hit-quark assignment
-  
+
   if(hit_quark==0) {
 
-     // no hit-quark assignement - selecting a random one for the input nucleon
-     // (should actually use PDFs here...)
-     RandomGen * rnd = RandomGen::Instance();
+     LOG("PythiaHad", pINFO)
+          << "No input hit quark. Selecting one based on specified PDF set";
 
-     double x = rnd->Random1().Rndm();
-     if (pdg::IsProton(hit_nucleon)) {
-         hit_quark = kPdgUQuark;
-         if(x < 0.3333) hit_quark = kPdgDQuark;
-     } else if(pdg::IsNeutron(hit_nucleon)) {
-         hit_quark = kPdgUQuark;
-         if(x < 0.6666) hit_quark = kPdgDQuark;
-     }
+     const PDFModelI * pdf_model =
+                  dynamic_cast<const PDFModelI *>
+                             (this->SubAlg("pdf-alg-name", "pdf-param-set"));
+
+     double x  = kinematics.x();
+     double Q2 = utils::kinematics::CalcQ2(interaction);
+
+     PDF pdf;
+     pdf.SetModel(pdf_model);
+     pdf.Calculate(x,Q2);
+
+     double xu  = pdf.UpValence();
+     double xd  = pdf.DownValence();
+     double xud = xu + xd;
+
+     RandomGen * rnd = RandomGen::Instance();
+     double t = xud * (rnd->Random1().Rndm()); // e [0, xu+xd]
+
+     hit_quark = kPdgUQuark;
+     if( (isp && t > xu/xud) || (isn && t > xd/xud) ) hit_quark = kPdgDQuark;
+
+     LOG("PythiaHad", pINFO) << "Selected hit quark pdgc = " << hit_quark;
   }
+
+  bool isu = pdg::IsUQuark(hit_quark);
+  bool isd = pdg::IsDQuark(hit_quark);
+  if( !(isu||isd) ) {
+    LOG("PythiaHad", pERROR) << "Can not handle quark: " << hit_quark;
+    exit(1);
+  }
+
+  //-- select diquark
+  if (isp && isu) diquark = kPdgUDDiquarkS1; /* u + ud */
+  if (isp && isd) diquark = kPdgUUDiquarkS1; /* d + uu */
+  if (isn && isu) diquark = kPdgDDDiquarkS1; /* u + dd */
+  if (isn && isd) diquark = kPdgUDDiquarkS1; /* d + ud */
 
   //-- PYTHIA->HADRONIZE:
-
   int ip = 0;
+  py2ent_(&ip, &hit_quark, &diquark, &W);
 
-  //-- hit nucleon = proton
-
-  if ( pdg::IsProton(hit_nucleon) ) {
-
-       /* u + ud */
-       if ( pdg::IsUQuark(hit_quark) )
-       {
-          diquark = kPdgUDDiquarkS1;
-          py2ent_(&ip, &hit_quark, &diquark, &W);
-       }
-       /* d + uu */
-       else if ( pdg::IsDQuark(hit_quark) )
-       {
-          diquark = kPdgUUDiquarkS1;
-          py2ent_(&ip, &hit_quark, &diquark, &W);
-       }
-       else {
-         LOG("PythiaHad", pERROR) << "Can not handle quark: " << hit_quark;
-       }
-  }
-
-  //-- hit nucleon = neutron
-
-  else if ( pdg::IsNeutron(hit_nucleon) ) {
-
-       /* u + dd */
-       if ( pdg::IsUQuark(hit_quark) )
-       {
-          diquark = kPdgDDDiquarkS1;
-          py2ent_(&ip, &hit_quark, &diquark, &W);
-       }          
-        /* d + ud */
-       else if ( pdg::IsDQuark(hit_quark) )
-       {
-          diquark = kPdgUDDiquarkS1;
-          py2ent_(&ip, &hit_quark, &diquark, &W);
-       }       
-       else {
-          LOG("PythiaHad", pERROR) << "Can not handle quark: " << hit_quark;
-       }
-
-  } else  {    
-      LOG("PythiaHad", pERROR) << "Can not handle nucleon: " << hit_nucleon;
-  }
-
+  //-- get LUJETS record
   fPythia->GetPrimaries();
-
   TClonesArray * pythia_particles =
-                           (TClonesArray *) fPythia->ImportParticles("All");
+                      (TClonesArray *) fPythia->ImportParticles("All");
 
   // copy PYTHIA container to a new TClonesArray so as to transfer ownership
   // of the container and of its elements to the calling method
 
-  TClonesArray * particle_list = new TClonesArray(
-                             "TMCParticle", pythia_particles->GetEntries() );
-                             
+  int np = pythia_particles->GetEntries();
+  assert(np>0);
+  TClonesArray * particle_list = new TClonesArray("TMCParticle", np);
   register unsigned int i = 0;
   TMCParticle * particle = 0;
-  
   TIter particle_iter(pythia_particles);
 
   while( (particle = (TMCParticle *) particle_iter.Next()) ) {
-    
        LOG("PythiaHad", pINFO)
-               << "Adding final state particle PDGC = " << particle->GetKF();
-
+               << "Adding final state particle pdgc = " << particle->GetKF();
        new ( (*particle_list)[i++] ) TMCParticle(*particle);
   }
 
   particle_list->SetOwner(true);
-    
   return particle_list;
 }
 //____________________________________________________________________________
