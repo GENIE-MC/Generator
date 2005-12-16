ecfd434b9bbb60c21f54a1491afcf99a83d2f6d2
costas <costas.andreopoulos@stfc.ac.uk>
2005-12-16 13:47:37 +0000

    A new utility namespace with breit wigner functions
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@560 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Utils/BWFunc.h
@@ -0,0 +1,34 @@
+//____________________________________________________________________________
+/*!
+
+\namespace genie::utils::bwfunc
+
+\brief     Breit Wigner functions
+
+\author    Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+           CCLRC, Rutherford Appleton Laboratory
+
+\created   November 22, 2004
+
+*/
+//____________________________________________________________________________
+
+#ifndef _BREIT_WIGNER_UTILS_H_
+#define _BREIT_WIGNER_UTILS_H_
+
+namespace genie  {
+namespace utils  {
+namespace bwfunc {
+
+  //-- A realistic Breit-Wigner distribution with L-dependent width.
+  double BreitWignerL(
+             double W, int L, double mass, double width0, double norm);
+
+  //-- A simple Breit-Wigner distribution.
+  double BreitWigner(double W, double mass, double width, double norm);
+
+} // bwfunc namespace
+} // utils  namespace
+} // genie  namespace
+
+#endif   // _BREIT_WIGNER_UTILS_H_
