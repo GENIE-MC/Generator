6013c48bf82c59a7b911404903466f65388cdb23
costas <costas.andreopoulos@stfc.ac.uk>
2006-03-16 14:55:47 +0000

    minor change to accommodate long algorithm names in printouts...
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@755 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Algorithm/AlgConfigPool.cxx
+++ src/Algorithm/AlgConfigPool.cxx
@@ -117,7 +117,7 @@ bool AlgConfigPool::LoadAlgConfig(void)
     string alg_name    = conf_file_iter->first;
     string file_name   = conf_file_iter->second;
 
-    SLOG("AlgConfigPool", pINFO) << "XML config for:"
+    SLOG("AlgConfigPool", pINFO) << "Load:"
          << setfill('.') << setw(35) << alg_name << " -> " << file_name;
 
     string full_path = config_dir + "/" + file_name;
