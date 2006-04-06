d26efaee10fa10ced74bcbb0a206d2afdcfc7e62
costas <costas.andreopoulos@stfc.ac.uk>
2006-04-06 15:52:29 +0000

    added adaptations of NeuGENs r_factor() and qelnuc() to take into account the suppression of differential EL/QEL cross section for nuclear targets
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@817 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Utils/NuclearUtils.cxx
+++ src/Utils/NuclearUtils.cxx
@@ -13,10 +13,16 @@
 */
 //____________________________________________________________________________
 
+#include <cstdlib>
+
 #include <TMath.h>
 
 #include "Conventions/Constants.h"
-#include "Interaction/Target.h"
+#include "Interaction/Interaction.h"
+#include "Messenger/Messenger.h"
+#include "Nuclear/FermiMomentumTablePool.h"
+#include "Nuclear/FermiMomentumTable.h"
+#include "PDG/PDGUtils.h"
 #include "Utils/NuclearUtils.h"
 
 using namespace genie;
@@ -85,4 +91,159 @@ double genie::utils::nuclear::Radius(const Target & target)
   return Rn;
 }
 //___________________________________________________________________________
+double genie::utils::nuclear::NuclQELXSecSuppression(
+                string kftable, double pmax, const Interaction * interaction)
+{
+// Computes the suppression of the EL/QEL differential cross section due to
+// nuclear effects
+// A direct adaptation of NeuGEN's qelnuc() and r_factor()
+
+  const InitialState & init_state = interaction->GetInitialState();
+  const ProcessInfo &  proc_info  = interaction->GetProcessInfo();
+  const Target &       target     = init_state.GetTarget();
+
+  if (!target.IsNucleus()) return 1.0; // no suppression for free nucleon tgt
+
+  double R = 1; // computed suppression factor
+
+  int target_pdgc         = target.PDGCode();
+  int struck_nucleon_pdgc = target.StruckNucleonPDGCode();
+  int final_nucleon_pdgc  = struck_nucleon_pdgc;
+
+  if(proc_info.IsWeakCC()) 
+          final_nucleon_pdgc = pdg::SwitchProtonNeutron(struck_nucleon_pdgc);
+
+  // get the requested Fermi momentum table 
+  FermiMomentumTablePool * kftp = FermiMomentumTablePool::Instance();
+  const FermiMomentumTable * kft  = kftp->GetTable(kftable);
+
+  double kFi = kft->FindClosestKF(target_pdgc, struck_nucleon_pdgc);
+  double kFf = (struck_nucleon_pdgc==final_nucleon_pdgc) ? kFi : 
+               kft->FindClosestKF(target_pdgc, final_nucleon_pdgc );
+
+  const Kinematics & kine = interaction->GetKinematics();
+
+  double q2 = kine.q2();
+  double q  = q2 * (0.25*q2/kNucleonMass_2 - 1.);
+
+  double kfa   = kFi * 2./kPi;
+  double kfa2  = TMath::Power(kfa,2);
+  double kFi4  = TMath::Power(kfa,2);
+  double rkf   = 1./(1. - kFi/4.);
+  double alpha = 1. - 6.*kfa2;
+  double beta  = 2. * rkf * kfa2 * kFi4;
+
+  double fm_area = FmArea(alpha,beta,kFi,pmax);
+
+  if (q <= kFf) {
+
+     double p1   = kFf - q;
+     double p2   = kFf + q;
+     double fmi1 = FmI1(alpha,beta,p1,p2,  kFi,kFf,q);
+     double fmi2 = FmI2(alpha,beta,p2,pmax,kFi,kFf,q);
+
+     R = 2*kPi * (fmi1 + 2*fmi2) / fm_area;
+
+  } else if (q > kFf && q <= (pmax-kFf)) {
+
+     double p1    = q - kFf;
+     double p2    = q + kFf;
+     double fmi1  = FmI1(alpha,beta, p1, p2,   kFi, kFf,q);
+     double fmi2a = FmI2(alpha,beta, 0., p1,   kFi, kFf,q);
+     double fmi2b = FmI2(alpha,beta, p2, pmax, kFi, kFf,q);
+
+     R = 2*kPi * (fmi1 + 2*(fmi2a+fmi2b)) / fm_area;
+
+  } else if (q > (pmax-kFf) && q <= (pmax+kFf)) {
+
+     double p1   = q - kFf;
+     double fmi2 = FmI2(alpha,beta, 0.,p1,  kFi,kFf,q);
+     double fmi1 = FmI1(alpha,beta, p1,pmax,kFi,kFf,q);
+
+     R = 2*kPi * (2.*fmi2 + fmi1) / fm_area;
+
+  } else if (q > (pmax+kFf)) {
+     R = 1.; 
+
+  } else {
+     LOG("Nuclear", pFATAL) << "Illegal input q = " << q;
+     exit(1);
+  }
+  return R;
+}
+//___________________________________________________________________________
+double genie::utils::nuclear::FmI1(double alpha, double beta, 
+                       double a, double b,  double kFi, double kFf, double q)
+{
+// Adapted from NeuGEN's fm_integral1() used in r_factor()
+
+  double f=0;
+
+  double q2   = TMath::Power(q,  2);
+  double a2   = TMath::Power(a,  2);
+  double b2   = TMath::Power(b,  2);
+  double kFi2 = TMath::Power(kFi,2);
+  double kFf2 = TMath::Power(kFf,2);
+
+  if(kFi < a) {
+     double lg = TMath::Log(b/a);
+
+     f = -beta * (kFf2-q2)/(4.*q) * (1./a2 - 1./b2) + beta/(2.*q) * lg;
+
+  } else if (kFi > b) {
+     double a4 = TMath::Power(a2,2);
+     double b4 = TMath::Power(b2,2);
+
+     f = - (kFf2-q2) * alpha/(4.*q) * (b2-a2) + alpha/(8.*q) * (b4-a4);
+
+  } else {
+     double a4   = TMath::Power(a2,2);
+     double kFi4 = TMath::Power(kFi2,2);
+     double lg   = TMath::Log(b/kFi);
+
+     f = - alpha*(kFf2-q2)/(4.*q)*(kFi2-a2) + alpha/(8.*q)*(kFi4 - a4)
+         - beta*(kFf2-q2)/(4.*q)*(1./kFi2 - 1./b2) + beta/(2.*q)*lg;
+  }
+
+  double integral2 = FmI2(alpha,beta,a,b,kFi,kFf,q);
+  double integral1 = integral2 + f;
+  
+  return integral1;
+}
+//___________________________________________________________________________
+double genie::utils::nuclear::FmI2(double alpha, double beta, 
+                       double a, double b,  double kFi, double kFf, double q)
+{
+// Adapted from NeuGEN's fm_integral2() used in r_factor()
+
+  double integral2 = 0;
+
+  if(kFi < a) {
+     integral2 = beta * (1./a - 1./b);
+
+  } else if(kFi > b) {
+     double a3 = TMath::Power(a,3);
+     double b3 = TMath::Power(b,3);
+     integral2 = alpha/3. * (b3 - a3);
+
+  } else {
+     double a3   = TMath::Power(a,3);
+     double kFi3 = TMath::Power(kFi,3);
+
+     integral2 = alpha/3. * (kFi3 - a3) + beta * (1./kFi - 1./b);
+  }
+  return integral2;
+}
+//___________________________________________________________________________
+double genie::utils::nuclear::FmArea(
+                         double alpha, double beta, double kf, double pmax)
+{
+// Adapted from NeuGEN's fm_area() used in r_factor()
+
+  double kf3 = TMath::Power(kf,3.);
+  double sum = 4.*kPi* (alpha * kf3/3. + beta*(1./kf - 1./pmax));
+  return sum;
+}
+//___________________________________________________________________________
+
 
--- src/Utils/NuclearUtils.h
+++ src/Utils/NuclearUtils.h
@@ -16,6 +16,10 @@
 #ifndef _NUCLEAR_UTILS_H_
 #define _NUCLEAR_UTILS_H_
 
+#include <string>
+
+using std::string;
+
 namespace genie {
 
 class Target;
@@ -24,10 +28,18 @@ namespace utils {
 
 namespace nuclear
 {
-  double BindEnergy            (const Target & target);
-  double BindEnergyPerNucleon  (const Target & target);
-  double BindEnergyLastNucleon (const Target & target);
-  double Radius                (const Target & target);
+  double BindEnergy             (const Target & target);
+  double BindEnergyPerNucleon   (const Target & target);
+  double BindEnergyLastNucleon  (const Target & target);
+  double Radius                 (const Target & target);
+
+  double NuclQELXSecSuppression (string kftable, double pmax, const Interaction * in);
+
+  double FmI1   (double alpha, double beta,
+                  double a, double b,  double kFi, double kFf, double q);
+  double FmI2   (double alpha, double beta,
+                  double a, double b,  double kFi, double kFf, double q);
+  double FmArea (double alpha, double beta, double kf, double pmax);
 
 } // nuclear namespace
 } // utils   namespace
