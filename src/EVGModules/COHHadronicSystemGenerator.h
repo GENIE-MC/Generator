99bbbe1ec1d7276bba8a55c6f16c1a1f23711e1a
costas <costas.andreopoulos@stfc.ac.uk>
2006-04-08 13:36:17 +0000

    added 1st version of COH hadronic system generator and modify the interaction list generator to add the number of f/s pion the interaction's exclusive tag
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@831 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/EVGModules/COHHadronicSystemGenerator.h
@@ -0,0 +1,42 @@
+//____________________________________________________________________________
+/*!
+
+\class   genie::COHHadronicSystemGenerator
+
+\brief   Generates the final state hadronic system in v COH interactions.
+
+         Is a concrete implementation of the EventRecordVisitorI interface.
+
+\author  Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+         CCLRC, Rutherford Appleton Laboratory
+
+\created October 03, 2004
+
+*/
+//____________________________________________________________________________
+
+#ifndef _COH_HADRONIC_SYSTEM_GENERATOR_H_
+#define _COH_HADRONIC_SYSTEM_GENERATOR_H_
+
+#include "EVGModules/HadronicSystemGenerator.h"
+
+namespace genie {
+
+class COHHadronicSystemGenerator : public HadronicSystemGenerator {
+
+public :
+  COHHadronicSystemGenerator();
+  COHHadronicSystemGenerator(string config);
+  ~COHHadronicSystemGenerator();
+
+  //-- implement the EventRecordVisitorI interface
+  void ProcessEventRecord(GHepRecord * event_rec) const;
+
+private:
+  void AddTargetNucleusRemnant (GHepRecord * event_rec) const;
+  void AddFinalStatePion       (GHepRecord * event_rec) const;
+};
+
+}      // genie namespace
+#endif // _COH_HADRONIC_SYSTEM_GENERATOR_H_
+
