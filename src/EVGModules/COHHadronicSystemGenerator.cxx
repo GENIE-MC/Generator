f95462ae049477ae7b4aa11281024a7f2d329782
costas <costas.andreopoulos@stfc.ac.uk>
2006-04-12 01:06:20 +0000

    removing SPP - updates at the COH hadronic and kinematics generators
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@866 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/EVGModules/COHHadronicSystemGenerator.cxx
+++ src/EVGModules/COHHadronicSystemGenerator.cxx
@@ -17,17 +17,23 @@
 
 #include <cstdlib>
 
+#include <TVector3.h>
+
+#include "Conventions/Constants.h"
 #include "EVGModules/COHHadronicSystemGenerator.h"
 #include "GHEP/GHepStatus.h"
 #include "GHEP/GHepParticle.h"
 #include "GHEP/GHepRecord.h"
 #include "Interaction/IUtils.h"
 #include "Messenger/Messenger.h"
+#include "Numerical/RandomGen.h"
 #include "PDG/PDGLibrary.h"
 #include "PDG/PDGCodes.h"
 #include "PDG/PDGUtils.h"
+#include "Utils/PrintUtils.h"
 
 using namespace genie;
+using namespace genie::constants;
 
 //___________________________________________________________________________
 COHHadronicSystemGenerator::COHHadronicSystemGenerator() :
@@ -49,58 +55,112 @@ COHHadronicSystemGenerator::~COHHadronicSystemGenerator()
 //___________________________________________________________________________
 void COHHadronicSystemGenerator::ProcessEventRecord(GHepRecord * evrec) const
 {
-// This method generates the final state hadronic system
+// This method generates the final state hadronic system (pion + nucleus) in 
+// COH interactions
+//
+  RandomGen * rnd = RandomGen::Instance();
 
-  //-- Add the final state nucleus 
-  this->AddTargetNucleusRemnant(evrec);
+  //-- Access neutrino, initial nucleus and final state prim. lepton entries
 
-  //-- Add the final state pion
-  this->AddFinalStatePion(evrec);
-}
-//___________________________________________________________________________
-void COHHadronicSystemGenerator::AddTargetNucleusRemnant(
-                                                   GHepRecord * evrec) const
-{
-  GHepParticle * nucleus = evrec->TargetNucleus();
-  assert(nucleus);
+  GHepParticle * nu  = evrec->Probe();
+  GHepParticle * Ni  = evrec->TargetNucleus();
+  GHepParticle * fsl = evrec->FinalStatePrimaryLepton();
 
-  int mom   = evrec->TargetNucleusPosition();
-  int ipdgc = nucleus->PdgCode();
+  assert(nu);
+  assert(Ni);
+  assert(fsl);
 
-  evrec->AddParticle(
-     ipdgc,kIStStableFinalState, mom,-1,-1,-1, 
-         nucleus->Px(), nucleus->Py(), nucleus->Pz(), nucleus->E(), 0,0,0,0);
-}
-//___________________________________________________________________________
-void COHHadronicSystemGenerator::AddFinalStatePion(GHepRecord * evrec) const
-{
-  //-- Determine the pdg code of the final state pion
+  //-- Determine the pdg code of the final state pion & nucleus
+
+  int nucl_pdgc = Ni->PdgCode(); // same as the initial nucleus
+
   Interaction * interaction = evrec->GetInteraction();
   const XclsTag & xcls_tag  = interaction->GetExclusiveTag();
 
   int pion_pdgc = 0;
-  if      (xcls_tag.NPi0()     ==1) pion_pdgc = kPdgPi0;
-  else if (xcls_tag.NPiPlus()  ==1) pion_pdgc = kPdgPiPlus;
-  else if (xcls_tag.NPiMinus() ==1) pion_pdgc = kPdgPiMinus;
+  if      (xcls_tag.NPi0()     == 1) pion_pdgc = kPdgPi0;
+  else if (xcls_tag.NPiPlus()  == 1) pion_pdgc = kPdgPiPlus;
+  else if (xcls_tag.NPiMinus() == 1) pion_pdgc = kPdgPiMinus;
   else {
      LOG("COHHadronicVtx", pFATAL)
-                          << "No final state pion information in XclsTag!";
+                      << "No final state pion information in XclsTag!";
      exit(1);
   }
 
-  //-- Figure out the final state pion 4-p
-  TLorentzVector p4(0,0,0,0);
+  //-- Generate the f/s pion and nucleus 4-p
 
-  //-- Figure out the mother particle position
+  // get particle masses
+  //double Mf   = Ni->Mass(); // Nf:=Ni
+  double mpi  = PDGLibrary::Instance()->Find(pion_pdgc)->Mass();
+  double mpi2 = TMath::Power(mpi,2);
+
+  // get selected kinematics
+  double yo = interaction->GetKinematics().y(true); 
+  double to = interaction->GetKinematics().t(true); 
+
+  SLOG("COHHadronicVtx", pDEBUG) << "yo = " << yo << ", to = " << to;
+
+  // 4-momentum transfer q=p(neutrino) - p(f/s lepton)
+  TLorentzVector q = *nu->P4() - *fsl->P4();
+  TVector3      q3 = q.Vect(); // spatial component
+
+  SLOG("COHHadronicVtx", pDEBUG) 
+       << "4-p transfer (q) @ LAB: " << utils::print::P4AsString(&q);
+
+  // compute pion energy and |momentum|
+  double Epi  = yo * nu->E();  
+  double Epi2 = TMath::Power(Epi,2);
+  double ppi2 = Epi2-mpi2;
+  double ppi  = TMath::Sqrt(TMath::Max(0.,ppi2));
+
+  SLOG("COHHadronicVtx", pDEBUG)
+                      << "f/s pion E = " << Epi << ", |p| = " << ppi;
+  assert(Epi>mpi);
+
+  // find angle eta between q and ppi 
+  // t = (q4-ppi4)^2 = (q0-ppi0)^2 - (q3-ppi3) 
+  // 
+  double q0_Epi_2 = TMath::Power(q.E() - Epi, 2);
+  double coseta   = (to - q0_Epi_2 + q3.Mag2() + ppi2)/ (2 * q3.Mag() * ppi);
+  double sineta   = (1.-TMath::Power(coseta,2));
+
+  SLOG("COHHadronicVtx", pDEBUG) << "cosine(pion, q) = " << coseta;
+
+  // compute transverse and longitudinal ppi components along q
+  double ppiL = ppi*coseta;
+  double ppiT = ppi*sineta;
+
+  // pion momentum in rotated frame (z along q)
+  double phi = 2*kPi* rnd->Random1().Rndm();
+  TVector3 ppi3(ppiT*TMath::Cos(phi),ppiT*TMath::Sin(phi),ppiL);
+
+  SLOG("COHHadronicVtx", pDEBUG) 
+      << "Pion 3-p with z along q: " << utils::print::Vec3AsString(&ppi3);
+
+  // rotate f/s pion 3-momentum at the Lab
+
+  ppi3.RotateUz(q3.Unit());
+
+  SLOG("COHHadronicVtx", pDEBUG) 
+               << "Pion 3-p @ LAB: " << utils::print::Vec3AsString(&ppi3);
+
+  // now figure out the f/s nucleus 4-p
+
+  double pxNf = nu->Px() + Ni->Px() - fsl->Px() - ppi3.Px();
+  double pyNf = nu->Py() + Ni->Py() - fsl->Py() - ppi3.Py();
+  double pzNf = nu->Pz() + Ni->Pz() - fsl->Pz() - ppi3.Pz();
+  double ENf  = nu->E()  + Ni->E()  - fsl->E()  - Epi;
+
+  //-- Save the particles at the GHEP record
+
   int mom = evrec->TargetNucleusPosition();
-  assert(mom>0);
-
-  //-- Add the final state pion at the EventRecord
-  LOG("COHHadronicVtx", pINFO)
-                             << "Adding pion [pdgc = " << pion_pdgc << "]";
-  TLorentzVector v4(0.,0.,0.,0.);
-
-  evrec->AddParticle(pion_pdgc, kIStStableFinalState, mom,-1,-1,-1, p4, v4);
+  
+  evrec->AddParticle(
+	    nucl_pdgc,kIStStableFinalState, mom,-1,-1,-1, 
+                               	       pxNf, pyNf, pzNf, ENf, 0, 0, 0, 0);
+  evrec->AddParticle(
+            pion_pdgc,kIStStableFinalState, mom,-1,-1,-1, 
+       	                     ppi3.Px(), ppi3.Py(),ppi3.Pz(),Epi, 0,0,0,0);
 }
 //___________________________________________________________________________
 
