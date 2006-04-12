32fd9dd0384d37fa2f6e9cc28d122dff1cb4bc66
costas <costas.andreopoulos@stfc.ac.uk>
2006-04-12 04:49:21 +0000

    added test program for the SAID hadron cross section splines
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@869 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/test/testSAIDHadronXSec.cxx
@@ -0,0 +1,36 @@
+//____________________________________________________________________________
+/*!
+
+\program testSAIDHadronXSec
+
+\brief   test program used for testing the SAID hadron cross section data
+
+\author  Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+         CCLRC, Rutherford Appleton Laboratory
+
+\created May 12, 2004
+*/
+//____________________________________________________________________________
+
+#include "Numerical/Spline.h"
+#include "Nuclear/SAIDHadronXSec.h"
+
+using namespace genie;
+
+int main(int argc, char ** argv)
+{
+  // load SAID hadron cross section data
+  SAIDHadronXSec * sxs = SAIDHadronXSec::Instance();
+
+  // save splines to a ROOT file
+  sxs -> PiplusPElasXSecSpl()  -> SaveAsROOT("said.root", "pip_p_elas", true);
+  sxs -> PiplusPInelXSecSpl()  -> SaveAsROOT("said.root", "pip_p_inel", false);
+  sxs -> PiminusPElasXSecSpl() -> SaveAsROOT("said.root", "pim_p_elas", false);
+  sxs -> PiminusPInelXSecSpl() -> SaveAsROOT("said.root", "pim_p_inel", false);
+  sxs -> PiminusPCExXSecSpl()  -> SaveAsROOT("said.root", "pim_p_cex",  false);
+  sxs -> PiplusAbsXSecSpl()    -> SaveAsROOT("said.root", "pip_abs",    false);
+  sxs -> NPElasXSecSpl()       -> SaveAsROOT("said.root", "np_elas",    false);
+  sxs -> NPInelXSecSpl()       -> SaveAsROOT("said.root", "np_inel",    false);
+  sxs -> PPElasXSecSpl()       -> SaveAsROOT("said.root", "pp_elas",    false);
+  sxs -> PPInelXSecSpl()       -> SaveAsROOT("said.root", "pp_inel",    false);
+}
