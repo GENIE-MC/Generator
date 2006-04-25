1fdf7e5d9c54c3279d28d4e5381a9d1304605e00
costas <costas.andreopoulos@stfc.ac.uk>
2006-04-25 18:58:06 +0000

    doxygen doc clean-up
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@896 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Base/QELFormFactors.cxx
+++ src/Base/QELFormFactors.cxx
@@ -1,19 +1,15 @@
 //____________________________________________________________________________
-/*!
+/*
+ Copyright (c) 2003-2006, GENIE Neutrino MC Generator Collaboration
+ All rights reserved.
+ For the licensing terms see $GENIE/USER_LICENSE.
 
-\class    genie::QELFormFactors
+ Author: Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+         CCLRC, Rutherford Appleton Laboratory - April 20, 2004
 
-\brief    A class holding Quasi Elastic (QEL) Form Factors.
+ For the class documentation see the corresponding header file.
 
-          This class is using the \b Strategy Pattern. \n
-          It can accept requests to calculate itself, for a given interaction,
-          that it then delegates to the algorithmic object, implementing the
-          QELFormFactorsModelI interface, that it finds attached to itself.
-
-\author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
-          CCLRC, Rutherford Appleton Laboratory
-
-\created  April 20, 2004
+ Important revisions after version 2.0.0 :
 
 */
 //____________________________________________________________________________
