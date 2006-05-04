c745af3cb2bc901c10beaca558f77f028e877a3e
costas <costas.andreopoulos@stfc.ac.uk>
2006-05-04 16:54:11 +0000

    remove redundant FromString()
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@943 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Interaction/ScatteringType.h
+++ src/Interaction/ScatteringType.h
@@ -3,7 +3,7 @@
 
 \class    genie::ScatteringType
 
-\brief    Enumeration of scattering types : QEL, DIS, RES, ...
+\brief    Enumeration of scattering types
 
 \author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
           CCLRC, Rutherford Appleton Laboratory
@@ -53,54 +53,12 @@ public:
       case(kScResonant) :          return "RES";      break;
       case(kScCoherent) :          return "COH";      break;
       case(kScDiffractive) :       return "DFR";      break;
-      case(kScNuElectronElastic) : return "NEL";      break;
+      case(kScNuElectronElastic) : return "NuEEL";    break;
       case(kScInverseMuDecay) :    return "IMD";      break;
       default :                    return "Unknown";  break;
     }
     return "Unknown";
   }
-  //__________________________________________________________________________
-/*
-  static ScatteringType_t FromString(string type)
-  {
-    //-- Make uppercase/lowercase irrelevant
-
-    for(unsigned int i=0; i<type.size(); i++) type[i] = toupper(type[i]);
-
-    //-- Figure out the ScatteringType_t from the input string
-
-    const char * t = type.c_str();
-
-    if ( strcmp(t,"ELASTIC") == 0 || strcmp(t,"ELS") == 0 ) return kScElastic;
-
-    else if ( strcmp(t,"QUASIELASTIC") == 0 ||
-                    strcmp(t,"QUASI-ELASTIC") == 0 ||
-                          strcmp(t,"QUASI ELASTIC") == 0 ||
-                                strcmp(t,"QEL") == 0 ) return kScQuasiElastic;
-
-    else if ( strcmp(t,"DEEPINELASTIC") == 0 ||
-                  strcmp(t,"DEEP-INELASTIC") == 0 ||
-                       strcmp(t,"DEEP INELASTIC") == 0 ||
-                               strcmp(t,"DIS") == 0 ) return kScDeepInelastic;
-
-    else if ( strcmp(t,"RESONANCE") == 0 ||
-                  strcmp(t,"SINGLE PION PRODUCTION") == 0 ||
-                       strcmp(t,"SINGLE-PION-PRODUCTION") == 0 ||
-                           strcmp(t,"SPP") == 0 ||
-                                    strcmp(t,"RES") == 0 ) return kScResonant;
-
-    else if ( strcmp(t,"COHERENT") == 0 ||
-                   strcmp(t,"COHERENT PION PRODUCTION") == 0 ||
-                         strcmp(t,"COHERENT-PION-PRODUCTION") == 0 ||
-                                    strcmp(t,"COH") == 0 ) return kScCoherent;
-
-    else if ( strcmp(t,"INVERSE-MUON-DECAY") == 0 ||
-                   strcmp(t,"INVERSE MUON DECAY") == 0 ||
-                              strcmp(t,"IMD") == 0 ) return kScInverseMuDecay;
-
-    else return kScNull;
-  }
-*/
   //__________________________________________________________________________
 };
 
