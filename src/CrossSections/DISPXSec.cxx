b7c1b349c3dfcb6a5bd6d26d1bf4fc02925d004d
costas <costas.andreopoulos@stfc.ac.uk>
2006-05-11 17:05:13 +0000

    speed-up
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@973 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/CrossSections/DISPXSec.cxx
+++ src/CrossSections/DISPXSec.cxx
@@ -54,8 +54,6 @@ double DISPXSec::XSec(
 {
   assert(kps==kPSxfE || kps==kPSyfE);
 
-  LOG("DISPXSec", pDEBUG) << *fConfig;
-
   if(! this -> ValidProcess    (interaction) ) return 0.;
   if(! this -> ValidKinematics (interaction) ) return 0.;
 
--- src/Radiative/BardinDISPXSec.cxx
+++ src/Radiative/BardinDISPXSec.cxx
@@ -58,8 +58,6 @@ BardinDISPXSec::~BardinDISPXSec()
 double BardinDISPXSec::XSec(
                   const Interaction * interaction, KinePhaseSpace_t kps) const
 {
-  LOG("Bardin", pDEBUG) << *fConfig;
-
   if(! this -> ValidProcess    (interaction) ) return 0.;
   if(! this -> ValidKinematics (interaction) ) return 0.;
 
