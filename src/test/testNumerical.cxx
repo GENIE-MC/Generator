77273742ac106fb3113a147bfece5c720aa65bc6
costas <costas.andreopoulos@stfc.ac.uk>
2006-05-17 13:25:55 +0000

    extend test program
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@987 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/test/testNumerical.cxx
+++ src/test/testNumerical.cxx
@@ -25,17 +25,32 @@
 
 using namespace genie;
 
-class GExampleFunc : public GSFunc
+class GExampleFunc1D : public GSFunc
 {
 public:
-  GExampleFunc() : GSFunc(1) {}
-  ~GExampleFunc() {}
+  GExampleFunc1D() : GSFunc(1) {}
+  ~GExampleFunc1D() {}
 
   double operator() (const vector<double> & p)
   {
     double x = p[0];
-    double y = 3*x*x+2*x+7;
-    return y;
+    double f = 3*x*x+2*x+7; // f(x)
+    return f;
+  }
+};
+
+class GExampleFunc2D : public GSFunc
+{
+public:
+  GExampleFunc2D() : GSFunc(2) {}
+  ~GExampleFunc2D() {}
+
+  double operator() (const vector<double> & p)
+  {
+    double x2 = TMath::Power(p[0],2);
+    double y2 = TMath::Power(p[1],2);
+    double f  = TMath::Exp(-x2+9)*TMath::Exp(-y2+16); // f(x,y)
+    return f;
   }
 };
 
@@ -45,22 +60,35 @@ int main(int argc, char ** argv)
 
   // get integrator
   AlgFactory * algf = AlgFactory::Instance();
-  const IntegratorI * integrator = 
+  const IntegratorI * integrator1 = 
         dynamic_cast<const IntegratorI *> (
            algf->GetAlgorithm("genie::Simpson1D","Default-Linear"));
+  const IntegratorI * integrator2 = 
+        dynamic_cast<const IntegratorI *> (
+           algf->GetAlgorithm("genie::Simpson2D","Default-Linear"));
 
-  // create function
-  GExampleFunc myfunction;
+  // create test functions & set limits
 
-  // set function limits
-  Range1D_t xlimits(0.,10.);
-  myfunction.SetParam(0,"x",xlimits);
+  // 1-D:
+  GExampleFunc1D myfunc1d;
 
-  // integrate function
-  double I = integrator->Integrate(myfunction);
+  Range1D_t lim(0,10);
+  myfunc1d.SetParam(0,"x", lim);
+
+  // 2-D:
+  GExampleFunc2D myfunc2d;
+
+  myfunc2d.SetParam(0,"x",-12.,12.);
+  myfunc2d.SetParam(1,"y",-12.,12.);
+
+  // integrate functions
+  double I1 = integrator1->Integrate(myfunc1d);
+  double I2 = integrator2->Integrate(myfunc2d);
 
   LOG("Main",pINFO) 
-              << "Integral <for given numerical accuracy> = " << I;
+         << "1-D Integral <for given numerical accuracy> = " << I1;
+  LOG("Main",pINFO) 
+         << "2-D Integral <for given numerical accuracy> = " << I2;
 
   //****** INTERPOLATION
 
@@ -68,14 +96,13 @@ int main(int argc, char ** argv)
 
   const int N  = 40;
   double x[N], y[N];
-  double dx = (xlimits.max - xlimits.min)/(N-1);
+  double dx = (lim.max - lim.min)/(N-1);
   vector<double> p(1);
 
   for(int i=0; i<N; i++) {
-
-    p[0] = xlimits.min + i*dx;
+    p[0] = lim.min + i*dx;
     x[i] = p[0];
-    y[i] = myfunction(p);
+    y[i] = myfunc1d(p);
   }
 
   // create an 1-D spline
@@ -83,14 +110,13 @@ int main(int argc, char ** argv)
 
   // evaluate the original function and the spline at various points
 
-
   int M = 100; // != N, otherwise we test only at the spline knots
-  dx = (xlimits.max - xlimits.min)/(M-1);
+  dx = (lim.max - lim.min)/(M-1);
 
   for(int i=0; i<M; i++) {
 
-    p[0] = xlimits.min + i*dx;
-    double yfunc = myfunction(p);
+    p[0] = lim.min + i*dx;
+    double yfunc = myfunc1d(p);
     double yspln = spline->Evaluate(p[0]);
 
     LOG("Main",pINFO) << "x = " << p[0] 
