a3a97ea25527b9570fd836760e74c6e9d1039ca3
costas <costas.andreopoulos@stfc.ac.uk>
2006-05-26 16:24:31 +0000

    macro to load libs in correct order when GENIE is used from CINT
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1009 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/scripts/loadlibs.C
@@ -0,0 +1,47 @@
+{
+ // PYTHIA 6 lib
+ gSystem->Load("libPythia6.so");
+
+ // ROOT libs
+ gSystem->Load("libPhysics");
+ gSystem->Load("libEG");
+ gSystem->Load("libEGPythia6");
+ gSystem->Load("libGeom");
+
+ // libxml2 and log4cpp libs
+ gSystem->Load("libxml2.so");
+ gSystem->Load("liblog4cpp.so");
+
+ // GENIE libs
+ gSystem->Load("libGMessenger.so");
+ gSystem->Load("libGRegistry.so");
+ gSystem->Load("libGAlgorithm.so");
+ gSystem->Load("libGNumerical.so");
+ gSystem->Load("libGInteraction.so");
+ gSystem->Load("libGUtils.so");
+ gSystem->Load("libGBase.so");
+ gSystem->Load("libGPDG.so");
+ gSystem->Load("libGPDF.so");
+ gSystem->Load("libGDecay.so");
+ gSystem->Load("libGFragmentation.so");
+ gSystem->Load("libGBaryonResonance.so");
+ gSystem->Load("libGNuclear.so");
+ gSystem->Load("libGLlewellynSmith.so");
+ gSystem->Load("libGCrossSections.so");
+ gSystem->Load("libGInvMuDecay.so");
+ gSystem->Load("libGCharm.so");
+ gSystem->Load("libGElas.so");
+ gSystem->Load("libGReinSeghal.so");
+ gSystem->Load("libGRadiative.so");
+ gSystem->Load("libGPaschos.so");
+ gSystem->Load("libGVHE.so");
+ gSystem->Load("libGPartonModel.so");
+ gSystem->Load("libGBodekYang.so");
+ gSystem->Load("libGHEP.so");
+ gSystem->Load("libGEVGCore.so");
+ gSystem->Load("libGEVGModules.so");
+ gSystem->Load("libGEVGDrivers.so");
+ gSystem->Load("libGGeo.so");
+ gSystem->Load("libGFluxDrivers.so");
+}
+
