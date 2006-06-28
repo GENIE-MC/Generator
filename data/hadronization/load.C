b553818eaf1f7c0472b0a931f567fea06596036b
costas <costas.andreopoulos@stfc.ac.uk>
2006-06-28 14:25:01 +0000

    Added more hadronization tuning data from T.Yang's collection of digitized plots
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1068 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- data/hadronization/load.C
+++ data/hadronization/load.C
@@ -1,4 +1,7 @@
 {
   TTree bfc;
   bfc.ReadFile("./BkwFwdCorrelation.data","nu/I:nuc/I:Wmin/F:Wmax/F:nF/F:nB/F:dnB/F");
+
+  TTree mult;
+  mult.ReadFile("./HadMultiplicityVsW2.data","nu/I:nuc/I:tgt/I:W2/F:n/F:dn/F:m/I:xfh/I");
 }
