5a11ca636ec7b4d8e508fe7498e12fbf2442b4bc
costas <costas.andreopoulos@stfc.ac.uk>
2006-06-28 18:51:21 +0000

    Added more data for hadronization model tuning
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1071 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- data/hadronization/load.C
+++ data/hadronization/load.C
@@ -3,8 +3,15 @@
   bfc.ReadFile("./hBkwFwdCorrelation.data","nu/I:nuc/I:Wmin/F:Wmax/F:nF/F:nB/F:dnB/F");
 
   TTree mult;
-  mult.ReadFile("./hMultiplicityVsW2.data","nu/I:nuc/I:tgt/I:W2/F:n/F:dn/F:m/I:xfh/I");
+  mult.ReadFile("./hMultiplicityVsW2.data","nu/I:nuc/I:tgt/I:W2/F:n/F:dn/F:nt/I:xfh/I");
 
   TTree pi0c;
   pi0c.ReadFile("./hPi0HCorrelation.data","nu/I:nuc/I:Wmin/F:Wmax/F:n/I:npi0/F:dnpi0/F:nt/I");
+
+  TTree dn;
+  dn.ReadFile("./hDispersionVsMult.data","nu/I:nuc/I:tgt/I:n/F:D/F:dD/F:nt/I");
+
+  TTree dnw;
+  dnw.ReadFile("./hRDispersionVsW2.data","nu/I:nuc/I:tgt/I:W2/F:DN/F:dDN/F:nt/I");
 }
+
