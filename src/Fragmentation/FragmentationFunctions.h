931572bdf3a3205d8bacab06cdd04b4f746566de
costas <costas.andreopoulos@stfc.ac.uk>
2006-07-31 16:47:31 +0000

    new files added in the new Fragmentation package
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1104 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Fragmentation/FragmentationFunctions.h
@@ -0,0 +1,42 @@
+//____________________________________________________________________________
+/*!
+
+\namespace genie::utils::frgmfunc
+
+\brief     Fragmentation functions
+
+\author    Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+           CCLRC, Rutherford Appleton Laboratory
+
+\created   June 15, 2004
+
+\cpright   Copyright (c) 2003-2006, GENIE Neutrino MC Generator Collaboration
+           All rights reserved.
+           For the licensing terms see $GENIE/USER_LICENSE.
+*/
+//____________________________________________________________________________
+
+#ifndef _FRAGMENTATION_FUNCTIONS_H_
+#define _FRAGMENTATION_FUNCTIONS_H_
+
+namespace genie    {
+namespace utils    {
+namespace frgmfunc {
+
+/*!
+ \fn    double collins_spiller_func(double * x, double * par)
+ \brief The Collins-Spiller fragmentation function
+*/
+  double collins_spiller_func(double * x, double * par);
+
+/*!
+  \fn    double peterson_func(double * x, double * par)
+  \brief The Peterson fragmentation function
+*/
+  double peterson_func(double * x, double * par);
+
+} // frgmfunc namespace
+} // utils    namespace
+} // genie    namespace
+
+#endif // _FRAGMENTATION_FUNCTIONS_H_
