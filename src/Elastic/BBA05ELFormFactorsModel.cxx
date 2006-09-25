733939973c037a835751552842726b630b225e4d
costas <costas.andreopoulos@stfc.ac.uk>
2006-09-25 17:49:57 +0000

    added BB2005 as in hep-ex/0602017
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1136 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Elastic/BBA05ELFormFactorsModel.cxx
+++ src/Elastic/BBA05ELFormFactorsModel.cxx
@@ -14,10 +14,12 @@
 */
 //____________________________________________________________________________
 
+#include "Elastic/BBA05Constants.h"
 #include "Elastic/BBA05ELFormFactorsModel.h"
 #include "Interaction/Interaction.h"
 
 using namespace genie;
+using namespace genie::constants;
 
 //____________________________________________________________________________
 BBA05ELFormFactorsModel::BBA05ELFormFactorsModel() :
@@ -37,23 +39,115 @@ BBA05ELFormFactorsModel::~BBA05ELFormFactorsModel()
 
 }
 //____________________________________________________________________________
-double BBA05ELFormFactorsModel::Gep(const Interaction * ) const
+double BBA05ELFormFactorsModel::Gep(const Interaction * interaction) const
 {
-  return 0;
+  double t   = this->tau(interaction);
+  double gep = this->BBA05Fit(t,fGep);
+  return gep;
 }
 //____________________________________________________________________________
-double BBA05ELFormFactorsModel::Gmp(const Interaction * ) const
+double BBA05ELFormFactorsModel::Gmp(const Interaction * interaction) const
 {
-  return 0;
+  double t   = this->tau(interaction);
+  double gmp = this->BBA05Fit(t,fGmp);
+  return gmp;
 }
 //____________________________________________________________________________
-double BBA05ELFormFactorsModel::Gen(const Interaction * ) const
+double BBA05ELFormFactorsModel::Gen(const Interaction * interaction) const
 {
-  return 0;
+  double t   = this->tau(interaction);
+  double gen = this->BBA05Fit(t,fGen);
+  return gen;
 }
 //____________________________________________________________________________
-double BBA05ELFormFactorsModel::Gmn(const Interaction * ) const
+double BBA05ELFormFactorsModel::Gmn(const Interaction * interaction) const
 {
-  return 0;
+  double t   = this->tau(interaction);
+  double gmn = this->BBA05Fit(t,fGmn);
+  return gmn;
+}
+//____________________________________________________________________________
+void BBA05ELFormFactorsModel::Configure(const Registry & config)
+{
+  Algorithm::Configure(config);
+  this->LoadConfig();
+}
+//____________________________________________________________________________
+void BBA05ELFormFactorsModel::Configure(string config)
+{
+  Algorithm::Configure(config);
+  this->LoadConfig();
+}
+//____________________________________________________________________________
+void BBA05ELFormFactorsModel::LoadConfig(void)
+{
+  //-- load the BBA05 model parameters
+  this->LoadBBA2005Params();
+}
+//____________________________________________________________________________
+void BBA05ELFormFactorsModel::LoadBBA2005Params(void)
+{
+// Fill private data members holding BBA2003 model parameters.
+// Use defaults for all configuration parameters not given in the config file.
+
+  // BBA2004 fit coefficients
+
+  fGep.a0 = fConfig->GetDoubleDef("Gep-a0", bba2005::kGep_a0);
+  fGep.a1 = fConfig->GetDoubleDef("Gep-a1", bba2005::kGep_a1);
+  fGep.a2 = fConfig->GetDoubleDef("Gep-a2", bba2005::kGep_a2);
+  fGep.b1 = fConfig->GetDoubleDef("Gep-b1", bba2005::kGep_b1);
+  fGep.b2 = fConfig->GetDoubleDef("Gep-b2", bba2005::kGep_b2);
+  fGep.b3 = fConfig->GetDoubleDef("Gep-b3", bba2005::kGep_b3);
+  fGep.b4 = fConfig->GetDoubleDef("Gep-b4", bba2005::kGep_b4);
+
+  fGmp.a0 = fConfig->GetDoubleDef("Gmp-a0", bba2005::kGmp_a0);
+  fGmp.a1 = fConfig->GetDoubleDef("Gmp-a1", bba2005::kGmp_a1);
+  fGmp.a2 = fConfig->GetDoubleDef("Gmp-a2", bba2005::kGmp_a2);
+  fGmp.b1 = fConfig->GetDoubleDef("Gmp-b1", bba2005::kGmp_b1);
+  fGmp.b2 = fConfig->GetDoubleDef("Gmp-b2", bba2005::kGmp_b2);
+  fGmp.b3 = fConfig->GetDoubleDef("Gmp-b3", bba2005::kGmp_b3);
+  fGmp.b4 = fConfig->GetDoubleDef("Gmp-b4", bba2005::kGmp_b4);
+
+  fGen.a0 = fConfig->GetDoubleDef("Gen-a0", bba2005::kGen_a0);
+  fGen.a1 = fConfig->GetDoubleDef("Gen-a1", bba2005::kGen_a1);
+  fGen.a2 = fConfig->GetDoubleDef("Gen-a2", bba2005::kGen_a2);
+  fGen.b1 = fConfig->GetDoubleDef("Gen-b1", bba2005::kGen_b1);
+  fGen.b2 = fConfig->GetDoubleDef("Gen-b2", bba2005::kGen_b2);
+  fGen.b3 = fConfig->GetDoubleDef("Gen-b3", bba2005::kGen_b3);
+  fGen.b4 = fConfig->GetDoubleDef("Gen-b4", bba2005::kGen_b4);
+
+  fGmn.a0 = fConfig->GetDoubleDef("Gmn-a0", bba2005::kGmn_a0);
+  fGmn.a1 = fConfig->GetDoubleDef("Gmn-a1", bba2005::kGmn_a1);
+  fGmn.a2 = fConfig->GetDoubleDef("Gmn-a2", bba2005::kGmn_a2);
+  fGmn.b1 = fConfig->GetDoubleDef("Gmn-b1", bba2005::kGmn_b1);
+  fGmn.b2 = fConfig->GetDoubleDef("Gmn-b2", bba2005::kGmn_b2);
+  fGmn.b3 = fConfig->GetDoubleDef("Gmn-b3", bba2005::kGmn_b3);
+  fGmn.b4 = fConfig->GetDoubleDef("Gmn-b4", bba2005::kGmn_b4);
+}
+//____________________________________________________________________________
+double BBA05ELFormFactorsModel::BBA05Fit(
+                                      double t, const BBA2005Fit_t & fp) const
+{
+  double t2 = TMath::Power(t, 2);
+  double t3 = TMath::Power(t, 3);
+  double t4 = TMath::Power(t2,2);
+
+  double Gn = (fp.a0) + (fp.a1*t) + (fp.a2*t2);
+  double Gd = 1 + (fp.b1*t) + (fp.b2*t2) + (fp.b3*t3) + (fp.b4*t4);
+
+  double G = Gn/Gd;
+  return G;
+}
+//____________________________________________________________________________
+double BBA05ELFormFactorsModel::tau(const Interaction * interaction) const
+{
+  const Kinematics & kine   = interaction->Kine();
+  const Target &     target = interaction->InitState().Tgt();
+
+  double q2 = kine.q2(); // momentum transfer, <0
+  double M2 = TMath::Power(target.HitNucMass(),2); // Mnucl^2
+
+  double t = -q2/(4*M2); 
+  return t;
 }
 //____________________________________________________________________________
--- src/Elastic/BBA05ELFormFactorsModel.h
+++ src/Elastic/BBA05ELFormFactorsModel.h
@@ -6,7 +6,7 @@
 \brief    Concrete implementation of the ELFormFactorsModelI interface.
           Computes elastic form factors using the BBA2005 parameterization.
 
-\ref
+\ref      R.Bradford, A.Bodek, H.Budd and J.Arrington, hep-ex/0602017
 
 \author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
           CCLRC, Rutherford Appleton Laboratory
@@ -26,6 +26,12 @@
 
 namespace genie {
 
+typedef struct SBBA2005Fit
+{
+  double a0, a1, a2, b1, b2, b3, b4;
+}
+BBA2005Fit_t;
+
 class BBA05ELFormFactorsModel : public ELFormFactorsModelI {
 
 public:
@@ -33,11 +39,32 @@ public:
   BBA05ELFormFactorsModel(string config);
   virtual ~BBA05ELFormFactorsModel();
 
-  //! implement the ELFormFactorsModelI interface
+  // implement the ELFormFactorsModelI interface
   double Gep (const Interaction * interaction) const;
   double Gmp (const Interaction * interaction) const;
   double Gen (const Interaction * interaction) const;
   double Gmn (const Interaction * interaction) const;
+
+  // overload Algorithm's Configure() to load the BBA2005Fit_t
+  // structs from the configuration Registry
+  void   Configure  (const Registry & config);
+  void   Configure  (string param_set);
+
+private:
+
+  // fill data members from the configuration Registry
+  void LoadConfig(void);
+  void LoadBBA2005Params(void);
+
+  // the actual BBA2005 fit function
+  double BBA05Fit (double tau, const BBA2005Fit_t & fp) const;
+  double tau      (const Interaction * interaction) const;
+
+  // model parameters.
+  BBA2005Fit_t fGep;   ///< BBA2005 fit coefficients for Gep
+  BBA2005Fit_t fGen;   ///< BBA2005 fit coefficients for Gen
+  BBA2005Fit_t fGmp;   ///< BBA2005 fit coefficients for Gmp
+  BBA2005Fit_t fGmn;   ///< BBA2005 fit coefficients for Gmn
 };
 
 }         // genie namespace
