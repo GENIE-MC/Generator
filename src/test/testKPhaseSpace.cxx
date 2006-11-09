28e9a86022b9a1abe51cebd064b80318a8c128a1
costas <costas.andreopoulos@stfc.ac.uk>
2006-11-09 11:34:23 +0000

    Added the testKPhaseSpace.cxx test program
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1196 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/test/testKPhaseSpace.cxx
@@ -0,0 +1,67 @@
+//____________________________________________________________________________
+/*!
+
+\program testKPhaseSpace
+
+\brief   program used for testing / debugging the kinematic phase space calc
+
+\author  Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+         CCLRC, Rutherford Appleton Laboratory
+
+\created June 20, 2004
+
+\cpright Copyright (c) 2003-2006, GENIE Neutrino MC Generator Collaboration
+         All rights reserved.
+         For the licensing terms see $GENIE/USER_LICENSE.
+*/
+//____________________________________________________________________________
+
+#include <TFile.h>
+#include <TTree.h>
+
+#include "Interaction/Interaction.h"
+#include "Messenger/Messenger.h"
+#include "PDG/PDGCodes.h"
+
+using namespace genie;
+
+void PrintLimits(const Interaction * interaction);
+
+//__________________________________________________________________________
+int main(int argc, char ** argv)
+{
+  // -- get a DIS interaction object & access its kinematics
+
+  int tgt         = kPdgTgtFe56;
+  int hit_nucleon = kPdgProton;
+  int neutrino    = kPdgNuMu;
+  double Ev       = 3;
+
+  Interaction * qelcc = Interaction::QELCC(tgt,hit_nucleon,neutrino,Ev);
+  Interaction * rescc = Interaction::RESCC(tgt,hit_nucleon,neutrino,Ev);
+  Interaction * discc = Interaction::DISCC(tgt,hit_nucleon,neutrino,Ev);
+
+  PrintLimits(qelcc);
+  PrintLimits(rescc);
+  PrintLimits(discc);
+
+  return 0;
+}
+//__________________________________________________________________________
+void PrintLimits(const Interaction * interaction)
+{
+  LOG("Main", pNOTICE) << *interaction;
+
+  const KPhaseSpace & phase_space = interaction->PhaseSpace();
+
+  Range1D_t xl  = phase_space.Limits(kKVx);
+  Range1D_t yl  = phase_space.Limits(kKVy);
+  Range1D_t Q2l = phase_space.Limits(kKVQ2);
+  Range1D_t Wl  = phase_space.Limits(kKVW);
+
+  LOG("Main", pNOTICE) << "x  e [" << xl.min  << ", " << xl.max  << "]";
+  LOG("Main", pNOTICE) << "y  e [" << yl.min  << ", " << yl.max  << "]";
+  LOG("Main", pNOTICE) << "Q2 e [" << Q2l.min << ", " << Q2l.max << "]";
+  LOG("Main", pNOTICE) << "W  e [" << Wl.min  << ", " << Wl.max  << "]";
+}
+//__________________________________________________________________________
