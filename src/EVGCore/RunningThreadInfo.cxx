83eed944d4eeeefc1ee75c724f8a75320dd6079e
costas <costas.andreopoulos@stfc.ac.uk>
2006-11-09 11:35:56 +0000

    Added RunningThreadInfo to pass info on the running thred to the running thread module
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1198 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/EVGCore/RunningThreadInfo.cxx
@@ -0,0 +1,51 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2006, GENIE Neutrino MC Generator Collaboration
+ All rights reserved.
+ For the licensing terms see $GENIE/USER_LICENSE.
+
+ Author: Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+         CCLRC, Rutherford Appleton Laboratory - July 15, 2005
+
+ For the class documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+
+*/
+//____________________________________________________________________________
+
+#include <iostream>
+
+#include "EVGCore/RunningThreadInfo.h"
+#include "EVGCore/EventGeneratorI.h"
+#include "Messenger/Messenger.h"
+
+using std::endl;
+using std::cout;
+
+using namespace genie;
+
+//____________________________________________________________________________
+RunningThreadInfo * RunningThreadInfo::fInstance = 0;
+//____________________________________________________________________________
+RunningThreadInfo::RunningThreadInfo()
+{
+  fInstance =  0;
+}
+//____________________________________________________________________________
+RunningThreadInfo::~RunningThreadInfo()
+{
+  cout << "RunningThreadInfo singleton dtor" << endl;
+  fInstance = 0;
+}
+//____________________________________________________________________________
+RunningThreadInfo * RunningThreadInfo::Instance()
+{
+  if(fInstance == 0) {
+    static RunningThreadInfo::Cleaner cleaner;
+    cleaner.DummyMethodAndSilentCompiler();
+    fInstance = new RunningThreadInfo;
+  }
+  return fInstance;
+}
+//____________________________________________________________________________
