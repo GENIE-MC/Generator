99d92a7994a9f139cb4dae41be288085c09739d6
costas <costas.andreopoulos@stfc.ac.uk>
2006-12-13 19:25:54 +0000

    placeholder for genie/gibuu interface
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1226 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/HadronTransport/GIBUU.cxx
@@ -0,0 +1,103 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2006, GENIE Neutrino MC Generator Collaboration
+ All rights reserved.
+ For the licensing terms see $GENIE/USER_LICENSE.
+
+ Author: Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+         CCLRC, Rutherford Appleton Laboratory - December 13, 2005
+
+ For the class documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+
+*/
+//____________________________________________________________________________
+
+#include <TLorentzVector.h>
+#include <TVector3.h>
+
+#include "EVGCore/EVGThreadException.h"
+#include "GHEP/GHepRecord.h"
+#include "GHEP/GHepParticle.h"
+#include "HadronTransport/GIBUU.h"
+#include "Interaction/Interaction.h"
+#include "Messenger/Messenger.h"
+
+using namespace genie;
+
+//___________________________________________________________________________
+GIBUU::GIBUU() :
+EventRecordVisitorI("genie::GIBUU")
+{
+
+}
+//___________________________________________________________________________
+GIBUU::GIBUU(string config) :
+EventRecordVisitorI("genie::GIBUU", config)
+{
+
+}
+//___________________________________________________________________________
+GIBUU::~GIBUU()
+{
+
+}
+//___________________________________________________________________________
+void GIBUU::ProcessEventRecord(GHepRecord * event) const
+{
+  //-- Check that we have an interaction with a nuclear target.
+  //   If not, do not call GIBUU
+  Interaction * interaction = event->Summary();
+  const InitialState & init_state = interaction->InitState();
+  if(!init_state.Tgt().IsNucleus() ) {
+    LOG("GIBUU", pDEBUG)
+        << "Not an interaction with a nuclear target. Skipping GIBUU call";
+    return;
+  }
+
+  //-- Access this module's configuration options and pass them to
+  //   the actual GIBUU code
+
+  //
+  // ...
+
+  //-- Translate GENIE GHepRecord to whatever FLUKA needs
+  LOG("GIBUU", pDEBUG) << "Translating: GENIE GHepRecord ---> GIBUU input";
+
+  //
+  // ...
+
+  //-- Run GIBUU
+  LOG("GIBUU", pINFO) << "************ Running FLUKA ************";
+
+  //
+  // ...
+
+  //-- Get FLUKA output & add it to GENIE GHepRecord
+  LOG("GIBUU", pDEBUG) << "Copying: GIBUU output ---> GENIE GHepRecord";
+
+  //
+  // ...
+
+}
+//___________________________________________________________________________
+void GIBUU::Configure(const Registry & config)
+{
+  Algorithm::Configure(config);
+  this->LoadConfig();
+}
+//___________________________________________________________________________
+void GIBUU::Configure(string config)
+{
+  Algorithm::Configure(config);
+  this->LoadConfig();
+}
+//___________________________________________________________________________
+void GIBUU::LoadConfig (void)
+{
+
+}
+//___________________________________________________________________________
+
+
---
+++ src/HadronTransport/GIBUU.h
@@ -0,0 +1,59 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::GIBUU
+
+\brief    A GENIE interface to the GIBUU hadron transport code.
+          Is a concerete implementation of the EventRecordVisitorI interface.
+          Note: the GIBUU code is *not included* in your GENIE installation.
+          You need to obtain GIBUU from its official distribution point.
+
+\ref      http://tp8.physik.uni-giessen.de:8080/GiBUU/
+
+          GIBUU team: 
+          U.Model, O.Bub, T.Gaitanos, K.Gallmeister, D.Kalok, M.Kaskulov,
+          A.Larionov, T.Leitner, B.Steinmu¼lle, L. Alvarez-Ruso, P. Mu¼hlich 
+
+          Please cite GIBUU separately if you include this event generation
+          module in your event generation threads.
+
+\author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+          CCLRC, Rutherford Appleton Laboratory
+
+\created  December 13, 2005
+
+\cpright  Copyright (c) 2003-2006, GENIE Neutrino MC Generator Collaboration
+          All rights reserved.
+          For the licensing terms see $GENIE/USER_LICENSE.
+*/
+//____________________________________________________________________________
+
+#ifndef _GIBUU_H_
+#define _GIBUU_H_
+
+#include "EVGCore/EventRecordVisitorI.h"
+
+namespace genie {
+
+class GIBUU : public EventRecordVisitorI {
+
+public:
+  GIBUU();
+  GIBUU(string config);
+ ~GIBUU();
+
+  //-- implement the EventRecordVisitorI interface
+  void ProcessEventRecord(GHepRecord * event_rec) const;
+
+  //-- overload the Algorithm::Configure() methods to load private data
+  //-- members from configuration options
+  void Configure(const Registry & config);
+  void Configure(string config);
+
+private:
+  void LoadConfig (void);
+
+};
+
+}      // genie namespace
+#endif // _GIBUU_H_
