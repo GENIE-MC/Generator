db4a8138ae0b780f01050d98694a150044330582
costas <costas.andreopoulos@stfc.ac.uk>
2007-01-29 18:53:09 +0000

    Some changes to the Splines to do block numerical operations on the splines. Would commit changes that overlopad the +,-,*,/ operators within the week.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1272 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Numerical/Spline.h
+++ src/Numerical/Spline.h
@@ -22,6 +22,7 @@
 
 #include <string>
 #include <fstream>
+#include <ostream>
 
 #include <TObject.h>
 #include <TSpline.h>
@@ -39,6 +40,7 @@ namespace genie {
 class Spline : public TObject {
 
 public:
+  //-- ctors & dtor
   Spline();
   Spline(string filename, string xtag="", string ytag="", bool is_xml = false);
   Spline(TNtupleD * ntuple, string xy, string cut="");
@@ -50,7 +52,7 @@ public:
   Spline(const TSpline3 & spline, int nknots);
   virtual ~Spline();
 
-  // load the Spline from XML, flat ASCII, ROOT ntuple/tree/tspline3, or SQL DB
+  //-- load the Spline from XML, flat ASCII, ROOT ntuple/tree/tspline3, or SQL DB
   bool   LoadFromXmlFile    (string filename, string xtag, string ytag);
   bool   LoadFromAsciiFile  (string filename);
   bool   LoadFromNtuple     (TNtupleD * nt, string xy, string cut = "");
@@ -58,38 +60,57 @@ public:
   bool   LoadFromDBase      (TSQLServer * db,  string query);
   bool   LoadFromTSpline3   (const TSpline3 & spline, int nknots);
 
-  // get xmin,xmax,nknots, check x variable against valid range and evaluate spline
-  int    NKnots(void) const {return fNKnots;}
-  double XMin  (void) const {return fXMin;  }
-  double XMax  (void) const {return fXMax;  }
+  //-- get xmin,xmax,nknots, check x variable against valid range and evaluate spline
+  int    NKnots             (void) const {return fNKnots;}
+  void   GetKnot            (int iknot, double & x, double & y) const;
+  double GetKnotX           (int iknot) const;
+  double GetKnotY           (int iknot) const;
+  double XMin               (void) const {return fXMin;  }
+  double XMax               (void) const {return fXMax;  }
   double Evaluate           (double x) const;
   bool   IsWithinValidRange (double x) const;
 
   void   SetName (string name) { fName = name; }
   string Name (void) const     { return fName; }
 
-  // save the Spline in XML, flat ASCII or ROOT format
+  //-- save the Spline in XML, flat ASCII or ROOT format
   void   SaveAsXml (string filename, string xtag, string ytag, string name="") const;
   void   SaveAsXml (ofstream & str,  string xtag, string ytag,
                                           string name="", bool insert = false) const;
   void   SaveAsText(string filename, string format="%10.6f\t%10.6f") const;
   void   SaveAsROOT(string filename, string name="", bool recreate=false) const;
 
-  // export Spline as TGraph or TSpline3
+  //-- export Spline as TGraph or TSpline3
   TGraph *   GetAsTGraph  (int np = 500, bool xscaling = false,
                            bool inlog=false, double fx=1., double fy=1.) const;
   TSpline3 * GetAsTSpline (void) const { return fInterpolator; }
 
-  // knot manipulation methods in additions to the TSpline3 ones
+  //-- knot manipulation methods in additions to the TSpline3 ones
   void FindClosestKnot(double x, double & xknot, double & yknot, Option_t * opt="-+") const;
   bool ClosestKnotValueIsZero(double x, Option_t * opt="-+") const;
 
+  //-- common mathematical operations applied simultaneously on all spline knots
+  void Add      (const Spline & spl, double c=1);
+  void Multiply (const Spline & spl, double c=1);
+  void Divide   (const Spline & spl, double c=1);
+  void Add      (double a);
+  void Multiply (double a);
+  void Divide   (double a);
+
+  //-- print knots
+  void Print(ostream & stream) const;
+
+  //-- overloaded operators
+  friend ostream & operator << (ostream & stream, const Spline & spl);
+
 private:
 
-  // initialize and build spline
+  //-- initialize and build spline
   void InitSpline  (void);
+  void ResetSpline (void);
   void BuildSpline (int nentries, double x[], double y[]);
 
+  //-- private data members
   string     fName;
   int        fNKnots;
   double     fXMin;
