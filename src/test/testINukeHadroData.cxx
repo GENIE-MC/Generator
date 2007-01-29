3e9f206bd1a15e6cb308c31faf20d5e8cbe94e8c
costas <costas.andreopoulos@stfc.ac.uk>
2007-01-29 18:54:14 +0000

    additions to the test programs
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1274 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/test/testINukeHadroData.cxx
+++ src/test/testINukeHadroData.cxx
@@ -22,7 +22,7 @@
 
 using namespace genie;
 
-int main(int argc, char ** argv)
+int main(int /*argc*/, char ** /*argv*/)
 {
   //-- load SAID hadron cross section data
 
@@ -42,91 +42,92 @@ int main(int argc, char ** argv)
   
   // Cross sections from SAID (Arndt, Workman, Strakovsky) PWA fit.
   //  
-  inukd -> XsPipPElas () -> SaveAsROOT("hdxs.root", "said_pip_p_elas", true);
-  inukd -> XsPipPReac () -> SaveAsROOT("hdxs.root", "said_pip_p_reac", false);
-  inukd -> XsPipDAbs  () -> SaveAsROOT("hdxs.root", "said_pip_d_abs",  false);
-  inukd -> XsPimPElas () -> SaveAsROOT("hdxs.root", "said_pim_p_elas", false);
-  inukd -> XsPimPReac () -> SaveAsROOT("hdxs.root", "said_pim_p_reac", false);
-  inukd -> XsPimPCEx  () -> SaveAsROOT("hdxs.root", "said_pim_p_cex",  false);
-  inukd -> XsPPElas   () -> SaveAsROOT("hdxs.root", "said_p_p_elas",   false);
-  inukd -> XsPPReac   () -> SaveAsROOT("hdxs.root", "said_p_p_reac",   false);
-  inukd -> XsNPElas   () -> SaveAsROOT("hdxs.root", "said_n_p_elas",   false);
-  inukd -> XsNPReac   () -> SaveAsROOT("hdxs.root", "said_n_p_reac",   false);
+  inukd -> XSecPipP_Elas () -> SaveAsROOT("hdxs.root", "said_pip_p_elas", true);
+  inukd -> XSecPipP_Reac () -> SaveAsROOT("hdxs.root", "said_pip_p_reac", false);
+  inukd -> XSecPipD_Abs  () -> SaveAsROOT("hdxs.root", "said_pip_d_abs",  false);
+  inukd -> XSecPimP_Elas () -> SaveAsROOT("hdxs.root", "said_pim_p_elas", false);
+  inukd -> XSecPimP_Reac () -> SaveAsROOT("hdxs.root", "said_pim_p_reac", false);
+  inukd -> XSecPimP_CEx  () -> SaveAsROOT("hdxs.root", "said_pim_p_cex",  false);
+  inukd -> XSecPP_Elas   () -> SaveAsROOT("hdxs.root", "said_p_p_elas",   false);
+  inukd -> XSecPP_Reac   () -> SaveAsROOT("hdxs.root", "said_p_p_reac",   false);
+  inukd -> XSecNP_Elas   () -> SaveAsROOT("hdxs.root", "said_n_p_elas",   false);
+  inukd -> XSecNP_Reac   () -> SaveAsROOT("hdxs.root", "said_n_p_reac",   false);
 
   // Cross sections from Mashnik's calculations (p+Fe)
   //
-  inukd -> XsPFeElas  () -> SaveAsROOT("hdxs.root", "mashnik_pfe_elas", false);
-  inukd -> XsPFeReac  () -> SaveAsROOT("hdxs.root", "mashnik_pfe_reac", false);
-  inukd -> XsPFeP     () -> SaveAsROOT("hdxs.root", "mashnik_pfe_p",    false);
-  inukd -> XsPFePP    () -> SaveAsROOT("hdxs.root", "mashnik_pfe_pp",   false);
-  inukd -> XsPFeNPP   () -> SaveAsROOT("hdxs.root", "mashnik_pfe_npp",  false);
-  inukd -> XsPFeNNP   () -> SaveAsROOT("hdxs.root", "mashnik_pfe_nnp",  false);
-  inukd -> XsPFeNNPP  () -> SaveAsROOT("hdxs.root", "mashnik_pfe_nnpp", false);
-  inukd -> XsPFePim   () -> SaveAsROOT("hdxs.root", "mashnik_pfe_pim",  false);
-  inukd -> XsPFePi0   () -> SaveAsROOT("hdxs.root", "mashnik_pfe_pi0",  false);
-  inukd -> XsPFePip   () -> SaveAsROOT("hdxs.root", "mashnik_pfe_pip",  false);
+  inukd -> XSecPFe_Elas  () -> SaveAsROOT("hdxs.root", "mashnik_pfe_elas", false);
+  inukd -> XSecPFe_Reac  () -> SaveAsROOT("hdxs.root", "mashnik_pfe_reac", false);
+  inukd -> XSecPFe_P     () -> SaveAsROOT("hdxs.root", "mashnik_pfe_p",    false);
+  inukd -> XSecPFe_N     () -> SaveAsROOT("hdxs.root", "mashnik_pfe_n",    false);
+  inukd -> XSecPFe_PP    () -> SaveAsROOT("hdxs.root", "mashnik_pfe_pp",   false);
+  inukd -> XSecPFe_NP    () -> SaveAsROOT("hdxs.root", "mashnik_pfe_np",   false);
+  inukd -> XSecPFe_NPP   () -> SaveAsROOT("hdxs.root", "mashnik_pfe_npp",  false);
+  inukd -> XSecPFe_NNP   () -> SaveAsROOT("hdxs.root", "mashnik_pfe_nnp",  false);
+  inukd -> XSecPFe_NNPP  () -> SaveAsROOT("hdxs.root", "mashnik_pfe_nnpp", false);
+  inukd -> XSecPFe_Pim   () -> SaveAsROOT("hdxs.root", "mashnik_pfe_pim",  false);
+  inukd -> XSecPFe_Pi0   () -> SaveAsROOT("hdxs.root", "mashnik_pfe_pi0",  false);
+  inukd -> XSecPFe_Pip   () -> SaveAsROOT("hdxs.root", "mashnik_pfe_pip",  false);
 
   // Cross sections from Mashnik's calculations (pi+Fe)
   //
-  inukd -> XsPiFeP    () -> SaveAsROOT("hdxs.root", "mashnik_pife_p",    false);
-  inukd -> XsPiFePP   () -> SaveAsROOT("hdxs.root", "mashnik_pife_pp",   false);
-  inukd -> XsPiFePPP  () -> SaveAsROOT("hdxs.root", "mashnik_pife_ppp",  false);
-  inukd -> XsPiFeN    () -> SaveAsROOT("hdxs.root", "mashnik_pife_n",    false);
-  inukd -> XsPiFeNN   () -> SaveAsROOT("hdxs.root", "mashnik_pife_nn",   false);
-  inukd -> XsPiFeNNN  () -> SaveAsROOT("hdxs.root", "mashnik_pife_nnn",  false);
-  inukd -> XsPiFeNP   () -> SaveAsROOT("hdxs.root", "mashnik_pife_np",   false);
-  inukd -> XsPiFeNPP  () -> SaveAsROOT("hdxs.root", "mashnik_pife_npp",  false);
-  inukd -> XsPiFeNPPP () -> SaveAsROOT("hdxs.root", "mashnik_pife_nppp", false);
-  inukd -> XsPiFeNNP  () -> SaveAsROOT("hdxs.root", "mashnik_pife_nnp",  false);
-  inukd -> XsPiFeNNPP () -> SaveAsROOT("hdxs.root", "mashnik_pife_nnpp", false);
-  inukd -> XsPiFePi0  () -> SaveAsROOT("hdxs.root", "mashnik_pife_pi0",  false);
+  inukd -> XSecPiFe_P    () -> SaveAsROOT("hdxs.root", "mashnik_pife_p",    false);
+  inukd -> XSecPiFe_PP   () -> SaveAsROOT("hdxs.root", "mashnik_pife_pp",   false);
+  inukd -> XSecPiFe_PPP  () -> SaveAsROOT("hdxs.root", "mashnik_pife_ppp",  false);
+  inukd -> XSecPiFe_N    () -> SaveAsROOT("hdxs.root", "mashnik_pife_n",    false);
+  inukd -> XSecPiFe_NN   () -> SaveAsROOT("hdxs.root", "mashnik_pife_nn",   false);
+  inukd -> XSecPiFe_NNN  () -> SaveAsROOT("hdxs.root", "mashnik_pife_nnn",  false);
+  inukd -> XSecPiFe_NP   () -> SaveAsROOT("hdxs.root", "mashnik_pife_np",   false);
+  inukd -> XSecPiFe_NPP  () -> SaveAsROOT("hdxs.root", "mashnik_pife_npp",  false);
+  inukd -> XSecPiFe_NPPP () -> SaveAsROOT("hdxs.root", "mashnik_pife_nppp", false);
+  inukd -> XSecPiFe_NNP  () -> SaveAsROOT("hdxs.root", "mashnik_pife_nnp",  false);
+  inukd -> XSecPiFe_NNPP () -> SaveAsROOT("hdxs.root", "mashnik_pife_nnpp", false);
+  inukd -> XSecPiFe_Pi0  () -> SaveAsROOT("hdxs.root", "mashnik_pife_pi0",  false);
 
   // Hadronic x-sections from experimental data (Ash & Carrol)
   //
-  inukd -> XsAshPiFeAbs () -> SaveAsROOT("hdxs.root", "exp_ash_pife_abs",  false);
-  inukd -> XsAshPiFeReac() -> SaveAsROOT("hdxs.root", "exp_ash_pife_reac", false);
-  inukd -> XsCarPiFeTot () -> SaveAsROOT("hdxs.root", "exp_car_pife_tot",  false);
+  inukd -> XSecAshPiFe_Abs () -> SaveAsROOT("hdxs.root", "exp_ash_pife_abs",  false);
+  inukd -> XSecAshPiFe_Reac() -> SaveAsROOT("hdxs.root", "exp_ash_pife_reac", false);
+  inukd -> XSecCarPiFe_Tot () -> SaveAsROOT("hdxs.root", "exp_car_pife_tot",  false);
 
   // Total x-sections needed from mean free path
   //
-  inukd -> XsPipTot() -> SaveAsROOT("hdxs.root", "tot_pip",  false);
-  inukd -> XsPimTot() -> SaveAsROOT("hdxs.root", "tot_pim",  false);
-  inukd -> XsPi0Tot() -> SaveAsROOT("hdxs.root", "tot_pi0",  false);
-  inukd -> XsPTot  () -> SaveAsROOT("hdxs.root", "tot_p",    false);
-  inukd -> XsNTot  () -> SaveAsROOT("hdxs.root", "tot_n",    false);
+  inukd -> XSecPip_Tot() -> SaveAsROOT("hdxs.root", "tot_pip",  false);
+  inukd -> XSecPim_Tot() -> SaveAsROOT("hdxs.root", "tot_pim",  false);
+  inukd -> XSecPi0_Tot() -> SaveAsROOT("hdxs.root", "tot_pi0",  false);
+  inukd -> XSecP_Tot  () -> SaveAsROOT("hdxs.root", "tot_p",    false);
+  inukd -> XSecN_Tot  () -> SaveAsROOT("hdxs.root", "tot_n",    false);
 
-/*
   // Fractions of total x-sections needed for particle fates
   //
-  inukd -> FrPipCEx    () -> SaveAsROOT("hdxs.root", "fr_pip_cex",    false);
-  inukd -> FrPipElas   () -> SaveAsROOT("hdxs.root", "fr_pip_elas",   false);
-  inukd -> FrPipReac   () -> SaveAsROOT("hdxs.root", "fr_pip_reac",   false);
-  inukd -> FrPipAbs    () -> SaveAsROOT("hdxs.root", "fr_pip_abs",    false);
-  inukd -> FrPimCEx    () -> SaveAsROOT("hdxs.root", "fr_pim_cex",    false);
-  inukd -> FrPimElas   () -> SaveAsROOT("hdxs.root", "fr_pim_elas",   false);
-  inukd -> FrPimReac   () -> SaveAsROOT("hdxs.root", "fr_pim_reac",   false);
-  inukd -> FrPimAbs    () -> SaveAsROOT("hdxs.root", "fr_pim_abs",    false);
-  inukd -> FrPi0CEx    () -> SaveAsROOT("hdxs.root", "fr_pi0_cex",    false);
-  inukd -> FrPi0Elas   () -> SaveAsROOT("hdxs.root", "fr_pi0_elas",   false);
-  inukd -> FrPi0Reac   () -> SaveAsROOT("hdxs.root", "fr_pi0_reac",   false);
-  inukd -> FrPi0Abs    () -> SaveAsROOT("hdxs.root", "fr_pi0_abs",    false);
-  inukd -> FrPReac     () -> SaveAsROOT("hdxs.root", "fr_p_reac",     false);
-  inukd -> FrNReac     () -> SaveAsROOT("hdxs.root", "fr_n_reac",     false);
-  inukd -> FrPiAElas   () -> SaveAsROOT("hdxs.root", "fr_pia_elas",   false);
-  inukd -> FrPiAInel   () -> SaveAsROOT("hdxs.root", "fr_pia_inel",   false);
-  inukd -> FrPiACEx    () -> SaveAsROOT("hdxs.root", "fr_pia_cex",    false);
-  inukd -> FrPiAAbs    () -> SaveAsROOT("hdxs.root", "fr_pia_abs",    false);
-  inukd -> FrPiAPP     () -> SaveAsROOT("hdxs.root", "fr_pia_pp",     false);
-  inukd -> FrPiANPP    () -> SaveAsROOT("hdxs.root", "fr_pia_npp",    false);
-  inukd -> FrPiANNP    () -> SaveAsROOT("hdxs.root", "fr_pia_nnp",    false);
-  inukd -> FrPiA4N4P   () -> SaveAsROOT("hdxs.root", "fr_pia_4n4p",   false);
-  inukd -> FrPiAPiProd () -> SaveAsROOT("hdxs.root", "fr_pia_piprod", false);
-  inukd -> FrPAElas    () -> SaveAsROOT("hdxs.root", "fr_pa_elas",    false);
-  inukd -> FrPAInel    () -> SaveAsROOT("hdxs.root", "fr_pa_inel",    false);
-  inukd -> FrPAAbs     () -> SaveAsROOT("hdxs.root", "fr_pa_abs",     false);
-  inukd -> FrPAPP      () -> SaveAsROOT("hdxs.root", "fr_pa_pp",      false);
-  inukd -> FrPANPP     () -> SaveAsROOT("hdxs.root", "fr_pa_npp",     false);
-  inukd -> FrPANNP     () -> SaveAsROOT("hdxs.root", "fr_pa_nnp",     false);
-  inukd -> FrPA4N4P    () -> SaveAsROOT("hdxs.root", "fr_pa_4n4p",    false);
-  inukd -> FrPAPiProd  () -> SaveAsROOT("hdxs.root", "fr_pa_piprod",  false); */
+  inukd -> FracPip_CEx    () -> SaveAsROOT("hdxs.root", "fr_pip_cex",    false);
+  inukd -> FracPip_Elas   () -> SaveAsROOT("hdxs.root", "fr_pip_elas",   false);
+  inukd -> FracPip_Reac   () -> SaveAsROOT("hdxs.root", "fr_pip_reac",   false);
+  inukd -> FracPip_Abs    () -> SaveAsROOT("hdxs.root", "fr_pip_abs",    false);
+  inukd -> FracPim_CEx    () -> SaveAsROOT("hdxs.root", "fr_pim_cex",    false);
+  inukd -> FracPim_Elas   () -> SaveAsROOT("hdxs.root", "fr_pim_elas",   false);
+  inukd -> FracPim_Reac   () -> SaveAsROOT("hdxs.root", "fr_pim_reac",   false);
+  inukd -> FracPim_Abs    () -> SaveAsROOT("hdxs.root", "fr_pim_abs",    false);
+  inukd -> FracPi0_CEx    () -> SaveAsROOT("hdxs.root", "fr_pi0_cex",    false);
+  inukd -> FracPi0_Elas   () -> SaveAsROOT("hdxs.root", "fr_pi0_elas",   false);
+  inukd -> FracPi0_Reac   () -> SaveAsROOT("hdxs.root", "fr_pi0_reac",   false);
+  inukd -> FracPi0_Abs    () -> SaveAsROOT("hdxs.root", "fr_pi0_abs",    false);
+  inukd -> FracP_Reac     () -> SaveAsROOT("hdxs.root", "fr_p_reac",     false);
+  inukd -> FracN_Reac     () -> SaveAsROOT("hdxs.root", "fr_n_reac",     false);
+  inukd -> FracPiA_Elas   () -> SaveAsROOT("hdxs.root", "fr_pia_elas",   false);
+  inukd -> FracPiA_Inel   () -> SaveAsROOT("hdxs.root", "fr_pia_inel",   false);
+  inukd -> FracPiA_CEx    () -> SaveAsROOT("hdxs.root", "fr_pia_cex",    false);
+  inukd -> FracPiA_Abs    () -> SaveAsROOT("hdxs.root", "fr_pia_abs",    false);
+  inukd -> FracPiA_PP     () -> SaveAsROOT("hdxs.root", "fr_pia_pp",     false);
+  inukd -> FracPiA_NPP    () -> SaveAsROOT("hdxs.root", "fr_pia_npp",    false);
+  inukd -> FracPiA_NNP    () -> SaveAsROOT("hdxs.root", "fr_pia_nnp",    false);
+  inukd -> FracPiA_4N4P   () -> SaveAsROOT("hdxs.root", "fr_pia_4n4p",   false);
+  inukd -> FracPiA_PiProd () -> SaveAsROOT("hdxs.root", "fr_pia_piprod", false);
+  inukd -> FracPA_Elas    () -> SaveAsROOT("hdxs.root", "fr_pa_elas",    false);
+  inukd -> FracPA_Inel    () -> SaveAsROOT("hdxs.root", "fr_pa_inel",    false);
+  inukd -> FracPA_Abs     () -> SaveAsROOT("hdxs.root", "fr_pa_abs",     false);
+  inukd -> FracPA_PP      () -> SaveAsROOT("hdxs.root", "fr_pa_pp",      false);
+  inukd -> FracPA_NPP     () -> SaveAsROOT("hdxs.root", "fr_pa_npp",     false);
+  inukd -> FracPA_NNP     () -> SaveAsROOT("hdxs.root", "fr_pa_nnp",     false);
+  inukd -> FracPA_4N4P    () -> SaveAsROOT("hdxs.root", "fr_pa_4n4p",    false);
+  inukd -> FracPA_PiProd  () -> SaveAsROOT("hdxs.root", "fr_pa_piprod",  false); 
 }
--- src/test/testNumerical.cxx
+++ src/test/testNumerical.cxx
@@ -3,7 +3,7 @@
 
 \program testNumerical
 
-\brief   
+\brief   testing GENIE's numerical utility classes
 
 \author  Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
          CCLRC, Rutherford Appleton Laboratory
@@ -25,6 +25,8 @@
 
 using namespace genie;
 
+//____________________________________________________________________________
+// example 1-D GSFunc
 class GExampleFunc1D : public GSFunc
 {
 public:
@@ -38,7 +40,8 @@ public:
     return f;
   }
 };
-
+//____________________________________________________________________________
+// example 2-D GSFunc
 class GExampleFunc2D : public GSFunc
 {
 public:
@@ -53,10 +56,24 @@ public:
     return f;
   }
 };
+//____________________________________________________________________________
+
+void testNumericalIntegration (void);
+void testSplineInterpolation  (void);
+void testSplineOperators      (void);
 
 int main(int /*argc*/, char ** /*argv*/)
 {
-  //****** NUMERICAL INTEGRATION
+  //testNumericalIntegration();
+  //testSplineInterpolation();
+  testSplineOperators();
+
+  return 0;
+}
+//____________________________________________________________________________
+void testNumericalIntegration(void)
+{
+// test numerial integration
 
   // get integrator
   AlgFactory * algf = AlgFactory::Instance();
@@ -71,13 +88,11 @@ int main(int /*argc*/, char ** /*argv*/)
 
   // 1-D:
   GExampleFunc1D myfunc1d;
-
   Range1D_t lim(0,10);
   myfunc1d.SetParam(0,"x", lim);
 
   // 2-D:
   GExampleFunc2D myfunc2d;
-
   myfunc2d.SetParam(0,"x",-12.,12.);
   myfunc2d.SetParam(1,"y",-12.,12.);
 
@@ -89,11 +104,18 @@ int main(int /*argc*/, char ** /*argv*/)
          << "1-D Integral <for given numerical accuracy> = " << I1;
   LOG("Main",pINFO) 
          << "2-D Integral <for given numerical accuracy> = " << I2;
+}
+//____________________________________________________________________________
+void testSplineInterpolation(void)
+{
+// test spline interpolation
 
-  //****** INTERPOLATION
+  //test func
+  GExampleFunc1D myfunc1d;
+  Range1D_t lim(0,10);
+  myfunc1d.SetParam(0,"x", lim);
 
   //evaluate the example function at various points
-
   const int N  = 40;
   double x[N], y[N];
   double dx = (lim.max - lim.min)/(N-1);
@@ -114,15 +136,56 @@ int main(int /*argc*/, char ** /*argv*/)
   dx = (lim.max - lim.min)/(M-1);
 
   for(int i=0; i<M; i++) {
-
     p[0] = lim.min + i*dx;
     double yfunc = myfunc1d(p);
     double yspln = spline->Evaluate(p[0]);
-
     LOG("Main",pINFO) << "x = " << p[0] 
                << ", f(x) = " << yfunc << ", spline(x) = " << yspln;
   }
   delete spline;
-
-  return 0;
 }
+//____________________________________________________________________________
+void testSplineOperators(void)
+{
+  const int N = 10;
+  double x1[N], y1[N];
+  double x2[N], y2[N];
+
+  double xmin = 0.;
+  double xmax = 1.;
+  double dx   = (xmax-xmin)/(N-1);
+
+  for(int i=0; i<N; i++) {
+    x1[i] = xmin + i*dx;
+    x2[i] = xmin + i*dx;
+    y1[i] = TMath::Sin(x1[i]) + 0.2;
+    y2[i] = TMath::Power(TMath::Cos(3*x2[i]), 2) + 0.5;
+  }
+
+  Spline spl1(N,x1,y1);
+  spl1.SetName("test spline #1");
+
+  Spline spl2(N,x1,y1);
+  spl2.SetName("test spline #2");
+
+  LOG("Main",pINFO) << "Printing spline_1";
+  LOG("Main",pINFO) << spl1;
+
+  LOG("Main",pINFO) << "Printing spline_2";
+  LOG("Main",pINFO) << spl2;
+
+  LOG("Main",pINFO) << "...adding 3 to spline_1";
+  spl1.Add(3.);
+  LOG("Main",pINFO) << spl1;
+
+  LOG("Main",pINFO) << "...multiplying spline_1 with 3";
+  spl1.Multiply(3.);
+  LOG("Main",pINFO) << spl1;
+
+  LOG("Main",pINFO) << "...multiplying spline_1 with 2*spline_2";
+  spl1.Multiply(spl2, 2.);
+  LOG("Main",pINFO) << spl1;
+}
+//____________________________________________________________________________
+
+
