24184f59bc50d7dbbd3150bcd4bcadaf8f79f39d
costas <costas.andreopoulos@stfc.ac.uk>
2007-02-22 15:23:14 +0000

    Write the GENIE/GIBUU interface - Add code for creating a dummy GIBUU lib in case GIBUU is not enabled
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1334 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/HadronTransport/GIBUU.cxx
+++ src/HadronTransport/GIBUU.cxx
@@ -4,8 +4,10 @@
  All rights reserved.
  For the licensing terms see $GENIE/USER_LICENSE.
 
- Author: Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
-         CCLRC, Rutherford Appleton Laboratory - December 13, 2005
+ Author:   
+    Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk> CCLRC, Rutherford Lab
+    Tina Leitner <Tina.J.Leitner@theo.physik.uni-giessen.de> Giessen Univ.
+    February 22, 2007
 
  For the class documentation see the corresponding header file.
 
@@ -20,6 +22,7 @@
 #include "EVGCore/EVGThreadException.h"
 #include "GHEP/GHepRecord.h"
 #include "GHEP/GHepParticle.h"
+#include "GHEP/GHepStatus.h"
 #include "HadronTransport/GIBUU.h"
 #include "Interaction/Interaction.h"
 #include "Messenger/Messenger.h"
@@ -55,23 +58,73 @@ void GIBUU::ProcessEventRecord(GHepRecord * event) const
   }
 
   //-- Translate GENIE GHepRecord to whatever FLUKA needs
+
   LOG("GIBUU", pDEBUG) << "Translating: GENIE GHepRecord ---> GIBUU input";
 
-  //
-  // ...
+  // set nuclear target
+  int target_pdg = nucltgt->Pdg();
+  SetNucleus(&target_pdg);
 
-  //-- Run GIBUU
+  // set hadrons to be transported out
+  int ihad=-1;
+  int ipos=-1;
+  map<int,int> mother_pos; // keep track of ith input hadron's position
+
+  TObjArrayIter piter(event);
+  GHepParticle * p = 0;
+  while( (p = (GHepParticle *) piter.Next()) )
+  {
+    ipos++;
+    GHepStatus_t ist = p->Status();
+    if(ist != kIStHadronInTheNucleus) continue;
+
+    ihad++;
+    int   inp_pdg = p->Pdg();
+    float inp_px  = (float) p->Px();
+    float inp_py  = (float) p->Py();
+    float inp_pz  = (float) p->Pz();
+    float inp_E   = (float) p->E();
+
+    AddHadron(&ihad, &inp_pdg, &inp_px, &inp_py, &inp_pz, &inp_E);
+
+    mother_pos[ihad] = ipos;
+  }  
+  int n_inp_had = mother_pos.size();
+    
+  //-- Run GIBUU (use GiBUU's GENIE hook)
   LOG("GIBUU", pNOTICE) << "************ Running GIBUU ************";
 
-  //
-  // ...
+  HadronTransportForGENIE();
 
   //-- Get FLUKA output & add it to GENIE GHepRecord
   LOG("GIBUU", pDEBUG) << "Copying: GIBUU output ---> GENIE GHepRecord";
 
-  //
-  // ...
+  GHepStatus_t out_ist = kIStStableFinalState;
+  for(int inp_had=0; inp_had < n_inp_had; inp_had++) {
 
+      int n_daughters = NumOfFinalStateHadrons(&inp_had);
+      int mom         = mother_pos[inp_had];
+
+      for(int out_had=0; out_had < n_daughters; out_had++) {
+
+        int   out_pdg = FinalStateHadronPDG (&inp_had, &out_had);
+        float out_px  = FinalStateHadronPX  (&inp_had, &out_had);
+        float out_py  = FinalStateHadronPY  (&inp_had, &out_had);
+        float out_pz  = FinalStateHadronPZ  (&inp_had, &out_had);
+        float out_e   = FinalStateHadronE   (&inp_had, &out_had);
+        float out_x   = FinalStateHadronX   (&inp_had, &out_had);
+        float out_y   = FinalStateHadronY   (&inp_had, &out_had);
+        float out_z   = FinalStateHadronZ   (&inp_had, &out_had);
+        float out_t   = FinalStateHadronT   (&inp_had, &out_had);
+
+        GHepParticle new_particle(
+             out_pdg, out_ist, mom,-1,-1,-1, 
+                     out_px,out_py,out_pz,out_e, out_x,out_y,out_z,out_t);
+  
+        event->AddParticle(new_particle);
+
+      } // input hadron daughters (output hadrons)
+  }//input hadrons
 }
 //___________________________________________________________________________
 void GIBUU::Configure(const Registry & config)
