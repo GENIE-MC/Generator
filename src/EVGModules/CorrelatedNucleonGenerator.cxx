766bc592f7728d46269f8146249281bb187e0858
costas <costas.andreopoulos@stfc.ac.uk>
2007-06-15 20:56:29 +0000

    adding module to generate the other nucleon in correlated NN pair
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1460 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/EVGModules/CorrelatedNucleonGenerator.cxx
@@ -0,0 +1,80 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2007, GENIE Neutrino MC Generator Collaboration
+ All rights reserved.
+ For the licensing terms see $GENIE/USER_LICENSE.
+
+ Author: Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+         CCLRC, Rutherford Appleton Laboratory - October 03, 2004
+
+ For the class documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+
+*/
+//____________________________________________________________________________
+
+#include "Algorithm/AlgConfigPool.h"
+#include "EVGModules/CorrelatedNucleonGenerator.h"
+#include "GHEP/GHepStatus.h"
+#include "GHEP/GHepParticle.h"
+#include "GHEP/GHepRecord.h"
+#include "Messenger/Messenger.h"
+
+using namespace genie;
+
+//___________________________________________________________________________
+CorrelatedNucleonGenerator::CorrelatedNucleonGenerator() :
+EventRecordVisitorI("genie::CorrelatedNucleonGenerator")
+{
+
+}
+//___________________________________________________________________________
+CorrelatedNucleonGenerator::CorrelatedNucleonGenerator(string config) :
+EventRecordVisitorI("genie::CorrelatedNucleonGenerator", config)
+{
+
+}
+//___________________________________________________________________________
+CorrelatedNucleonGenerator::~CorrelatedNucleonGenerator()
+{
+
+}
+//___________________________________________________________________________
+void CorrelatedNucleonGenerator::ProcessEventRecord(GHepRecord * evrec) const
+{
+  if(!fSimulateCorrelN) return;
+
+  Interaction *  interaction = evrec       -> Summary();
+  InitialState * init_state  = interaction -> InitStatePtr();
+  Target *       tgt         = init_state  -> TgtPtr();
+
+  // do nothing for non-nuclear targets
+  if(!tgt->IsNucleus()) return;
+
+
+
+}
+//___________________________________________________________________________
+void CorrelatedNucleonGenerator::Configure(const Registry & config)
+{
+  Algorithm::Configure(config);
+  this->LoadConfig();
+}
+//____________________________________________________________________________
+void CorrelatedNucleonGenerator::Configure(string config)
+{
+  Algorithm::Configure(config);
+  this->LoadConfig();
+}
+//____________________________________________________________________________
+void CorrelatedNucleonGenerator::LoadConfig(void)
+{
+  AlgConfigPool * confp = AlgConfigPool::Instance();
+  const Registry * gc = confp->GlobalParameterList();
+
+  fSimulateCorrelN = fConfig->GetBoolDef(
+                   "SimCorrelNuc", gc->GetBool("SimulateCorrelatedNucleon"));
+}
+//____________________________________________________________________________
+
