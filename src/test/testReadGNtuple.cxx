116da78278517f44308b37e26e11ba83d8ec9bb1
costas <costas.andreopoulos@stfc.ac.uk>
2007-06-15 05:58:02 +0000

    there is no PR-style event tree any more
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1454 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/test/testReadGNtuple.cxx
+++ src/test/testReadGNtuple.cxx
@@ -31,9 +31,6 @@
 #include "Ntuple/NtpMCFormat.h"
 #include "Ntuple/NtpMCTreeHeader.h"
 #include "Ntuple/NtpMCEventRecord.h"
-#include "Ntuple/NtpMCPlainRecord.h"
-#include "Ntuple/NtpMCSummary.h"
-#include "Ntuple/NtpMCGHepEntry.h"
 #include "Messenger/Messenger.h"
 
 using std::string;
@@ -43,7 +40,7 @@ bool   testCommandLineArgs(int argc);
 string getRootFilename    (int argc, char ** argv);
 bool   checkRootFilename  (string filename);
 void   readNtpEventRecord (TTree * tree); // reads GNtpER.root files
-void   readNtpPlainRecord (TTree * tree); // reads GNtpPR.root files
+
 //___________________________________________________________________
 int main(int argc, char ** argv)
 {
@@ -71,10 +68,6 @@ int main(int argc, char ** argv)
   LOG("Main", pINFO) << "This ntuple's format is : "
                              << NtpMCFormat::SuggestedUsage(format);
   switch(format) {
-     case kNFPlainRecord:
-       // simple ntuple for analysis in bare-ROOT sessions
-       readNtpPlainRecord(tree);
-       break;
      case kNFEventRecord:
        // ntuple for passing data to other applications (eg the MC
        // simulation framework of an experiment)
@@ -126,9 +119,6 @@ void readNtpEventRecord(TTree * tree)
 {
 // This ntuple contains a single TBranch with NtpMCEventRecord
 // objects in its leaves
-// See NtpMCEventRecord for a description of ntuple contents
-// This ntuple format is better used for moving GENIE data between
-// applications (eg GENIE -> GEANT)
 
   if(!tree) return;
 
@@ -150,44 +140,3 @@ void readNtpEventRecord(TTree * tree)
   }
 }
 //___________________________________________________________________
-void readNtpPlainRecord(TTree * tree)
-{
-// This ntuple contains a single TBranch with NtpMCPlainRecord
-// objects in its leaves
-// See NtpMCPlainRecord for a description of ntuple contents
-// This ntuple format allows someone to analyze GENIE ntuples in
-// bare ROOT sessions without loading the GENIE libraries.
-
-  if(!tree) return;
-
-  NtpMCPlainRecord * mcrec = 0;
-  tree->SetBranchAddress("gmcrec", &mcrec);
-
-  //-- loop over TTree NtpMC records, get & print the events
-
-  for(int i = 0; i < tree->GetEntries(); i++) {
-    tree->GetEntry(i);
-
-    NtpMCRecHeader rec_header = mcrec->hdr;    // record header
-    NtpMCSummary   summary    = mcrec->mc;     // MC event summary
-    TClonesArray & ghep       = *(mcrec->ghep);// Ntuple event record
-
-    LOG("Main", pINFO) << rec_header;
-    LOG("Main", pINFO) << "MC event summary: " << summary;
-
-    // this loops over a simplified (ntuple, struct-like) version of
-    // the EventRecord object which only contains public member data
-    LOG("Main", pINFO) << "Ntuple-version of the EventRecord object:";
-    LOG("Main", pINFO)
-              << "|Idx | Ist |         PDG | mom | daughter  |"
-                         <<"      Px |      Py |      Pz |       E |";
-
-    for(int ientry = 0; ientry < ghep.GetEntries(); ientry++) {
-      NtpMCGHepEntry * ghep_entry =
-                   dynamic_cast<NtpMCGHepEntry *> (ghep[ientry]);
-      LOG("Main", pINFO) << *ghep_entry;
-    }
-  }
-}
-//___________________________________________________________________
-
