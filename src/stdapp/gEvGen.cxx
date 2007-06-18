7507ad03fb4dd05cd2496bf1793095a041e13f44
costas <costas.andreopoulos@stfc.ac.uk>
2007-06-18 17:48:25 +0000

    important upgrade to the driver - accepts flux info either from an input vector file or by specifying a functional form - see details at the howto
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1468 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/stdapp/gEvGen.cxx
+++ src/stdapp/gEvGen.cxx
@@ -3,32 +3,73 @@
 
 \program gevgen
 
-\brief   Example program driving GENIE event generation modules
+\brief   GENIE v+A event generation driver 
 
          Syntax :
-           gevgen [-n nev] [-s] -e E -p nupdg -t tgtpdg [-r run#]
+           gevgen [-n nev] [-s] -e E -p nupdg -t tgtpdg [-r run#] [-f flux]
 
          Options :
            [] denotes an optional argument
            -n specifies the number of events to generate
-           -s turns on cross section spline building at job initialization
+           -s turns on cross section spline generation at job initialization
+              Not using the cross section splines will slow down the event
+              generation considerably. Since building the splines is a 
+              considerable overhead we recommend building them in advance
+              (see gmkspl utility) and loading via the $GSPLOAD env. variable.
            -e specifies the neutrino energy
-	      (if what follows the -e option is a comma separated pair of
-               values it will be interpreted as an energy range and neutrino
-               energies will be generated uniformly in this range).
-              Note that this is the energy of an "interacting" neutrino, not 
-              the energy of a "flux" neutrino -- the  neutrino is forced to 
-              interact anyway (see GMCJDriver for how to input fluxes)
+	      If what follows the -e option is a comma separated pair of values
+              it will be interpreted as an energy range and neutrino energies 
+              will be generated uniformly in this range. Note that this is the 
+              energy of an "interacting" neutrino, not the energy of a "flux" 
+              neutrino (the neutrino is forced to interact anyway)
+              However, if a flux has been determined using the -f option then 
+              the energy range will be interpreted as the energy range of flux
+              neutrinos
            -p specifies the neutrino PDG code
            -t specifies the target PDG code (std format: 1aaazzz000)
            -r specifies the MC run number
-
-         Example:
+           -f specifies the neutrino flux spectrum - it can be either a function,
+              eg 'x*x+4*exp(-x)' of a text file containing (energy,flux) pairs 
+              (see $GENIE/data/flux/) for few examples. In this case the GMCJDriver
+              is partially utilized to the use the specified flux and a trivial
+              'point geometry'. See $GENIE/src/test/testMCJobDriver.cxx to see
+              how you can generate events for arbitrarily complex fluxes and 
+              detector geometries. Note that in order to use the -f option you
+              must have enabled the flux and geometry drivers during the GENIE
+              installation (see installation instructions)
+ 
+         Examples:
+         (1)
            gevgen -n 300 -s -e 6.5 -p 14 -t 1056026000 -r 10000
 
-           will build cross section splines during the initialization step,
-           and will generate 300 events of muon neutrinos (pdg = 14) on Iron
-           (A=56,Z=26) at E = 6.5 GeV.
+           will generate 300 events of muon neutrinos (pdg = 14) on Iron (A=56,Z=26) 
+           at E = 6.5 GeV. The -s option will force it to generate cross section
+           splines during the job initialization. Only the cross section splines
+           not loaded via $GSPLOAD (see below) will be generated. Note that building
+           cross section splines for all necessary processes is a time consuming step
+           and we do recommend building them in advance (using the gmkspl utility)
+           and loading them via $GSPLOAD.
+
+         (2)
+           gevgen -n 300 -s -e 1,5 -p 14 -t 1056026000 -r 10000
+
+           like above except that interactions will be generated uniformly at the
+           energy range from 1 to 5 GeV (note: uniform spectrum of interacting 
+           neutrinos _not_ flux neutrinos).
+
+          (3)
+           gevgen -n 30000 -s -e 1,4 -p 14 -t 1016008000 -r 10000 -f 'x*x*exp((-x*x+3)/4)'
+
+           will generate 30000 events of muon neutrinos (pdg = 14) on Oxygen
+           (A=16,Z=8) using a neutrino flux of the specified functional form at the 
+           energy range from 1 to 4 GeV. Above comments on cross section splines apply
+           here too.
+
+          (4)
+           gevgen -n 30000 -s -e 1,4 -p 14 -t 1016008000 -r 10000 -f $GENIE/data/flux/nufact.data
+
+           like above except that the neutrino flux is taken from the input vector file 
+           rather than the input functional form.
 
          Other control options:
 
@@ -96,16 +137,24 @@
 #include <TFile.h>
 #include <TTree.h>
 #include <TSystem.h>
+#include <TVector3.h>
+#include <TH1.h>
+#include <TF1.h>
 
 #include "Conventions/XmlParserStatus.h"
+#include "Conventions/GBuild.h"
+#include "Conventions/Controls.h"
 #include "EVGCore/EventRecord.h"
+#include "EVGDrivers/GFluxI.h"
 #include "EVGDrivers/GEVGDriver.h"
+#include "EVGDrivers/GMCJDriver.h"
 #include "EVGDrivers/GMCJMonitor.h"
 #include "Interaction/Interaction.h"
 #include "Messenger/Messenger.h"
 #include "Ntuple/NtpWriter.h"
 #include "Ntuple/NtpMCFormat.h"
 #include "Numerical/RandomGen.h"
+#include "Numerical/Spline.h"
 #include "PDG/PDGCodes.h"
 #include "PDG/PDGUtils.h"
 #include "Utils/XSecSplineList.h"
@@ -113,14 +162,29 @@
 #include "Utils/CmdLineArgParserUtils.h"
 #include "Utils/CmdLineArgParserException.h"
 
+#ifdef __GENIE_FLUX_DRIVERS_ENABLED__
+#ifdef __GENIE_GEOM_DRIVERS_ENABLED__
+#define __CAN_GENERATE_EVENTS_USING_A_FLUX__
+#endif
+#endif
+
+#ifdef __CAN_GENERATE_EVENTS_USING_A_FLUX__
+#include "FluxDrivers/GCylindTH1Flux.h"
+#include "Geo/PointGeomAnalyzer.h"
+#endif
+
 using std::string;
 using std::vector;
 using std::ostringstream;
 
 using namespace genie;
+using namespace genie::controls;
 
 void GetCommandLineArgs (int argc, char ** argv);
 void PrintSyntax        (void);
+
+void GenerateEventsUsingANeutrinoFlux(void);
+void GenerateEventsAtFixedEnergies   (void);
 
 //Default options (override them using the command line arguments):
 int           kDefOptNevents   = 100;            // n-events to generate
@@ -135,46 +199,160 @@ double        gOptNuEnergyRange;// max-min neutrino energy
 int           gOptNuPdgCode;    // neutrino PDG code
 int           gOptTgtPdgCode;   // target PDG code
 Long_t        gOptRunNu;        // run number
-
+string        gOptFlux;         //
+bool          gOptUsingFlux = false;
 //____________________________________________________________________________
 int main(int argc, char ** argv)
 {
   //-- parse command line arguments
   GetCommandLineArgs(argc,argv);
   
-  //-- print the options you got from command line arguments
-
-  LOG("gevgen", pINFO) << "Number of events requested = " << gOptNevents;
-  LOG("gevgen", pINFO) << "Building splines at init.  = " << gOptBuildSplines; 
-  LOG("gevgen", pINFO) << "Neutrino PDG code          = " << gOptNuPdgCode;
-  LOG("gevgen", pINFO) << "Target PDG code            = " << gOptTgtPdgCode;
-  LOG("gevgen", pINFO) << "MC Run Number              = " << gOptRunNu;
-  if(gOptNuEnergyRange>0) {
-    LOG("gevgen", pINFO) << "Neutrino energy            = [" 
-        << gOptMinNuEnergy << ", " << gOptMinNuEnergy+gOptNuEnergyRange << "]";
-  } else {
-    LOG("gevgen", pINFO) << "Neutrino energy            = " << gOptMinNuEnergy;
-  }
-
-  //-- create the GENIE high level event generation interface object
-  //   for the given initial state
-
-  InitialState init_state(gOptTgtPdgCode, gOptNuPdgCode);
-
-  GEVGDriver driver;
-  driver.Configure(init_state);
-
   //-- Autoload splines (from the XML file pointed at the $GSPLOAD env. var.,
   //   if the env. var. has been set)
   XSecSplineList * xspl = XSecSplineList::Instance();
   xspl->AutoLoad();
 
+  //-- Generate neutrino events
+  //
+  if(gOptUsingFlux) GenerateEventsUsingANeutrinoFlux();
+  else              GenerateEventsAtFixedEnergies();
+
+  return 0;
+}
+//____________________________________________________________________________
+void GenerateEventsUsingANeutrinoFlux(void)
+{
+#ifdef __CAN_GENERATE_EVENTS_USING_A_FLUX__
+
+  //-- create the flux driver
+  double emin = gOptMinNuEnergy;
+  double emax = gOptMinNuEnergy+gOptNuEnergyRange;
+  double de   = gOptNuEnergyRange;
+
+  flux::GCylindTH1Flux * flux = new flux::GCylindTH1Flux;
+
+  int flux_bins    = 300;
+  int flux_entries = 100000;
+  TH1D * spectrum   = new TH1D("spectrum","neutrino flux", flux_bins, emin, emax);
+
+  // check whether the input flux is a file or a functional form
+  //
+  bool input_is_file = ! gSystem->AccessPathName(gOptFlux.c_str());
+  if(input_is_file) {
+    Spline * input_flux = new Spline(gOptFlux.c_str());
+
+    // generate the flux hisogram from the input flux file
+    int    n     = 100;
+    double estep = (emax-emin)/(n-1);
+    double ymax  = -1, ry = -1, gy = -1, e = -1;
+    for(int i=0; i<n; i++) {
+      e = emin + i*estep;
+      ymax = TMath::Max(ymax, input_flux->Evaluate(e));
+    }
+    ymax *= 1.3;
+
+    RandomGen * r = RandomGen::Instance();
+
+    for(int ientry=0; ientry<flux_entries; ientry++) {
+      bool accept = false;
+      unsigned int iter=0;
+      while(!accept) {
+        iter++;
+        if(iter > kRjMaxIterations) {
+           LOG("gevgen", pFATAL) << "Couldn't generate a flux histogram";
+           exit(1);
+        }
+        e = emin + de * r->RndEvg().Rndm();
+        gy = ymax * r->RndEvg().Rndm();
+        ry = input_flux->Evaluate(e);
+        accept = gy < ry;
+        if(accept) spectrum->Fill(e);
+      }
+    }
+    delete input_flux;
+
+  } else {
+    // generate the flux histogram from the input functional form
+    TF1 *  input_func = new TF1("input_func", gOptFlux.c_str(), emin, emax);
+    spectrum->FillRandom("input_func", 100000);
+    delete input_func;
+  }
+  // save input flux
+
+  TFile f("./input-flux.root","recreate");
+  spectrum->Write();
+  f.Close();
+
+  TVector3 bdir (0,0,1);
+  TVector3 bspot(0,0,0);
+
+  flux->SetNuDirection      (bdir);
+  flux->SetBeamSpot         (bspot);
+  flux->SetTransverseRadius (0.0);
+  flux->AddEnergySpectrum   (gOptNuPdgCode, spectrum);
+
+  GFluxI * flux_driver = dynamic_cast<GFluxI *>(flux);
+
+  //-- create a trivial point geometry
+  GeomAnalyzerI * geom_driver = new geometry::PointGeomAnalyzer(gOptTgtPdgCode);
+
+  //-- create the monte carlo job driver
+  GMCJDriver * mcj_driver = new GMCJDriver;
+  mcj_driver->UseFluxDriver(flux_driver);
+  mcj_driver->UseGeomAnalyzer(geom_driver);
+  mcj_driver->Configure();
+  mcj_driver->UseSplines();
+
+  //-- initialize an Ntuple Writer
+  NtpWriter ntpw(kDefOptNtpFormat, gOptRunNu);
+  ntpw.Initialize();
+
+  //-- create an MC Job Monitor
+  GMCJMonitor mcjmonitor(gOptRunNu);
+
+  //-- generate events / print the GHEP record / add it to the ntuple
+  int ievent = 0;
+  while ( ievent < gOptNevents) {
+
+     LOG("gevgen", pNOTICE) << " *** Generating event............ " << ievent;
+
+     // generate a single event for neutrinos coming from the specified flux
+     EventRecord * event = mcj_driver->GenerateEvent();
+
+     LOG("gevgen", pINFO) << "Generated Event GHEP Record: " << *event;
+
+     // add event at the output ntuple, refresh the mc job monitor & clean-up
+     ntpw.AddEventRecord(ievent, event);
+     mcjmonitor.Update(ievent,event);
+     ievent++;
+     delete event;
+  }
+
+  //-- save the generated MC events
+  ntpw.Save();
+
+#else
+
+  LOG("gevgen", pERROR) 
+    << "\n   To be able to generate neutrino events from a flux you need" 
+    << "\n   to add the following configuration options at your GENIE installation:" 
+    << "\n   --enable-flux-drivers  --enable-geom-drivers \n" ;
+
+#endif
+}
+//____________________________________________________________________________
+void GenerateEventsAtFixedEnergies(void)
+{
+  //-- create the GENIE high level event generation object for the given 
+  //   initial state
+  InitialState init_state(gOptTgtPdgCode, gOptNuPdgCode);
+
+  GEVGDriver evg_driver;
+  evg_driver.Configure(init_state);
+
   //-- If splines are used, then create any spline that is needed but is not
   //   already loaded (can built them all here if no spline at all is loaded)
-  if(gOptBuildSplines) driver.CreateSplines();
-
-  //-- this driver produces events for monoenergetic neutrinos
-  TLorentzVector nu_p4(0.,0.,gOptMinNuEnergy,gOptMinNuEnergy); // px,py,pz,E (GeV)
+  if(gOptBuildSplines) evg_driver.CreateSplines();
 
   //-- initialize an Ntuple Writer
   NtpWriter ntpw(kDefOptNtpFormat, gOptRunNu);
@@ -193,30 +371,28 @@ int main(int argc, char ** argv)
      LOG("gevgen", pNOTICE) << " *** Generating event............ " << ievent;
 
      // generate neutrino energy (if an energy range was defined)
+     double Ev = 0;
      if(gOptNuEnergyRange>0) {
-       double Ev = gOptMinNuEnergy + gOptNuEnergyRange * r->RndEvg().Rndm();
-       nu_p4.SetPxPyPzE(0,0,Ev,Ev);
+       Ev = gOptMinNuEnergy + gOptNuEnergyRange * r->RndEvg().Rndm();
+     } else {
+       Ev = gOptMinNuEnergy;
      }
+     TLorentzVector nu_p4(0.,0.,Ev,Ev); // px,py,pz,E (GeV)
 
      // generate a single event
-     EventRecord * event = driver.GenerateEvent(nu_p4);
+     EventRecord * event = evg_driver.GenerateEvent(nu_p4);
 
      LOG("gevgen", pINFO) << "Generated Event GHEP Record: " << *event;
 
-     // add event at the output ntuple
+     // add event at the output ntuple, refresh the mc job monitor & clean up
      ntpw.AddEventRecord(ievent, event);
-
-     // refresh the mc job monitor
      mcjmonitor.Update(ievent,event);
-
      ievent++;
      delete event;
   }
 
   //-- save the generated MC events
   ntpw.Save();
-
-  return 0;
 }
 //____________________________________________________________________________
 void GetCommandLineArgs(int argc, char ** argv)
@@ -225,7 +401,7 @@ void GetCommandLineArgs(int argc, char ** argv)
 
   //-- Optional arguments
 
-  //number of events:
+  // number of events:
   try {
     LOG("gevgen", pINFO) << "Reading number of events to generate";
     gOptNevents = genie::utils::clap::CmdLineArgAsInt(argc,argv,'n');
@@ -237,7 +413,7 @@ void GetCommandLineArgs(int argc, char ** argv)
     }
   }
 
-  //run number:
+  // run number:
   try {
     LOG("gevgen", pINFO) << "Reading MC run number";
     gOptRunNu = genie::utils::clap::CmdLineArgAsInt(argc,argv,'r');
@@ -248,12 +424,22 @@ void GetCommandLineArgs(int argc, char ** argv)
     }
   }
 
+  // flux functional form
+  try {
+    LOG("gevgen", pINFO) << "Reading flux function";
+    gOptFlux = genie::utils::clap::CmdLineArgAsString(argc,argv,'f');
+    gOptUsingFlux = true;
+  } catch(exceptions::CmdLineArgParserException e) {
+    if(!e.ArgumentFound()) {
+    }
+  }
+
   //spline building option
   gOptBuildSplines = genie::utils::clap::CmdLineArgAsBool(argc,argv,'s');
 
   //-- Required arguments
 
-  //neutrino energy:
+  // neutrino energy:
   try {
     LOG("gevgen", pINFO) << "Reading neutrino energy";
     string nue = genie::utils::clap::CmdLineArgAsString(argc,argv,'e');
@@ -280,7 +466,7 @@ void GetCommandLineArgs(int argc, char ** argv)
     }
   }
 
-  //neutrino PDG code:
+  // neutrino PDG code:
   try {
     LOG("gevgen", pINFO) << "Reading neutrino PDG code";
     gOptNuPdgCode = genie::utils::clap::CmdLineArgAsInt(argc,argv,'p');
@@ -292,7 +478,7 @@ void GetCommandLineArgs(int argc, char ** argv)
     }
   }
 
-  //target PDG code:
+  // target PDG code:
   try {
     LOG("gevgen", pINFO) << "Reading target PDG code";
     gOptTgtPdgCode = genie::utils::clap::CmdLineArgAsInt(argc,argv,'t');
@@ -302,6 +488,22 @@ void GetCommandLineArgs(int argc, char ** argv)
       PrintSyntax();
       exit(1);
     }
+  }
+
+  // print the command line options
+  LOG("gevgen", pINFO) << "Number of events requested = " << gOptNevents;
+  LOG("gevgen", pINFO) << "Building splines at init.  = " << gOptBuildSplines; 
+  LOG("gevgen", pINFO) << "Neutrino PDG code          = " << gOptNuPdgCode;
+  LOG("gevgen", pINFO) << "Target PDG code            = " << gOptTgtPdgCode;
+  LOG("gevgen", pINFO) << "MC Run Number              = " << gOptRunNu;
+  if(gOptNuEnergyRange>0) {
+    LOG("gevgen", pINFO) << "Neutrino energy            = [" 
+        << gOptMinNuEnergy << ", " << gOptMinNuEnergy+gOptNuEnergyRange << "]";
+  } else {
+    LOG("gevgen", pINFO) << "Neutrino energy            = " << gOptMinNuEnergy;
+  }
+  if(gOptUsingFlux) {
+    LOG("gevgen", pINFO) << "Flux                       = " << gOptFlux;
   }
 }
 //____________________________________________________________________________
