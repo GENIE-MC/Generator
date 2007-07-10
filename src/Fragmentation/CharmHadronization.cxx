a6425ef63df96787ed4df1cbf00931fe0ed74e28
costas <costas.andreopoulos@stfc.ac.uk>
2007-07-10 01:16:04 +0000

    *** empty log message ***
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1539 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Fragmentation/CharmHadronization.cxx
+++ src/Fragmentation/CharmHadronization.cxx
@@ -25,6 +25,7 @@
 #include "Fragmentation/FragmentationFunctionI.h"
 #include "Messenger/Messenger.h"
 #include "Numerical/RandomGen.h"
+#include "Numerical/Spline.h"
 #include "PDG/PDGCodes.h"
 #include "PDG/PDGUtils.h"
 #include "PDG/PDGLibrary.h"
@@ -84,7 +85,9 @@ TClonesArray * CharmHadronization::Hadronize(
   //-- Generate a charmed hadron PDG code and generate its energy
   //   based on the input fragmentation function
 
+  int nupdg = init_state.ProbePdg();
   int cpdgc = 0;
+
   double mC=0., mC2=0., EC=0., EC2=0., z=0., pC2=0.;
 
   unsigned int itry = 0;
@@ -92,10 +95,10 @@ TClonesArray * CharmHadronization::Hadronize(
 
   while(itry++<1000 && !found) {
 
-    cpdgc = this->GenerateCharmHadron(Ev);  // generate hadron
-    z     = fFragmFunc->GenerateZ();        // generate z(=Eh/Ev)
-    mC    = pdglib->Find(cpdgc)->Mass();    // lookup mass
-    EC    = z*Eh;                           // charm hadron energy
+    cpdgc = this->GenerateCharmHadron(nupdg,Ev); // generate hadron
+    z     = fFragmFunc->GenerateZ();             // generate z(=Eh/Ev)
+    mC    = pdglib->Find(cpdgc)->Mass();         // lookup mass
+    EC    = z*Eh;                                // charm hadron energy
     mC2   = TMath::Power(mC,2);
     EC2   = TMath::Power(EC,2);
     pC2   = EC2-mC2;
@@ -191,7 +194,7 @@ TClonesArray * CharmHadronization::Hadronize(
 
     new ((*particle_list)[0]) TMCParticle (1,cpdgc,
 	     -1,-1,-1, p4C.Px(),p4C.Py(),p4C.Pz(),p4C.E(),mC, 0,0,0,0,0);
-    new ((*particle_list)[1]) TMCParticle (11,kPdgHadronicBlob,
+    new ((*particle_list)[1]) TMCParticle (1,kPdgHadronicBlob,
              -1,-1,-1, p4R.Px(),p4R.Py(),p4R.Pz(),p4R.E(),WR, 0,0,0,0,0);
 
     return particle_list;
@@ -337,6 +340,30 @@ TH1D * CharmHadronization::MultiplicityProb(
   return 0;
 }
 //____________________________________________________________________________
+int CharmHadronization::GenerateCharmHadron(int nupdg, double EvLab) const
+{
+  // generate a charmed hadron pdg code using a charm fraction table
+
+  RandomGen * rnd = RandomGen::Instance();
+  double r  = rnd->RndHadro().Rndm();
+
+  if(pdg::IsNeutrino(nupdg)) {
+     double tf = 0;
+     if      (r < (tf+=fD0FracSpl->Evaluate(EvLab)))  return kPdgD0;       // D^0
+     else if (r < (tf+=fDpFracSpl->Evaluate(EvLab)))  return kPdgDP;       // D^+
+     else if (r < (tf+=fDsFracSpl->Evaluate(EvLab)))  return kPdgDPs;      // Ds^+
+     else                                             return kPdgLambdaPc; // Lamda_c^+
+
+  } else if(pdg::IsAntiNeutrino(nupdg)) {
+     if      (r < fD0BarFrac)          return kPdgAntiD0;
+     else if (r < fD0BarFrac+fDmFrac)  return kPdgDM;
+     else                              return kPdgDMs;
+  }
+
+  LOG("CharmHad", pERROR) << "Could not generate a charm hadron!";
+  return 0;
+}
+//____________________________________________________________________________
 void CharmHadronization::Configure(const Registry & config)
 {
   Algorithm::Configure(config);
@@ -358,41 +385,30 @@ void CharmHadronization::LoadConfig(void)
 
   //-- Get a fragmentation function
   fFragmFunc = dynamic_cast<const FragmentationFunctionI *> (
-                                         this->SubAlg("FragmentationFunc"));
+    this->SubAlg("FragmentationFunc"));
   assert(fFragmFunc);
 
   fCharmPT2pdf = new TF1("fCharmPT2pdf", "exp(-0.213362-6.62464*x)",0,0.6);
-}
-//____________________________________________________________________________
-int CharmHadronization::GenerateCharmHadron(double Ev) const
-{
-  // generate a charmed hadron pdg code using a charm fraction table
 
-  //------ eventually the charm fraction table should be an external object
-  //       that gets loaded from its XML configuration file -----------------/
+  // neutrino charm fractions: D^0, D^+, Ds^+ (remainder: Lamda_c^+)
+  //
+  const int nc = 15;
+  double ec[nc] = {0.,5.,10.,15.,20.,25.,30.,35.,40.,50.,60.,70.,80.,90.,100.};
 
-  RandomGen * rnd = RandomGen::Instance();
+  double d0frac[nc] = { .000, .320, .460,  .500,  .520,  .530, .540, .540,
+                        .540, .550,  .550,  .560,  .570, .580, .600  };
+  double dpfrac[nc] = { .000, .120, .180,  .200,  .200,  .210, .210, .210,
+                        .210, .210,  .220,  .220,  .220, .230, .230  };
+  double dsfrac[nc] = { .000, .054, .078,  .130,  .130,  .140, .140, .140,
+                        .140, .140,  .140,  .140,  .140, .150, .150  };
+ 
+  fD0FracSpl = new Spline(nc, ec, d0frac);
+  fDpFracSpl = new Spline(nc, ec, dpfrac);
+  fDsFracSpl = new Spline(nc, ec, dsfrac);
 
-  double rndm = rnd->RndHadro().Rndm();
-
-  if(Ev <= 20) {
-     if      (rndm <= 0.32)                  return kPdgD0;       // D^0
-     else if (rndm >  0.32 && rndm <= 0.37)  return kPdgDP;       // D^+
-     else if (rndm >  0.37 && rndm <= 0.55)  return kPdgDPs;      // Ds^+
-     else                                    return kPdgLambdaPc; // Lamda_c^+
-
-  } else if (Ev > 20 && Ev <=40)  {
-     if      (rndm <= 0.50)                  return kPdgD0;
-     else if (rndm >  0.50 && rndm <= 0.60)  return kPdgDP;
-     else if (rndm >  0.60 && rndm <= 0.82)  return kPdgDPs;
-     else                                    return kPdgLambdaPc;
-
-  } else {
-     if      (rndm <= 0.64)                  return kPdgD0;
-     else if (rndm >  0.64 && rndm <= 0.86)  return kPdgDP;
-     else if (rndm >  0.86 && rndm <= 0.95)  return kPdgDPs;
-     else                                    return kPdgLambdaPc;
-  }
-  return 0;
+  // anti-neutrino charm fractions: bar(D^0), D^-, (remainder: Ds^-)
+  //
+  fD0BarFrac = 0.667;
+  fDmFrac    = 0.222;
 }
 //____________________________________________________________________________
