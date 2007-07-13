31d1db70dd3a9e8d0afd0727c9d88e2a85a25717
costas <costas.andreopoulos@stfc.ac.uk>
2007-07-13 12:47:24 +0000

    same change to re-enable the older root version required for the T2K/nd280 vertical slice test
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1562 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Decay/PythiaDecayer.cxx
+++ src/Decay/PythiaDecayer.cxx
@@ -16,7 +16,12 @@
 
 #include <vector>
 
+#include <RVersion.h>
+#if ROOT_VERSION_CODE >= ROOT_VERSION(5,15,6)
 #include <TMCParticle.h>
+#else
+#include <TMCParticle6.h>
+#endif
 
 #include "BaryonResonance/BaryonResUtils.h"
 #include "Decay/PythiaDecayer.h"
