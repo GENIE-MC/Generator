7ff4cab9d79cdb4f285e6a0507850d0ea63b356b
costas <costas.andreopoulos@stfc.ac.uk>
2007-10-05 16:09:55 +0000

    Extend to generate hadron+nucleus events for an incident hadron kinetic energy spectrum. The spectrum can be input at the command line as a functional form or a path to a flux file.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1677 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/stdapp/ghAEvGen.cxx
+++ src/stdapp/ghAEvGen.cxx
@@ -7,7 +7,7 @@
 	 Similar to NEUGEN's pitest (S.Dytman & H.Gallagher)
 
          Syntax :
-           ghAevgen [-n nev] -p hadron_pdg -t tgt_pdg [-r run#] -k KE
+           ghAevgen [-n nev] -p hadron_pdg -t tgt_pdg [-r run#] -k KE [-f flux]
 
          Options :
            [] Denotes an optional argument
@@ -16,13 +16,30 @@
            -t Specifies the nuclear target PDG code (10LZZZAAAI)
            -r Specifies the MC run number (default: 0)
            -k Specifies the incoming hadron's kinetic energy (in GeV).
-	      If a comma-separated set of numbers is supplied (eg 0.1,1.2)
-	      then the hadrons will be fired with an uniform kinetic energy
-	      distribution within that range.
+              The same option can be use to specify a kinetic energy range as
+	      a comma-separated set of numbers (eg 0.1,1.2).
+              If no flux is specified then the hadrons will be fired with a 
+              uniform kinetic energy distribution within the specified range.
+	      If a kinetic energy spectrum is supplied then the hadrons will be
+              fired with a kinetic energy following the input spectrum within 
+              the specified range.
+           -f Specifies the incoming hadron's kinetic energy spectrum - 
+              it can be either a function, eg 'x*x+4*exp(-x)' or a text file 
+              containing 2 columns corresponding to (kinetic energy {GeV}, 'flux').
 	      
-         Example:
-           ghAevgen -n 100000 -p 211 -t 1000260560 -k 0.165
-           will generate 100k pi^{+} + Fe56 events at E(pi^{+})=165 MeV
+         Examples:
+
+           shell% ghAevgen -n 100000 -p 211 -t 1000260560 -k 0.165
+           * will generate 100k pi^{+}+Fe56 events with a pi^{+} kinetic
+             energy of 165 MeV
+
+           shell% ghAevgen -n 100000 -p 211 -t 1000260560 -k 0.165,1.200
+           * will generate 100k pi^{+}+Fe56 events with the pi^{+} kinetic
+             energy distributed uniformly in the [165 MeV, 1200 MeV] range
+
+           shell% ghAevgen -n 100000 -p 211 -t 1000260560 -k 0.165,1.200 -f '1/x'
+           * will generate 100k pi^{+}+Fe56 events with the pi^{+} kinetic
+             energy distributed as f(KE) = 1/KE in the [165 MeV, 1200 MeV] range
 
 \authors  Minsuk Kim and Steve Dytman
           University of Pittsburgh
@@ -42,7 +59,14 @@
 
 #include <cassert>
 
+#include <TSystem.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1D.h>
+#include <TF1.h>
+
 #include "Algorithm/AlgFactory.h"
+#include "Conventions/Controls.h"
 #include "EVGCore/EventRecord.h"
 #include "EVGDrivers/GMCJMonitor.h"
 #include "EVGCore/EventRecordVisitorI.h"
@@ -54,6 +78,7 @@
 #include "Ntuple/NtpWriter.h"
 #include "Ntuple/NtpMCFormat.h"
 #include "Numerical/RandomGen.h"
+#include "Numerical/Spline.h"
 #include "PDG/PDGCodes.h"
 #include "PDG/PDGLibrary.h"
 #include "Utils/StringUtils.h"
@@ -61,9 +86,11 @@
 #include "Utils/CmdLineArgParserException.h"
 
 using namespace genie;
+using namespace genie::controls;
 
-void GetCommandLineArgs (int argc, char ** argv);
-void PrintSyntax        (void);
+void GetCommandLineArgs         (int argc, char ** argv);
+void BuildKineticEnergySpectrum (void);
+void PrintSyntax                (void);
 
 //Default options 
 int     kDefOptNevents   = 10000;   // n-events to generate
@@ -76,6 +103,11 @@ Long_t        gOptRunNu;            // run number
 int           gOptInpHadPdgCode;    // incoming hadron PDG code
 double        gOptInpHadKE;         // incoming hadron kinetic enegy (GeV) or min energy in specified range
 double        gOptInpHadDeltaKE;    // incoming hadron kinetic energy range (GeV)
+string        gOptFlux;             // input flux (function or flux file)
+
+// globals
+bool          gOptUsingFlux = false;
+TH1D *        gSpectrum     = 0;
 
 //____________________________________________________________________________
 int main(int argc, char ** argv)
@@ -83,13 +115,17 @@ int main(int argc, char ** argv)
   //-- parse command line arguments
   GetCommandLineArgs(argc,argv);
 
+  //-- build the incident hadron kinetic energy spectrum, if required
+  if(gOptUsingFlux) 
+	BuildKineticEnergySpectrum();
+
   //-- get the intranuke module
   AlgFactory * algf = AlgFactory::Instance();
   const EventRecordVisitorI * intranuke = 
             dynamic_cast<const EventRecordVisitorI *> (
                      algf->GetAlgorithm("genie::Intranuke","hA-TestMode"));
 
-  //-- initialize an Ntuple Writer
+  //-- initialize an Ntuple Writer to save GHEP records into a ROOT tree
   NtpWriter ntpw(kNFEventRecord, gOptRunNu);
   ntpw.Initialize();
 
@@ -116,8 +152,12 @@ int main(int argc, char ** argv)
       
       //-- generate a kinetic energy & form the input 4-momenta
       double ke = 0;
-      if(gOptInpHadDeltaKE<0) { ke = gOptInpHadKE; }
-      else                    { ke = gOptInpHadKE + rnd->RndGen().Rndm() * gOptInpHadDeltaKE; }
+      if(gOptUsingFlux) {
+        ke = gSpectrum->GetRandom();
+      } else {
+        if(gOptInpHadDeltaKE<0) { ke = gOptInpHadKE; }
+        else                    { ke = gOptInpHadKE + rnd->RndGen().Rndm() * gOptInpHadDeltaKE; }
+      }
 
       double Eh  = mh + ke;
       double pzh = TMath::Sqrt(TMath::Max(0.,Eh*Eh-mh*mh));
@@ -152,7 +192,76 @@ int main(int argc, char ** argv)
   //-- save the generated MC events
   ntpw.Save();
 
+  //-- clean-up
+  if(gSpectrum) {
+    delete gSpectrum;
+    gSpectrum = 0;
+  }
+
   return 0;
+}
+//____________________________________________________________________________
+void BuildKineticEnergySpectrum(void)
+{
+  if(gSpectrum) {
+    delete gSpectrum;
+    gSpectrum = 0;
+  }
+
+  int    flux_bins    = 300;
+  int    flux_entries = 100000;
+  double ke_min       = gOptInpHadKE; 
+  double ke_max       = gOptInpHadKE + gOptInpHadDeltaKE; 
+  double dke          = gOptInpHadDeltaKE; 
+
+  gSpectrum  = new TH1D(
+    "spectrum","hadron kinetic energy spectrum", flux_bins, ke_min, ke_max);
+
+  // check whether the input flux is a file or a functional form
+  //
+  bool input_is_file = ! gSystem->AccessPathName(gOptFlux.c_str());
+  if(input_is_file) {
+    Spline * input_flux = new Spline(gOptFlux.c_str());  
+
+    // generate the flux hisogram from the input flux file
+    int    n       = 100;
+    double ke_step = (ke_max-ke_min)/(n-1);
+    double ymax  = -1, ry = -1, gy = -1, ke = -1;
+    for(int i=0; i<n; i++) {
+      ke   = ke_min + i*ke_step;
+      ymax = TMath::Max(ymax, input_flux->Evaluate(ke));
+    }
+    ymax *= 1.3;
+  
+    RandomGen * r = RandomGen::Instance();
+
+    for(int ientry=0; ientry<flux_entries; ientry++) {
+      bool accept = false;
+      unsigned int iter=0;
+      while(!accept) {
+        iter++;
+        if(iter > kRjMaxIterations) {
+           LOG("ghAevgen", pFATAL) << "Couldn't generate a flux histogram";
+           exit(1);
+        }
+        ke = ke_min + dke * r->RndGen().Rndm();
+        gy = ymax * r->RndGen().Rndm();
+        ry = input_flux->Evaluate(ke);
+        accept = gy < ry;
+        if(accept) gSpectrum->Fill(ke);
+      }
+    }
+    delete input_flux;
+  
+  } else {
+    // generate the flux histogram from the input functional form
+    TF1 *  input_func = new TF1("input_func", gOptFlux.c_str(), ke_min, ke_max);
+    gSpectrum->FillRandom("input_func", flux_entries);
+    delete input_func;
+  }
+  TFile f("./input-hadron-flux.root","recreate");   
+  gSpectrum->Write();
+  f.Close(); 
 }
 //____________________________________________________________________________
 void GetCommandLineArgs(int argc, char ** argv)
@@ -233,6 +342,17 @@ void GetCommandLineArgs(int argc, char ** argv)
     }
   }
 
+  // flux functional form or flux file
+  try {
+    LOG("ghAevgen", pINFO) << "Reading hadron's kinetic energy spectrum";
+    gOptFlux = genie::utils::clap::CmdLineArgAsString(argc,argv,'f');
+    gOptUsingFlux = true;
+  } catch(exceptions::CmdLineArgParserException e) {
+    if(!e.ArgumentFound()) {
+    }
+  }
+
+
   LOG("ghAevgen", pINFO) << "Number of events requested = " << gOptNevents;
   LOG("ghAevgen", pINFO) << "MC Run Number              = " << gOptRunNu;
   LOG("ghAevgen", pINFO) << "Incoming hadron PDG code   = " << gOptInpHadPdgCode;
@@ -245,6 +365,10 @@ void GetCommandLineArgs(int argc, char ** argv)
         << "Hadron input KE range      = [" 
         << gOptInpHadKE << ", " << gOptInpHadKE+gOptInpHadDeltaKE << "]";
   }
+  if(gOptUsingFlux) {
+    LOG("ghAevgen", pINFO) << "Input flux                 = " 
+                           << gOptFlux;
+  }
 }
 //____________________________________________________________________________
 void PrintSyntax(void)
@@ -252,7 +376,8 @@ void PrintSyntax(void)
   LOG("ghAevgen", pNOTICE)
     << "\n\n" 
     << "Syntax:" << "\n"
-    << "   ghAevgen [-n nev] -p hadron_pdg -t tgt_pdg [-r run] [-a R0] -k KE"
+    << "   ghAevgen [-n nev] -p hadron_pdg -t tgt_pdg [-r run] "
+    << "[-a R0] -k KE [-f flux]"
     << "\n";
 }
 //____________________________________________________________________________
