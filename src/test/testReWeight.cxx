6d0bc6efd49dd29dce4f5cc5bf56034cf0b88596
costas <costas.andreopoulos@stfc.ac.uk>
2007-10-09 18:43:08 +0000

    updated version of the test program
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1682 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/test/testReWeight.cxx
+++ src/test/testReWeight.cxx
@@ -31,152 +31,68 @@
 #include "Ntuple/NtpMCTreeHeader.h"
 #include "Ntuple/NtpMCEventRecord.h"
 #include "Messenger/Messenger.h"
-#include "ReWeight/MCModel.h"
-#include "ReWeight/WeightCalculator.h"
+#include "ReWeight/ReWeightCrossSection.h"
 #include "Utils/CmdLineArgParserUtils.h"
 #include "Utils/CmdLineArgParserException.h"
 
 using namespace genie;
 
-//func prototypes
-void    GetCommandLineArgs (int argc, char ** argv);
-void    PrintSyntax        (void);
-MCModel CreateOldXSecModel (void);
-MCModel CreateNewXSecModel (void);
+void GetCommandLineArgs (int argc, char ** argv);
+void PrintSyntax        (void);
 
-//input options (from command line arguments):
-string gOptInpFile;
+string gOptInpFile; // input options (from command line arguments):
 
 //___________________________________________________________________
 int main(int argc, char ** argv)
 {
   //-- get the command line arguments
   GetCommandLineArgs(argc, argv);
-  LOG("test", pNOTICE) << "Input  GENIE/ROOT file: " << gOptInpFile;
-
-  //-- define the 'old' & 'new' MCModel
-  MCModel old_model = CreateOldXSecModel();
-  MCModel new_model = CreateNewXSecModel();
 
   //-- create a weight calculator
-  WeightCalculator wcalc;
-  wcalc.OldCrossSectionModel(old_model);
-  wcalc.NewCrossSectionModel(new_model);
+  ReWeightCrossSection wcalc;
 
-  //-- open the ROOT file and get the TTree & its header
-  TTree *           tree = 0;
-  NtpMCTreeHeader * thdr = 0;
+  //-- open the file and get the TTree & its header / set branch addr.
 
   TFile file(gOptInpFile.c_str(),"READ");
-
-  tree = dynamic_cast <TTree *>           ( file.Get("gtree")  );
-  thdr = dynamic_cast <NtpMCTreeHeader *> ( file.Get("header") );
+  TTree *           tree = dynamic_cast <TTree *>           ( file.Get("gtree")  );
+  NtpMCTreeHeader * thdr = dynamic_cast <NtpMCTreeHeader *> ( file.Get("header") );
 
   LOG("test", pINFO) << "Input tree header: " << *thdr;
 
   NtpMCFormat_t format = thdr->format;
   assert(format == kNFEventRecord); // only ER trees in this test
 
-  //-- The ER ntuple contains a single TBranch with NtpMCEventRecord
-  //   objects in its leaves. Set the branch address for reading it.
   NtpMCEventRecord * mcrec = 0;
   tree->SetBranchAddress("gmcrec", &mcrec);
 
-  //-- loop over TTree NtpMC records
-  for(int i = 0; i< tree->GetEntries(); i++) {
+  //-- loop over the event tree (GHEP records) and reweight events
 
+  for(int i = 0; i< tree->GetEntries(); i++) 
+  {
     tree->GetEntry(i);
 
-    // get/print the next event and mc record header
     NtpMCRecHeader rec_header = mcrec->hdr;
     EventRecord &  event      = *(mcrec->event);
 
     LOG("test", pINFO) << rec_header;
     LOG("test", pINFO) << event;
 
+    wcalc.HandleInitState( event.Summary()->InitState() );
+
     // reweight the event
+    double wght = wcalc.NewWeight(event);
 
-    LOG("test", pINFO) << "Got the event. Computing new weight";
-
-    double wght = wcalc.ReWeight(event);
-
-    LOG("test", pINFO)  << "Old weight = " << event.Weight();
-    LOG("test", pINFO)  << "New weight = " << wght;
+    LOG("test", pINFO)  
+        << "Re-weighting: old wght. = " << event.Weight() 
+        << ", new wght. = " << wght;
   }
-
   LOG("test", pINFO)  << "Done!";
   return 0;
 }
 //___________________________________________________________________
-MCModel CreateOldXSecModel(void)
-{
-  MCModel     model;
-  ProcessInfo proc;
-  AlgId       alg;
-
-  proc.Set(kScQuasiElastic,  kIntWeakCC);
-  alg.SetId("genie::QELPXSec","CC-Default-BBA2003");
-  model.UseXSecAlg(proc,alg);
-
-  proc.Set(kScQuasiElastic,  kIntWeakNC);
-  alg.SetId("genie::QELPXSec","NC-Default");
-  model.UseXSecAlg(proc,alg);
-
-  proc.Set(kScDeepInelastic, kIntWeakCC);
-  alg.SetId("genie::DISPartonModelPXSec","CC-Default");
-  model.UseXSecAlg(proc,alg);
-
-  proc.Set(kScDeepInelastic, kIntWeakNC);
-  alg.SetId("genie::DISPartonModelPXSec","NC-Default");
-  model.UseXSecAlg(proc,alg);
-
-  proc.Set(kScResonant, kIntWeakCC);
-  alg.SetId("genie::RSExclusiveRESPXSec","Default");
-  model.UseXSecAlg(proc,alg);
-
-  proc.Set(kScResonant, kIntWeakNC);
-  alg.SetId("genie::RSExclusiveRESPXSec","Default");
-  model.UseXSecAlg(proc,alg);
-
-  return model;
-}
-//___________________________________________________________________
-MCModel CreateNewXSecModel(void)
-{
-  MCModel     model;
-  ProcessInfo proc;
-  AlgId       alg;
-
-  proc.Set(kScQuasiElastic,  kIntWeakCC);
-  alg.SetId("genie::QELPXSec","CC-Default");
-  model.UseXSecAlg(proc,alg);
-
-  proc.Set(kScQuasiElastic,  kIntWeakNC);
-  alg.SetId("genie::QELPXSec","NC-Default");
-  model.UseXSecAlg(proc,alg);
-
-  proc.Set(kScDeepInelastic, kIntWeakCC);
-  alg.SetId("genie::DISPartonModelPXSec","CC-Default");
-  model.UseXSecAlg(proc,alg);
-
-  proc.Set(kScDeepInelastic, kIntWeakNC);
-  alg.SetId("genie::DISPartonModelPXSec","NC-Default");
-  model.UseXSecAlg(proc,alg);
-
-  proc.Set(kScResonant, kIntWeakCC);
-  alg.SetId("genie::RSExclusiveRESPXSec","Default");
-  model.UseXSecAlg(proc,alg);
-
-  proc.Set(kScResonant, kIntWeakNC);
-  alg.SetId("genie::RSExclusiveRESPXSec","Default");
-  model.UseXSecAlg(proc,alg);
-
-  return model;
-}
-//___________________________________________________________________
 void GetCommandLineArgs(int argc, char ** argv)
 {
-  //get input ROOT file (containing a GENIE ER ntuple)
+  //get input ROOT file (containing a GENIE ER/GHEP ntuple)
   try {
     LOG("test", pINFO) << "Reading input filename";
     gOptInpFile = utils::clap::CmdLineArgAsString(argc,argv,'f');
@@ -196,6 +112,9 @@ void GetCommandLineArgs(int argc, char ** argv)
       << "Input ROOT file [" << gOptInpFile << "] is not accessible";
     exit(2);
   }
+
+  LOG("test", pNOTICE) 
+      << "Input GENIE event file: " << gOptInpFile;
 }
 //___________________________________________________________________
 void PrintSyntax(void)
