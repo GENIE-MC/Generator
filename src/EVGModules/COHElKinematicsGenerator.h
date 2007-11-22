98bfad16c71d2ca494d3820cb27459954748e243
costas <costas.andreopoulos@stfc.ac.uk>
2007-11-22 19:15:47 +0000

    added event generation module for generating the kinematics in coherent elastic events
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1715 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/EVGModules/COHElKinematicsGenerator.h
@@ -0,0 +1,56 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::COHElKinematicsGenerator
+
+\brief    Generates values for the kinematic variables describing coherent 
+          neutrino-nucleus elastic scattering events.
+          Is a concrete implementation of the EventRecordVisitorI interface.
+
+\author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+          STFC, Rutherford Appleton Laboratory
+
+\created  November 22, 2007
+
+\cpright  Copyright (c) 2003-2007, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _COHEL_KINEMATICS_GENERATOR_H_
+#define _COHEL_KINEMATICS_GENERATOR_H_
+
+#include "EVGModules/KineGeneratorWithCache.h"
+#include "Utils/Range1.h"
+
+namespace genie {
+
+class COHElKinematicsGenerator : public KineGeneratorWithCache {
+
+public :
+  COHElKinematicsGenerator();
+  COHElKinematicsGenerator(string config);
+ ~COHElKinematicsGenerator();
+
+  //-- implement the EventRecordVisitorI interface
+  void ProcessEventRecord(GHepRecord * event_rec) const;
+
+  //-- overload the Algorithm::Configure() methods to load private data
+  //-- members from configuration options
+  void Configure(const Registry & config);
+  void Configure(string config);
+
+public:
+  //-- methods to load sub-algorithms and config data from the Registry
+  void LoadConfig (void);
+
+  //-- overload KineGeneratorWithCache method to compute max xsec
+  double ComputeMaxXSec (const Interaction * in) const;
+
+  //-- overload KineGeneratorWithCache method to get energy
+  double Energy         (const Interaction * in) const;
+};
+
+}      // genie namespace
+#endif // _COHEL_KINEMATICS_GENERATOR_H_
