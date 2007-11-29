9b5e2975b860ee327bbef0ea5c63e6ea37f6f53f
costas <costas.andreopoulos@stfc.ac.uk>
2007-11-29 15:52:02 +0000

    add module for coherent elastic hadrnic system generation
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1719 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/EVGModules/COHElHadronicSystemGenerator.cxx
@@ -0,0 +1,60 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2007, GENIE Neutrino MC Generator Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+ or see $GENIE/LICENSE
+
+ Author: Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+         STFC, Rutherford Appleton Laboratory - October 03, 2004
+
+ For the class documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+ @ Nov 29, 2007 - CA
+   Was first added in 2.0.1
+
+*/
+//____________________________________________________________________________
+
+#include <cstdlib>
+
+#include "EVGModules/COHElHadronicSystemGenerator.h"
+#include "GHEP/GHepStatus.h"
+#include "GHEP/GHepParticle.h"
+#include "GHEP/GHepRecord.h"
+#include "Messenger/Messenger.h"
+
+using namespace genie;
+
+//___________________________________________________________________________
+COHElHadronicSystemGenerator::COHElHadronicSystemGenerator() :
+HadronicSystemGenerator("genie::COHElHadronicSystemGenerator")
+{
+
+}
+//___________________________________________________________________________
+COHElHadronicSystemGenerator::COHElHadronicSystemGenerator(string config) :
+HadronicSystemGenerator("genie::COHElHadronicSystemGenerator", config)
+{
+
+}
+//___________________________________________________________________________
+COHElHadronicSystemGenerator::~COHElHadronicSystemGenerator()
+{
+
+}
+//___________________________________________________________________________
+void COHElHadronicSystemGenerator::ProcessEventRecord(GHepRecord * evrec) const
+{
+// This method generates the final state hadronic system in COH elastic events
+//
+  int imom = evrec->TargetNucleusPosition();
+  int pdgc = evrec->Particle(imom)->Pdg();
+
+  TLorentzVector p4nuc = this->Hadronic4pLAB(evrec);
+  TLorentzVector v4(0.,0.,0.,0.);
+
+  evrec->AddParticle(pdgc,kIStStableFinalState, imom,-1,-1,-1, p4nuc, v4);
+}
+//___________________________________________________________________________
+
