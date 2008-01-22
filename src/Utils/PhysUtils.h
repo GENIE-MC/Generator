c642250600d781dd259efe2d0d81befea3e23059
costas <costas.andreopoulos@stfc.ac.uk>
2008-01-22 19:27:28 +0000

    Added R99118
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@1809 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Utils/PhysUtils.h
@@ -0,0 +1,37 @@
+//____________________________________________________________________________
+/*!
+
+\namespace  genie::utils::phys
+
+\brief      Various physics formulas
+
+\author     Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+            STFC, Rutherford Appleton Laboratory
+
+\created    January 22, 2008
+
+\cpright    Copyright (c) 2003-2008, GENIE Neutrino MC Generator Collaboration
+            For the full text of the license visit http://copyright.genie-mc.org
+            or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _PHYS_UTILS_H_
+#define _PHYS_UTILS_H_
+
+namespace genie {
+namespace utils {
+
+namespace phys
+{
+  //
+  // parameterizations of the longitudinal to transverse cross section ratio R 
+  //
+  double R99118   (double x, double Q2); ///< PRL 98, 142301, 2007
+  double RWhitlow (double x, double Q2);
+
+} // phys  namespace
+} // utils namespace
+} // genie namespace
+
+#endif // _PHYS_UTILS_H_
