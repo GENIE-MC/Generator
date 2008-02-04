89e7f71419616c880af6f4bc86b3efe3a75e465d
costas <costas.andreopoulos@stfc.ac.uk>
2008-02-04 19:20:43 +0000

    added GeantToPdg() conversion - fixed err with a \bar{Sigma} pdg code
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1811 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/PDG/PDGUtils.cxx
+++ src/PDG/PDGUtils.cxx
@@ -17,6 +17,7 @@
 #include <cassert>
 
 #include "BaryonResonance/BaryonResUtils.h"
+#include "Messenger/Messenger.h"
 #include "PDG/PDGUtils.h"
 #include "PDG/PDGCodes.h"
 
@@ -266,6 +267,56 @@ bool genie::pdg::IsHadron(int pdgc)
 bool genie::pdg::IsBaryonResonance(int pdgc)
 {
   return utils::res::IsBaryonResonance(pdgc);
+}
+//____________________________________________________________________________
+int genie::pdg::GeantToPdg(int geant_code)
+{
+  if(geant_code ==  3) return kPdgElectron;     //    11 / e-
+  if(geant_code ==  2) return kPdgPositron;     //   -11 / e+
+  if(geant_code ==  6) return kPdgMuon;         //    13 / mu-
+  if(geant_code ==  5) return kPdgAntiMuon;     //   -13 / mu+             
+  if(geant_code == 34) return kPdgTau;          //    15 / tau-
+  if(geant_code == 33) return kPdgAntiTau;      //   -15 / tau+              
+  if(geant_code ==  8) return kPdgPiP;          //   211 / pi+
+  if(geant_code ==  9) return kPdgPiM;          //  -211 / pi-
+  if(geant_code ==  7) return kPdgPi0;          //   111 / pi0
+  if(geant_code == 17) return kPdgEta;          //   221 / eta
+  if(geant_code == 11) return kPdgKP;           //   321 / K+
+  if(geant_code == 12) return kPdgKM;           //  -321 / K-
+  if(geant_code == 10) return kPdgK0L;          //   130 / K0_{long}
+  if(geant_code == 16) return kPdgK0S;          //   310 / K0_{short}
+  if(geant_code == 35) return kPdgDP;           //   411 / D+
+  if(geant_code == 36) return kPdgDM;           //  -411 / D-
+  if(geant_code == 37) return kPdgD0;           //   421 / D0
+  if(geant_code == 38) return kPdgAntiD0;       //  -421 / \bar{D0}
+  if(geant_code == 39) return kPdgDPs;          //   431 / D+_{s}
+  if(geant_code == 40) return kPdgDMs;          //  -431 / D-_{s}
+  if(geant_code ==  1) return kPdgGamma;        //    22 / photon
+  if(geant_code == 44) return kPdgZ0;           //    23 / Z
+  if(geant_code == 42) return kPdgWP;           //    24 / W+
+  if(geant_code == 43) return kPdgWM;           //   -24 / W-
+  if(geant_code == 14) return kPdgProton;       //  2212
+  if(geant_code == 15) return kPdgAntiProton;   // -2212
+  if(geant_code == 13) return kPdgNeutron;      //  2112
+  if(geant_code == 25) return kPdgAntiNeutron;  // -2112
+  if(geant_code == 18) return kPdgLambda;       //  3122 / Lambda
+  if(geant_code == 26) return kPdgAntiLambda;   // -3122 / \bar{Lambda}
+  if(geant_code == 19) return kPdgSigmaP;       //  3222 / Sigma+
+  if(geant_code == 20) return kPdgSigma0;       //  3212 / Sigma0
+  if(geant_code == 21) return kPdgSigmaM;       //  3112 / Sigma-
+  if(geant_code == 29) return kPdgAntiSigmaP;   // -3112 / \bar{Sigma+}
+  if(geant_code == 28) return kPdgAntiSigma0;   // -3212 / \bar{Sigma0}
+  if(geant_code == 27) return kPdgAntiSigmaM;   // -3112 / \barSigma-}
+  if(geant_code == 22) return kPdgXi0;          //  3322 / Xi0
+  if(geant_code == 23) return kPdgXiM;          //  3312 / Xi-
+  if(geant_code == 30) return kPdgAntiXi0;      // -3322 / \bar{Xi0}
+  if(geant_code == 31) return kPdgAntiXiP;      // -3312 / \bar{Xi+}
+  if(geant_code == 24) return kPdgOmegaM;       //  3332 / Omega-
+  if(geant_code == 32) return kPdgAntiOmegaP;   // -3332 / \bar{Omega+}
+
+  LOG("PDG", pWARN)
+        << "Can not convert geant code: " << geant_code << " to PDG";
+  return 0;
 }
 //____________________________________________________________________________
 
