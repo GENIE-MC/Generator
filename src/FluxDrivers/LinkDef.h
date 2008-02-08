3c6165db1419fe8ee6e158ed5d14e4838c13669e
costas <costas.andreopoulos@stfc.ac.uk>
2008-02-08 15:27:30 +0000

    add a trivial case for using monoenergetic neutrinos with the geometry drivers handling target mixes and geometries
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1827 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/FluxDrivers/LinkDef.h
+++ src/FluxDrivers/LinkDef.h
@@ -11,6 +11,7 @@
 #pragma link C++ class genie::flux::GJPARCNuFluxPassThroughInfo;
 
 #pragma link C++ class genie::flux::GCylindTH1Flux;
+#pragma link C++ class genie::flux::GMonoEnergeticFlux;
 
 //#pragma link C++ class genie::flux::GAtmoFlux;
 #pragma link C++ class genie::flux::GFlukaAtmo3DFlux;
