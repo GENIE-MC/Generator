17fccacb8dff31da95e234da52335f1b9428961a
costas <costas.andreopoulos@stfc.ac.uk>
2008-03-08 01:05:14 +0000

    Add event generation module to handle nuclear de-excitations. Initially just for O16
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@1881 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/EVGModules/NucDeExcitationSim.h
@@ -0,0 +1,50 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::NucDeExcitationSim
+
+\brief    Generates nuclear de-excitation gamma rays
+
+\author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+          STFC, Rutherford Appleton Laboratory
+
+\ref      16O: 
+           H.Ejiri,Phys.Rev.C48,1442(1993); 
+           K.Kobayashi et al., Nucl.Phys.B (proc Suppl) 139 (2005)
+
+\created  March 05, 2008
+
+\cpright  Copyright (c) 2003-2008, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _NUCLEAR_DEEXCITATION_H_
+#define _NUCLEAR_DEEXCITATION_H_
+
+#include <TLorentzVector.h>
+
+#include "EVGCore/EventRecordVisitorI.h"
+
+namespace genie {
+
+class NucDeExcitationSim : public EventRecordVisitorI {
+
+public :
+  NucDeExcitationSim();
+  NucDeExcitationSim(string config);
+ ~NucDeExcitationSim();
+
+  //-- implement the EventRecordVisitorI interface
+  void ProcessEventRecord (GHepRecord * evrec) const;
+
+private:
+  void           OxygenTargetSim      (GHepRecord * evrec) const;
+  void           AddPhoton            (GHepRecord * evrec, double E0, double t) const;
+  double         PhotonEnergySmearing (double E0, double t) const;
+  TLorentzVector Photon4P             (double E) const;
+};
+
+}      // genie namespace
+#endif // _NUCLEAR_DEEXCITATION_H_
