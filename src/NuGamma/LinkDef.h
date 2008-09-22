38a7e74e74c2599a701c69e30bf979d04543e567
costas <costas.andreopoulos@stfc.ac.uk>
2008-09-22 22:59:30 +0000

    Added new package for AM nu-gamma
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2029 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/NuGamma/LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace genie;
+
+#pragma link C++ class genie::AMNuGammaInteractionListGenerator;
+#pragma link C++ class genie::AMNuGammaGenerator;
+#pragma link C++ class genie::H3AMNuGammaPXSec;
+
+#endif
