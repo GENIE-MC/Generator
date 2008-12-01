f1bdb9cf0bb87bbbbb8cce928a71a288b6254e0e
costas <costas.andreopoulos@stfc.ac.uk>
2008-12-01 22:45:57 +0000

    Robert Hatchers's loadincs.C mods
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2056 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/scripts/gcint/loadincs.C
+++ src/scripts/gcint/loadincs.C
@@ -19,10 +19,19 @@
   gROOT->SetMacroPath(mp.Data());
   gSystem->SetIncludePath(ip);
 
+  // additions to .include must be done individually or CINT will
+  // try to quote all the spaces as a single path
+
   TString dip = ".include ";
   dip += genie_topdir.Data();
-  dip += " /usr/local/include/";
-  dip += " /usr/local/include/log4cpp";
+  dip += "/src ";
   gROOT->ProcessLine(dip.Data());
+
+  dip = ".include /usr/local/include/";
+  gROOT->ProcessLine(dip.Data());
+
+  dip = ".include /usr/local/include/log4cpp";
+  gROOT->ProcessLine(dip.Data());
+
 }
 
