326ed73ece9f0505c4960db3516b190274c9c14c
costas <costas.andreopoulos@stfc.ac.uk>
2008-12-06 13:36:30 +0000

    updated PDF test program
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2076 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/test/testPDFLIB.cxx
+++ src/test/testPDFLIB.cxx
@@ -20,6 +20,7 @@
 #include <TFile.h>
 #include <TMath.h>
 
+#include "Algorithm/AlgFactory.h"
 #include "Messenger/Messenger.h"
 #include "PDF/PDFModelI.h"
 #include "PDF/PDFLIB.h"
@@ -27,69 +28,37 @@
 
 using namespace genie;
 
-void fill_ntuple(TNtuple * nt, double q2, 
-                                   int nptype, int ngroup, int nset);
-
 //___________________________________________________________________
 int main(int /*argc*/, char ** /*argv*/)
 {
-  //-- Q^2 for which I extract PDFs
-
-  const int nq2 = 3;
-  double q2[nq2] = { 1, 2, 4 };
-
-  //-- PDF sets / check PDFLIB definitions
-
-  const int npdfs = 4;
-                             /*CTEQ*/ /*GRV98*/
-  const int nptype[npdfs] = {  1,  1,  1,  1 };
-  const int ngroup[npdfs] = {  4,  4,  5,  5 };
-  const int nset[npdfs]   = { 32, 41, 12, 13 };
-
-  TNtuple * ntuple = new TNtuple("ntuple","pdfs",
-                          "uv:dv:us:ds:s:g:x:Q2:nptype:ngroup:nset");
-
-  for(int ipdf = 0; ipdf < npdfs; ipdf++) {
-   for(int iq2 = 0; iq2 < nq2; iq2++) 
-     fill_ntuple(ntuple, q2[iq2], nptype[ipdf], ngroup[ipdf], nset[ipdf]);
-  }
-
-  TFile f("./genie-pdflib.root","recreate");
-  ntuple->Write("pdflib");
-
-  f.Close();
-  delete ntuple;
-
-  return 0;
-}
-//___________________________________________________________________
-void fill_ntuple(TNtuple * nt, 
-                         double Q2, int nptype, int ngroup, int nset)
-{
-  PDFModelI * pdfmodel = new PDFLIB();
-  
-  Registry * conf = new Registry();
-
-  conf->Set("Nptype", nptype);
-  conf->Set("Ngroup", ngroup);
-  conf->Set("Nset",   nset);
-
-  pdfmodel->Configure(*conf);
-
-  PDF pdf;
-  pdf.SetModel(pdfmodel);
-
-  const int nx = 300;
-
+  // x, Q2 range
+  const int nQ2 = 3;
+  const int nx  = 300;
+  double Q2arr[nQ2] = { 1.0, 2.0, 4.0 };
   const double xmin_idx = -2;
   const double xmax_idx =  0;
   const double dx_idx   = (xmax_idx-xmin_idx)/(nx-1);
 
-  for(int ix=0; ix<nx; ix++) {
+  // Output ntuple
+  TNtuple * nt = new TNtuple("nt","pdfs","uv:dv:us:ds:s:g:x:Q2");
 
-      double x = TMath::Power(10, xmin_idx + ix * dx_idx);
+  // PDF model
+  AlgFactory * algf = AlgFactory::Instance();
+  const PDFModelI * pdfmodel = 
+        dynamic_cast<const PDFModelI *> (
+                    algf->GetAlgorithm("genie::PDFLIB","GRVLO"));
+  
+  PDF pdf;
+  pdf.SetModel(pdfmodel);
+
+  // Extract PDFs
+  for(int iq2 = 0; iq2 < nQ2; iq2++) {
+    for(int ix = 0; ix < nx; ix++) {
+
+      double Q2 = Q2arr[iq2];
+      double x  = TMath::Power(10, xmin_idx + ix * dx_idx);
+
       pdf.Calculate(x, Q2);
-
       LOG("Main", pINFO) << "PDFs:\n" << pdf;
 
       nt->Fill(pdf.UpValence(),
@@ -98,11 +67,15 @@ void fill_ntuple(TNtuple * nt,
                pdf.DownSea(),
                pdf.Strange(),
                pdf.Gluon(),
-               x, Q2, nptype, ngroup, nset);
+               x, Q2);
+    }
   }
+  TFile f("./genie-pdflib.root","recreate");
+  nt->Write("pdflib");
 
-  delete pdfmodel;
-  delete conf;
+  f.Close();
+  delete nt;
+
+  return 0;
 }
 //___________________________________________________________________
-
