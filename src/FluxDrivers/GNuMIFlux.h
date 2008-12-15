5e72d561d4d419a0a2730892c8afbf237161ad5e
rhatcher <rhatcher@fnal.gov>
2008-12-15 22:52:10 +0000

    Progress on managing numi flux.  Make use of .h and .C files generated from the ntuples themselves via MakeClass to automate setting branches and creating the proper leaves.  Unfortunaely the geant3 and geant4 GNuMI ntuples have different formats.  All the most important variables are there in each, but upper/lower case varies.  A critical difference is the switch from Float_t to Double_t which means one can't simply use the same structure an change branch assignments.  The common format GNuMIFluxPassThrough needs a Copy() function defined for each type of ntuple.
    The g3 case has been implemented, but the g4 version is a skeleton.
    The retained variables could do with a review to ensure that we're not
    carrying more than necessary.  The helper classes g3numi and g4numi
    srouce (.h + .C) live in a subdirectory GNuMINtuple that needn't
    otherwise be exposed to any GENIE user -- the actual code gets into
    the library by having both #include "gXnumi.h" and #include "gXnumi.C".
    
    The x-y reweight function has been integrated into GNuMIFluxPassThrough.
    This has been tested against the fortran version. (this set of code
    still retains some of that testing code which should be purged in
    the next iteration).  Generally the values are comparable at the
    expected precision (g3 32-bit float ~2.5e-5 or better in general
    for the x-y weight, even better for the neutrino energy). There are
    some large outliers when calculating x-y weights for muon decays
    because the algorithm, as written, depends on taking the difference
    in two largish numbers (e.g. ~6 - ~6 = ~0.002) which results in
    a large loss of fractional error precision when starting with floats.
    
    Some interfaces have been put in place for handling coordinate
    transformations between the detector and beam, but these are not
    fully implemented and/or tested.
    
    
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2109 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/FluxDrivers/GNuMIFlux.h
+++ src/FluxDrivers/GNuMIFlux.h
@@ -28,15 +28,20 @@
 #include <string>
 #include <iostream>
 
-#include <TLorentzVector.h>
 #include <TVector3.h>
+#include <TLorentzVector.h>
+#include <TLorentzRotation.h>
 
 #include "EVGDrivers/GFluxI.h"
 #include "PDG/PDGUtils.h"
 
 class TFile;
-class TTree;
+class TChain;
 class TBranch;
+
+// MakeClass created classes for handling flux files
+class g3numi;
+class g4numi;
 
 using std::string;
 using std::ostream;
@@ -74,14 +79,11 @@ public :
   void SetFluxParticles (const PDGCodeList & particles);       ///< specify list of flux neutrino species
   void SetMaxEnergy     (double Ev);                           ///< specify maximum flx neutrino energy
   void SetFilePOT       (double pot);                          ///< POTs per input flux file
-  void SetUpstreamZ     (double z0);                           ///< set flux neutrino initial z position (upstream of the detector)
+  void SetGenWeighted   (bool genwgt=false) { fGenWeighted = genwgt; } ///< toggle whether GenerateNext() returns weight=1 flux (initial default false)
+
   void SetNumOfCycles   (int n);                               ///< set how many times to cycle through the ntuple (default: 1 / n=0 means 'infinite')
   void SetTreeName      (string name);                         ///< set input tree name (default: "h10")
   void ScanForMaxWeight (void);                                ///< scan for max flux weight (before generating unweighted flux neutrinos)
-  void UseFluxAtFarDetCenter  (void);
-  void UseFluxAtNearDetCenter (void);
-
-  void PrintCurrent (void);  ///< print current entry from leaves
 
   double   POT_curr       (void);                              ///< current average POT
   long int NFluxNeutrinos (void) const { return fNNeutrinos; } ///< number of flux neutrinos looped so far
@@ -89,6 +91,42 @@ public :
 
   const GNuMIFluxPassThroughInfo &
      PassThroughInfo(void) { return *fCurrentEntry; } ///< GNuMIFluxPassThroughInfo
+
+  void PrintCurrent (void);  ///< print current entry from leaves
+
+  // configure a flux window (or point) where E_nu and weight are evaluated
+
+  void UseFluxAtNearDetCenter (void);  ///< MINOS NearDet "center" (as found in ntuple)
+  void UseFluxAtFarDetCenter  (void);  ///< MINOS FarDet "center" (as found in ntuple)
+
+  // GNuMIFlux uses "cm" as the length unit consistently internally (this the is
+  // units used by both the g3 and g4 ntuples).  User interactions setting the
+  // beam-to-detector coordinate transform, flux window, and the returned position
+  // might need to be in other units.  
+  typedef enum ELengthUnits { kmeter, kcm, kmm, kfm } LengthUnits_t;
+  LengthUnits_t GetLengthUnits() const { return fLengthUnits; }
+  void          SetLengthUnits(LengthUnits_t units) { fLengthUnits = units; SetLengthScale(); }
+
+  typedef enum EStdFluxWindow {
+    kMinosNearDet,      // appropriate for Near Detector
+    kMinosFarDet,       // appropriate for Far Detector
+    kMinosNearRock,     // appropriate for Near rock generation
+    kMinosFarRock,      // appropriate for Far rock generation
+    kMinosNearCenter,   // point location mimic near value in ntuple
+    kMinosFarCenter     // point location mimic far value in ntuple
+  } StdFluxWindow_t;
+
+  // set both flux window and coordinate transform for some standard conditions
+  bool SetBeamFluxWindow(StdFluxWindow_t stdwindow, double padding=0);  ///< return false if unhandled
+  
+  // rwh: upgrade allow flux window set/get in beam coords as optional flag to *etFluxWindow
+  void SetFluxWindow(TLorentzVector  p1, TLorentzVector  p2, TLorentzVector  p3); ///< 3 points define a plane (by default in beam coordinates)
+  void GetFluxWindow(TLorentzVector& p1, TLorentzVector& p2, TLorentzVector& p3) const; ///< 3 points define a plane in beam coordinate 
+  void SetDetectorCoord(TLorentzVector det0, TLorentzRotation detrot);  ///< set detector position / orientation in beam system
+  void GetDetectorCoord(TLorentzVector& det0, TLorentzRotation& detrot) const;  ///< set detector position / orientation in beam system
+
+  void SetUpstreamZ     (double z0);                           ///< set flux neutrino initial z position (upstream of the detector)
+
 
 private:
 
@@ -100,126 +138,77 @@ private:
   void CleanUp               (void);
   void ResetCurrent          (void);
 
+  void SetLengthScale        (void);
+
   // Private data members
   //
-  double         fMaxEv;       ///< maximum energy
-  PDGCodeList *  fPdgCList;    ///< list of neutrino pdg-codes
+  double         fMaxEv;          ///< maximum energy
+  PDGCodeList *  fPdgCList;       ///< list of neutrino pdg-codes
 
-  int            fgPdgC;       ///< running generated nu pdg-code
-  TLorentzVector fgP4;         ///< running generated nu 4-momentum
-  TLorentzVector fgX4;         ///< running generated nu 4-position
+  int            fgPdgC;          ///< running generated nu pdg-code
+  TLorentzVector fgP4;            ///< running generated nu 4-momentum
+  TLorentzVector fgX4;            ///< running generated nu 4-position
 
-  TFile *   fNuFluxFile;       ///< input flux file
-  TTree *   fNuFluxTree;       ///< input flux ntuple
-  string    fNuFluxTreeName;   ///< input flux ntuple name
-  long int  fNEntries;         ///< number offlux ntuple entries
-  long int  fIEntry;           ///< current flux ntuple entry
-  double    fMaxWeight;        ///< max flux neutrino weight in input file
-  double    fFilePOT;          ///< file POT normalization
-  double    fZ0;               ///< configurable starting z position for each flux neutrino (in detector coord system)
-  int       fNCycles;          ///< how many times to cycle through the flux ntuple
-  int       fICycle;           ///< current cycle
-  double    fWeight;           ///< current neutrino weight
-  double    fSumWeight;        ///< sum of weights for neutrinos thrown so far
-  long int  fNNeutrinos;       ///< number of flux neutrinos thrown so far
-  int       fUseFluxAtDetCenter;  ///< use flux at near (-1) or far (+1) det center?
+  string    fNuFluxFilePattern;   ///< wildcarded path
+  string    fNuFluxTreeName;      ///< Tree name "h10" (g3) or "nudata" (g4)
+  TChain*   fNuFluxTree;          ///< TTree in g3numi or g4numi // REF ONLY!
+  g3numi*   fG3NuMI;              ///< g3numi ntuple
+  g4numi*   fG4NuMI;              ///< g4numi ntuple
+  int       fNFiles;              ///< number of files in chain
+
+  long int  fNEntries;            ///< number of flux ntuple entries
+  long int  fIEntry;              ///< current flux ntuple entry
+  double    fMaxWeight;           ///< max flux neutrino weight in input file
+  double    fFilePOT;             ///< file POT normalization
+  double    fZ0;                  ///< configurable starting z position for each flux neutrino (in detector coord system)
+  int       fNCycles;             ///< how many times to cycle through the flux ntuple
+  int       fICycle;              ///< current cycle
+  double    fWeight;              ///< current neutrino weight
+  double    fSumWeight;           ///< sum of weights for neutrinos thrown so far
+  long int  fNNeutrinos;          ///< number of flux neutrinos thrown so far
+  bool      fGenWeighted;         ///< does GenerateNext() give weights?
   bool      fDetLocIsSet;         ///< is a flux location (near/far) set?
+  // 
+  LengthUnits_t fLengthUnits;       ///< units for coordinates in user exchanges
+  double        fLengthScaleB2U;    ///< scale factor beam (cm) --> user
+  TLorentzVector   fFluxWindowBase; ///< base point for flux window
+  TLorentzVector   fFluxWindowDir1; ///< extent for flux window (direction 1)
+  TLorentzVector   fFluxWindowDir2; ///< extent for flux window (direction 2)
+  TLorentzVector   fDetectorZero;   ///< detector (0,0,0) in beam coordinates
+  TLorentzRotation fDetectorRot;    ///< detector axis' unit vectors in beam coordinate
 
-  //-- gnumi ntuple branches
-  //   maintained names/comments from gnumi documentation
-  //   http://www.hep.utexas.edu/~zarko/wwwgnumi/v19/
-  //
-  TBranch * fBr_run;           ///< Run number
-  TBranch * fBr_evtno;         ///< Event number (proton on target)
-  TBranch * fBr_Ndxdz;         ///< Neutrino direction slopes for a random decay
-  TBranch * fBr_Ndydz;         ///< see above
-  TBranch * fBr_Npz;           ///< Neutrino momentum (GeV/c) along z direction (beam axis)
-  TBranch * fBr_Nenergy;       ///< Neutrino energy (GeV/c) for a random decay
-  TBranch * fBr_NdxdzNea;      ///< Neutrino direction slopes for a decay forced at center of near detector
-  TBranch * fBr_NdydzNea;      ///< see above
-  TBranch * fBr_NenergyN;      ///< Neutrino energy for a decay forced at center of near detector
-  TBranch * fBr_NWtNear;       ///< Neutrino weight for a decay forced at center of near detector
-  TBranch * fBr_NdxdzFar;      ///< Repeats of above, for a decay forces to the center of the far detector.
-  TBranch * fBr_NdydzFar;      ///< see above
-  TBranch * fBr_NenergyF;      ///< see above
-  TBranch * fBr_NWtFar;        ///< see above
-  TBranch * fBr_Norig;         ///< Obsolete...
-  TBranch * fBr_Ndecay;        ///< Decay mode that produced neutrino
-                               ///   1  K0L -> nue pi- e+
-                               ///   2  K0L -> nuebar pi+ e-
-                               ///   3  K0L -> numu pi- mu+
-                               ///   4  K0L -> numubar pi+ mu-
-                               ///   5  K+  -> numu mu+
-                               ///   6  K+  -> nue pi0 e+
-                               ///   7  K+  -> numu pi0 mu+
-                               ///   8  K-  -> numubar mu-
-                               ///   9  K-  -> nuebar pi0 e-
-                               ///  10  K-  -> numubar pi0 mu-
-                               ///  11  mu+ -> numubar nue e+
-                               ///  12  mu- -> numu nuebar e-
-                               ///  13  pi+ -> numu mu+
-                               ///  14  pi- -> numubar mu-
-  TBranch * fBr_Ntype;         ///< Neutrino flavor (56=numu, 55=numu-bar, 53=nue, 52=nue-bar)
-  TBranch * fBr_Vx;            ///< X position of hadron/muon decay
-  TBranch * fBr_Vy;            ///< Y position of hadron/muon decay
-  TBranch * fBr_Vz;            ///< Z position of hadron/muon decay
-  TBranch * fBr_pdPx;          ///< Parent X momentum at decay point
-  TBranch * fBr_pdPy;          ///< Parent Y momentum at decay point
-  TBranch * fBr_pdPz;          ///< Parent Z momentum at decay point
-  TBranch * fBr_ppdxdz;        ///< Parent dxdz direction at production
-  TBranch * fBr_ppdydz;        ///< Parent dydz direction at production
-  TBranch * fBr_pppz;          ///< Parent Z momentum at production
-  TBranch * fBr_ppenergy;      ///< Parent energy at production
-  TBranch * fBr_ppmedium;      ///< Tracking medium number where parent was produced
-  TBranch * fBr_ptype;         ///< Parent GEANT code particle ID
-  TBranch * fBr_ppvx;          ///< Parent production vertex X,Y,Z (cm)
-  TBranch * fBr_ppvy;          ///< see above
-  TBranch * fBr_ppvz;          ///< see above
-  TBranch * fBr_muparpx;       ///< Repeat of information above, but for muon neutrino parents
-  TBranch * fBr_muparpy;       ///< see above
-  TBranch * fBr_muparpz;       ///< see above
-  TBranch * fBr_mupare;        ///< see above
-  TBranch * fBr_Necm;          ///< Neutrino energy in COM frame
-  TBranch * fBr_Nimpwt;        ///< Weight of neutrino parent
-  TBranch * fBr_xpoint;        ///< Debugging hook -- unused
-  TBranch * fBr_ypoint;        ///< Debugging hook -- unused
-  TBranch * fBr_zpoint;        ///< Debugging hook -- unused
-  TBranch * fBr_tvx;           ///< X exit point of parent particle at the target
-  TBranch * fBr_tvy;           ///< Y exit point of parent particle at the target
-  TBranch * fBr_tvz;           ///< Z exit point of parent particle at the target
-  TBranch * fBr_tpx;           ///< Parent momentum exiting the target (X)
-  TBranch * fBr_tpy;           ///< Parent momentum exiting the target (Y)
-  TBranch * fBr_tpz;           ///< Parent momentum exiting the target (Z)
-  TBranch * fBr_tptype;        ///< Parent particle ID exiting the target (GEANT code)
-  TBranch * fBr_tgen;          ///< Parent generation in cascade
-                               ///  1 = primary proton
-                               ///  2 = particles produced by proton interaction
-                               ///  3 = particles produced by interactions of the 2's, ...
-  TBranch * fBr_tgptype;       ///< Type of particle that created a particle flying of the target
-  TBranch * fBr_tgppx;         ///< Momentum of a particle, that created a particle that flies off the target, at the interaction point.
-  TBranch * fBr_tgppy;         ///< see above
-  TBranch * fBr_tgppz;         ///< see above
-  TBranch * fBr_tprivx;        ///< Primary particle interaction vertex
-  TBranch * fBr_tprivy;        ///< see above
-  TBranch * fBr_tprivz;        ///< see above
-  TBranch * fBr_beamx;         ///< Primary proton origin
-  TBranch * fBr_beamy;         ///< see above
-  TBranch * fBr_beamz;         ///< see above
-  TBranch * fBr_beampx;        ///< Primary proton momentum
-  TBranch * fBr_beampy;        ///< see above
-  TBranch * fBr_beampz;        ///< see above
-  //
-  // corresponding leaves are part of owned structure
-  //
-  GNuMIFluxPassThroughInfo* fCurrentEntry;
+  int       fUseFluxAtDetCenter;  ///< use flux at near (-1) or far (+1) det center from ntuple?
+
+  GNuMIFluxPassThroughInfo* fCurrentEntry;  ///< copy of current ntuple entry info (owned structure)
 
 };
 
+#define GNUMI_TEST_XY_WGT
+#ifdef  GNUMI_TEST_XY_WGT
+class xypartials { 
+  // intermediate partial info from xy reweighting for comparison w/ f77 version
+public:
+  xypartials() { ; }
+  void ReadStream(ifstream& myfile);
+  int  Compare(const xypartials& other) const;
+  void Print() const;
+  // actual data
+  double parent_mass, parentp, parent_energy;
+  double gamma, beta_mag, enuzr, rad;
+  double costh_pardet, theta_pardet, emrat, eneu;
+  double sangdet, wgt;
+  double betanu[3], p_nu[3], partial1, p_dcm_nu[4];
+  double muparent_px, muparent_py, muparent_pz;
+  double gammamp, betamp[3], partial2, p_pcm_mp[3], p_pcm;
+  double costhmu, wgt_ratio;
+  int ptype, ntype;
+};
+#endif
 
-// A small persistable C-struct -like class that mirrors the structure of 
-// the gnumi ntuples.  This can then be stored as an extra branch of
-// the output event tree -alongside with the generated event branch- 
-// for use further upstream in the analysis chain -eg beam reweighting etc-)
+// A small persistable C-struct -like class that mirrors (some of) the structure of 
+// the gnumi ntuples.  This can then be stored as an extra branch of the output event 
+// tree -alongside with the generated event branch- for use further upstream in the 
+// analysis chain - e.g. beam reweighting etc.
 //
 class GNuMIFluxPassThroughInfo: public TObject {
 public:
@@ -229,8 +218,17 @@ public:
    */
    virtual ~GNuMIFluxPassThroughInfo() { };
 
+   void Copy(const g3numi*);
+   void Copy(const g4numi*);
+
    void Reset();  //
    void ConvertPartCodes();
+
+   int CalcEnuWgt(double xpos, double ypos, double zpos, double& enu, double& wgt_xy
+#ifdef  GNUMI_TEST_XY_WGT
+                  , xypartials& partials
+#endif
+                  ) const;
 
    friend ostream & operator << (ostream & stream, const GNuMIFluxPassThroughInfo & info);
 
@@ -239,70 +237,71 @@ public:
 
    // maintained variable names from gnumi ntuples
    // see http://www.hep.utexas.edu/~zarko/wwwgnumi/v19/[/v19/output_gnumi.html]
-   int   run;
-   int   evtno;
-   float ndxdz;
-   float ndydz;
-   float npz;
-   float nenergy;
-   float ndxdznea;
-   float ndydznea;
-   float nenergyn;
-   float nwtnear;
-   float ndxdzfar;
-   float ndydzfar;
-   float nenergyf;
-   float nwtfar;
-   int   norig;
-   int   ndecay;
-   int   ntype;
-   float vx;
-   float vy;
-   float vz;
-   float pdpx;
-   float pdpy;
-   float pdpz;
-   float ppdxdz;
-   float ppdydz;
-   float pppz;
-   float ppenergy;
-   int   ppmedium;
-   int   ptype;     // converted to PDG
-   float ppvx;
-   float ppvy;
-   float ppvz;
-   float muparpx;
-   float muparpy;
-   float muparpz;
-   float mupare;
-   float necm;
-   float nimpwt;
-   float xpoint;
-   float ypoint;
-   float zpoint;
-   float tvx;
-   float tvy;
-   float tvz;
-   float tpx;
-   float tpy;
-   float tpz;
-   int   tptype;   // converted to PDG
-   int   tgen;
-   int   tgptype;  // converted to PDG
-   float tgppx;
-   float tgppy;
-   float tgppz;
-   float tprivx;
-   float tprivy;
-   float tprivz;
-   float beamx;
-   float beamy;
-   float beamz;
-   float beampx;
-   float beampy;
-   float beampz;    
 
-ClassDef(GNuMIFluxPassThroughInfo,2)
+   Int_t    run;
+   Int_t    evtno;
+   Double_t ndxdz;
+   Double_t ndydz;
+   Double_t npz;
+   Double_t nenergy;
+   Double_t ndxdznea;
+   Double_t ndydznea;
+   Double_t nenergyn;
+   Double_t nwtnear;
+   Double_t ndxdzfar;
+   Double_t ndydzfar;
+   Double_t nenergyf;
+   Double_t nwtfar;
+   Int_t    norig;
+   Int_t    ndecay;
+   Int_t    ntype;
+   Double_t vx;
+   Double_t vy;
+   Double_t vz;
+   Double_t pdpx;
+   Double_t pdpy;
+   Double_t pdpz;
+   Double_t ppdxdz;
+   Double_t ppdydz;
+   Double_t pppz;
+   Double_t ppenergy;
+   Int_t    ppmedium;
+   Int_t    ptype;     // converted to PDG
+   Double_t ppvx;
+   Double_t ppvy;
+   Double_t ppvz;
+   Double_t muparpx;
+   Double_t muparpy;
+   Double_t muparpz;
+   Double_t mupare;
+   Double_t necm;
+   Double_t nimpwt;
+   Double_t xpoint;
+   Double_t ypoint;
+   Double_t zpoint;
+   Double_t tvx;
+   Double_t tvy;
+   Double_t tvz;
+   Double_t tpx;
+   Double_t tpy;
+   Double_t tpz;
+   Int_t    tptype;   // converted to PDG
+   Int_t    tgen;
+   Int_t    tgptype;  // converted to PDG
+   Double_t tgppx;
+   Double_t tgppy;
+   Double_t tgppz;
+   Double_t tprivx;
+   Double_t tprivy;
+   Double_t tprivz;
+   Double_t beamx;
+   Double_t beamy;
+   Double_t beamz;
+   Double_t beampx;
+   Double_t beampy;
+   Double_t beampz;    
+
+ClassDef(GNuMIFluxPassThroughInfo,3)
 };
 
 } // flux namespace
