11873d4acf42b1b8e66da21f757dacf09c0c67ac
costas <costas.andreopoulos@stfc.ac.uk>
2009-02-09 16:30:51 +0000

    skeleton for diffractive generator
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2185 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Diffractive/LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace genie;
+
+#pragma link C++ class genie::DfrcInteractionListGenerator;
+#pragma link C++ class genie::DfrcGenerator;
+#pragma link C++ class genie::DfrcPXSec;
+
+#endif
