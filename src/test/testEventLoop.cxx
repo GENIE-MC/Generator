a8cb0f38e8419d55e9e9677385078510eca0f126
costas <costas.andreopoulos@stfc.ac.uk>
2009-02-09 20:47:22 +0000

    add testEventLoop program to be used as a template for user analysis
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2197 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/test/testEventLoop.cxx
@@ -0,0 +1,161 @@
+//____________________________________________________________________________
+/*!
+
+\program gtestRegistry
+
+\brief   Program used for testing / debugging GENIE's Registry
+
+\author  Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         STFC, Rutherford Appleton Laboratory
+
+\created May 4, 2004
+
+\cpright Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
+         For the full text of the license visit http://copyright.genie-mc.org
+         or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#include <string>
+
+#include <TSystem.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TIterator.h>
+
+#include "EVGCore/EventRecord.h"
+#include "GHEP/GHepParticle.h"
+#include "Ntuple/NtpMCFormat.h"
+#include "Ntuple/NtpMCTreeHeader.h"
+#include "Ntuple/NtpMCEventRecord.h"
+#include "Messenger/Messenger.h"
+#include "PDG/PDGCodes.h"
+#include "Utils/CmdLineArgParserUtils.h"
+#include "Utils/CmdLineArgParserException.h"
+
+using std::string;
+using namespace genie;
+
+void GetCommandLineArgs (int argc, char ** argv);
+
+int    gOptNEvt;
+string gOptInpFilename;
+
+//___________________________________________________________________
+int main(int argc, char ** argv)
+{
+  GetCommandLineArgs (argc, argv);
+
+  //-- open the ROOT file and get the TTree & its header
+  TTree *           tree = 0;
+  NtpMCTreeHeader * thdr = 0;
+
+  TFile file(gOptInpFilename.c_str(),"READ");
+
+  tree = dynamic_cast <TTree *>           ( file.Get("gtree")  );
+  thdr = dynamic_cast <NtpMCTreeHeader *> ( file.Get("header") );
+
+  if(!tree) return 1;
+
+  NtpMCEventRecord * mcrec = 0;
+  tree->SetBranchAddress("gmcrec", &mcrec);
+
+  int nev = (gOptNEvt > 0) ?
+        TMath::Min(gOptNEvt, (int)tree->GetEntries()) :
+        (int) tree->GetEntries();
+
+  //
+  // Loop over all events
+  //
+  for(int i = 0; i < nev; i++) {
+
+    // get next tree entry
+    tree->GetEntry(i);
+
+    // get the GENIE event
+    EventRecord &  event = *(mcrec->event);
+
+    LOG("myAnalysis", pNOTICE) << event;
+
+    //
+    // Put your event analysis code here 
+    //
+    // ... ... ... ... ...
+    // ... ... ... ... ...
+    //
+    // 
+
+    
+
+    //
+    // Loop over all particles in this event
+    //
+
+    GHepParticle * p = 0;
+    TIter event_iter(&event);
+
+    while((p=dynamic_cast<GHepParticle *>(event_iter.Next())))
+    {
+       //
+       // Put your event analysis code here 
+       //
+       // ... ... ... ... ...
+       // ... ... ... ... ...
+       //
+       //
+
+       // EXAMPLE: Print out the energy of all final state pions.
+       if (p->Status() == kIStStableFinalState ) {
+	  if (p->Pdg() == kPdgPi0 ||
+	      p->Pdg() == kPdgPiP ||
+	      p->Pdg() == kPdgPiM) 
+           {
+            LOG("myAnalysis", pNOTICE)  
+               << "Got a : << p->Name() << " with E = " << p->E() << " GeV"; 
+          }
+       }
+
+    }// end loop over particles	
+    
+    // clear current mc event record
+    mcrec->Clear();
+
+  }//end loop over events
+
+  // close input GHEP event file
+  file.Close();
+
+  LOG("myAnalysis", pNOTICE)  << "Done!";
+
+  return 0;
+}
+//___________________________________________________________________
+void GetCommandLineArgs(int argc, char ** argv)
+{
+  LOG("myAnalysis", pINFO) << "Parsing commad line arguments";
+
+  // get GENIE event sample
+  try {
+    LOG("myAnalysis", pINFO) << "Reading event sample filename";
+    gOptInpFilename = utils::clap::CmdLineArgAsString(argc,argv,'f');
+  } catch(exceptions::CmdLineArgParserException e) {
+    if(!e.ArgumentFound()) {
+      LOG("myAnalysis", pFATAL) 
+        << "Unspecified input filename - Exiting";
+      exit(1);
+    }
+  }
+
+  // number of events to analyse
+  try {    
+    LOG("myAnalysis", pINFO) << "Reading number of events to analyze";
+    gOptNEvt = genie::utils::clap::CmdLineArgAsInt(argc,argv,'n');
+  } catch(exceptions::CmdLineArgParserException e) {
+    if(!e.ArgumentFound()) {
+      LOG("myAnalysis", pINFO)
+        << "Unspecified number of events to analyze - Use all";
+      gOptNEvt = -1;
+    }
+  }
+}
+//_________________________________________________________________________________
