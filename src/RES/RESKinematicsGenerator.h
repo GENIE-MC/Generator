a86ee48403ffe7131415acb2a8a88b88617758d4
costas <costas.andreopoulos@stfc.ac.uk>
2009-03-03 15:39:27 +0000

    moved RES generation code into a new package
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2221 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/RES/RESKinematicsGenerator.h
@@ -0,0 +1,54 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::RESKinematicsGenerator
+
+\brief    Generates resonance event (v+N->l+Resonance) kinematics.
+          Is a concrete implementation of the EventRecordVisitorI interface.
+
+\author   Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+          STFC, Rutherford Appleton Laboratory
+
+\created  November 18, 2004
+
+\cpright  Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _RES_KINEMATICS_GENERATOR_H_
+#define _RES_KINEMATICS_GENERATOR_H_
+
+#include "EVGModules/KineGeneratorWithCache.h"
+#include "Utils/Range1.h"
+
+class TF2;
+
+namespace genie {
+
+class RESKinematicsGenerator : public KineGeneratorWithCache {
+
+public :
+  RESKinematicsGenerator();
+  RESKinematicsGenerator(string config);
+ ~RESKinematicsGenerator();
+
+  // implement the EventRecordVisitorI interface
+  void ProcessEventRecord(GHepRecord * event_rec) const;
+
+  // overload the Algorithm::Configure() methods to load private data
+  // members from configuration options
+  void Configure(const Registry & config);
+  void Configure(string config);
+
+private:
+  void   LoadConfig      (void);
+  double ComputeMaxXSec  (const Interaction * interaction) const;
+
+  mutable TF2 * fEnvelope; ///< 2-D envelope used for importance sampling
+  double fWcut;            ///< Wcut parameter in DIS/RES join scheme
+};
+
+}      // genie namespace
+#endif // _RES_KINEMATICS_GENERATOR_H_
