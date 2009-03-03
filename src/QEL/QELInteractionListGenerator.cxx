efe6679efeb68ec710accf4532eafc0a9f898ed8
costas <costas.andreopoulos@stfc.ac.uk>
2009-03-03 15:38:44 +0000

    moved QEL generation code into a new package
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2220 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/QEL/QELInteractionListGenerator.cxx
@@ -0,0 +1,221 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+ or see $GENIE/LICENSE
+
+ Author: Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         STFC, Rutherford Appleton Laboratory - May 13, 2005
+
+ For the class documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+ @ Mar 03, 2009 - CA
+   Moved into the new QEL package from its previous location (EVGModules)
+
+*/
+//____________________________________________________________________________
+
+#include "EVGCore/InteractionList.h"
+#include "Interaction/Interaction.h"
+#include "Messenger/Messenger.h"
+#include "PDG/PDGCodes.h"
+#include "PDG/PDGUtils.h"
+#include "QEL/QELInteractionListGenerator.h"
+
+using namespace genie;
+
+//___________________________________________________________________________
+QELInteractionListGenerator::QELInteractionListGenerator() :
+InteractionListGeneratorI("genie::QELInteractionListGenerator")
+{
+
+}
+//___________________________________________________________________________
+QELInteractionListGenerator::QELInteractionListGenerator(string config):
+InteractionListGeneratorI("genie::QELInteractionListGenerator",  config)
+{
+
+}
+//___________________________________________________________________________
+QELInteractionListGenerator::~QELInteractionListGenerator()
+{
+
+}
+//___________________________________________________________________________
+InteractionList * QELInteractionListGenerator::CreateInteractionList(
+                                       const InitialState & init_state) const
+{
+  LOG("IntLst", pINFO)
+     << "InitialState = " << init_state.AsString();
+
+  if      (fIsCC && !fIsCharm) 
+                return this->CreateInteractionListCC(init_state);
+  else if (fIsNC && !fIsCharm) 
+                return this->CreateInteractionListNC(init_state);
+  else if (fIsCC &&  fIsCharm) 
+                return this->CreateInteractionListCharmCC(init_state);
+  else {
+     LOG("IntLst", pWARN)
+       << "Unknown InteractionType! Returning NULL InteractionList "
+       << "for init-state: " << init_state.AsString();
+     return 0;
+  }
+  return 0;
+}
+//___________________________________________________________________________
+InteractionList * QELInteractionListGenerator::CreateInteractionListCC(
+                                       const InitialState & init_state) const
+{
+  InteractionList * intlist = new InteractionList;
+
+  ProcessInfo   proc_info(kScQuasiElastic, kIntWeakCC);
+  Interaction * interaction = new Interaction(init_state, proc_info);
+
+  int      nupdg   = init_state.ProbePdg();
+  bool     isnu    = pdg::IsNeutrino     (nupdg);
+  bool     isnubar = pdg::IsAntiNeutrino (nupdg);
+
+  Target * target  = interaction->InitStatePtr()->TgtPtr();
+  bool     hasP    = (target->Z() > 0);
+  bool     hasN    = (target->N() > 0);
+
+  if (isnu && hasN) {
+     target->SetHitNucPdg(kPdgNeutron);
+     intlist->push_back(interaction);
+
+  } else if (isnubar && hasP) {
+     target->SetHitNucPdg(kPdgProton);
+     intlist->push_back(interaction);
+
+  } else {
+     LOG("IntLst", pWARN)
+       << "Returning NULL InteractionList for init-state: "
+       << init_state.AsString();
+     delete interaction;
+     delete intlist;
+     return 0;
+  }
+  return intlist;
+}
+//___________________________________________________________________________
+InteractionList * QELInteractionListGenerator::CreateInteractionListNC(
+                                       const InitialState & init_state) const
+{
+  InteractionList * intlist = new InteractionList;
+
+  int nuclpdg[2] = { kPdgProton, kPdgNeutron };
+
+  int      nupdg   = init_state.ProbePdg();
+  bool     isnu    = pdg::IsNeutrino     (nupdg);
+  bool     isnubar = pdg::IsAntiNeutrino (nupdg);
+
+  if(!isnu && !isnubar) {
+     LOG("IntLst", pWARN)
+       << "Can not handle probe! Returning NULL InteractionList "
+       << "for init-state: " << init_state.AsString();
+     delete intlist;
+     return 0;
+  }
+
+  for(int i=0; i<2; i++) {
+
+     ProcessInfo   proc_info(kScQuasiElastic, kIntWeakNC);
+     Interaction * interaction = new Interaction(init_state, proc_info);
+
+     Target * target  = interaction->InitStatePtr()->TgtPtr();
+     bool     hasP    = (target->Z() > 0);
+     bool     hasN    = (target->N() > 0);
+
+     if(nuclpdg[i] == kPdgProton  && !hasP) {
+       delete interaction;
+       continue;
+     }
+     if(nuclpdg[i] == kPdgNeutron  && !hasN) {
+       delete interaction;
+       continue;
+     }
+     target->SetHitNucPdg(nuclpdg[i]);
+     intlist->push_back(interaction);
+  }
+
+  if(intlist->size() == 0) {
+     LOG("IntLst", pERROR)
+       << "Returning NULL InteractionList for init-state: "
+       << init_state.AsString();
+     delete intlist;
+     return 0;
+  }
+  return intlist;
+}
+//___________________________________________________________________________
+InteractionList * 
+  QELInteractionListGenerator::CreateInteractionListCharmCC(
+                                      const InitialState & init_state) const
+{
+  //   vl + n --> l- + Lambda_{c}^{+} (2285)
+  //   vl + n --> l- + Sigma_{c}^{+}  (2455)
+  //   vl + p --> l- + Sigma_{c}^{++} (2455)
+
+  int  nupdg = init_state.ProbePdg();
+  bool isnu = pdg::IsNeutrino(nupdg);
+  if(!isnu) {
+     LOG("IntLst", pERROR)
+       << "Returning NULL InteractionList for init-state: "
+       << init_state.AsString();
+     return 0;
+  }
+
+  const int nch = 3;
+  int nuclpdg [nch] = { kPdgNeutron,  kPdgNeutron, kPdgProton   };
+  int charmpdg[nch] = { kPdgLambdaPc, kPdgSigmaPc, kPdgSigmaPPc };
+
+  InteractionList * intlist = new InteractionList;
+
+  for(int i=0; i<nch; i++) {
+
+     ProcessInfo   proc_info(kScQuasiElastic, kIntWeakCC);
+     Interaction * interaction = new Interaction(init_state, proc_info);
+
+     Target * target  = interaction->InitStatePtr()->TgtPtr();
+     bool hasP = (target->Z() > 0);
+     bool hasN = (target->N() > 0);
+
+     XclsTag * xcls = interaction->ExclTagPtr();
+
+     if(nuclpdg[i] == kPdgProton  && !hasP) {
+       delete interaction;
+       continue;
+     }
+     if(nuclpdg[i] == kPdgNeutron  && !hasN) {
+       delete interaction;
+       continue;
+     }
+     target->SetHitNucPdg(nuclpdg[i]);
+     xcls->SetCharm(charmpdg[i]);
+
+     intlist->push_back(interaction);
+  }
+  return intlist;
+}
+//____________________________________________________________________________
+void QELInteractionListGenerator::Configure(const Registry & config)
+{
+  Algorithm::Configure(config);
+  this->LoadConfigData();
+}
+//____________________________________________________________________________
+void QELInteractionListGenerator::Configure(string config)
+{
+  Algorithm::Configure(config);
+  this->LoadConfigData();
+}
+//____________________________________________________________________________
+void QELInteractionListGenerator::LoadConfigData(void)
+{
+  fIsCC    = fConfig->GetBoolDef("is-CC",    false);
+  fIsNC    = fConfig->GetBoolDef("is-NC",    false);
+  fIsCharm = fConfig->GetBoolDef("is-Charm", false);
+}
+//____________________________________________________________________________
+
---
+++ src/QEL/QELKinematicsGenerator.h
@@ -0,0 +1,50 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::QELKinematicsGenerator
+
+\brief    Generates values for the kinematic variables describing QEL neutrino
+          interaction events.
+          Is a concrete implementation of the EventRecordVisitorI interface.
+
+\author   Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+          STFC, Rutherford Appleton Laboratory
+
+\created  October 03, 2004
+
+\cpright  Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _QEL_KINEMATICS_GENERATOR_H_
+#define _QEL_KINEMATICS_GENERATOR_H_
+
+#include "EVGModules/KineGeneratorWithCache.h"
+#include "Utils/Range1.h"
+
+namespace genie {
+
+class QELKinematicsGenerator : public KineGeneratorWithCache {
+
+public :
+  QELKinematicsGenerator();
+  QELKinematicsGenerator(string config);
+ ~QELKinematicsGenerator();
+
+  // implement the EventRecordVisitorI interface
+  void ProcessEventRecord(GHepRecord * event_rec) const;
+
+  // overload the Algorithm::Configure() methods to load private data
+  // members from configuration options
+  void Configure(const Registry & config);
+  void Configure(string config);
+
+private:
+  void   LoadConfig     (void);
+  double ComputeMaxXSec (const Interaction * in) const;
+};
+
+}      // genie namespace
+#endif // _QEL_KINEMATICS_GENERATOR_H_
