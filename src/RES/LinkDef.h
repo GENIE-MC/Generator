58b5b97444ac78603a505af8ef5149bb27ab6a56
costas <costas.andreopoulos@stfc.ac.uk>
2009-03-03 15:39:27 +0000

    moved RES generation code into a new package
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2221 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/RES/LinkDef.h
@@ -0,0 +1,18 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace genie;
+
+#pragma link C++ class genie::RESPrimaryLeptonGenerator;
+#pragma link C++ class genie::RESHadronicSystemGenerator;
+#pragma link C++ class genie::RESKinematicsGenerator;
+#pragma link C++ class genie::RESInteractionListGenerator;
+
+#pragma link C++ class genie::RSPPHadronicSystemGenerator;
+#pragma link C++ class genie::RSPPResonanceSelector;
+#pragma link C++ class genie::RSPPInteractionListGenerator;
+
+#endif
