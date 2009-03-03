75df9e0eee835ad020b1bc1b0d7f4c914ea638fd
costas <costas.andreopoulos@stfc.ac.uk>
2009-03-03 15:38:44 +0000

    moved QEL generation code into a new package
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2220 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/QEL/LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace genie;
+
+#pragma link C++ class genie::QELPrimaryLeptonGenerator;
+#pragma link C++ class genie::QELHadronicSystemGenerator;
+#pragma link C++ class genie::QELInteractionListGenerator;
+#pragma link C++ class genie::QELKinematicsGenerator;
+
+#endif
