62cb5c20b58d492c8911414c2b8ad1afd608cd52
costas <costas.andreopoulos@stfc.ac.uk>
2009-03-03 15:39:46 +0000

    moved DIS generation code into a new package
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2222 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/DIS/LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace genie;
+
+#pragma link C++ class genie::DISPrimaryLeptonGenerator;
+#pragma link C++ class genie::DISHadronicSystemGenerator;
+#pragma link C++ class genie::DISInteractionListGenerator;
+#pragma link C++ class genie::DISKinematicsGenerator;
+
+#endif
