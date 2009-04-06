54282a55baf19c20005e8519c5877d5595109354
candreop <costas.andreopoulos@stfc.ac.uk>
2009-04-06 17:30:05 +0000

    updated script
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2279 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/scripts/production/rutherford_tier2/submit-nuint09_jobs.pl
+++ src/scripts/production/rutherford_tier2/submit-nuint09_jobs.pl
@@ -10,13 +10,14 @@
 # Options:
 #  --run           : Comma separated list of run numbers
 #  --version       : GENIE version number
+# [--nsubruns]     : number of subruns per run
 # [--production]   :
 # [--cycle]        :
 # [--use-valgrind] :
 #
 # Examples:
-#  perl submit-nuint09_jobs.pl --production nuint09 --cycle 01 --version v2.5.1 --run 1001
-#  perl submit-nuint09_jobs.pl --production nuint09 --cycle 01 --version v2.5.1 --run all
+#  perl submit-nuint09_jobs.pl --production nuint09 --cycle 01 --version v2.5.1 --run 1001 --nsubruns=5
+#  perl submit-nuint09_jobs.pl --production nuint09 --cycle 01 --version v2.5.1 --run all  --nsubruns=5
 #
 # Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
 # STFC, Rutherford Appleton Lab
@@ -24,42 +25,43 @@
 #
 # SAMPLES:
 #......................................................................
-#  run   | nev  |  init state      | energy  | processes
-#  nu.   |      |                  | (GeV)   | enabled
+# run number      |  init state      | energy  | processes
+#                 |                  | (GeV)   | enabled
+# (xx=subrun_id)  |
 #......................................................................
 #
-# 1001   | 500k | numu    + C12    | 0.5     | COH-CC
-# 1002   | 500k | numu    + C12    | 1.0     | COH-CC
-# 1003   | 500k | numu    + C12    | 1.5     | COH-CC
-# 1011   | 500k | numu    + C12    | 0.5     | COH-NC
-# 1012   | 500k | numu    + C12    | 1.0     | COH-NC
-# 1013   | 500k | numu    + C12    | 1.5     | COH-NC
-# 2001   | 500k | numu    + C12    | 0.5     | QEL-CC
-# 2002   | 500k | numu    + C12    | 1.0     | QEL-CC
-# 3001   | 500k | numu    + C12    | 0.5     | RES-CC
-# 3002   | 500k | numu    + C12    | 1.0     | RES-CC
+# 1001xx          | numu    + C12    | 0.5     | COH-CC
+# 1002xx          | numu    + C12    | 1.0     | COH-CC
+# 1003xx          | numu    + C12    | 1.5     | COH-CC
+# 1011xx          | numu    + C12    | 0.5     | COH-NC
+# 1012xx          | numu    + C12    | 1.0     | COH-NC
+# 1013xx          | numu    + C12    | 1.5     | COH-NC
+# 2001xx          | numu    + C12    | 0.5     | QEL-CC
+# 2002xx          | numu    + C12    | 1.0     | QEL-CC
+# 3001xx          | numu    + C12    | 0.5     | RES-CC
+# 3002xx          | numu    + C12    | 1.0     | RES-CC 
 #
-# 1101   | 500k | numu    + O16    | 0.5     | COH-CC
-# 1102   | 500k | numu    + O16    | 1.0     | COH-CC
-# 1103   | 500k | numu    + O16    | 1.5     | COH-CC
-# 1111   | 500k | numu    + O16    | 0.5     | COH-NC
-# 1112   | 500k | numu    + O16    | 1.0     | COH-NC
-# 1113   | 500k | numu    + O16    | 1.5     | COH-NC
-# 2101   | 500k | numu    + O16    | 0.5     | QEL-CC
-# 2102   | 500k | numu    + O16    | 1.0     | QEL-CC
-# 3101   | 500k | numu    + O16    | 0.5     | RES-CC
-# 3102   | 500k | numu    + O16    | 1.0     | RES-CC
+# 1101xx          | numu    + O16    | 0.5     | COH-CC
+# 1102xx          | numu    + O16    | 1.0     | COH-CC
+# 1103xx          | numu    + O16    | 1.5     | COH-CC
+# 1111xx          | numu    + O16    | 0.5     | COH-NC
+# 1112xx          | numu    + O16    | 1.0     | COH-NC
+# 1113xx          | numu    + O16    | 1.5     | COH-NC
+# 2101xx          | numu    + O16    | 0.5     | QEL-CC
+# 2102xx          | numu    + O16    | 1.0     | QEL-CC
+# 3101xx          | numu    + O16    | 0.5     | RES-CC
+# 3102xx          | numu    + O16    | 1.0     | RES-CC
 #
-# 1201   | 500k | numu    + Fe56   | 0.5     | COH-CC
-# 1202   | 500k | numu    + Fe56   | 1.0     | COH-CC
-# 1203   | 500k | numu    + Fe56   | 1.5     | COH-CC
-# 1211   | 500k | numu    + Fe56   | 0.5     | COH-NC
-# 1212   | 500k | numu    + Fe56   | 1.0     | COH-NC
-# 1213   | 500k | numu    + Fe56   | 1.5     | COH-NC
-# 2201   | 500k | numu    + Fe56   | 0.5     | QEL-CC
-# 2202   | 500k | numu    + Fe56   | 1.0     | QEL-CC
-# 3201   | 500k | numu    + Fe56   | 0.5     | RES-CC
-# 3202   | 500k | numu    + Fe56   | 1.0     | RES-CC
+# 1201xx          | numu    + Fe56   | 0.5     | COH-CC
+# 1202xx          | numu    + Fe56   | 1.0     | COH-CC
+# 1203xx          | numu    + Fe56   | 1.5     | COH-CC
+# 1211xx          | numu    + Fe56   | 0.5     | COH-NC
+# 1212xx          | numu    + Fe56   | 1.0     | COH-NC
+# 1213xx          | numu    + Fe56   | 1.5     | COH-NC
+# 2201xx          | numu    + Fe56   | 0.5     | QEL-CC
+# 2202xx          | numu    + Fe56   | 1.0     | QEL-CC
+# 3201xx          | numu    + Fe56   | 0.5     | RES-CC
+# 3202xx          | numu    + Fe56   | 1.0     | RES-CC
 #......................................................................
 #
 
@@ -70,6 +72,7 @@ use File::Path;
 $iarg=0;
 foreach (@ARGV) {
   if($_ eq '--run')           { $runnu         = $ARGV[$iarg+1]; }
+  if($_ eq '--nsubruns')      { $nsubruns      = $ARGV[$iarg+1]; }
   if($_ eq '--version')       { $genie_version = $ARGV[$iarg+1]; }
   if($_ eq '--production')    { $production    = $ARGV[$iarg+1]; }
   if($_ eq '--cycle')         { $cycle         = $ARGV[$iarg+1]; }
@@ -81,51 +84,20 @@ unless defined $runnu;
 die("** Aborting [Undefined GENIE version. Use the --version option]")
 unless defined $genie_version;
 
+$nsubruns       = 1                         unless defined $nsubruns;
 $use_valgrind   = 0                         unless defined $use_valgrind;
 $production     = "nuint09\_$genie_version" unless defined $production;
 $cycle          = "01"                      unless defined $cycle;
 
 $queue          = "prod";
-$time_limit     = "30:00:00";
+$time_limit     = "60:00:00";
 $topdir         = "/opt/ppd/t2k";
 $genie_inst_dir = "$topdir/GENIE/";
 $genie_setup    = "$genie_inst_dir/$genie_version-setup";
 $jobs_dir       = "$topdir/GENIE/scratch/$production\_$cycle";
 $xspl_file      = "$topdir/GENIE/data/job_inputs/xspl/gxspl-t2k-$genie_version.xml";
 $mcseed         = 210921029;
-
-%nevents_hash = ( 
-  '1001' =>  '500000',
-  '1002' =>  '500000',
-  '1003' =>  '500000',
-  '1011' =>  '500000',
-  '1012' =>  '500000',
-  '1013' =>  '500000',
-  '2001' =>  '500000',
-  '2002' =>  '500000',
-  '3001' =>  '500000',
-  '3002' =>  '500000',
-  '1101' =>  '500000',
-  '1102' =>  '500000',
-  '1103' =>  '500000',
-  '1111' =>  '500000',
-  '1112' =>  '500000',
-  '1113' =>  '500000',
-  '2101' =>  '500000',
-  '2102' =>  '500000',
-  '3101' =>  '500000',
-  '3102' =>  '500000',
-  '1201' =>  '500000',
-  '1202' =>  '500000',
-  '1203' =>  '500000',
-  '1211' =>  '500000',
-  '1212' =>  '500000',
-  '1213' =>  '500000',
-  '2201' =>  '500000',
-  '2202' =>  '500000',
-  '3201' =>  '500000',
-  '3202' =>  '500000'
-);
+$nev_per_subrun = 100000;
 
 %nupdg_hash = ( 
   '1001' =>  '14',
@@ -266,50 +238,61 @@ mkpath ($jobs_dir, {verbose => 1, mode=>0777});
 print "Input runs: $runnu \n";
 
 for my $curr_runnu (keys %gevgl_hash)  {
-  print "Checking benchmark run: ...... $curr_runnu \n";
+  #print "Checking benchmark run: ...... $curr_runnu \n";
 
   if($runnu=~m/$curr_runnu/ || $runnu eq "all") {
-    print "** matched -> submitting job \n";
+    print "** submitting run: $curr_runnu \n";
 
     #
     # get runnu-dependent info
     #
-    $nev   = $nevents_hash {$curr_runnu};
     $nu    = $nupdg_hash   {$curr_runnu};
     $tgt   = $tgtpdg_hash  {$curr_runnu};
     $en    = $energy_hash  {$curr_runnu};
     $gevgl = $gevgl_hash   {$curr_runnu};
 
-    $batch_script  = "$jobs_dir/nuint09job-$curr_runnu.pbs";
-    $logfile_evgen = "$jobs_dir/nuint09job-$curr_runnu.evgen.log";
-    $logfile_conv  = "$jobs_dir/nuint09job-$curr_runnu.conv.log";
-    $logfile_pbse  = "$jobs_dir/nuint09job-$curr_runnu.pbs_e.log";
-    $logfile_pbso  = "$jobs_dir/nuint09job-$curr_runnu.pbs_o.log";
-
-    $grep_pipe     = "grep -B 20 -A 30 -i \"warn\\|error\\|fatal\"";
-    $valgrind_cmd  = "valgrind --tool=memcheck --error-limit=no --leak-check=yes --show-reachable=yes";
-    $evgen_cmd     = "gevgen -n $nev -s -e $en -p $nu -t $tgt -r $curr_runnu | grep_pipe &> $logfile_evgen";
-    $conv_cmd      = "gntpc -f gst -i gntp.$curr_runnu.ghep.root | grep -B 100 -A 30 -i \"warn\\|error\\|fatal\" &> $logfile_conv";
-
-    # create the PBS script
     #
-    open(PBS, ">$batch_script") or die("Can not create the PBS batch script");
-    print PBS "#!/bin/bash \n";
-    print PBS "#PBS -l cput=$time_limit \n";
-    print PBS "#PBS -o $logfile_pbso \n";
-    print PBS "#PBS -e $logfile_pbse \n";
-    print PBS "source $genie_setup \n"; 
-    print PBS "cd $jobs_dir \n";
-    print PBS "export GSPLOAD=$xspl_file \n";
-    print PBS "export GEVGL=$gevgl \n";
-    print PBS "export GSEED=$mcseed  \n";
-    print PBS "$evgen_cmd \n";
-    print PBS "$conv_cmd \n";
-
-    print "EXEC: $evgen_cmd \n";
-
-    # submit job
+    # submit subruns
     #
-    `qsub -q $queue $batch_script`;
+    for($isubrun = 0; $isubrun < $nsubruns; $isubrun++) {
+
+       $curr_subrunnu = 100 * $curr_runnu + $isubrun;
+
+       $batch_script  = "$jobs_dir/nuint09job-$curr_subrunnu.pbs";
+       $logfile_evgen = "$jobs_dir/nuint09job-$curr_subrunnu.evgen.log";
+       $logfile_conv  = "$jobs_dir/nuint09job-$curr_subrunnu.conv.log";
+       $logfile_pbse  = "$jobs_dir/nuint09job-$curr_subrunnu.pbs_e.log";
+       $logfile_pbso  = "$jobs_dir/nuint09job-$curr_subrunnu.pbs_o.log";
+
+       $curr_seed     = $mcseed + $isubrun;
+       $grep_pipe     = "grep -B 20 -A 30 -i \"warn\\|error\\|fatal\"";
+       $valgrind_cmd  = "valgrind --tool=memcheck --error-limit=no --leak-check=yes --show-reachable=yes";
+       $evgen_cmd     = "gevgen -n $nev_per_subrun -s -e $en -p $nu -t $tgt -r $curr_subrunnu | grep_pipe &> $logfile_evgen";
+       $conv_cmd      = "gntpc -f gst -i gntp.$curr_subrunnu.ghep.root | grep -B 100 -A 30 -i \"warn\\|error\\|fatal\" &> $logfile_conv";
+
+       # create the PBS script
+       #
+       open(PBS, ">$batch_script") or die("Can not create the PBS batch script");
+       print PBS "#!/bin/bash \n";
+       print PBS "#PBS -l cput=$time_limit \n";
+       print PBS "#PBS -o $logfile_pbso \n";
+       print PBS "#PBS -e $logfile_pbse \n";
+       print PBS "source $genie_setup \n"; 
+       print PBS "cd $jobs_dir \n";
+       print PBS "export GSPLOAD=$xspl_file \n";
+       print PBS "export GEVGL=$gevgl \n";
+       print PBS "export GSEED=$curr_seed \n";
+       print PBS "$evgen_cmd \n";
+       print PBS "$conv_cmd \n";
+
+       print "EXEC: $evgen_cmd \n";
+
+       #
+       # submit job
+       #
+       `qsub -q $queue $batch_script`;
+
+    } #subruns
+
   }
 }
