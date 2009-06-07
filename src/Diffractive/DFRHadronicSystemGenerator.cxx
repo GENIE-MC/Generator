5bdd5470188b3f99a5894fd5f2292ffe850142cc
costas <costas.andreopoulos@stfc.ac.uk>
2009-06-07 18:59:39 +0000

    Cont'd diffractive thread devel: Solved problem with hadrons coming off the mass shell.
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2335 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Diffractive/DFRHadronicSystemGenerator.cxx
+++ src/Diffractive/DFRHadronicSystemGenerator.cxx
@@ -22,9 +22,11 @@
 
 #include "Conventions/Constants.h"
 #include "Diffractive/DFRHadronicSystemGenerator.h"
+#include "EVGCore/EVGThreadException.h"
 #include "GHEP/GHepStatus.h"
 #include "GHEP/GHepParticle.h"
 #include "GHEP/GHepRecord.h"
+#include "GHEP/GHepFlags.h"
 #include "Messenger/Messenger.h"
 #include "Numerical/RandomGen.h"
 #include "PDG/PDGLibrary.h"
@@ -60,24 +62,18 @@ void DFRHadronicSystemGenerator::ProcessEventRecord(GHepRecord * evrec) const
 //
   RandomGen * rnd = RandomGen::Instance();
 
-  Interaction * interaction = evrec->Summary();
-  const XclsTag & xcls_tag  = interaction->ExclTag();
-
   //-- Access neutrino, initial nucleus and final state prim. lepton entries
   GHepParticle * nu  = evrec->Probe();
-//  GHepParticle * Ni  = evrec->TargetNucleus();
   GHepParticle * Ni  = evrec->HitNucleon();
   GHepParticle * fsl = evrec->FinalStatePrimaryLepton();
   assert(nu);
   assert(Ni);
   assert(fsl);
 
-  const TLorentzVector & vtx   = *(nu->X4());
-  const TLorentzVector & p4nu  = *(nu ->P4());
-  const TLorentzVector & p4fsl = *(fsl->P4());
+  Interaction * interaction = evrec->Summary();
 
-  //-- Determine the pdg code of the final state pion & nucleus
-  int nucl_pdgc = Ni->Pdg(); // same as the initial nucleus
+  //-- Determine the pdg code of the final state pion 
+  const XclsTag & xcls_tag  = interaction->ExclTag();
   int pion_pdgc = 0;
   if      (xcls_tag.NPi0()     == 1) pion_pdgc = kPdgPi0;
   else if (xcls_tag.NPiPlus()  == 1) pion_pdgc = kPdgPiP;
@@ -88,45 +84,61 @@ void DFRHadronicSystemGenerator::ProcessEventRecord(GHepRecord * evrec) const
      exit(1);
   }
 
-  //-- basic kinematic inputs
-  double E    = nu->E();  
-  double M    = kNucleonMass;
+  //-- Determine the pdg code of the recoil nucleon
+  int nucl_pdgc = Ni->Pdg(); // same as the hit  nucleon
+
+
+  const TLorentzVector & p4nu  = *(nu ->P4());
+  const TLorentzVector & p4Ni  = *(Ni ->P4());
+  const TLorentzVector & p4fsl = *(fsl->P4());
+
+  // q at LAB
+  TLorentzVector q = p4nu - p4fsl;        
+
+  // q at NRF
+  TLorentzVector qnrf(q);
+  TVector3 beta = p4Ni.BoostVector();
+  qnrf.Boost(-beta);
+
+  const InitialState & init_state = interaction -> InitState();
+  const Target & target = init_state.Tgt();
+
+  double E    = init_state.ProbeE(kRfHitNucRest);  // neutrino energy
+  double M    = target.HitNucMass();
   double mpi  = PDGLibrary::Instance()->Find(pion_pdgc)->Mass();
   double mpi2 = TMath::Power(mpi,2);
   double xo   = interaction->Kine().x(true); 
   double yo   = interaction->Kine().y(true); 
   double to   = interaction->Kine().t(true); 
-
-  SLOG("DFRHadronicVtx", pINFO) 
-         << "Ev = "<< E 
-         << ", xo = " << xo << ", yo = " << yo << ", to = " << to;
-
-  //-- compute pion energy and |momentum|
-  double Epi  = yo * E;  
+  double Epi  = qnrf.E() - 0.5*to/M;  
   double Epi2 = TMath::Power(Epi,2);
   double ppi2 = Epi2-mpi2;
   double ppi  = TMath::Sqrt(TMath::Max(0.,ppi2));
 
+  SLOG("DFRHadronicVtx", pINFO) 
+         << "Ev = "<< E 
+         << ", xo = " << xo << ", yo = " << yo << ", to = " << to;
   SLOG("DFRHadronicVtx", pINFO)
          << "f/s pion E = " << Epi << ", |p| = " << ppi;
 
-  assert(Epi>mpi);
-
-  //-- 4-momentum transfer q=p(neutrino) - p(f/s lepton)
-  //   Note: m^2 = q^2 < 0
-  //   Also, since the nucleus is heavy all energy loss is comminicated to
-  //   the outgoing pion  Rein & Seghal, Nucl.Phys.B223.29-44(1983), p.35
-
-  TLorentzVector q = p4nu - p4fsl;
-
-  SLOG("DFRHadronicVtx", pINFO) 
-          << "\n 4-p transfer q @ LAB: " << utils::print::P4AsString(&q);
-
-  //-- find angle theta between q and ppi (xi=costheta)
-  //   note: t=|(ppi-q)^2|, Rein & Seghal, Nucl.Phys.B223.29-44(1983), p.36
+  // find angle theta between q and ppi (xi=costheta)
  
-  double xi = 1. + M*xo/Epi - 0.5*mpi2/Epi2 - 0.5*to/Epi2;
-  xi /= TMath::Sqrt((1.+2.*M*xo/Epi)*(1.-mpi2/Epi2));
+  double xi = -to - mpi2 - qnrf.M2() + 2*qnrf.E()*Epi;
+  xi /= (2 * TMath::Sqrt(Epi2-mpi2) * qnrf.Vect().Mag());
+
+  if(xi < 0. || xi > 1. || Epi<= mpi) {
+     LOG("DFRKinematics", pWARN) << "Invalid selected kinematics; Attempt regenerating";
+     evrec->EventFlags()->SetBitNumber(kKineGenErr, true);
+     genie::exceptions::EVGThreadException exception;
+     exception.SetReason("Invalid selected kinematics");
+     exception.SwitchOnStepBack();
+     exception.SetReturnStep(0);
+     throw exception;
+  }
+
+  LOG("DFRHadronicVtx", pINFO) << "|to| = " << -to;
+  LOG("DFRHadronicVtx", pINFO) << "q2   = " << qnrf.M2();
+  LOG("DFRHadronicVtx", pINFO) << "xi   = " << xi;
 
   double costheta  = xi;
   double sintheta  = TMath::Sqrt(TMath::Max(0.,1.-xi*xi));
@@ -136,36 +148,38 @@ void DFRHadronicSystemGenerator::ProcessEventRecord(GHepRecord * evrec) const
   // compute transverse and longitudinal ppi components along q
   double ppiL = ppi*costheta;
   double ppiT = ppi*sintheta;
+  double phi  = 2*kPi* rnd->RndHadro().Rndm();
 
-  double phi = 2*kPi* rnd->RndHadro().Rndm();
+  TVector3 ppi3(0,ppiT,ppiL);        // @ NRF' (NRF rotated so that q along z)
 
-  TVector3 ppi3(0,ppiT,ppiL);
+  ppi3.RotateZ(phi);                 // randomize transverse components
+  ppi3.RotateUz(qnrf.Vect().Unit()); // align longit. component with q in NRF
 
-  ppi3.RotateUz(q.Vect().Unit()); // align longit. component with q in LAB
-  ppi3.RotateZ(phi);              // randomize transverse components
+  TLorentzVector p4pi(ppi3,Epi);
+  p4pi.Boost(beta);
 
-  SLOG("DFRHadronicVtx", pINFO) 
-               << "Pion 3-p @ LAB: " << utils::print::Vec3AsString(&ppi3);
 
-  // now figure out the f/s nucleus 4-p
+  //-- Now figure out the f/s nucleon 4-p
 
-  double pxNf = nu->Px() + Ni->Px() - fsl->Px() - ppi3.Px();
-  double pyNf = nu->Py() + Ni->Py() - fsl->Py() - ppi3.Py();
-  double pzNf = nu->Pz() + Ni->Pz() - fsl->Pz() - ppi3.Pz();
-  double ENf  = nu->E()  + Ni->E()  - fsl->E()  - Epi;
+  double pxNf = nu->Px() + Ni->Px() - fsl->Px() - p4pi.Px();
+  double pyNf = nu->Py() + Ni->Py() - fsl->Py() - p4pi.Py();
+  double pzNf = nu->Pz() + Ni->Pz() - fsl->Pz() - p4pi.Pz();
+  double ENf  = nu->E()  + Ni->E()  - fsl->E()  - p4pi.E();
 
   //-- Save the particles at the GHEP record
 
-//  int mom = evrec->TargetNucleusPosition();
   int mom = evrec->HitNucleonPosition();
+  const TLorentzVector & vtx = *(nu->X4());
 
-  evrec->AddParticle(
-     nucl_pdgc,kIStStableFinalState, mom,-1,-1,-1, 
-     pxNf, pyNf, pzNf, ENf, 0, 0, 0, 0);
+  const Target & tgt = interaction->InitState().Tgt();
+  GHepStatus_t ist = (tgt.IsNucleus()) ?
+        kIStHadronInTheNucleus : kIStStableFinalState;
 
-  evrec->AddParticle(
-     pion_pdgc,kIStStableFinalState, mom,-1,-1,-1, 
-     ppi3.Px(), ppi3.Py(),ppi3.Pz(),Epi, vtx.X(), vtx.Y(), vtx.Z(), vtx.T());
+  evrec->AddParticle(nucl_pdgc, ist, mom,-1,-1,-1, 
+    pxNf, pyNf, pzNf, ENf, 0, 0, 0, 0);
+
+  evrec->AddParticle(pion_pdgc, ist, mom,-1,-1,-1, 
+    p4pi.Px(), p4pi.Py(),p4pi.Pz(),p4pi.E(), vtx.X(), vtx.Y(), vtx.Z(), vtx.T());
 }
 //___________________________________________________________________________
 
