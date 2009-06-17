06042281c75abda8739e8df0add62f5963a7f297
costas <costas.andreopoulos@stfc.ac.uk>
2009-06-17 16:14:26 +0000

    v2.4.4 backports: Add #include <cstring> so that recent versions of gcc won't complain.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/branches/R-2_4_4@2349 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Interaction/InteractionType.h
+++ src/Interaction/InteractionType.h
@@ -5,12 +5,12 @@
 
 \brief    Enumeration of interaction types: e/m, weak cc, weak nc
 
-\author   Costas Andreopoulos <C.V.Andreopoulos@rl.ac.uk>
+\author   Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
           STFC, Rutherford Appleton Laboratory
 
 \created  May 06, 2004
 
-\cpright  Copyright (c) 2003-2008, GENIE Neutrino MC Generator Collaboration
+\cpright  Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
           For the full text of the license visit http://copyright.genie-mc.org
           or see $GENIE/LICENSE
 */ 
@@ -21,6 +21,7 @@
 
 #include <cassert>
 #include <string>
+#include <cstring>
 
 using std::string;
 
