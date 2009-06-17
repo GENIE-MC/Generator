d00e5f889fce993f666961e066ff7eef8d9c4062
costas <costas.andreopoulos@stfc.ac.uk>
2009-06-17 16:25:24 +0000

    v2.4.4 backports: Added RecursiveExhaust() function contributed by Jacek Holeczek.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/branches/R-2_4_4@2356 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Geo/GeoUtils.h
@@ -0,0 +1,39 @@
+//____________________________________________________________________________
+/*!
+
+\namespace  genie::utils::geometry
+
+\brief      Geometry utilities
+
+\author     Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+            STFC, Rutherford Appleton Laboratory
+
+\created    March 26, 2009
+
+\cpright    Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
+            For the full text of the license visit http://copyright.genie-mc.org
+            or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _GEO_UTILS_H_
+#define _GEO_UTILS_H_
+
+#include <string>
+
+class TGeoVolume;
+
+using std::string;
+
+namespace genie {
+namespace utils {
+
+namespace geometry   
+{
+  void RecursiveExhaust(TGeoVolume *topvol, string volnames, bool exhaust);
+
+} // geometry namespace
+} // utils namespace
+} // genie namespace
+
+#endif // _GEO_UTILS_H_
