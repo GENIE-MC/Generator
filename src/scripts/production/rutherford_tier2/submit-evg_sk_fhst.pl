4f9dba4fecfe499767508f2b5d7e3d9ce821a129
costas <costas.andreopoulos@stfc.ac.uk>
2009-08-28 12:02:05 +0000

    cont'd devel
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2424 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/scripts/production/rutherford_tier2/submit-evg_sk_fhst.pl
+++ src/scripts/production/rutherford_tier2/submit-evg_sk_fhst.pl
@@ -1,29 +1,28 @@
-#--------------------------------------------------------------------------
-# Submit a GENIE/SK event generation job using a histogram-based neutrino 
-# flux description
+#------------------------------------------------------------------------------------------
+# Submit a GENIE/SK event generation job using a histogram-based neutrino flux description
 #
 # For use at the RAL/PPD Tier2 PBS batch farm.
 #
 # Syntax:
-#  perl submit-evg_sk_fhst.pl --run run_number --neutrino nu_type
+#   shell% perl submit-evg_sk_fhst.pl <options>
 #
-# where: 
-#
+# Options:
 #    --run           : 1,2,3,...
 #    --neutrino      : numu, numubar, nue, nuesig
 #    --version       : GENIE version
-#   [--arch]         : default: SL5_64bit
-#   [--production]   : default:
-#   [--cycle]        : default:
+#   [--arch]         : <SL4_32bit, SL5_64bit>, default: SL5_64bit
+#   [--production]   : default: superkmc_<version>
+#   [--cycle]        : default: 01
 #   [--use-valgrind] : default: off
 #   [--queue]        : default: prod
+#   [--softw-topdir] : default: /opt/ppd/t2k/GENIE
 #
 # Example:
-#  perl submit-evg_sk_fhst.pl --run 180 --neutrino numubar
-##
+#   shell& perl submit-evg_sk_fhst.pl --run 180 --neutrino numubar --version v2.5.1
+#
 # Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
 # STFC, Rutherford Appleton Lab
-#--------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
 
 #!/usr/bin/perl
 
@@ -41,6 +40,7 @@ foreach (@ARGV) {
   if($_ eq '--cycle')         { $cycle         = $ARGV[$iarg+1]; }
   if($_ eq '--use-valgrind')  { $use_valgrind  = $ARGV[$iarg+1]; }
   if($_ eq '--queue')         { $queue         = $ARGV[$iarg+1]; }
+  if($_ eq '--softw-topdir')  { $softw_topdir  = $ARGV[$iarg+1]; }  
   $iarg++;
 }
 die("** Aborting [Undefined run number. Use the --run option]")
@@ -50,19 +50,17 @@ unless defined $neutrino;
 die("** Aborting [Undefined GENIE version. Use the --version option]")
 unless defined $genie_version; 
 
-$GENIE_TOP_DIR  = "/opt/ppd/t2k/GENIE";
-
-$use_valgrind   = 0           unless defined $use_valgrind;
-$arch           = "SL5_64bit" unless defined $arch;
-$production     = "test"      unless defined $production;
-$cycle          = "01"        unless defined $cycle;
-$queue          = "prod"      unless defined $queue;
-
+$use_valgrind   = 0                            unless defined $use_valgrind;
+$arch           = "SL5_64bit"                  unless defined $arch;
+$production     = "superkmc\_$genie_version"   unless defined $production;
+$cycle          = "01"                         unless defined $cycle;
+$queue          = "prod"                       unless defined $queue;
+$softw_topdir   = "/opt/ppd/t2k/GENIE"         unless defined $softw_topdir;
 $nevents        = "2000";   
 $time_limit     = "05:00:00";
-$production_dir = "$GENIE_TOP_DIR/scratch";
-$inputs_dir     = "$GENIE_TOP_DIR/data/job_inputs";
-$genie_setup    = "$GENIE_TOP_DIR/builds/$arch/$genie_version-setup";
+$production_dir = "$softw_topdir/scratch";
+$inputs_dir     = "$softw_topdir/data/job_inputs";
+$genie_setup    = "$softw_topdir/builds/$arch/$genie_version-setup";
 $geom_tgt_mix   = "1000080160[0.8879],1000010010[0.1121]";
 $xspl_file      = "$inputs_dir/xspl/gxspl-t2k-$genie_version.xml";
 $flux_file      = "$inputs_dir/t2k_flux/07a/sk/hist.nu.sk.root";
