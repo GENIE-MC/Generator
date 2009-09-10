b81d1a9e94016647ac46e32c250a633ccb452b02
costas <costas.andreopoulos@stfc.ac.uk>
2009-09-10 18:09:09 +0000

    *** empty log message ***
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2459 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/ReWeight/GReWeightINukeHelper.cxx
@@ -0,0 +1,41 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+ or see $GENIE/LICENSE
+
+ Author: Jim Dobson <J.Dobson07 \at imperial.ac.uk>
+         Imperial College London
+
+         Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         STFC, Rutherford Appleton Laboratory 
+
+ For the class documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+
+*/
+//____________________________________________________________________________
+
+#include "ReWeight/GReWeightINukeHelper.h"
+#include "Messenger/Messenger.h"
+
+using namespace genie;
+using namespace genie::rew;
+
+//___________________________________________________________________________
+GReWeightINukeHelper::GReWeightINukeHelper(void)
+{
+  this->Initialize();
+}
+//___________________________________________________________________________
+GReWeightINukeHelper::~GReWeightINukeHelper(void)
+{
+
+}
+//___________________________________________________________________________
+void GReWeightINukeHelper::Initialize(void)
+{
+
+}
+//___________________________________________________________________________
--- src/ReWeight/LinkDef.h
+++ src/ReWeight/LinkDef.h
@@ -15,5 +15,7 @@
 #pragma link C++ class genie::rew::GReWeight;
 #pragma link C++ class genie::rew::GReWeightNuXSec;
 #pragma link C++ class genie::rew::GReWeightNuXSecHelper;
+//#pragma link C++ class genie::rew::GReWeightINuke;
+#pragma link C++ class genie::rew::GReWeightINukeHelper;
 
 #endif
