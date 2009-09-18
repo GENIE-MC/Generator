39a9c71b211f9b69c5e3f84ec94d9ce7365592dd
costas <costas.andreopoulos@stfc.ac.uk>
2009-09-18 15:40:10 +0000

    Update naming
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2509 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/LlewellynSmith/LwlynSmithQELCCPXSec.h
@@ -0,0 +1,61 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::LwlynSmithQELCCPXSec
+
+\brief    Computes neutrino-nucleon(nucleus) QELCC differential cross section
+          Is a concrete implementation of the XSecAlgorithmI interface. \n
+
+\ref      C.H.Llewellyn Smith, Physics Reports (Sect. C of Physics Letters) 3,
+          No. 5  (1972) 261-379
+
+\author   Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+          STFC, Rutherford Appleton Laboratory
+
+\created  May 05, 2004
+
+\cpright  Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _QEL_PARTIAL_XSEC_H_
+#define _QEL_PARTIAL_XSEC_H_
+
+#include "Base/XSecAlgorithmI.h"
+#include "Base/QELFormFactors.h"
+
+namespace genie {
+
+class QELFormFactorsModelI;
+class XSecIntegratorI;
+
+class LwlynSmithQELCCPXSec : public XSecAlgorithmI {
+
+public:
+  LwlynSmithQELCCPXSec();
+  LwlynSmithQELCCPXSec(string config);
+  virtual ~LwlynSmithQELCCPXSec();
+
+  // XSecAlgorithmI interface implementation
+  double XSec            (const Interaction * i, KinePhaseSpace_t k) const;
+  double Integral        (const Interaction * i) const;
+  bool   ValidProcess    (const Interaction * i) const;
+
+  // Override the Algorithm::Configure methods to load configuration
+  // data to private data members
+  void Configure (const Registry & config);
+  void Configure (string param_set);
+
+private:
+  void LoadConfig (void);
+
+  mutable QELFormFactors       fFormFactors;      ///<
+  const QELFormFactorsModelI * fFormFactorsModel; ///<
+  const XSecIntegratorI *      fXSecIntegrator;   ///<
+  double                       fCos8c2;           ///< cos^2(cabbibo angle)
+};
+
+}       // genie namespace
+#endif  // _QEL_PARTIAL_XSEC_H_
