e154ebe4b225d0cb42163fcb515ba49332b27d99
costas <costas.andreopoulos@stfc.ac.uk>
2009-10-02 22:51:10 +0000

    Add dummy `UnInhibitDecay(int,TDecayChannel*) const' and `InhibitDecay(int, TDecayChannel*) const' methods to conform to the DecayModelI interface. To implement soon.
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2544 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Decay/BaryonResonanceDecayer.h
+++ src/Decay/BaryonResonanceDecayer.h
@@ -9,15 +9,7 @@
           an N-body phase space generator. Since the resonance can be produced
           off-shell, decay channels with total-mass > W are suppressed. \n
 
-          Unlike PythiaDecayer, which relies on PYTHIA, this algorithm is not
-          based on any rich underlying model. Should be used for decaying
-          baryon resonances only. \n
-
-          The baryon resonance PDG codes follow the MINOS extensions to
-          PDG tables. \n
-
-          The BaryonResonanceDecayer is a concrete implementation of the
-          DecayModelI interface.
+          Is a concrete implementation of the DecayModelI interface.
 
 \author   Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
           STFC, Rutherford Appleton Laboratory
@@ -34,6 +26,7 @@
 #define _BARYON_RESONANCE_DECAYER_H_
 
 #include <TGenPhaseSpace.h>
+#include <TLorentzVector.h>
 
 #include "Decay/DecayModelI.h"
 
@@ -46,14 +39,16 @@ public:
   BaryonResonanceDecayer(string config);
   virtual ~BaryonResonanceDecayer();
 
-  //! implement the DecayModelI interface
-  bool           IsHandled  (int pdgc)                    const;
-  void           Initialize (void)                        const;
-  TClonesArray * Decay      (const DecayerInputs_t & inp) const;
-  double         Weight     (void)                        const;
+  // implement the DecayModelI interface
+  bool           IsHandled      (int pdgc)                      const;
+  void           Initialize     (void)                          const;
+  TClonesArray * Decay          (const DecayerInputs_t & inp)   const;
+  double         Weight         (void)                          const;
+  void           InhibitDecay   (int pdg, TDecayChannel * dc=0) const;
+  void           UnInhibitDecay (int pdg, TDecayChannel * dc=0) const;
 
-  //! overload the Algorithm::Configure() methods to load private data
-  //!  members from configuration options
+  // overload the Algorithm::Configure() methods to load private data
+  // members from configuration options
   void Configure(const Registry & config);
   void Configure(string config);
 
