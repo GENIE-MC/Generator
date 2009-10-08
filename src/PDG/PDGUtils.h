e6bd428716f6b44c9eb963b6ff2c452c3b854bce
costas <costas.andreopoulos@stfc.ac.uk>
2009-10-08 11:26:01 +0000

    Added IsNegChargedLepton(), IsPosChargedLepton()
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2558 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/PDG/PDGUtils.h
+++ src/PDG/PDGUtils.h
@@ -23,57 +23,58 @@ namespace genie {
 
 namespace pdg
 {
-  bool IsPseudoParticle(int pdgc);
+  bool IsPseudoParticle   (int pdgc);
+  bool IsIon              (int pdgc);
+  bool IsParticle         (int pdgc); ///< not ion or pseudo-particle
 
-  int  IonPdgCodeToZ   (int pdgc);
-  int  IonPdgCodeToA   (int pdgc);
-  int  IonPdgCode      (int A, int Z);
-  int  IonPdgCode      (int A, int Z, int L, int I);
-  bool IsIon           (int pdgc);
+  int  IonPdgCodeToZ      (int pdgc);
+  int  IonPdgCodeToA      (int pdgc);
+  int  IonPdgCode         (int A, int Z);
+  int  IonPdgCode         (int A, int Z, int L, int I);
+  
+  bool IsLepton           (int pdgc);
+  bool IsNeutralLepton    (int pdgc);
+  bool IsChargedLepton    (int pdgc);
 
-  bool IsParticle      (int pdgc); 
-  
-  bool IsNeutrino      (int pdgc);
-  bool IsAntiNeutrino  (int pdgc);
+  bool IsNeutrino         (int pdgc);
+  bool IsAntiNeutrino     (int pdgc);
+  bool IsNegChargedLepton (int pdgc);
+  bool IsPosChargedLepton (int pdgc);
 
-  bool IsLepton        (int pdgc);
-  bool IsNeutralLepton (int pdgc);
-  bool IsChargedLepton (int pdgc);
-
-  bool IsNuE           (int pdgc);
-  bool IsNuMu          (int pdgc);
-  bool IsNuTau         (int pdgc);
-  bool IsAntiNuE       (int pdgc);
-  bool IsAntiNuMu      (int pdgc);
-  bool IsAntiNuTau     (int pdgc);
+  bool IsNuE              (int pdgc);
+  bool IsNuMu             (int pdgc);
+  bool IsNuTau            (int pdgc);
+  bool IsAntiNuE          (int pdgc);
+  bool IsAntiNuMu         (int pdgc);
+  bool IsAntiNuTau        (int pdgc);
   
-  bool IsElectron      (int pdgc);
-  bool IsPositron      (int pdgc);
-  bool IsMuon          (int pdgc);
-  bool IsAntiMuon      (int pdgc);
-  bool IsTau           (int pdgc);
-  bool IsAntiTau       (int pdgc);
+  bool IsElectron         (int pdgc);
+  bool IsPositron         (int pdgc);
+  bool IsMuon             (int pdgc);
+  bool IsAntiMuon         (int pdgc);
+  bool IsTau              (int pdgc);
+  bool IsAntiTau          (int pdgc);
   
-  bool IsDiQuark       (int pdgc);
-  bool IsQuark         (int pdgc);
-  bool IsUQuark        (int pdgc);
-  bool IsDQuark        (int pdgc);
-  bool IsSQuark        (int pdgc);
-  bool IsCQuark        (int pdgc);
-  bool IsAntiQuark     (int pdgc);
-  bool IsAntiUQuark    (int pdgc);
-  bool IsAntiDQuark    (int pdgc);
-  bool IsAntiSQuark    (int pdgc);
-  bool IsAntiCQuark    (int pdgc);
+  bool IsDiQuark          (int pdgc);
+  bool IsQuark            (int pdgc);
+  bool IsUQuark           (int pdgc);
+  bool IsDQuark           (int pdgc);
+  bool IsSQuark           (int pdgc);
+  bool IsCQuark           (int pdgc);
+  bool IsAntiQuark        (int pdgc);
+  bool IsAntiUQuark       (int pdgc);
+  bool IsAntiDQuark       (int pdgc);
+  bool IsAntiSQuark       (int pdgc);
+  bool IsAntiCQuark       (int pdgc);
   
-  bool IsPion            (int pdgc);
-  bool IsProton          (int pdgc);
-  bool IsNeutron         (int pdgc);
-  bool IsNucleon         (int pdgc);
-  bool IsNeutronOrProton (int pdgc);
-  bool IsHadron          (int pdgc);
-  bool IsBaryonResonance (int pdgc);
-  bool Is2NucleonCluster (int pdgc);
+  bool IsPion             (int pdgc);
+  bool IsProton           (int pdgc);
+  bool IsNeutron          (int pdgc);
+  bool IsNucleon          (int pdgc);
+  bool IsNeutronOrProton  (int pdgc);
+  bool IsHadron           (int pdgc);
+  bool IsBaryonResonance  (int pdgc);
+  bool Is2NucleonCluster  (int pdgc);
   
   int  SwitchProtonNeutron    (int pdgc);
   int  Neutrino2ChargedLepton (int pdgc);
