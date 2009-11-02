4700992c80656d0d585979d74b225a661ececb2c
costas <costas.andreopoulos@stfc.ac.uk>
2009-11-02 17:31:27 +0000

    *** empty log message ***
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2663 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/scripts/production/rutherford_tier2/submit-vld_nuclatten.pl
+++ src/scripts/production/rutherford_tier2/submit-vld_nuclatten.pl
@@ -1,18 +1,20 @@
 #!/usr/bin/perl
 
-#---------------------------------------------------------------------------------------------------------------------
-# Submit jobs to generate all data needed for validating GENIE's hadronization model.
+#-----------------------------------------------------------------------------------------------------------
+# Submit jobs to generate data needed for validating GENIE's hadronization + intranuclear transport model 
+# (with emphasis  on the modeling of medium effects to the hadronization) in a set of comparisons against 
+# the CLAS and HERMES nuclear attenuation data.
 #
-# The generated data can be fed into GENIE's `gvld_free_nucleon_hadronz_test' utility.
+# The generated data can be fed into GENIE's `gvld_medium_effects_hadronz_test' utility.
 #
 # Syntax:
-#   perl submit-vld_xsec.pl <options>
+#   perl submit-vld_nuclatten.pl <options>
 #
 # Options:
 #  --version       : GENIE version number
 # [--nsubruns]     : number of subruns per run, default: 1
 # [--arch]         : <SL4_32bit, SL5_64bit>, default: SL5_64bit
-# [--production]   : production name, default: hadro_<version>
+# [--production]   : production name, default: hadnucvld_<version>
 # [--cycle]        : cycle in current production, default: 01
 # [--use-valgrind] : default: off
 # [--queue]        : default: prod
@@ -20,20 +22,31 @@
 #
 # Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
 # STFC, Rutherford Appleton Lab
-#---------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------
 #
 # EVENT SAMPLES:
+#
+# Run number key: IZZZAAAJJxxx
+# I   :  1->e-, 2->e+, 3->mu-, 4->mu+
+# ZZZ :  nuclear target atomic number (eg 026 for Fe56)
+# AAA :  nuclear target mass   number (eg 056 for Fe56)
+# JJ  :  flux setting 
+#            01 -> 12.0 GeV [HERMES]
+#            02 -> 27.6 GeV [HERMES]
+# xxx :  sub-run ID, 001-999, 20k events each
+#
 #...................................................................................
 # run number      |  init state      | energy   | GEVGL              | flux
 #                 |                  | (GeV)    | setting            |
 #...................................................................................
 #
-# 1000xx          | numu    + n      | 0.5-80.  | HadronizationTest  | 1/E
-# 1100xx          | numu    + p      | 0.5-80.  | HadronizationTest  | 1/E
-# 1200xx          | numubar + n      | 0.5-80.  | HadronizationTest  | 1/E
-# 1300xx          | numubar + p      | 0.5-80.  | HadronizationTest  | 1/E
+# 100100202xxx    | e-    + D2       | 27.6     | EM                | -
+# 100200402xxx    | e-    + He4      | 27.6     | EM                | -
+# 100701402xxx    | e-    + N14      | 27.6     | EM                | -
+# 101002002xxx    | e-    + Ne20     | 27.6     | EM                | -
+# 103608302xxx    | e-    + Kr83     | 27.6     | EM                | -
+# 105413102xxx    | e-    + Xe131    | 27.6     | EM                | -
 #
-# xx : Run ID, 01-99, 20k events each
 #...................................................................................
 #
 
@@ -59,47 +72,57 @@ unless defined $genie_version;
 $nsubruns       = 1                           unless defined $nsubruns;
 $use_valgrind   = 0                           unless defined $use_valgrind;
 $arch           = "SL5_64bit"                 unless defined $arch;
-$production     = "hadrnzvld\_$genie_version" unless defined $production;
+$production     = "hadnucvld\_$genie_version" unless defined $production;
 $cycle          = "01"                        unless defined $cycle;
 $queue          = "prod"                      unless defined $queue;
 $softw_topdir   = "/opt/ppd/t2k/GENIE"        unless defined $softw_topdir;
 $time_limit     = "60:00:00";
 $genie_setup    = "$softw_topdir/builds/$arch/$genie_version-setup";
 $jobs_dir       = "$softw_topdir/scratch/$production\_$cycle";
-$xspl_file      = "$softw_topdir/data/job_inputs/xspl/gxspl-freenuc-$genie_version.xml";
+$xspl_file      = "$softw_topdir/data/job_inputs/xspl/gxspl-emode-$genie_version.xml";
 $mcseed         = 210921029;
 $nev_per_subrun = 20000;
 
 # inputs for event generation jobs
-%evg_nupdg_hash = ( 
-  '1000' =>   '14',
-  '1100' =>   '14',
-  '1200' =>  '-14',
-  '1300' =>  '-14'
+%evg_pdg_hash = ( 
+  '100100202' =>   '11',
+  '100200402' =>   '11',
+  '100701402' =>   '11',
+  '101002002' =>   '11',
+  '103608302' =>   '11',
+  '105413102' =>   '11'
 );
 %evg_tgtpdg_hash = ( 
-  '1000' =>  '1000000010',
-  '1100' =>  '1000010010',
-  '1200' =>  '1000000010',
-  '1300' =>  '1000010010'
+  '100100202' =>   '1000010020',
+  '100200402' =>   '1000020040',
+  '100701402' =>   '1000070140',
+  '101002002' =>   '1000100200',
+  '103608302' =>   '1000360830',
+  '105413102' =>   '1000541310'
 );
 %evg_energy_hash = ( 
-  '1000' =>  '0.5,80.0',
-  '1100' =>  '0.5,80.0',
-  '1200' =>  '0.5,80.0',
-  '1300' =>  '0.5,80.0',
+  '100100202' =>   '27.6',
+  '100200402' =>   '27.6',
+  '100701402' =>   '27.6',
+  '101002002' =>   '27.6',
+  '103608302' =>   '27.6',
+  '105413102' =>   '27.6'
 );
 %evg_gevgl_hash = ( 
-  '1000' =>  'HadronizationTest',
-  '1100' =>  'HadronizationTest',
-  '1200' =>  'HadronizationTest',
-  '1300' =>  'HadronizationTest',
+  '100100202' =>   'EM',
+  '100200402' =>   'EM',
+  '100701402' =>   'EM',
+  '101002002' =>   'EM',
+  '103608302' =>   'EM',
+  '105413102' =>   'EM'
 );
 %evg_fluxopt_hash = ( 
-  '1000' =>  '-f "1/x"',
-  '1100' =>  '-f "1/x"',
-  '1200' =>  '-f "1/x"',
-  '1300' =>  '-f "1/x"',
+  '100100202' =>   '',
+  '100200402' =>   '',
+  '100701402' =>   '',
+  '101002002' =>   '',
+  '103608302' =>   '',
+  '105413102' =>   ''
 );
 
 # make the jobs directory
@@ -118,7 +141,7 @@ for my $curr_runnu (keys %evg_gevgl_hash)  {
     #
     # get runnu-dependent info
     #
-    $nu      = $evg_nupdg_hash   {$curr_runnu};
+    $probe   = $evg_pdg_hash     {$curr_runnu};
     $tgt     = $evg_tgtpdg_hash  {$curr_runnu};
     $en      = $evg_energy_hash  {$curr_runnu};
     $gevgl   = $evg_gevgl_hash   {$curr_runnu};
@@ -127,7 +150,7 @@ for my $curr_runnu (keys %evg_gevgl_hash)  {
     # submit subruns
     for($isubrun = 0; $isubrun < $nsubruns; $isubrun++) {
 
-       $curr_subrunnu = 100 * $curr_runnu + $isubrun;
+       $curr_subrunnu = 1000 * $curr_runnu + $isubrun;
 
        $batch_script  = "$jobs_dir/hdzvld-$curr_subrunnu.pbs";
        $logfile_evgen = "$jobs_dir/hdzvld-$curr_subrunnu.evgen.log";
@@ -138,7 +161,7 @@ for my $curr_runnu (keys %evg_gevgl_hash)  {
        $curr_seed     = $mcseed + $isubrun;
        $grep_pipe     = "grep -B 20 -A 30 -i \"warn\\|error\\|fatal\"";
        $valgrind_cmd  = "valgrind --tool=memcheck --error-limit=no --leak-check=yes --show-reachable=yes";
-       $evgen_cmd     = "gevgen -n $nev_per_subrun -s -e $en -p $nu -t $tgt -r $curr_subrunnu $fluxopt | grep_pipe &> $logfile_evgen";
+       $evgen_cmd     = "gevgen -n $nev_per_subrun -s -e $en -p $probe -t $tgt -r $curr_subrunnu $fluxopt | grep_pipe &> $logfile_evgen";
 
        # create the PBS script
        open(PBS, ">$batch_script") or die("Can not create the PBS batch script");
