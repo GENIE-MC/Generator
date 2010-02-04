496c75ef108ff9ed52597d8dc2565c9e513b2845
rhatcher <rhatcher@fnal.gov>
2010-02-04 20:21:43 +0000

    Make GenerateTrimmedList() a private method; users are now required to supply the TrimSegment() method for all derived classes.  This way they don't have to worry about maintaining the list only how to trim any particular PathSegment.  Also, class now is configurable about whether to retain a null segment; and also serves as a repository for the swimmer on whether to fetch the geometry heirachy "path" (which turns out to be a non-trivial overhead so we don't want to fetch it if we don't need to [despite my previous tests to the contrary ... I messed up the testing])
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2694 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Geo/GeomVolSelectorI.cxx
+++ src/Geo/GeomVolSelectorI.cxx
@@ -16,25 +16,48 @@
 //#include "EVGDrivers/GeomVolSelectorI.h"
 //rwh -- for now during test phase
 #include "Geo/GeomVolSelectorI.h"
+#include "Geo/PathSegmentList.h"
 
 using namespace genie;
 using namespace genie::geometry;
 
 //____________________________________________________________________________
 GeomVolSelectorI::GeomVolSelectorI() 
+  : fRemoveEntries(false), fNeedPath(false), fName("no-name")
 {
-  fName = "no-name";
 }
 
 //____________________________________________________________________________
 GeomVolSelectorI::GeomVolSelectorI(std::string name) 
+  : fRemoveEntries(false), fNeedPath(false), fName(name)
 {
-  fName = name;
 }
 
 //___________________________________________________________________________
 GeomVolSelectorI::~GeomVolSelectorI()
 {
 
+}
+//___________________________________________________________________________
+
+PathSegmentList* 
+GeomVolSelectorI::GenerateTrimmedList(const PathSegmentList* untrimmed) const
+{
+  PathSegmentList* trimmed = new PathSegmentList();
+
+  const genie::geometry::PathSegmentList::PathSegmentV_t& segments = 
+    untrimmed->GetPathSegmentV();
+  genie::geometry::PathSegmentList::PathSegVCItr_t sitr = segments.begin();
+  genie::geometry::PathSegmentList::PathSegVCItr_t sitr_end = segments.end();
+
+  for ( ; sitr != sitr_end ; ++sitr ) {
+    PathSegment ps = *sitr;  // new PathSegment is a copy of old
+    this->TrimSegment(ps);
+    if ( fRemoveEntries && ps.GetSummedStepRange() == 0 ) continue; // remove null segments
+    // now put (adjusted) entry on trimmed list
+    trimmed->AddSegment(ps);
+  }
+
+  return trimmed;
 }
 //___________________________________________________________________________
