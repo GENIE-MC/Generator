03103854ed0e4e03db086e4154ee61a7b8b53c56
candreop <costas.andreopoulos@stfc.ac.uk>
2010-02-23 00:15:54 +0000

    Added test program for atmospheric flux drivers
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2704 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/test/testFluxAtmo.cxx
@@ -0,0 +1,136 @@
+//____________________________________________________________________________
+/*!
+
+\program gtestFluxAtmo
+
+\brief   Test program for atmospheric flux drivers
+
+\author  Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         STFC, Rutherford Appleton Laboratory
+
+\created August 22, 2005
+
+\cpright Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
+         For the full text of the license visit http://copyright.genie-mc.org
+         or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#include <TFile.h>
+#include <TNtuple.h>
+#include <TSystem.h>
+#include <TH1D.h>
+#include <TF1.h>
+
+#include "FluxDrivers/GFlukaAtmo3DFlux.h"
+#include "FluxDrivers/GBartolAtmoFlux.h"
+#include "Messenger/Messenger.h"
+#include "PDG/PDGCodes.h"
+
+using namespace genie;
+using namespace genie::flux;
+
+const unsigned int kNEvents = 100000;
+
+TNtuple * runGFlukaAtmo3DFluxDriver (void);
+TNtuple * runGBartolAtmoFluxDriver  (void);
+TNtuple * createFluxNtuple          (GFluxI * flux);
+
+//____________________________________________________________________________
+int main(int /*argc*/, char ** /*argv*/)
+{
+  LOG("Main", pINFO)  << "Running GFlukaAtmo3DFlux driver test";
+  TNtuple * ntfluka = runGFlukaAtmo3DFluxDriver();
+  ntfluka->SetTitle("GFlukaAtmo3DFlux driver data");
+
+  LOG("Main", pINFO)  << "Running GBartolAtmoFlux driver test";
+  TNtuple * ntbartol = runGBartolAtmoFluxDriver();
+  ntbartol->SetTitle("GBartolAtmoFlux driver data");
+
+  LOG("Main", pINFO) << "Saving flux ntuples";
+
+  TFile f("./genie-flux-drivers.root","recreate");
+  ntfluka  -> Write("ntfluka");
+  ntbartol -> Write("ntbartol");
+  f.Close();
+
+  LOG("Main", pINFO)  << "Done!";
+
+  return 0;
+}
+//____________________________________________________________________________
+TNtuple * runGFlukaAtmo3DFluxDriver(void)
+{
+  string base_dir = 
+    (gSystem->Getenv("GFLUX_FLUKA3DATMO") ?
+       gSystem->Getenv("GFLUX_FLUKA3DATMO") : ".");
+
+  double Rlongitudinal  = 1000.; //m
+  double Rtransverse    = 100.;  //m
+
+  GFlukaAtmo3DFlux * flux = new GFlukaAtmo3DFlux;
+
+  flux -> SetFluxFile ( kPdgNuMu,     base_dir + "/sdave_numu07.dat"  );
+  flux -> SetFluxFile ( kPdgAntiNuMu, base_dir + "/sdave_anumu07.dat" );
+  flux -> SetFluxFile ( kPdgNuE,      base_dir + "/sdave_nue07.dat"   );
+  flux -> SetFluxFile ( kPdgAntiNuE,  base_dir + "/sdave_anue07.dat"  );
+  flux -> SetRadii(Rlongitudinal, Rtransverse);
+  flux -> LoadFluxData();
+  flux -> GenerateWeighted(true);
+//flux -> ForceMaxEnergy(3);
+
+  LOG("Main", pINFO) << "Generating events";
+  TNtuple * fluxntp = createFluxNtuple(dynamic_cast<GFluxI*>(flux));
+  return fluxntp;
+}
+//____________________________________________________________________________
+TNtuple * runGBartolAtmoFluxDriver(void)
+{
+  string base_dir = 
+    (gSystem->Getenv("GFLUX_BGLRS3DATMO") ?
+       gSystem->Getenv("GFLUX_BGLRS3DATMO") : ".");
+
+  double Rlongitudinal  = 1000.; //m
+  double Rtransverse    = 100.;  //m
+
+  GBartolAtmoFlux * flux = new GBartolAtmoFlux;
+
+  flux -> SetFluxFile ( kPdgNuMu,     base_dir + "/f210_3_z.kam_num" );
+  flux -> SetFluxFile ( kPdgAntiNuMu, base_dir + "/f210_3_z.kam_nbm" );
+  flux -> SetFluxFile ( kPdgNuE,      base_dir + "/f210_3_z.kam_nue" );
+  flux -> SetFluxFile ( kPdgAntiNuE,  base_dir + "/f210_3_z.kam_nbe" );
+  flux -> SetRadii(Rlongitudinal, Rtransverse);
+  flux -> LoadFluxData();
+//flux -> GenerateWeighted(true);
+//flux -> ForceMaxEnergy(300);
+
+  LOG("Main", pINFO) << "Generating events";
+  TNtuple * fluxntp = createFluxNtuple(dynamic_cast<GFluxI*>(flux));
+  delete flux;
+
+  return fluxntp;
+}
+//____________________________________________________________________________
+TNtuple * createFluxNtuple(GFluxI * flux)
+{
+  TNtuple * fluxntp = 
+     new TNtuple("fluxntp", "flux", "x:y:z:t:px:py:pz:E:pdgc:wght");
+
+  LOG("Main", pINFO) << "Generating flux neutrinos";
+
+  unsigned int ievent = 0;
+  while(ievent++ < kNEvents) {
+    LOG("Main", pINFO)  << "Event number: " << ievent;
+    flux->GenerateNext();
+    int    pdgc = flux->PdgCode();
+    double wght = flux->Weight();
+    const TLorentzVector & x4 = flux->Position();
+    const TLorentzVector & p4 = flux->Momentum();
+    fluxntp->Fill( 
+      x4.X(),  x4.Y(),  x4.Z(),  x4.T(),
+      p4.Px(), p4.Py(), p4.Pz(), p4.E(), pdgc, wght);
+  }
+  return fluxntp;
+}
+//____________________________________________________________________________
+
