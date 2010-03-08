5427bd58ffa56c3413d36213a06653a232351df8
candreop <costas.andreopoulos@stfc.ac.uk>
2010-03-08 14:18:07 +0000

    update copyright notice in prep for next physics release
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2740 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/BaryonResonance/BreitWignerRes.h
+++ src/BaryonResonance/BreitWignerRes.h
@@ -11,7 +11,7 @@
 
 \created  November 22, 2004
 
-\cpright  Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
+\cpright  Copyright (c) 2003-2010, GENIE Neutrino MC Generator Collaboration
           For the full text of the license visit http://copyright.genie-mc.org
           or see $GENIE/LICENSE
 */
