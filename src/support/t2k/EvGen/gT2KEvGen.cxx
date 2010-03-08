eb5617dce9b4537d7ab513373b87ae8685436453
candreop <costas.andreopoulos@stfc.ac.uk>
2010-03-08 14:33:08 +0000

    update copyright notice in prep for next physics release
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2771 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/support/t2k/EvGen/gT2KEvGen.cxx
+++ src/support/t2k/EvGen/gT2KEvGen.cxx
@@ -318,7 +318,7 @@
 
 \created February 05, 2008
 
-\cpright Copyright (c) 2003-2009, GENIE Neutrino MC Generator Collaboration
+\cpright Copyright (c) 2003-2010, GENIE Neutrino MC Generator Collaboration
          For the full text of the license visit http://copyright.genie-mc.org
          or see $GENIE/LICENSE
 */
