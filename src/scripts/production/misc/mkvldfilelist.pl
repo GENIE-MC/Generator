a10949ab87dd10f74d06955a296015c242c89a13
candreop <costas.andreopoulos@stfc.ac.uk>
2010-03-16 14:23:43 +0000

    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2824 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/scripts/production/misc/mkvldfilelist.pl
@@ -0,0 +1,47 @@
+#-------------------------------------------------------------------------------------------
+# Create XML file list expected by GENIE validation programs
+#
+# Syntax:
+#    % perl mkvldfilelist.pl <options>
+#
+# Options:
+#    --dir    : file math
+#    --model  : physics model tag
+#
+# Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+# STFC, Rutherford Appleton Lab
+#-------------------------------------------------------------------------------------------
+
+#!/usr/bin/perl
+
+$iarg=0;
+foreach (@ARGV) {
+   if($_ eq '--dir')     { $directory   = $ARGV[$iarg+1]; }
+   if($_ eq '--model')   { $model_name  = $ARGV[$iarg+1]; }
+   $iarg++;
+}
+
+die("** Aborting [Can not read the directory (if any) specified via the --dir option]")
+unless -d $directory;
+die("** Aborting [You need to specify a data file `tag' using the --model option]")
+unless defined $model_name;
+
+@files = <$directory/*.root>;
+
+print "\n\n";
+print "\t <model name=\"$model_name\"> \n";
+
+foreach $file (@files) {
+  if($file=~/.ghep.root$/) {
+     print "\t\t <evt_file format=\"ghep\"> $file </evt_file> \n";
+  }
+  elsif($file=~/.gst.root$/) {
+     print "\t\t <evt_file format=\"gst\"> $file </evt_file> \n";
+  }
+  else{
+     print "\t\t <xsec_file> $file </xsec_file> \n";
+  }
+}
+
+print "\t </model> \n\n";
+
