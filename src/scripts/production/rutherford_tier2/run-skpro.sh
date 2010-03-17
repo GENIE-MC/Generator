5edd949123160eb72156be34103c2327662fa416
candreop <costas.andreopoulos@stfc.ac.uk>
2010-03-17 23:27:28 +0000

    Add script to submit all jobs for a standard SuperK MC production
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2836 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/scripts/production/rutherford_tier2/run-skpro.sh
@@ -0,0 +1,25 @@
+#
+# Submit all jobs for a standard SuperK MC production.
+#
+# Syntax:
+#   ./run-skpro.sh [version] [production] [cycle]
+#
+# Notes:
+# - See production/rutherford_tier2/submit-evg_sk_fhst.pl for details about each actual MC job.
+# - See misc/generate_sk_flux_histograms.C for the derivation of SK flux from the JNUBEAM flux ntuples.
+# - Using standard SK statistics: 500 numu, 50 numubar, 50 nue, 50 nuesig files (2k events each, see submit-evg_sk_fhst.pl)
+# - Remember to switch on the 'decay' flag for all charmed hadrons in UserPhysicsOptions.xml
+#
+# C.Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+#
+
+#!/bin/bash
+
+VRS=$1
+PRO=$2
+CYC=$3
+
+perl bulk-submit.pl --cmd "perl submit-evg_sk_fhst.pl --neutrino numu    --version $VRS --production $PRO --cycle $CYC" --arg "--run" --min 1 --max 500
+perl bulk-submit.pl --cmd "perl submit-evg_sk_fhst.pl --neutrino numubar --version $VRS --production $PRO --cycle $CYC" --arg "--run" --min 1 --max 50
+perl bulk-submit.pl --cmd "perl submit-evg_sk_fhst.pl --neutrino nue     --version $VRS --production $PRO --cycle $CYC" --arg "--run" --min 1 --max 50
+perl bulk-submit.pl --cmd "perl submit-evg_sk_fhst.pl --neutrino nuesig  --version $VRS --production $PRO --cycle $CYC" --arg "--run" --min 1 --max 50
