83f38f3c721edbee9f97313128170084baaf3232
candreop <costas.andreopoulos@stfc.ac.uk>
2010-04-08 16:17:37 +0000

    use genie::units
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2854 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/test/testPREM.cxx
+++ src/test/testPREM.cxx
@@ -20,6 +20,7 @@
 #include <TNtuple.h>
 
 #include "Conventions/Constants.h"
+#include "Conventions/Units.h"
 #include "Utils/PREM.h"
 
 using namespace genie;
@@ -29,13 +30,13 @@ int main(int /*argc*/, char ** /*argv*/)
 {
   TNtuple * earth_density = new TNtuple("earth_density","","r:rho");
 
-  const double dr   = 1; // km
-  const double rmax = constants::kREarth + 10; // km
+  const double dr   = 1. * units::km; 
+  const double rmax = constants::kREarth;
 
   double r = 0;
   while(r < rmax) {
-     double rho = utils::prem::Density(r*units::km) / units::g_cm3; 
-     earth_density->Fill(r, rho);
+     double rho = utils::prem::Density(r); 
+     earth_density->Fill(r/units::km, rho/units::g_cm3);
      r += dr;
   }
 
