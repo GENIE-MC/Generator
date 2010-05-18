6e09f1317bea72f12645d026cb033cae539edcc3
candreop <costas.andreopoulos@stfc.ac.uk>
2010-05-18 14:04:58 +0000

    cont'd rew devel for T2K summer 2010 analysis
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@2880 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/ReWeight/GReWeightNuXSecCCQE.cxx
@@ -0,0 +1,277 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2010, GENIE Neutrino MC Generator Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+ or see $GENIE/LICENSE
+
+ Authors: Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+          STFC, Rutherford Appleton Laboratory
+
+          Jim Dobson <J.Dobson07 \at imperial.ac.uk>
+          Imperial College London
+
+ For the class documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+ @ Aug 01, 2009 - CA
+   Was adapted from Jim's and Costas' T2K-specific GENIE reweighting code. 
+   First included in v2.5.1.
+ @ May 17, 2010 - CA
+   Code extracted from GReWeightNuXSec and redeveloped in preparation for 
+   the Summer 2010 T2K analyses.
+
+*/
+//____________________________________________________________________________
+
+#include <TMath.h>
+
+#include "Algorithm/AlgFactory.h"
+#include "Algorithm/AlgConfigPool.h"
+#include "Base/XSecAlgorithmI.h"
+#include "Conventions/Units.h"
+#include "Conventions/Controls.h"
+#include "EVGCore/EventRecord.h"
+#include "GHEP/GHepParticle.h"
+#include "Interaction/Interaction.h"
+#include "Messenger/Messenger.h"
+#include "PDG/PDGCodes.h"
+#include "ReWeight/GReWeightNuXSecCCQE.h"
+#include "ReWeight/GSystSet.h"
+#include "ReWeight/GSystUncertainty.h"
+#include "Registry/Registry.h"
+
+using namespace genie;
+using namespace genie::rew;
+
+//_______________________________________________________________________________________
+GReWeightNuXSecCCQE::GReWeightNuXSecCCQE() 
+{
+  this->Init();
+}
+//_______________________________________________________________________________________
+GReWeightNuXSecCCQE::~GReWeightNuXSecCCQE()
+{
+
+}
+//_______________________________________________________________________________________
+bool GReWeightNuXSecCCQE::IsHandled(GSyst_t syst)
+{
+   bool handle;
+
+   switch(syst) {
+
+     case ( kXSecTwkDial_NormCCQE    ) :
+     case ( kXSecTwkDial_MaCCQEshape ) :
+       if(fMode==kModeNormAndMaShape) { 
+          handle = true;  
+       } else { 
+          handle = false; 
+       }
+       break;
+
+     case ( kXSecTwkDial_MaCCQE ) :
+       if(fMode==kModeMa) { 
+          handle = true;  
+       } else { 
+          handle = false; 
+       }
+       break;
+
+     default:
+          handle = false;
+          break;
+   }
+
+   return handle;
+}
+//_______________________________________________________________________________________
+void GReWeightNuXSecCCQE::SetSystematic(GSyst_t syst, double twk_dial)
+{
+  if(!this->IsHandled(syst)) return;
+
+  switch(syst) {
+    case ( kXSecTwkDial_NormCCQE ) :
+      fNormTwkDial = twk_dial;
+      break;
+    case ( kXSecTwkDial_MaCCQEshape ) :
+    case ( kXSecTwkDial_MaCCQE ) :
+      fMaTwkDial = twk_dial;
+      break;
+    default:
+      break;
+  }
+}
+//_______________________________________________________________________________________
+void GReWeightNuXSecCCQE::Reset(void)
+{
+  fNormTwkDial = 0.;
+  fNormCurr    = fNormDef;
+  fMaTwkDial   = 0.; 
+  fMaCurr      = fMaDef;
+
+  this->Reconfigure();
+}
+//_______________________________________________________________________________________
+void GReWeightNuXSecCCQE::Reconfigure(void)
+{
+  GSystUncertainty * fracerr = GSystUncertainty::Instance();
+
+  if(fMode==kModeMa) {   
+     double fracerr_ma = fracerr->OneSigmaErr(kXSecTwkDial_MaCCQE);
+     fMaCurr   = fMaDef * (1. + fMaTwkDial * fracerr_ma);
+  }
+  else
+  if(fMode==kModeNormAndMaShape) { 
+     double fracerr_norm = fracerr->OneSigmaErr(kXSecTwkDial_NormCCQE);
+     double fracerr_mash = fracerr->OneSigmaErr(kXSecTwkDial_MaCCQEshape);
+     fNormCurr = fNormDef * (1. + fNormTwkDial * fracerr_norm);
+     fMaCurr   = fMaDef   * (1. + fMaTwkDial   * fracerr_mash);
+  }
+
+  fNormCurr = TMath::Max(0., fNormCurr);
+  fMaCurr   = TMath::Max(0., fMaCurr  );
+
+  Registry & r = *fXSecModelConfig;
+
+  r.Set(fMaPath, fMaCurr); 
+  fXSecModel->Configure(r);
+
+//LOG("ReW, pDEBUG) << *fXSecModel;
+}
+//_______________________________________________________________________________________
+double GReWeightNuXSecCCQE::CalcWeight(const genie::EventRecord & event) 
+{
+  bool is_qe = event.Summary()->ProcInfo().IsQuasiElastic();
+  bool is_cc = event.Summary()->ProcInfo().IsWeakCC();
+  if(!is_qe || !is_cc) return 1.;
+
+  int nupdg = event.Probe()->Pdg();
+  if(nupdg==kPdgNuMu     && !fRewNumu   ) return 1.;
+  if(nupdg==kPdgAntiNuMu && !fRewNumubar) return 1.;
+  if(nupdg==kPdgNuE      && !fRewNue    ) return 1.;
+  if(nupdg==kPdgAntiNuE  && !fRewNuebar ) return 1.;
+
+  if(fMode==kModeMa) {
+     double wght = this->CalcWeightMa(event);
+     return wght;
+  } 
+  else 
+  if(fMode==kModeNormAndMaShape) {
+     double wght = 
+         this->CalcWeightNorm    (event) *
+         this->CalcWeightMaShape (event);
+     return wght;
+  }
+
+  return 1.;
+}
+//_______________________________________________________________________________________
+double GReWeightNuXSecCCQE::CalcChisq()
+{
+  double chisq = 0.;
+  if(fMode==kModeMa) {   
+     chisq += TMath::Power(fMaTwkDial, 2.);
+  }
+  else
+  if(fMode==kModeNormAndMaShape) { 
+     chisq += TMath::Power(fNormTwkDial, 2.);
+     chisq += TMath::Power(fMaTwkDial,   2.);
+  }
+  return chisq;
+}
+//_______________________________________________________________________________________
+void GReWeightNuXSecCCQE::Init(void)
+{
+  AlgId id("genie::LwlynSmithQELCCPXSec","Default");
+
+  AlgFactory * algf = AlgFactory::Instance();
+  Algorithm * alg = algf->AdoptAlgorithm(id);
+
+  fXSecModel = dynamic_cast<XSecAlgorithmI*>(alg);
+  fXSecModel->AdoptSubstructure();
+
+  fXSecModelConfig = new Registry(fXSecModel->GetConfig());
+//LOG("ReW, pDEBUG) << *fXSecModelConfig;
+
+  this->SetMode(kModeNormAndMaShape);
+
+  this->RewNue    (true);
+  this->RewNuebar (true);
+  this->RewNumu   (true);
+  this->RewNumubar(true);
+
+  this->SetMaPath("FormFactorsAlg/Ma");
+
+  fNormTwkDial = 0.;
+  fNormDef     = 1.;
+  fNormCurr    = fNormDef;
+  fMaTwkDial   = 0.; 
+  fMaDef       = fXSecModelConfig->GetDouble(fMaPath);
+  fMaCurr      = fMaDef;
+}
+//_______________________________________________________________________________________
+double GReWeightNuXSecCCQE::CalcWeightNorm(const genie::EventRecord & /*event*/) 
+{
+  bool tweaked = (TMath::Abs(fNormTwkDial) > controls::kASmallNum);
+  if(!tweaked) return 1.0;
+
+  double wght = fNormCurr;
+  return wght;
+}
+//_______________________________________________________________________________________
+double GReWeightNuXSecCCQE::CalcWeightMa(const genie::EventRecord & event) 
+{
+  bool tweaked = (TMath::Abs(fMaTwkDial) > controls::kASmallNum);
+  if(!tweaked) return 1.0;
+
+  Interaction * interaction = event.Summary();
+
+  interaction->KinePtr()->UseSelectedKinematics();
+  interaction->SetBit(kIAssumeFreeNucleon);
+
+  double old_xsec   = event.DiffXSec();
+  double old_weight = event.Weight();
+  double new_xsec   = fXSecModel->XSec(interaction, kPSQ2fE);
+  double new_weight = old_weight * (new_xsec/old_xsec);
+
+  interaction->KinePtr()->ClearRunningValues();
+  interaction->ResetBit(kIAssumeFreeNucleon);
+
+  return new_weight;
+}
+//_______________________________________________________________________________________
+double GReWeightNuXSecCCQE::CalcWeightMaShape(const genie::EventRecord & event) 
+{
+  bool tweaked = (TMath::Abs(fMaTwkDial) > controls::kASmallNum);
+  if(!tweaked) return 1.0;
+
+  Interaction * interaction = event.Summary();
+
+  interaction->KinePtr()->UseSelectedKinematics();
+  interaction->SetBit(kIAssumeFreeNucleon);
+
+  double old_xsec   = event.DiffXSec();
+  double old_weight = event.Weight();
+  double new_xsec   = fXSecModel->XSec(interaction, kPSQ2fE);
+  double new_weight = old_weight * (new_xsec/old_xsec);
+
+//LOG("ReW", pDEBUG) << "differential cross section (old) = " << old_xsec;
+//LOG("ReW", pDEBUG) << "differential cross section (new) = " << new_xsec;
+//LOG("ReW", pDEBUG) << "event generation weight = " << old_weight;
+//LOG("ReW", pDEBUG) << "new weight = " << new_weight;
+
+  double old_integrated_xsec = event.XSec();
+  double new_integrated_xsec = fXSecModel->Integral(interaction);
+  assert(new_integrated_xsec > 0);
+  new_weight *= (old_integrated_xsec/new_integrated_xsec);
+
+//LOG("ReW", pDEBUG) << "integrated cross section (old) = " << old_integrated_xsec;
+//LOG("ReW", pDEBUG) << "integrated cross section (new) = " << new_integrated_xsec;
+//LOG("ReW", pDEBUG) << "new weight (normalized to const integral) = " << new_weight;
+
+  interaction->KinePtr()->ClearRunningValues();
+  interaction->ResetBit(kIAssumeFreeNucleon);
+
+  return new_weight;
+}
+//_______________________________________________________________________________________
