95c88b562eeca5210ba771b30a6d8caf3ba1a6a8
candreop <costas.andreopoulos@stfc.ac.uk>
2010-05-19 11:28:54 +0000

    new reweighting test program
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@2885 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/test/testRewght.cxx
@@ -0,0 +1,208 @@
+//____________________________________________________________________________
+/*!
+
+\program gtestRewght
+
+\brief   A simple program to illustrate how to use the GENIE event reweighting.
+
+\syntax  gtestRewght -f filename [-n nev]
+
+         where 
+         [] is an optional argument
+         -f specifies a GENIE event file (GHEP format)
+         -n specifies the number of events to process (default: all)
+
+\author  Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         STFC, Rutherford Appleton Laboratory
+
+\created May 19, 2010
+
+\cpright Copyright (c) 2003-2010, GENIE Neutrino MC Generator Collaboration
+         For the full text of the license visit http://copyright.genie-mc.org
+         or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+
+#include <string>
+
+#include <TSystem.h>
+#include <TFile.h>
+#include <TTree.h>
+
+#include "EVGCore/EventRecord.h"
+#include "Ntuple/NtpMCFormat.h"
+#include "Ntuple/NtpMCTreeHeader.h"
+#include "Ntuple/NtpMCEventRecord.h"
+#include "Messenger/Messenger.h"
+#include "ReWeight/GReWeightI.h"
+#include "ReWeight/GSystSet.h"
+#include "ReWeight/GReWeight.h"
+#include "ReWeight/GReWeightNuXSecCCQE.h"
+#include "ReWeight/GReWeightNuXSecCCRES.h"
+#include "ReWeight/GReWeightNuXSecCOH.h"
+#include "ReWeight/GReWeightNonResonanceBkg.h"
+#include "ReWeight/GReWeightFGM.h"
+#include "ReWeight/GReWeightDISNuclMod.h"
+#include "ReWeight/GReWeightResonanceDecay.h"
+#include "ReWeight/GReWeightFZone.h"
+#include "ReWeight/GReWeightINuke.h"
+#include "ReWeight/GReWeightAGKY.h"
+#include "Utils/CmdLineArgParserUtils.h"
+#include "Utils/CmdLineArgParserException.h"
+
+using std::string;
+
+using namespace genie;
+using namespace genie::rew;
+
+void GetCommandLineArgs (int argc, char ** argv);
+
+int    gOptNEvt;
+string gOptInpFilename;
+
+//___________________________________________________________________
+int main(int argc, char ** argv)
+{
+  GetCommandLineArgs (argc, argv);
+
+  // open the ROOT file and get the TTree & its header
+  TTree *           tree = 0;
+  NtpMCTreeHeader * thdr = 0;
+
+  TFile file(gOptInpFilename.c_str(),"READ");
+
+  tree = dynamic_cast <TTree *>           ( file.Get("gtree")  );
+  thdr = dynamic_cast <NtpMCTreeHeader *> ( file.Get("header") );
+
+  if(!tree) return 1;
+
+  LOG("main", pNOTICE) << "Input tree header: " << *thdr;
+
+  int nev = (gOptNEvt > 0) ?
+        TMath::Min(gOptNEvt, (int)tree->GetEntries()) :
+        (int) tree->GetEntries();
+
+  LOG("main", pNOTICE) << "Will process " << nev << " events";
+
+  //
+  // Create a GReWeight object and add to it a set of 
+  // weight calculators
+  //
+
+  GReWeight rw;
+
+  rw.AdoptWghtCalc( "xsec_ccqe",       new GReWeightNuXSecCCQE      );
+  rw.AdoptWghtCalc( "xsec_ccres",      new GReWeightNuXSecCCRES     );
+  rw.AdoptWghtCalc( "xsec_coh",        new GReWeightNuXSecCOH       );
+  rw.AdoptWghtCalc( "xsec_nonresbkg",  new GReWeightNonResonanceBkg );
+  rw.AdoptWghtCalc( "nuclear_qe",      new GReWeightFGM             );
+  rw.AdoptWghtCalc( "nuclear_dis",     new GReWeightDISNuclMod      );
+  rw.AdoptWghtCalc( "hadro_res_decay", new GReWeightResonanceDecay  );
+  rw.AdoptWghtCalc( "hadro_fzone",     new GReWeightFZone           );
+  rw.AdoptWghtCalc( "hadro_intranuke", new GReWeightINuke           );
+  rw.AdoptWghtCalc( "hadro_agky",      new GReWeightAGKY            );
+
+  //
+  // Include a list of systematic parameters (more to be found at
+  // GSyst.h). Weight calculators included above must be able to handle
+  // the parameters included below.
+  //
+
+  GSystSet & syst = rw.Systematics();
+  syst.Include(kXSecTwkDial_NormCCQE    );
+  syst.Include(kXSecTwkDial_MaCCQEshape );
+  syst.Include(kXSecTwkDial_NormCCRES   );
+  syst.Include(kXSecTwkDial_MaCCRESshape);
+  syst.Include(kXSecTwkDial_RvpCC1pi    );
+  syst.Include(kXSecTwkDial_RvnCC1pi    );
+  syst.Include(kXSecTwkDial_MaCOHpi     );
+  syst.Include(kINukeTwkDial_MFP_pi     );
+  syst.Include(kINukeTwkDial_MFP_N      );
+
+  //
+  // Set non-default values and re-configure.
+  // Each tweaking dial t modifies a physics parameter p as:
+  // p_{tweaked} = p_{default} ( 1 + t * dp/p )
+  // So setting a tweaking dial to +/-1 modifies a physics quantity
+  // by +/- 1sigma.
+  // Default fractional errors are defined in GSystUncertainty
+  // and can be overriden.
+  //
+
+  syst.SetCurValue(kXSecTwkDial_NormCCQE,     +1.0);
+  syst.SetCurValue(kXSecTwkDial_MaCCQEshape,  +1.0);
+  syst.SetCurValue(kXSecTwkDial_NormCCRES,    -1.0);
+  syst.SetCurValue(kXSecTwkDial_MaCCRESshape, -1.0);
+  syst.SetCurValue(kXSecTwkDial_RvpCC1pi,     +0.5);
+  syst.SetCurValue(kXSecTwkDial_RvnCC1pi,     +0.5);
+  syst.SetCurValue(kXSecTwkDial_MaCOHpi,      -0.5);
+  syst.SetCurValue(kINukeTwkDial_MFP_pi,      +1.0);
+  syst.SetCurValue(kINukeTwkDial_MFP_N,       -1.0);
+
+  rw.Reconfigure();
+
+  //
+  // Concrete weight calculators can be retrieved and fine-tuned.
+  // For example:
+
+  GReWeightNuXSecCCQE * rwccqe = 
+    dynamic_cast<GReWeightNuXSecCCQE *> (rw.WghtCalc("xsec_ccqe"));
+  rwccqe -> RewNue    (false); 
+  rwccqe -> RewNuebar (false); 
+  rwccqe -> RewNumubar(false); 
+
+  //
+  // Event loop
+  //
+
+  NtpMCEventRecord * mcrec = 0;
+  tree->SetBranchAddress("gmcrec", &mcrec);
+
+  for(int i = 0; i < nev; i++) {
+    tree->GetEntry(i);
+
+    EventRecord & event = *(mcrec->event);
+    LOG("main", pNOTICE) << event;
+
+    double wght = rw.CalcWeight(event);
+    LOG("main", pNOTICE) << "Overall weight = " << wght;
+
+    mcrec->Clear();
+  }
+
+  file.Close();
+
+  LOG("main", pNOTICE)  << "Done!";
+  return 0;
+}
+//___________________________________________________________________
+void GetCommandLineArgs(int argc, char ** argv)
+{
+  LOG("main", pINFO) << "*** Parsing command line arguments";
+
+  // get GENIE event sample
+  try {
+    LOG("main", pINFO) << "Reading event sample filename";
+    gOptInpFilename = utils::clap::CmdLineArgAsString(argc,argv,'f');
+  } catch(exceptions::CmdLineArgParserException e) {
+    if(!e.ArgumentFound()) {
+      LOG("main", pFATAL) 
+        << "Unspecified input filename - Exiting";
+      exit(1);
+    }
+  }
+
+  // number of events:
+  try {    
+    LOG("main", pINFO) << "Reading number of events to analyze";
+    gOptNEvt = genie::utils::clap::CmdLineArgAsInt(argc,argv,'n');
+  } catch(exceptions::CmdLineArgParserException e) {
+    if(!e.ArgumentFound()) {
+      LOG("main", pINFO)
+        << "Unspecified number of events to analyze - Use all";
+      gOptNEvt = -1;
+    }
+  }
+}
+//_________________________________________________________________________________
