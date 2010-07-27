f995a57ae79ccb4061ea059649765131e2150b0b
candreop <costas.andreopoulos@stfc.ac.uk>
2010-07-27 16:38:45 +0000

    added printout
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@3050 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/contrib/misc/test_units.C
+++ src/contrib/misc/test_units.C
@@ -14,11 +14,13 @@ void test_units(void)
   cout << "1 m in GeV^-1 = " << units::m  << endl;
   cout << "1 F in GeV^-1 = " << units::fm << endl;
 
-  double cm2m       = units::cm  / units::m;
-  double mev2gev    = units::MeV / units::GeV;
-  double to1E_38cm2 = 1. / (1E-38 * units::cm2);
+  const double cm2m       = units::cm  / units::m;
+  const double mev2gev    = units::MeV / units::GeV;
+  const double to1E_38cm2 = 1. / (1E-38 * units::cm2);
+  const double fm2tomb    = units::fm2 / units::mb;
 
   cout << "cm     -> m          ? => x" << cm2m       << endl;
   cout << "MeV    -> GeV        ? => x" << mev2gev    << endl;
   cout << "GeV^-2 -> 1E-38 cm^2 ? => x" << to1E_38cm2 << endl;
+  cout << "fm^2   -> mbarn      ? => x" << fm2tomb    << endl;
 }
