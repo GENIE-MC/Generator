0792427d1398ce9e30c532b8372f51993e2d3e95
rhatcher <rhatcher@fnal.gov>
2010-08-04 20:04:24 +0000

    Rework GeomVolSelectorFiducial to be more OO and less FORTRAN-like. Split out fiducial volume shape handling to separate inheritable class and use polymorphism to handle different shapes.  Add the ability to specify the shapes in master ROOT geometry coordinates and then tranform them into the necessary "top vol" coordinates.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@3105 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Geo/FidShape.cxx
@@ -0,0 +1,251 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2010, GENIE Neutrino MC Generator Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+ or see $GENIE/LICENSE
+
+ Author: Robert Hatcher <rhatcher@fnal.gov>
+
+ For the class documentation see the corresponding header file.
+
+*/
+//____________________________________________________________________________
+
+#include "Messenger/Messenger.h"
+#include "Geo/FidShape.h"
+#include "Geo/ROOTGeomAnalyzer.h"
+
+using namespace genie;
+using namespace genie::geometry;
+
+//___________________________________________________________________________
+void PlaneParam::ConvertMaster2Top(const ROOTGeomAnalyzer* rgeom)
+{
+  // convert a plane equation from master coordinates to "top vol" coordinates
+  TVector3 dir(a,b,c);
+  rgeom->Master2TopDir(dir);  // new a,b,c
+  TVector3 zero(0,0,0);
+  rgeom->Master2Top(zero);
+  a = dir.X();
+  b = dir.Y();
+  c = dir.Z();
+  d = d - ( a*zero.X() + b*zero.Y() + c*zero.Z() );
+
+}
+
+//___________________________________________________________________________
+RayIntercept FidSphere::Intercept(const TVector3& start, const TVector3& dir) const
+{
+  // A new neutrino ray has been set, calculate the entrance/exit distances.
+  // This sets fDistIn/fDistOut for an sphere
+  RayIntercept intercept;
+
+  TVector3 oc = fCenter - start;
+  Double_t loc2 = oc.Mag2();
+  Double_t r2 = fSRadius*fSRadius;
+  //LOG("GeomVolSel", pNOTICE) << " loc2 = " << loc2 << " r2 " << r2;
+  // if ( loc2 > r2 ) ray originates outside the sphere
+  const TVector3& d = dir;
+  Double_t d2 = d.Mag2();
+  Double_t tca = oc.Dot(d)/d2;
+  //if ( tca < 0.0 ) sphere _center_ behind the ray orgin
+  Double_t lhc2 = ( r2 -loc2 )/d2 + tca*tca;
+  if ( lhc2 < 0.0 ) return intercept; // ray misses the sphere
+  intercept.fIsHit = true;
+  Double_t lhc = TMath::Sqrt(lhc2);
+
+  intercept.fDistIn  = tca - lhc;
+  if ( intercept.fDistIn >= 0.0 ) intercept.fSurfIn  = 1;    // we hit
+  else                            intercept.fDistIn  = 0.0;  // started inside
+
+  intercept.fDistOut = tca + lhc;
+  intercept.fSurfOut = 1;
+
+  return intercept;
+}
+
+//___________________________________________________________________________
+void FidSphere::ConvertMaster2Top(const ROOTGeomAnalyzer* rgeom)
+{
+  rgeom->Master2Top(fCenter);
+}
+
+//___________________________________________________________________________
+RayIntercept FidCylinder::InterceptUncapped(const TVector3& start, const TVector3& dir) const
+{
+  // A new neutrino ray has been set, calculate the entrance/exit distances.
+  // This sets fDistIn/fDistOut for an infinite cylinder
+  // Take as "hit" if the ray is parallel to the axis but inside the radius
+  RayIntercept intercept;
+
+  TVector3 rc   = start - fCylBase;
+  TVector3 n    = dir.Cross(fCylAxis);
+  Double_t len  = n.Mag();
+  Double_t dist = 0;
+  if ( len == 0.0 ) {
+    // ray is parallel to axis
+    Double_t dist = rc.Dot(fCylAxis);
+    TVector3 d = rc - dist*fCylAxis;
+    dist = d.Mag();
+    //LOG("GeomVolSel", pNOTICE) << " len = " << len << " is parallel, dist " << dist << ", " << fCylRadius;
+    if ( dist <= fCylRadius ) {
+      intercept.fIsHit   = true;  // inside is considered a hit
+      intercept.fSurfIn  = 0;
+      intercept.fSurfOut = 0;
+      return intercept;
+    }
+  }
+  // ray is not parallel
+  if ( len != 1.0 ) n.SetMag(1.);  // normalize if it isn't already
+  dist = TMath::Abs(rc.Dot(n));    // closest approach distance
+  //LOG("GeomVolSel", pNOTICE) << " len = " << len << " not parallel, dist " << dist << ", " << fCylRadius;
+  if ( dist <= fCylRadius ) {
+    intercept.fIsHit = true; // yes, it hits
+    intercept.fSurfIn  = 0;
+    intercept.fSurfOut = 0;
+    TVector3 o = rc.Cross(fCylAxis);
+    Double_t t = - o.Dot(n)/len;
+    o = n.Cross(fCylAxis);
+    o.SetMag(1.);
+    Double_t s = TMath::Abs( TMath::Sqrt(fCylRadius*fCylRadius-dist*dist) /
+                             dir.Dot(o)    );
+    intercept.fDistIn  = t - s;
+    intercept.fDistOut = t + s;
+    //LOG("GeomVolSel", pNOTICE) << " hits, t = " << t << " s = " << s;
+  }
+  return intercept;
+}
+
+//___________________________________________________________________________
+RayIntercept FidCylinder::Intercept(const TVector3& start, const TVector3& dir) const
+{
+  // A new neutrino ray has been set, calculate the entrance/exit distances.
+
+  RayIntercept intercept = InterceptUncapped(start,dir);  // find where ray hits the infinite cylinder
+  // trim this down by applying the end caps
+  if ( intercept.fIsHit ) {
+    if ( fCylCap1.IsValid() ) {
+      Double_t vd1 = fCylCap1.Vd(dir);
+      Double_t vn1 = fCylCap1.Vn(start);
+      if ( vd1 != 0.0 ) {
+        Double_t t1 = -vn1 / vd1;
+        if ( vd1 < 0.0 && t1 > intercept.fDistIn  ) 
+          { intercept.fDistIn  = t1;  intercept.fSurfIn  = 1; }
+        if ( vd1 > 0.0 && t1 < intercept.fDistOut ) 
+          { intercept.fDistOut = t1;  intercept.fSurfOut = 1; }
+      }
+    }
+    if ( fCylCap2.IsValid() ) {
+      Double_t vd2 = fCylCap2.Vd(dir);
+      Double_t vn2 = fCylCap2.Vn(start);
+      if ( vd2 != 0.0 ) {
+        Double_t t2 = -vn2 / vd2;
+        if ( vd2 < 0.0 && t2 > intercept.fDistIn  ) 
+          { intercept.fDistIn  = t2;  intercept.fSurfIn  = 2; }
+        if ( vd2 > 0.0 && t2 < intercept.fDistOut ) 
+          { intercept.fDistOut = t2;  intercept.fSurfOut = 2; }
+      }
+    }
+  }
+  return intercept;
+}
+
+//___________________________________________________________________________
+void FidCylinder::ConvertMaster2Top(const ROOTGeomAnalyzer* rgeom)
+{
+  rgeom->Master2Top(fCylBase);
+  rgeom->Master2TopDir(fCylAxis);
+  fCylCap1.ConvertMaster2Top(rgeom);
+  fCylCap2.ConvertMaster2Top(rgeom);
+}
+
+//___________________________________________________________________________
+RayIntercept FidPolyhedron::Intercept(const TVector3& start, const TVector3& dir) const
+{
+  // A new neutrino ray has been set, calculate the entrance/exit distances.
+  // Calculate the point of intersection of a ray (directed line) and a
+  // *convex* polyhedron constructed from the intersection of a list
+  // of planar equations (no check on convex condition).
+
+  RayIntercept intercept;
+
+  Double_t tnear = -DBL_MAX;
+  Double_t tfar  =  DBL_MAX;
+  Int_t surfNear = -1;
+  Int_t surfFar  = -1;
+  Bool_t parallel = false;
+
+  // test each plane in the polyhedron
+  for ( size_t iface=0; iface < fPolyFaces.size(); ++iface ) {
+    const PlaneParam& pln = fPolyFaces[iface];
+    if ( ! pln.IsValid() ) continue;
+
+    // calculate numerator, denominator to "t" = distance along ray to intersection w/ pln
+    Double_t vd = pln.Vd(dir);
+    Double_t vn = pln.Vn(start);
+    
+    //LOG("GeomVolSel", pNOTICE)
+    //  << " face " << iface << " [" << pln.a << "," << pln.b << "," << pln.c << "," << pln.d
+    //  << "] vd=" << vd << " vn=" << vn;
+
+    if ( vd == 0.0 ) {
+      // ray is parallel to plane - check if ray origin is inside plane's half-space
+      //LOG("GeomVolSel", pNOTICE)
+      //  << " vd=0, " << " possibly parallel ";
+      if ( vn > 0.0 ) { parallel = true; break; }  // wrong side ... complete miss
+    } else {
+      // ray is not parallel to plane -- get the distance to the plane
+      Double_t t = -vn / vd; // notice negative sign!
+      //LOG("GeomVolSel", pNOTICE) << "   t=" << t << " tnear=" << tnear << " tfar=" << tfar;
+      if ( vd < 0.0 ) {
+        // front face: t is a near point
+        if ( t > tnear ) {
+          surfNear = iface;
+          tnear    = t;
+        }
+      } else {
+        // back face: t is a far point
+        if ( t < tfar ) {
+          surfFar = iface;
+          tfar    = t;
+        }
+      }
+      //LOG("GeomVolSel", pNOTICE) << "     new surf " <<  surfNear << "," << surfFar 
+      //                           << " tnear=" << tnear << " tfar=" << tfar;
+    }
+  }
+  if ( ! parallel ) {
+    // survived all the tests
+    if ( tnear > 0.0 ) {
+      if ( tnear < tfar ) {
+        //LOG("GeomVolSel", pNOTICE) << "is hit case1 ";
+        intercept.fIsHit   = true;
+        intercept.fSurfIn  = surfNear;
+        intercept.fSurfOut = surfFar;
+      }
+    } else {
+      if ( tfar > 0.0 ) {
+        //LOG("GeomVolSel", pNOTICE) << "is hit case2 ";
+        intercept.fIsHit   = true;
+        intercept.fSurfIn  = -1;
+        intercept.fSurfOut = surfFar;
+      }
+    }
+  }
+  intercept.fDistIn  = tnear;
+  intercept.fDistOut = tfar;
+  //LOG("GeomVolSel", pNOTICE) << " hit? " << (fIsHit?"true":"false") 
+  //                           << " dist in " << fDistIn << " out " << fDistOut;
+  return intercept;
+}
+
+//___________________________________________________________________________
+void FidPolyhedron::ConvertMaster2Top(const ROOTGeomAnalyzer* rgeom)
+{
+  for (unsigned int i = 0; i < fPolyFaces.size(); ++i ) {
+    PlaneParam& aplane = fPolyFaces[i];
+    aplane.ConvertMaster2Top(rgeom);
+  }
+}
+
+//___________________________________________________________________________
--- src/Geo/GeomVolSelectorFiducial.cxx
+++ src/Geo/GeomVolSelectorFiducial.cxx
@@ -13,7 +13,6 @@
 
 #include "Messenger/Messenger.h"
 #include "Geo/GeomVolSelectorFiducial.h"
-#include "Geo/PathSegmentList.h"
 #include "Utils/StringUtils.h"
 
 using namespace genie;
@@ -28,8 +27,8 @@ const Double_t gLocalInfNeg = -gLocalInfPos;
 
 //____________________________________________________________________________
 GeomVolSelectorFiducial::GeomVolSelectorFiducial() 
-  : GeomVolSelectorBasic(), fSelectReverse(false), fFidType(kUnknown),
-    fCurrPathSegmentList(0)
+  : GeomVolSelectorBasic(), fSelectReverse(false)
+  , fShape(0), fCurrPathSegmentList(0)
 {
   fName = "Fiducial";
 }
@@ -37,6 +36,9 @@ GeomVolSelectorFiducial::GeomVolSelectorFiducial()
 //___________________________________________________________________________
 GeomVolSelectorFiducial::~GeomVolSelectorFiducial()
 {
+  if ( fShape ) delete fShape;
+  fShape = 0;
+  fCurrPathSegmentList = 0;  // was reference only
 }
 
 //___________________________________________________________________________
@@ -45,7 +47,7 @@ void GeomVolSelectorFiducial::TrimSegment(PathSegment& ps) const
   // First trim the segment based on the ray vs. cylinder or box 
   // Then trim futher according to the Basic parameters
   
-  if ( ! fIsHit ) {
+  if ( ! fIntercept.fIsHit ) {
     // simple case when ray doesn't intersect the fiducial volume at all
     if ( fSelectReverse ) {
       // fiducial is reversed (ie. only want regions outside) 
@@ -68,18 +70,16 @@ void GeomVolSelectorFiducial::TrimSegment(PathSegment& ps) const
       Double_t slo = srs_itr->first;
       Double_t shi = srs_itr->second;
       Bool_t split = false;
-      Double_t slo_mod1, shi_mod1, slo_mod2, shi_mod2;
+      StepRange step1, step2;
       // determine new trimmed or split steps
-      ismod |= NewStepPairs(dist,slo,shi,split,
-                            slo_mod1,shi_mod1,slo_mod2,shi_mod2);
+      ismod |= NewStepPairs(fSelectReverse,dist,slo,shi,
+                            fIntercept,split,step1,step2);
       // build up new step list
-      bool nonzerostep = ( slo_mod1 != shi_mod1 );
+      bool nonzerostep = ( step1.first != step1.second );
       if ( nonzerostep || ! fRemoveEntries ) {
-        StepRange rng1(slo_mod1,shi_mod1);
-        modifiedStepRangeSet.push_back(rng1);
+        modifiedStepRangeSet.push_back(step1);
         if (split) {
-          StepRange rng2(slo_mod2,shi_mod2);
-          modifiedStepRangeSet.push_back(rng2);
+          modifiedStepRangeSet.push_back(step2);
         }
       }
     } // loop over step range set elements
@@ -87,17 +87,15 @@ void GeomVolSelectorFiducial::TrimSegment(PathSegment& ps) const
 
   } // fIsHit
 
-
-
   GeomVolSelectorBasic::TrimSegment(ps);
 }
 
 //___________________________________________________________________________
-Bool_t GeomVolSelectorFiducial::NewStepPairs(Double_t raydist, 
-                                             Double_t slo, Double_t shi, 
+Bool_t GeomVolSelectorFiducial::NewStepPairs(Bool_t selectReverse, Double_t raydist, 
+                                             Double_t slo, Double_t shi,
+                                             const RayIntercept& intercept, 
                                              Bool_t& split,
-                                             Double_t& slo_mod1, Double_t& shi_mod1,
-                                             Double_t& slo_mod2, Double_t& shi_mod2) const
+                                             StepRange& step1, StepRange& step2)
 {
   // modifying a step based on a range
   // there seem to be six possible cases:    
@@ -114,43 +112,42 @@ Bool_t GeomVolSelectorFiducial::NewStepPairs(Double_t raydist,
   //            #        #
   //
   bool ismodified = true;
-  slo_mod1 = slo;
-  shi_mod1 = shi;
+  step1 = StepRange(slo,shi);
   split = false;
   // dist in/out are relative to the ray origin
-  Double_t sdistin  = fDistIn  - raydist;
-  Double_t sdistout = fDistOut - raydist;
+  Double_t sdistin  = intercept.fDistIn  - raydist;
+  Double_t sdistout = intercept.fDistOut - raydist;
   // do remaining calculations relative to steps within this segment
   if ( slo < sdistin ) {
     if ( shi < sdistin ) {
       // case 0
-      if ( fSelectReverse ) { ismodified = false; }
-      else                  { slo_mod1 = 0; shi_mod1 = 0; }
+      if ( selectReverse ) { ismodified = false; }
+      else                 { step1 = StepRange(0,0); }
     } else if ( shi < sdistout ) {
       // case 1
-      if ( fSelectReverse ) { shi_mod1 = sdistin; }
-      else                  { slo_mod1 = sdistin; }
+      if ( selectReverse ) { step1.second = sdistin; }
+      else                 { step1.first  = sdistin; }
     } else {
       // case 2
-      if ( fSelectReverse ) { split = true;
-        slo_mod1 = slo;      shi_mod1 = sdistin;
-        slo_mod2 = sdistout; shi_mod2 = shi; }
-      else                  { slo_mod1 = sdistin; shi_mod1 = sdistout; }
+      if ( selectReverse ) { split = true;
+        step1 = StepRange(slo,sdistin); 
+        step2 = StepRange(sdistout,shi); }
+      else                 { step1 = StepRange(sdistin,sdistout); }
     }
   } else if ( slo < sdistout ) {
     if ( shi < sdistout ) {
       // case 3
-      if ( fSelectReverse ) { slo_mod1 = 0; shi_mod1 = 0; }
-      else                  { ismodified = false; }
+      if ( selectReverse ) { step1 = StepRange(0,0); }
+      else                 { ismodified = false; }
     } else {
       // case 4
-      if ( fSelectReverse ) { slo_mod1 = sdistout; }
-      else                  { shi_mod1 = sdistout; }
+      if ( selectReverse ) { step1.first  = sdistout; }
+      else                 { step1.second = sdistout; }
     }
   } else {
     // case 5
-    if ( fSelectReverse ) { ismodified = false; }
-    else                  { slo_mod1 = 0; shi_mod1 = 0; }
+    if ( selectReverse ) { ismodified = false; }
+    else                 { step1 = StepRange(0,0); }
   }
 
   return ismodified;
@@ -165,22 +162,12 @@ void GeomVolSelectorFiducial::BeginPSList(const PathSegmentList* untrimmed) cons
 
   fCurrPathSegmentList = untrimmed;
 
-  fIsHit   = false;
-  fDistIn  = gLocalInfNeg;
-  fDistOut = gLocalInfPos;
-  fSurfIn  = -1;
-  fSurfOut = -1;
-
-  switch ( fFidType ) {
-  case kSphere:      CalcRay2Sphere(); break;
-  case kZCylinder:   CalcRay2Cyl();    break;
-  case kCylinder:    CalcRay2Cyl();    break;
-  case kBox:         CalcRay2Poly();   break;
-  case kConvexPoly:  CalcRay2Poly();   break;
-  default:
-    // do nothing
-    LOG("GeomVolSel", pFATAL) << "no support for FidType=" << fFidType;
-    break;
+  if ( ! fShape ) {
+    LOG("GeomVolSel", pFATAL) << "no shape defined";
+    fIntercept = RayIntercept();
+  } else {
+    fIntercept = fShape->Intercept(fCurrPathSegmentList->GetStartPos(),
+                                   fCurrPathSegmentList->GetDirection());
   }
 
 }
@@ -194,63 +181,64 @@ void GeomVolSelectorFiducial::EndPSList() const
 }
 
 //___________________________________________________________________________
+void GeomVolSelectorFiducial::AdoptFidShape(FidShape* shape)
+{
+  if ( fShape ) delete fShape;
+  fShape = shape;
+}
+//___________________________________________________________________________
+void GeomVolSelectorFiducial::ConvertShapeMaster2Top(const ROOTGeomAnalyzer* rgeom)
+{
+  if ( fShape ) fShape->ConvertMaster2Top(rgeom);
+}
+//___________________________________________________________________________
 void GeomVolSelectorFiducial::SetSphere(Double_t x0, Double_t y0, Double_t z0, Double_t radius)
 {
-  fCenter  = TVector3(x0,y0,z0);
-  fSRadius = radius;
-  fFidType = kSphere;
+  AdoptFidShape(new FidSphere(TVector3(x0,y0,z0),radius));
 }
 
 //___________________________________________________________________________
 void GeomVolSelectorFiducial::SetZCylinder(Double_t x0, Double_t y0, Double_t radius, 
                                            Double_t zmin, Double_t zmax)
 {
-  // A new neutrino ray has been set, calculate the entrance/exit distances.
   // This sets parameters for a cylinder parallel to the z-axis
   Double_t base[] = { x0, y0,  0 };
   Double_t axis[] = {  0,  0,  1 };
   Double_t cap1[] = {  0,  0, -1,  zmin };
   Double_t cap2[] = {  0,  0, +1, -zmax };  // note sign change
   this->SetCylinder(base,axis,radius,cap1,cap2);
-
-  fFidType = kZCylinder;
 }
 
 //___________________________________________________________________________
 void GeomVolSelectorFiducial::SetCylinder(Double_t* base, Double_t* axis, Double_t radius,
                                           Double_t* cap1, Double_t* cap2)
 {
-  // A new neutrino ray has been set, calculate the entrance/exit distances.
   // This sets parameters for a cylinder
-  fFidType = kCylinder;
-  fCylBase = TVector3(base);
-  fCylAxis = TVector3(axis);
-  fCylRadius = radius;
-  fCylCap1 = PlaneParam(cap1);
-  fCylCap2 = PlaneParam(cap2);
+  AdoptFidShape(new FidCylinder(TVector3(base),TVector3(axis),radius,
+                                PlaneParam(cap1),PlaneParam(cap2)));
 }
 
 //___________________________________________________________________________
 void GeomVolSelectorFiducial::SetBox(Double_t* xyzmin, Double_t* xyzmax)
 {
-  // A new neutrino ray has been set, calculate the entrance/exit distances.
   // This sets parameters for a box
 
+  double boxXYZMin[3], boxXYZMax[3];
   for ( int j = 0; j < 3; ++j ) {
-    fBoxXYZMin[j] = TMath::Min(xyzmin[j],xyzmax[j]);
-    fBoxXYZMax[j] = TMath::Max(xyzmax[j],xyzmax[j]);
+    boxXYZMin[j] = TMath::Min(xyzmin[j],xyzmax[j]);
+    boxXYZMax[j] = TMath::Max(xyzmax[j],xyzmax[j]);
   }
 
-  fPolyFaces.clear();
+  FidPolyhedron* poly = new FidPolyhedron();
   // careful about sign of "d" vs. direction normal
-  PlaneParam pln0(-1,0,0, xyzmin[0]);  fPolyFaces.push_back(pln0);
-  PlaneParam pln1(0,-1,0, xyzmin[1]);  fPolyFaces.push_back(pln1);
-  PlaneParam pln2(0,0,-1, xyzmin[2]);  fPolyFaces.push_back(pln2);
-  PlaneParam pln3(+1,0,0,-xyzmax[0]);  fPolyFaces.push_back(pln3);
-  PlaneParam pln4(0,+1,0,-xyzmax[1]);  fPolyFaces.push_back(pln4);
-  PlaneParam pln5(0,0,+1,-xyzmax[2]);  fPolyFaces.push_back(pln5);
+  PlaneParam pln0(-1,0,0, xyzmin[0]);  poly->push_back(pln0);
+  PlaneParam pln1(0,-1,0, xyzmin[1]);  poly->push_back(pln1);
+  PlaneParam pln2(0,0,-1, xyzmin[2]);  poly->push_back(pln2);
+  PlaneParam pln3(+1,0,0,-xyzmax[0]);  poly->push_back(pln3);
+  PlaneParam pln4(0,+1,0,-xyzmax[1]);  poly->push_back(pln4);
+  PlaneParam pln5(0,0,+1,-xyzmax[2]);  poly->push_back(pln5);
 
-  fFidType = kBox;
+  AdoptFidShape(poly);
 }
 
 //___________________________________________________________________________
@@ -259,195 +247,20 @@ void GeomVolSelectorFiducial::SetZPolygon(Int_t n, Double_t x0, Double_t y0, Dou
 {
   // phi=0 will put flat face towards +x
   // inscribed radius
-  fPolyFaces.clear();
+
+  FidPolyhedron* poly = new FidPolyhedron();
 
   Double_t dphir = TMath::TwoPi()/n;
   Double_t phi0r = phi0deg * TMath::DegToRad();
   for ( int iface = 0; iface < n; ++iface ) {
     Double_t dx = TMath::Cos(dphir*iface + phi0r);
     Double_t dy = TMath::Sin(dphir*iface + phi0r);
-    PlaneParam face(dx,dy,0,-inradius-dx*x0-dy*y0); fPolyFaces.push_back(face);
+    PlaneParam face(dx,dy,0,-inradius-dx*x0-dy*y0); poly->push_back(face);
   }
-  PlaneParam plnz1(0,0,-1, zmin);  fPolyFaces.push_back(plnz1);
-  PlaneParam plnz2(0,0,+1,-zmax);  fPolyFaces.push_back(plnz2);
+  PlaneParam plnz1(0,0,-1, zmin);  poly->push_back(plnz1);
+  PlaneParam plnz2(0,0,+1,-zmax);  poly->push_back(plnz2);
 
-  fFidType = kConvexPoly;
-}
-
-//___________________________________________________________________________
-void GeomVolSelectorFiducial::CalcRay2Sphere() const
-{
-  // A new neutrino ray has been set, calculate the entrance/exit distances.
-  // This sets fDistIn/fDistOut for an sphere
-  TVector3 oc = fCenter - fCurrPathSegmentList->GetStartPos();
-  Double_t loc2 = oc.Mag2();
-  Double_t r2 = fSRadius*fSRadius;
-  //LOG("GeomVolSel", pNOTICE) << " loc2 = " << loc2 << " r2 " << r2;
-  // if ( loc2 > r2 ) ray originates outside the sphere
-  const TVector3& d = fCurrPathSegmentList->GetDirection();
-  Double_t d2 = d.Mag2();
-  Double_t tca = oc.Dot(d)/d2;
-  //if ( tca < 0.0 ) sphere _center_ behind the ray orgin
-  Double_t lhc2 = ( r2 -loc2 )/d2 + tca*tca;
-  if ( lhc2 < 0.0 ) return; // ray misses the sphere
-  fIsHit = true;
-  Double_t lhc = TMath::Sqrt(lhc2);
-  fDistIn  = tca - lhc;
-  if ( fDistIn >= 0.0 ) fSurfIn  = 1;    // we hit
-  else                  fDistIn  = 0.0;  // started inside
-  fDistOut = tca + lhc;
-  fSurfOut = 1;
-}
-
-//___________________________________________________________________________
-void GeomVolSelectorFiducial::CalcRay2CylUncapped() const
-{
-  // A new neutrino ray has been set, calculate the entrance/exit distances.
-  // This sets fDistIn/fDistOut for an infinite cylinder
-  // Take as "hit" if the ray is parallel to the axis but inside the radius
-
-  TVector3 rc   = fCurrPathSegmentList->GetStartPos() - fCylBase;
-  TVector3 n    = fCurrPathSegmentList->GetDirection().Cross(fCylAxis);
-  Double_t len  = n.Mag();
-  Double_t dist = 0;
-  if ( len == 0.0 ) {
-    // ray is parallel to axis
-    Double_t dist = rc.Dot(fCylAxis);
-    TVector3 d = rc - dist*fCylAxis;
-    dist = d.Mag();
-    //LOG("GeomVolSel", pNOTICE) << " len = " << len << " is parallel, dist " << dist << ", " << fCylRadius;
-    if ( dist <= fCylRadius ) {
-      fIsHit   = true;  // inside is considered a hit
-      fSurfIn  = 0;
-      fSurfOut = 0;
-      return;
-    }
-  }
-  // ray is not parallel
-  if ( len != 1.0 ) n.SetMag(1.);  // normalize if it isn't already
-  dist = TMath::Abs(rc.Dot(n));    // closest approach distance
-  //LOG("GeomVolSel", pNOTICE) << " len = " << len << " not parallel, dist " << dist << ", " << fCylRadius;
-  if ( dist <= fCylRadius ) {
-    fIsHit = true; // yes, it hits
-    fSurfIn  = 0;
-    fSurfOut = 0;
-    TVector3 o = rc.Cross(fCylAxis);
-    Double_t t = - o.Dot(n)/len;
-    o = n.Cross(fCylAxis);
-    o.SetMag(1.);
-    Double_t s = TMath::Abs( TMath::Sqrt(fCylRadius*fCylRadius-dist*dist) /
-                             fCurrPathSegmentList->GetDirection().Dot(o)    );
-    fDistIn  = t - s;
-    fDistOut = t + s;
-    //LOG("GeomVolSel", pNOTICE) << " hits, t = " << t << " s = " << s;
-  }
-}
-
-//___________________________________________________________________________
-void GeomVolSelectorFiducial::CalcRay2Cyl() const
-{
-  // A new neutrino ray has been set, calculate the entrance/exit distances.
-
-  CalcRay2CylUncapped();  // find where ray hits the infinite cylinder
-  // trim this down by applying the end caps
-  if ( fIsHit ) {
-    if ( fCylCap1.IsValid() ) {
-      Double_t vd1 = fCylCap1.Vd(fCurrPathSegmentList->GetDirection());
-      Double_t vn1 = fCylCap1.Vn(fCurrPathSegmentList->GetStartPos());
-      if ( vd1 != 0.0 ) {
-        Double_t t1 = -vn1 / vd1;
-        if ( vd1 < 0.0 && t1 > fDistIn  ) { fDistIn  = t1;  fSurfIn  = 1; }
-        if ( vd1 > 0.0 && t1 < fDistOut ) { fDistOut = t1;  fSurfOut = 1; }
-      }
-    }
-    if ( fCylCap2.IsValid() ) {
-      Double_t vd2 = fCylCap2.Vd(fCurrPathSegmentList->GetDirection());
-      Double_t vn2 = fCylCap2.Vn(fCurrPathSegmentList->GetStartPos());
-      if ( vd2 != 0.0 ) {
-        Double_t t2 = -vn2 / vd2;
-        if ( vd2 < 0.0 && t2 > fDistIn  ) { fDistIn  = t2;  fSurfIn  = 2; }
-        if ( vd2 > 0.0 && t2 < fDistOut ) { fDistOut = t2;  fSurfOut = 2; }
-      }
-    }
-  }
-
-}
-
-//___________________________________________________________________________
-void GeomVolSelectorFiducial::CalcRay2Poly() const
-{
-  // A new neutrino ray has been set, calculate the entrance/exit distances.
-  // Calculate the point of intersection of a ray (directed line) and a
-  // *convex* polyhedron constructed from the intersection of a list
-  // of planar equations (no check on convex condition).
-
-  Double_t tnear = gLocalInfNeg;
-  Double_t tfar  = gLocalInfPos;
-  Int_t surfNear = -1;
-  Int_t surfFar  = -1;
-  Bool_t parallel = false;
-
-  // test each plane in the polyhedron
-  for ( size_t iface=0; iface < fPolyFaces.size(); ++iface ) {
-    const PlaneParam& pln = fPolyFaces[iface];
-    if ( ! pln.IsValid() ) continue;
-
-    // calculate numerator, denominator to "t" = distance along ray to intersection w/ pln
-    Double_t vd = pln.Vd(fCurrPathSegmentList->GetDirection());
-    Double_t vn = pln.Vn(fCurrPathSegmentList->GetStartPos());
-    
-    //LOG("GeomVolSel", pNOTICE)
-    //  << " face " << iface << " [" << pln.a << "," << pln.b << "," << pln.c << "," << pln.d
-    //  << "] vd=" << vd << " vn=" << vn;
-
-    if ( vd == 0.0 ) {
-      // ray is parallel to plane - check if ray origin is inside plane's half-space
-      //LOG("GeomVolSel", pNOTICE)
-      //  << " vd=0, " << " possibly parallel ";
-      if ( vn > 0.0 ) { parallel = true; break; }  // wrong side ... complete miss
-    } else {
-      // ray is not parallel to plane -- get the distance to the plane
-      Double_t t = -vn / vd; // notice negative sign!
-      //LOG("GeomVolSel", pNOTICE) << "   t=" << t << " tnear=" << tnear << " tfar=" << tfar;
-      if ( vd < 0.0 ) {
-        // front face: t is a near point
-        if ( t > tnear ) {
-          surfNear = iface;
-          tnear    = t;
-        }
-      } else {
-        // back face: t is a far point
-        if ( t < tfar ) {
-          surfFar = iface;
-          tfar    = t;
-        }
-      }
-      //LOG("GeomVolSel", pNOTICE) << "     new surf " <<  surfNear << "," << surfFar 
-      //                           << " tnear=" << tnear << " tfar=" << tfar;
-    }
-  }
-  if ( ! parallel ) {
-    // survived all the tests
-    if ( tnear > 0.0 ) {
-      if ( tnear < tfar ) {
-        //LOG("GeomVolSel", pNOTICE) << "is hit case1 ";
-        fIsHit   = true;
-        fSurfIn  = surfNear;
-        fSurfOut = surfFar;
-      }
-    } else {
-      if ( tfar > 0.0 ) {
-        //LOG("GeomVolSel", pNOTICE) << "is hit case2 ";
-        fIsHit   = true;
-        fSurfIn  = -1;
-        fSurfOut = surfFar;
-      }
-    }
-  }
-  fDistIn  = tnear;
-  fDistOut = tfar;
-  //LOG("GeomVolSel", pNOTICE) << " hit? " << (fIsHit?"true":"false") 
-  //                           << " dist in " << fDistIn << " out " << fDistOut;
+  AdoptFidShape(poly);
 }
 
 //___________________________________________________________________________
