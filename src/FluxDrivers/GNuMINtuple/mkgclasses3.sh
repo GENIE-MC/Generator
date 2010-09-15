34767e10b2f988a7d3f8105dc37e28d251c8a4e6
rhatcher <rhatcher@fnal.gov>
2010-09-15 18:00:36 +0000

    script used to generate *.h/*.C files via MakeClass for different beam flux file formats: gnumi (geant3), g4numi, flugg.  Keep around in case new formats appear or layout changes.
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@3152 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/FluxDrivers/GNuMINtuple/mkgclasses3.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+do_mkclass="yes"
+
+for fbase in g3numi g4numi flugg
+do
+  tname="xyzzy"
+  if [ "$fbase" = "g3numi" ] ; then tname="h10" ; fi
+  if [ "$fbase" = "flugg"  ] ; then tname="h10" ; fi
+  if [ "$fbase" = "g4numi" ] ; then tname="nudata" ; fi
+
+  if [ "yes" = "${do_mkclass}" ] ; then
+    cat > make_generic.C <<EOF
+void make_generic()
+{
+  TFile* g3file = TFile::Open("generic_${fbase}.root","READONLY");
+  gROOT->ProcessLine("${tname}->MakeClass();");
+}
+EOF
+    root -b -q make_generic.C
+    rm -f make_generic.C
+  fi
+  
+  fnameh=${fbase}.h
+  fnameC=${fbase}.C
+  rm -f ${fnameh} ${fnameC}
+  touch ${fnameh} ${fnameC}
+
+  cat ${tname}.h | sed -e "s/${tname}/${fbase}/g" >> $fnameh
+  cat ${tname}.C | sed -e "s/${tname}/${fbase}/g" >> $fnameC
+
+done
+
+
+# End-of-Script
