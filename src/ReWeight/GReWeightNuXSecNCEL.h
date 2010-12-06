e3a8bf5557d6b729ff63bd141c404766defc5bbf
candreop <costas.andreopoulos@stfc.ac.uk>
2010-12-06 22:01:17 +0000

    code to reweight NCEL, from trunk
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/branches/R-2_6_2@3180 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/ReWeight/GReWeightNuXSecNCEL.h
@@ -0,0 +1,91 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::rew::GReWeightNuXSecNCEL
+
+\brief    Reweighting NCEL GENIE neutrino cross sections
+
+\author   Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+          STFC, Rutherford Appleton Laboratory
+
+\created  Nov 25, 2010
+
+\cpright  Copyright (c) 2003-2010, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _G_REWEIGHT_NU_XSEC_NCEL_H_
+#define _G_REWEIGHT_NU_XSEC_NCEL_H_
+
+#include <map>
+#include <string>
+
+#include "ReWeight/GReWeightI.h"
+
+using std::map;
+using std::string;
+
+class TFile;
+class TNtupleD;
+
+namespace genie {
+
+class XSecAlgorithmI;
+class Registry;
+
+namespace rew   {
+
+ class GReWeightNuXSecNCEL : public GReWeightI 
+ {
+ public:
+   GReWeightNuXSecNCEL();
+  ~GReWeightNuXSecNCEL();
+
+   // implement the GReWeightI interface
+   bool   IsHandled      (GSyst_t syst);
+   void   SetSystematic  (GSyst_t syst, double val);
+   void   Reset          (void);
+   void   Reconfigure    (void);
+   double CalcWeight     (const EventRecord & event);
+   double CalcChisq      (void);
+
+   // various config options
+   void RewNue      (bool tf ) { fRewNue     = tf;   }
+   void RewNuebar   (bool tf ) { fRewNuebar  = tf;   }
+   void RewNumu     (bool tf ) { fRewNumu    = tf;   }
+   void RewNumubar  (bool tf ) { fRewNumubar = tf;   }
+   void SetMaPath   (string p) { fMaPath     = p;    }
+   void SetEtaPath  (string p) { fEtaPath    = p;    }
+
+ private:
+
+   void Init(void);
+
+   XSecAlgorithmI * fXSecModelDef;    ///< default model
+   XSecAlgorithmI * fXSecModel;       ///< tweaked model
+   Registry *       fXSecModelConfig; ///< config in tweaked model
+
+   bool   fRewNue;       ///< reweight nu_e CC?
+   bool   fRewNuebar;    ///< reweight nu_e_bar CC?
+   bool   fRewNumu;      ///< reweight nu_mu CC?
+   bool   fRewNumubar;   ///< reweight nu_mu_bar CC?
+   string fMaPath;       ///< M_{A} path in config Registry
+   string fEtaPath;      ///< eta path in config Registry
+   double fMaTwkDial;    ///<
+   double fMaDef;        ///<
+   double fMaCurr;       ///<
+   double fEtaTwkDial;    ///<
+   double fEtaDef;        ///<
+   double fEtaCurr;       ///<
+
+   TFile *    fTestFile;
+   TNtupleD * fTestNtp;
+ };
+
+} // rew   namespace
+} // genie namespace
+
+#endif
+
