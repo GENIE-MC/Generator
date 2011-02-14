b4394c3684eff553a46a62f07e9a3842e94f3548
rhatcher <rhatcher@fnal.gov>
2011-02-14 18:34:02 +0000

    Code to allow neutrino flavor modification.  The GFluxBlender class is an adapter that conforms to the GFluxI interface and is initialized with a real flux generator, but uses a concrete GFlavorMixerI object to report a different neutrino flavor.  GFlavorMap is an example GFlavorMixerI class that can map from one flavor to another based on the user configuration string (it can also split flavors based on fixed proportions).  This is useful when dealing with existing large sets of flux files where creating a copy with just the flavors modified would be impractical.  A user could also create a GFlavorMixerI to do oscillations/decay/decoherence for generating challenge sets (w/ caveats about changes of flavor over the scale of the detector, i.e. this won't work for short baseline experiments).
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@3208 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/FluxDrivers/GFlavorMixerI.cxx
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////
+/// \file  GFlavorMixerI.cxx
+/// \brief GENIE interface for flavor modification
+///
+/// \version $Id: GFlavorMixerI.cxx,v 1.1.1.1 2010/12/22 16:18:52 p-nusoftart Exp $
+/// \author  Robert Hatcher <rhatcher \at fnal.gov>
+///          Fermi National Accelerator Laboratory
+///
+/// \update  2010-10-31 initial version
+////////////////////////////////////////////////////////////////////////
+
+#include "FluxDrivers/GFlavorMixerI.h"
+
+namespace genie {
+namespace flux {
+
+  GFlavorMixerI::GFlavorMixerI() { ; }
+  GFlavorMixerI::~GFlavorMixerI() { ; }
+
+} // namespace flux
+} // namespace genie
