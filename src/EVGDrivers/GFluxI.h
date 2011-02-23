d21825c598da32ae5ff3b753a7b0f48cc66e551c
jdobson <jdobson>
2011-02-23 17:29:49 +0000

    Modify GFluxI interface by adding Index, Clear and GenerateWeighted methods. These methods are needed so that any flux driver inheriting from GFluxI can be used by new flux interaction probability optimisation code in GMCJDriver.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@3213 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/EVGDrivers/GFluxI.h
+++ src/EVGDrivers/GFluxI.h
@@ -13,11 +13,20 @@
 \cpright  Copyright (c) 2003-2010, GENIE Neutrino MC Generator Collaboration
           For the full text of the license visit http://copyright.genie-mc.org
           or see $GENIE/LICENSE
+
+ @ Feb 22, 2011 - JD
+   Changed the interface by adding the GFluxI::Clear, GFluxI::Index and 
+   GFluxI::GenerateWeighted methods needed so that can be used with the new 
+   pre-generation of flux interaction probabilities functionality added to
+   GMCJDriver. 
+
 */
 //____________________________________________________________________________
 
 #ifndef _G_FLUX_I_H_
 #define _G_FLUX_I_H_
+
+#include <TObject.h>
 
 class TLorentzVector;
 
@@ -41,6 +50,9 @@ public :
   virtual const TLorentzVector & Momentum      (void) = 0; ///< returns the flux neutrino 4-momentum 
   virtual const TLorentzVector & Position      (void) = 0; ///< returns the flux neutrino 4-position (note: expect SI rather than physical units)
   virtual bool                   End           (void) = 0; ///< true if no more flux nu's can be thrown (eg reaching end of beam sim ntuples)
+  virtual long int               Index         (void) = 0; ///< returns corresponding index for current flux neutrino (e.g. for a flux ntuple returns the current entry number)
+  virtual void                   Clear            (Option_t * opt   ) = 0; ///< reset state variables based on opt
+  virtual void                   GenerateWeighted (bool gen_weighted) = 0; ///< set whether to generate weighted or unweighted neutrinos
 
 protected:
   GFluxI();
