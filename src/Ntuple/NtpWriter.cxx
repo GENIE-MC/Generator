91b1a2b3922d021ef1edff5955bc040736666ca5
candreop <costas.andreopoulos@stfc.ac.uk>
2011-03-01 18:23:19 +0000

    Allow better output filename customization.
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/branches/R-2_6_2@3238 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Ntuple/NtpWriter.cxx
+++ src/Ntuple/NtpWriter.cxx
@@ -5,7 +5,7 @@
  or see $GENIE/LICENSE
 
  Author: Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
-         STFC, Rutherford Appleton Laboratory - October 01, 2004
+         STFC, Rutherford Appleton Laboratory 
 
  For the class documentation see the corresponding header file.
 
@@ -14,6 +14,11 @@
    Made event branch a priv data member and modified code accordingly.
    Added method to hand over the event tree so that additional user-defined
    branches (eg flux info) may be added.
+ @ Mar 26, 2010 - CA
+   Added CustomizeFilename() and CustomizeFilenamePrefix() to allow the use
+   to customize either the entire output name or just the prefix before the
+   run number.
+
 */
 //____________________________________________________________________________
 
@@ -50,6 +55,8 @@ fNtpMCTreeHeader(0)
   LOG("Ntp", pNOTICE) << "Run number: " << runnu;
   LOG("Ntp", pNOTICE)
     << "Requested G/ROOT tree format: " << NtpMCFormat::AsString(fNtpFormat);
+
+  this->SetDefaultFilename();
 }
 //____________________________________________________________________________
 NtpWriter::~NtpWriter()
@@ -83,12 +90,12 @@ void NtpWriter::AddEventRecord(int ievent, const EventRecord * ev_rec)
   }
 }
 //____________________________________________________________________________
-void NtpWriter::Initialize(string filename_prefix)
+void NtpWriter::Initialize()
 {
   LOG("Ntp",pINFO) << "Initializing GENIE output MC tree";
 
-  this->OpenFile(filename_prefix); // open ROOT file
-  this->CreateTree();              // create output tree
+  this->OpenFile(fOutFilename); // open ROOT file
+  this->CreateTree();           // create output tree
 
   //-- create the event branch
   this->CreateEventBranch(); 
@@ -106,19 +113,35 @@ void NtpWriter::Initialize(string filename_prefix)
   environment.TakeSnapshot()->Write();
 }
 //____________________________________________________________________________
-void NtpWriter::OpenFile(string filename_prefix)
+void NtpWriter::CustomizeFilename(string filename)
+{
+ fOutFilename = filename;
+}
+//____________________________________________________________________________
+void NtpWriter::CustomizeFilenamePrefix (string prefix)
+{
+  this->SetDefaultFilename(prefix);
+}
+//____________________________________________________________________________
+void NtpWriter::SetDefaultFilename(string filename_prefix)
+{
+  ostringstream fnstr;
+  fnstr << filename_prefix  << "." 
+        << fRunNu << "."
+        << NtpMCFormat::FilenameTag(fNtpFormat)
+        << ".root";
+
+  fOutFilename = fnstr.str();
+}
+//____________________________________________________________________________
+void NtpWriter::OpenFile(string filename)
 {
   if(fOutFile) delete fOutFile;
 
-  // modify the filename to add the run number & the ntuple format
-  ostringstream filename;
-  filename << filename_prefix  << "." 
-           << fRunNu << "."
-           << NtpMCFormat::FilenameTag(fNtpFormat)
-           << ".root";
+  LOG("Ntp", pINFO) 
+      << "Opening the output ROOT file: " << filename;
 
-  LOG("Ntp", pINFO) << "Opening the output ROOT file: " << filename;
-  fOutFile = new TFile(filename.str().c_str(),"RECREATE");
+  fOutFile = new TFile(filename.c_str(),"RECREATE");
 }
 //____________________________________________________________________________
 void NtpWriter::CreateTree(void)
