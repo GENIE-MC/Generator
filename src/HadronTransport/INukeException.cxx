03f9af7e5b7f93339960e0aebe5775975e7b5f49
dytman <dytman@pitt.edu>
2011-10-10 13:42:20 +0000

    new try/catch
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@3345 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/HadronTransport/INukeException.cxx
@@ -0,0 +1,63 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2011, GENIE Neutrino MC Generator Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+ or see $GENIE/LICENSE
+
+ Author: Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         STFC, Rutherford Appleton Laboratory - September 27, 2005
+
+ For the class documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+
+*/
+//____________________________________________________________________________
+
+#include "HadronTransport/INukeException.h"
+#include "Messenger/Messenger.h"
+
+using std::endl;
+using namespace genie::exceptions;
+
+//___________________________________________________________________________
+namespace genie {
+ namespace exceptions {
+  ostream & operator<< (ostream& stream, const INukeException & exc)
+  {
+   exc.Print(stream);
+   return stream;
+  }
+ }
+}
+//___________________________________________________________________________
+INukeException::INukeException()
+{
+  this->Init();
+}
+//___________________________________________________________________________
+INukeException::INukeException(const INukeException & exc)
+{
+  this->Copy(exc);
+}
+//___________________________________________________________________________
+INukeException::~INukeException()
+{
+
+}
+//___________________________________________________________________________
+void INukeException::Init(void)
+{
+  fReason     = "";
+}
+//___________________________________________________________________________
+void INukeException::Copy(const INukeException & exc)
+{
+  fReason     = exc.fReason;
+}
+//___________________________________________________________________________
+void INukeException::Print(ostream & stream) const
+{
+  stream << "**EXCEPTION Reason: " << this->ShowReason() << endl;
+}
+//___________________________________________________________________________
