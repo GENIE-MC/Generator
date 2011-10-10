dc7e6dcd94b051207590c7d19f99fb4a2c6f5e1d
dytman <dytman@pitt.edu>
2011-10-10 13:42:11 +0000

    new try/catch
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@3344 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/HadronTransport/INukeException.h
@@ -0,0 +1,64 @@
+//____________________________________________________________________________
+/*!
+
+\class   genie::exceptions::INukeException
+
+\brief   An exception thrown by SimulateHadronState for kinematics problems
+
+\author  Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         STFC, Rutherford Appleton Laboratory
+         Steve Dytman
+         
+
+\created September 27, 2005
+
+\cpright Copyright (c) 2003-2011, GENIE Neutrino MC Generator Collaboration
+         For the full text of the license visit http://copyright.genie-mc.org
+         or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _INUKE_EXCEPTION_H_
+#define _INUKE_EXCEPTION_H_
+
+#include <string>
+#include <ostream>
+
+#include <TMath.h>
+
+using std::string;
+using std::ostream;
+
+namespace genie {
+namespace exceptions {
+
+class Interaction;
+
+class INukeException {
+
+public :
+
+  INukeException();
+  INukeException(const INukeException & exception);
+  ~INukeException();
+
+  void   SetReason  (string reason) { fReason     = reason;          }
+
+  string ShowReason  (void) const { return fReason;     }
+
+  void Init  (void);
+  void Copy  (const INukeException & exception);
+  void Print (ostream & stream) const;
+
+  friend ostream & operator << (
+             ostream & stream, const INukeException & exception);
+
+private:
+
+  string fReason;
+};
+
+}      // exceptions namespace
+}      // genie namespace
+
+#endif // _INUKE_EXCEPTION_H_
