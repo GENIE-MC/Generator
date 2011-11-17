943065a2e8c7e7f8e2051eacb398790db7b09b21
candreop <costas.andreopoulos@stfc.ac.uk>
2011-11-17 15:30:48 +0000

    Add enumeration of event generation modes (lepton-nucleus, hadron-nucleus, nucleon decay etc)
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@3374 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Conventions/GMode.h
@@ -0,0 +1,35 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::GEvGenMode_t
+
+\brief    Enumeration of GENIE event generation modes
+
+\author   Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+          STFC, Rutherford Appleton Laboratory
+
+\created  Nov 10, 2011
+
+\cpright  Copyright (c) 2003-2011, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _GENIE_MODE_H_
+#define _GENIE_MODE_H_
+
+namespace genie {
+
+typedef enum EGEvGenMode {
+
+  kGMdUnknown = 0,
+  kGMdLeptonNucleus,   // chg.lepton/neutrino + nucleon/nucleus scattering
+  kGMdHadronNucleus,   // hadron + nucleon/nucleus scattering
+  kGMdNucleonDecay     // nucleon decay
+
+} GEvGenMode_t;
+
+} // genie namespace
+
+#endif 
