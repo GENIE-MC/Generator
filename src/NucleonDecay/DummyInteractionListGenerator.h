46e1b5fd108728814e96560ce4c9057a1efd175b
candreop <costas.andreopoulos@stfc.ac.uk>
2011-11-17 18:31:31 +0000

    New nucleon decay package
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@3384 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/NucleonDecay/DummyInteractionListGenerator.h
@@ -0,0 +1,38 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::DummyInteractionListGenerator
+
+\brief    
+
+\author   Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+          STFC, Rutherford Appleton Laboratory
+
+\created  November 10, 2011
+
+\cpright  Copyright (c) 2003-2011, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _DUMMY_INTERACTION_GENERATOR_H_
+#define _DUMMY_INTERACTION_GENERATOR_H_
+
+#include "EVGCore/InteractionListGeneratorI.h"
+
+namespace genie {
+
+class DummyInteractionListGenerator : public InteractionListGeneratorI {
+
+public :
+  DummyInteractionListGenerator();
+  DummyInteractionListGenerator(string config);
+ ~DummyInteractionListGenerator();
+
+  // implement the InteractionListGeneratorI interface
+  InteractionList * CreateInteractionList(const InitialState & init) const;
+};
+
+}      // genie namespace
+#endif // _DUMMY_INTERACTION_GENERATOR_H_
