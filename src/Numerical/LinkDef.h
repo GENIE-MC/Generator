43d3b32dc0260b9f6a7a8a8e2dc90434388e7311
candreop <costas.andreopoulos@stfc.ac.uk>
2013-01-24 16:42:28 +0000

    Remove a few unused / not-implemented numerical code classes. Soon will use GSL for all numerical stuff.
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@3723 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Numerical/LinkDef.h
+++ src/Numerical/LinkDef.h
@@ -14,14 +14,11 @@
 #pragma link C++ class genie::GSFunc;
 #pragma link C++ class genie::GVFunc;
 #pragma link C++ class genie::FunctionMap;
-#pragma link C++ class genie::MinimizerI;
 #pragma link C++ class genie::IntegratorI;
 #pragma link C++ class genie::Simpson1D;
 #pragma link C++ class genie::Simpson2D;
 #pragma link C++ class genie::Simpson2DFixN;
 #pragma link C++ class genie::SimMCIntegrator;
-#pragma link C++ class genie::VegasMCIntegrator;
-#pragma link C++ class genie::MiserMCIntegrator;
 #pragma link C++ class genie::Spline;
 #pragma link C++ class genie::BLI2DGrid;
 #pragma link C++ class genie::BLI2DUnifGrid;
