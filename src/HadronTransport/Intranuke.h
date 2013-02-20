2d2e62ae0bb68a4e70f9671372e329370415ad1b
dytman <dytman@pitt.edu>
2013-02-20 18:41:20 +0000

    take out unused DeltaMass param
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@3857 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/HadronTransport/Intranuke.h
+++ src/HadronTransport/Intranuke.h
@@ -109,7 +109,6 @@ protected:
   double       fNucCEXFac;    ///< charge exchange xsec correction factor (hN Mode)
   double       fEPreEq;       ///< threshold for pre-equilibrium reaction
   double       fFermiFac;     ///< testing parameter to modify fermi momentum
-  double       fDeltaMass;
   double       fFreeStep;     ///< produced particle free stem, in fm
   double       fFermiMomentum;     ///< whether or not particle collision is pauli blocked
   bool         fDoFermi;      ///< whether or not to do fermi mom. 
