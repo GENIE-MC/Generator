80a877c4ea39951e9cec0e1a9ada24d9ec4f9a80
dytman <dytman@pitt.edu>
2013-03-26 21:17:50 +0000

    add more validation
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@3904 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ data/validation/hA/dbldiff_xs/nucleon/p-to-n/runfast-meier.sh
@@ -0,0 +1,20 @@
+#gevgen_hadron -n 500000 -p 2212 -t 1000060120 -k .113 -m hA -r 8050 --message-thresholds Messenger_laconic.xml
+#gntpc -i gntp.inuke.8050.ghep.root -f ginuke -o Mar_26_13_p_C_113_v280_hA.ginuke.root --message-thresholds Messenger_laconic.xml
+#gevgen_hadron -n 500000 -p 2212 -t 1000260560 -k .113 -m hA -r 8051 --message-thresholds Messenger_laconic.xml
+#gntpc -i gntp.inuke.8051.ghep.root -f ginuke -o Mar_26_13_p_Fe_113_v280_hA.ginuke.root --message-thresholds Messenger_laconic.xml
+gevgen_hadron -n 500000 -p 2212 -t 1000822080 -k .113 -m hA -r 8053 --message-thresholds Messenger_laconic.xml
+gntpc -i gntp.inuke.8053.ghep.root -f ginuke -o Mar_26_13_p_Pb_113_v280_hA.ginuke.root --message-thresholds Messenger_laconic.xml
+gevgen_hadron -n 500000 -p 2212 -t 1000130270 -k .256 -m hA -r 8054 --message-thresholds Messenger_laconic.xml
+gntpc -i gntp.inuke.8054.ghep.root -f ginuke -o Mar_26_13_p_Al_256_v280_hA.ginuke.root --message-thresholds Messenger_laconic.xml
+
+gevgen_hadron -n 500000 -p 2212 -t 1000060120 -k .113 -m hN -r 8060 --message-thresholds Messenger_laconic.xml
+gntpc -i gntp.inuke.8060.ghep.root -f ginuke -o Mar_26_13_p_C_113_v280_hN.ginuke.root --message-thresholds Messenger_laconic.xml
+gevgen_hadron -n 500000 -p 2212 -t 1000260560 -k .113 -m hN -r 8061 --message-thresholds Messenger_laconic.xml
+gntpc -i gntp.inuke.8061.ghep.root -f ginuke -o Mar_26_13_p_Fe_113_v280_hN.ginuke.root --message-thresholds Messenger_laconic.xml
+gevgen_hadron -n 500000 -p 2212 -t 1000822080 -k .113 -m hN -r 8063 --message-thresholds Messenger_laconic.xml
+gntpc -i gntp.inuke.8063.ghep.root -f ginuke -o Mar_26_13_p_Pb_113_v280_hN.ginuke.root --message-thresholds Messenger_laconic.xml
+gevgen_hadron -n 500000 -p 2212 -t 1000130270 -k .256 -m hN -r 8064 --message-thresholds Messenger_laconic.xml
+gntpc -i gntp.inuke.8064.ghep.root -f ginuke -o Mar_26_13_p_Al_256_v280_hN.ginuke.root --message-thresholds Messenger_laconic.xml
+
+
+
