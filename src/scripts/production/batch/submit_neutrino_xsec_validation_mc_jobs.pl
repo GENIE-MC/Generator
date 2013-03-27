62cb112f290d16d9939c626c6fa81baa5912452a
dytman <dytman@pitt.edu>
2013-03-27 21:38:45 +0000

    add batch-system none
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@3916 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/scripts/production/batch/submit_neutrino_xsec_validation_mc_jobs.pl
+++ src/scripts/production/batch/submit_neutrino_xsec_validation_mc_jobs.pl
@@ -15,7 +15,7 @@
 #   [--production]   : production name, default: <version>
 #   [--cycle]        : cycle in current production, default: 01
 #   [--use-valgrind] : default: off
-#   [--batch-system] : <PBS, LSF>, default: PBS
+#   [--batch-system] : <PBS, LSF, none>, default: PBS
 #   [--queue]        : default: prod
 #   [--softw-topdir] : default: /opt/ppd/t2k/softw/GENIE
 #
@@ -241,6 +241,11 @@ for my $curr_runnu (keys %evg_gevgl_hash)  {
            `bsub < $batch_script`;
        } #LSF
 
+       # no batch system, run jobs interactively
+       if($batch_system eq 'none') {
+          system("source $genie_setup; cd $jobs_dir; export GSPLOAD=$xspl_file; export GEVGL=$gevgl; export GSEED=$curr_seed; $evgen_cmd; $conv_cmd");
+       } # interactive mode
+
     } # loop over subruns
  # } #checking whether to submit current run
 } # loop over runs
@@ -290,5 +295,10 @@ for my $curr_runnu (keys %evg_gevgl_hash)  {
       close(LSF);
      `bsub < $batch_script`;
    } #LSF
+
+   # no batch system, run jobs interactively
+   if($batch_system eq 'none') {
+      system("source $genie_setup; unset GEVGL; unset GSPLOAD; cd $jobs_dir; $gsp12root_cmd");
+   } # interactive mode
   
 }
