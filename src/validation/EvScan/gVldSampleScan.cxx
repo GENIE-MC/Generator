c98c15fb7090479796b200b46e2788b47059c9b4
candreop <costas.andreopoulos@stfc.ac.uk>
2013-04-03 16:09:19 +0000

    updated event scanning program
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/trunk@3924 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/validation/EvScan/gVldSampleScan.cxx
+++ src/validation/EvScan/gVldSampleScan.cxx
@@ -6,7 +6,12 @@
 \brief   A simple validation program to read-in a GHEP event tree and test 
          whether the generated events obey basic conservation laws
 
-\syntax  gvld_samplev_scan -f ghep_event_file [-n nevents]
+\syntax  gvld_sample_scan 
+             -f ghep_event_file 
+            [-o output_error_log_file]
+            [-n nev1[,nev2]]
+            [--check-energy-momentum-conservation]
+            [--check-charge-conservation]
 
 \author  Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
          STFC, Rutherford Appleton Laboratory
@@ -22,6 +27,8 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <sstream>
+#include <fstream>
 
 #include <TSystem.h>
 #include <TFile.h>
@@ -37,39 +44,49 @@
 #include "Messenger/Messenger.h"
 #include "Utils/CmdLnArgParser.h"
 
+using std::ostringstream;
 using std::string;
 using std::vector;
 using std::setw;
 using std::setprecision;
 using std::setfill;
 using std::ios;
+using std::endl;
 
 using namespace genie;
 
 void GetCommandLineArgs (int argc, char ** argv);
 void PrintSyntax        (void);
 bool CheckRootFilename  (string filename);
-int  Strangeness        (int pdgc);
 
-int    gOptNEvt;
-string gOptInpFilename;
+// checks
+void CheckEnergyMomentumConservation (void);
+void CheckChargeConservation         (void);
+
+// options
+string   gOptInpFilename = "";
+string   gOptOutFilename = "";
+Long64_t gOptNEvtL = -1;
+Long64_t gOptNEvtH = -1;
+bool     gOptCheckEnergyMomentumConservation = false;
+bool     gOptCheckChargeConservation = false;
+
+Long64_t gFirstEventNum = -1;
+Long64_t gLastEventNum  = -1;
+
+TTree *            gEventTree = 0;
+NtpMCEventRecord * gMCRec = 0;
+ofstream           gErrLog;
 
 //____________________________________________________________________________
 int main(int argc, char ** argv)
 {
   GetCommandLineArgs (argc, argv);
 
-  //-- open the ROOT file and get the TTree & its header
-  TTree *           tree = 0;
-  NtpMCTreeHeader * thdr = 0;
-
   TFile file(gOptInpFilename.c_str(),"READ");
 
-  tree = dynamic_cast <TTree *>           ( file.Get("gtree")  );
-  thdr = dynamic_cast <NtpMCTreeHeader *> ( file.Get("header") );
-
+  NtpMCTreeHeader * thdr = dynamic_cast <NtpMCTreeHeader *> ( file.Get("header") );
   LOG("gvldtest", pINFO) << "Input tree header: " << *thdr;
-
   NtpMCFormat_t format = thdr->format;
   if(format != kNFGHEP) {
       LOG("gvldtest", pERROR) 
@@ -79,99 +96,95 @@ int main(int argc, char ** argv)
       return 3;
   }
 
-  NtpMCEventRecord * mcrec = 0;
-  tree->SetBranchAddress("gmcrec", &mcrec);
+  gEventTree = dynamic_cast <TTree *> (file.Get("gtree"));
+  gEventTree->SetBranchAddress("gmcrec", &gMCRec);
 
-  PDGLibrary * pdglib = PDGLibrary::Instance();
+  Long64_t nev = gEventTree->GetEntries();
+  if(gOptNEvtL == -1 && gOptNEvtH == -1) {
+    // read all events
+    gFirstEventNum = 0;
+    gLastEventNum  = nev-1;
+  }
+  else {
+    // read a range of events
+    gFirstEventNum = TMath::Max((Long64_t)0,  gOptNEvtL);
+    gLastEventNum = TMath::Min(nev-1,        gOptNEvtH);
+    if(gLastEventNum - gFirstEventNum < 0) {
+      LOG("gevdump", pFATAL) << "Invalid event range";
+      PrintSyntax();
+      gAbortingInErr = true;
+      exit(1);
+    }
+  }
 
-  //
-  map<int, int> all_particles;   // all
-  map<int, int> fs_particles;    // final state particles
-  map<int, int> primh_particles; // primary hadronic system particles
-  map<int, int> dec_particles;   // particles marked as decayed
+  
+  if(gOptOutFilename.size() == 0) {
+     ostringstream logfile;
+     logfile << gOptInpFilename << ".errlog";
+     gOptOutFilename = logfile.str();
+  }
+  gErrLog.open(gOptOutFilename.c_str());
+  gErrLog << "# ..................................................................................." << endl;
+  gErrLog << "# Error log for event file " << gOptInpFilename << endl;
+  gErrLog << "# ..................................................................................." << endl;
+  gErrLog << "# " << endl;
 
-  long int all_n   = 0;
-  long int fs_n    = 0;
-  long int primh_n = 0;
-  long int dec_n   = 0;
+  if(gOptCheckEnergyMomentumConservation) {
+     CheckEnergyMomentumConservation();
+  }
 
-  double E_init  = 0, E_fin  = 0; // E
-  double px_init = 0, px_fin = 0; // px
-  double py_init = 0, py_fin = 0; // py
-  double pz_init = 0, pz_fin = 0; // pz
-  double Q_init  = 0, Q_fin  = 0; // charge
-  double s_init  = 0, s_fin  = 0; // strangeness
+  if(gOptCheckChargeConservation) {
+     CheckChargeConservation();
+  }
 
-  vector<int> evtnum;
+  gErrLog.close();
 
-  int nev = (gOptNEvt > 0) ?
-                 TMath::Min(gOptNEvt, (int)tree->GetEntries()) :
-                 (int) tree->GetEntries();
+  return 0;
+}
+//____________________________________________________________________________
+void CheckEnergyMomentumConservation (void)
+{
+  gErrLog << "# Events failing the energy-momentum conservation test:" << endl;
+  gErrLog << "# " << endl;
 
-  // Event loop
+  int nerr = 0;
 
-  for(int i = 0; i < nev; i++) {
-    tree->GetEntry(i);
+  for(Long64_t i = gFirstEventNum; i <= gLastEventNum; i++) 
+  {
+    gEventTree->GetEntry(i);
 
-    NtpMCRecHeader rec_header = mcrec->hdr;
-    EventRecord &  event      = *(mcrec->event);
+    NtpMCRecHeader rec_header = gMCRec->hdr;
+    EventRecord &  event      = *(gMCRec->event);
 
-    LOG("gvldtest", pNOTICE) << "Checking event.... " << i;
+    LOG("gvldtest", pINFO) << "Checking event.... " << i;
+
+    double E_init  = 0, E_fin  = 0; // E
+    double px_init = 0, px_fin = 0; // px
+    double py_init = 0, py_fin = 0; // py
+    double pz_init = 0, pz_fin = 0; // pz
 
     GHepParticle * p = 0;
     TIter event_iter(&event);
     while ( (p = dynamic_cast<GHepParticle *>(event_iter.Next())) ) {
 
-     int          pdgc = p->Pdg();
-     GHepStatus_t ist  = p->Status();
+      GHepStatus_t ist  = p->Status();
 
-     //
-     // Check energy & momentum, charge and strangeness conservation
-     //
-
-     if(ist == kIStInitialState) {
-       E_init  += p->E();
-       px_init += p->Px();
-       py_init += p->Py();
-       pz_init += p->Pz();
-       Q_init  += p->Charge();
-     //s_init  += pdglib->Find(p->Pdg())->Strangeness();
-       s_init  += Strangeness(pdgc);
-
-     }
-     if(ist == kIStStableFinalState || 
-        ist == kIStFinalStateNuclearRemnant) {
-       E_fin   += p->E();
-       px_fin  += p->Px();
-       py_fin  += p->Py();
-       pz_fin  += p->Pz();
-       Q_fin   += p->Charge();
-     //s_fin   += pdglib->Find(p->Pdg())->Strangeness();
-       s_fin   += Strangeness(pdgc);
-     }
-
-
-     //
-     // Check particle frequencies
-     //
-
-     all_particles[pdgc]++;
-     all_n++;
-  
-     if (ist == kIStStableFinalState) {
-       fs_particles[pdgc]++;
-       fs_n++;
-     }
-     else if (ist == kIStDecayedState) {
-       dec_particles[pdgc]++;
-       dec_n++;
-     }
-     else if (ist == kIStHadronInTheNucleus) {
-       primh_particles[pdgc]++;
-       primh_n++;
-     }
-
-    } // particle loop
+      if(ist == kIStInitialState) 
+      {
+         E_init  += p->E();
+         px_init += p->Px();
+         py_init += p->Py();
+         pz_init += p->Pz();
+       }
+       if(ist == kIStStableFinalState || 
+          ist == kIStFinalStateNuclearRemnant) 
+       {
+         E_fin   += p->E();
+         px_fin  += p->Px();
+         py_fin  += p->Py();
+         pz_fin  += p->Pz();
+       }
+    }//p
 
     double epsilon = 1E-3; 
 
@@ -179,142 +192,91 @@ int main(int argc, char ** argv)
     bool px_conserved = TMath::Abs(px_init - px_fin) < epsilon;
     bool py_conserved = TMath::Abs(py_init - py_fin) < epsilon;
     bool pz_conserved = TMath::Abs(pz_init - pz_fin) < epsilon;
-    bool Q_conserved  = TMath::Abs(Q_init  - Q_fin)  < epsilon;
-    bool s_conserved  = TMath::Abs(s_init  - s_fin)  < epsilon;
 
     bool ok = E_conserved  && 
               px_conserved &&
               py_conserved &&
-              pz_conserved &&
-              Q_conserved  &&
-              s_conserved;
+              pz_conserved;
 
     if(!ok) {
-       if(!E_conserved) {
-          LOG("gvldtest", pERROR) << "** E is not conserved at this event";
-       }
-       if(!px_conserved) {
-          LOG("gvldtest", pERROR) << "** Px is not conserved at this event";
-       }
-       if(!py_conserved) {
-          LOG("gvldtest", pERROR) << "** Py is not conserved at this event";
-       }
-       if(!pz_conserved) {
-          LOG("gvldtest", pERROR) << "** Pz is not conserved at this event";
-       }
-       if(!Q_conserved) {
-          LOG("gvldtest", pERROR) << "** Q is not conserved at this event";
-       }
-       if(!s_conserved) {
-          LOG("gvldtest", pERROR) << "** s is not conserved at this event";
-       }
+       LOG("gvldtest", pERROR) 
+         << " ** Energy-momentum non-conservation in event: " << i 
+         << "\n"
+         << event;
+       gErrLog << i << endl;    
+       nerr++;
+    }
 
-       LOG("gvldtest", pERROR) << rec_header;
-       LOG("gvldtest", pERROR) << event;
+  }//i
 
-       evtnum.push_back(i);
-    } 
-
-    mcrec->Clear();
-
-  } // event loop
-
-  file.Close();
-
-  //
-  // ** Reporting
-  //
-
-  LOG("gvldtest", pINFO)  << "Found " << evtnum.size() << " problems";
-  for(vector<int>::const_iterator it = evtnum.begin();
-      it != evtnum.end(); ++it) {
-     LOG("gvldtest", pINFO)  << "Look at event: " << *it;    
+  if(nerr == 0) {
+    gErrLog << "none" << endl;    
   }
-
- map<int, int>::const_iterator it;
-
-  LOG("gvldtest", pINFO)  << "*** All particles:";
-  for(it = all_particles.begin(); it != all_particles.end(); ++it) {
-     int pdgc = it->first;
-     int n    = it->second;
-     double r = (all_n>0) ? (double)n / (double)all_n : 0.;
-     LOG("gvldtest", pINFO)
-       << setfill(' ') << setw(15) 
-       << pdglib->Find(pdgc)->GetName() << " :"
-       << setfill('.') << setw(15) 
-       << n << " (" << 100*r << " %)";
-  }
-  LOG("gvldtest", pINFO)  << "*** Final state particles:";
-  for(it = fs_particles.begin(); it != fs_particles.end(); ++it) {
-     int pdgc = it->first;
-     int n    = it->second;
-     double r = (fs_n>0) ? (double)n / (double)fs_n : 0.;
-     LOG("gvldtest", pINFO)
-       << setfill(' ') << setw(15) 
-       << pdglib->Find(pdgc)->GetName() << " :"
-       << setfill('.') << setw(15) 
-       << n << " (" << 100*r << " %)";
-  }
-  LOG("gvldtest", pINFO)  << "*** Decayed particles:";
-  for(it = dec_particles.begin(); it != dec_particles.end(); ++it) {
-     int pdgc = it->first;
-     int n    = it->second;
-     double r = (dec_n>0) ? (double)n / (double)dec_n : 0.;
-     LOG("gvldtest", pINFO)
-       << setfill(' ') << setw(15) 
-       << pdglib->Find(pdgc)->GetName() << " :"
-       << setfill('.') << setw(15) 
-       << n << " (" << 100*r << " %)";
-  } 
-
-  LOG("gvldtest", pINFO)  << "*** Primary hadronic system:";
-  for(it = primh_particles.begin(); it != primh_particles.end(); ++it) {
-     int pdgc = it->first;
-     int n    = it->second;
-     double r = (primh_n > 0) ? (double)n / (double)primh_n : 0;
-     LOG("gvldtest", pINFO)
-       << setfill(' ') << setw(15) 
-       << pdglib->Find(pdgc)->GetName() << " :"
-       << setfill('.') << setw(15) 
-       << n << " (" << 100*r << " %)";
-  }
-
-  LOG("gvldtest", pINFO)  << "Done!";
-
-  return 0;
 }
 //____________________________________________________________________________
-int Strangeness(int pdgc)
+void CheckChargeConservation(void)
 {
-  switch (pdgc) {
+  gErrLog << "# Events failing the charge conservation test:" << endl;
+  gErrLog << "# " << endl;
 
-   case ( kPdgKP         ) : // K^+
-   case ( kPdgK0         ) : // K^0
-   case ( kPdgDPs        ) : // Ds^+
-   case ( kPdgAntiLambda ) : // \bar{Lambda^0}
-   case ( kPdgAntiSigmaP ) : // \bar{Sigma+}
-   case ( kPdgAntiSigma0 ) : // \bar{Sigma0}
-   case ( kPdgAntiSigmaM ) : // \bar{Sigma-}
-   
-    return 1;
-    break;
+  int nerr = 0;
 
-   case ( kPdgKM         ) : // K^-
-   case ( kPdgAntiK0     ) : // \bar{K^0}
-   case ( kPdgDMs        ) : // Ds^-
-   case ( kPdgLambda     ) : // Lambda^0
-   case ( kPdgSigmaP     ) : // Sigma+
-   case ( kPdgSigma0     ) : // Sigma0
-   case ( kPdgSigmaM     ) : // Sigma-
+  for(Long64_t i = gFirstEventNum; i <= gLastEventNum; i++) 
+  {
+    gEventTree->GetEntry(i);
 
-    return -1;
-    break;
+    NtpMCRecHeader rec_header = gMCRec->hdr;
+    EventRecord &  event      = *(gMCRec->event);
 
-   default:
-    return 0;
+    LOG("gvldtest", pINFO) << "Checking event.... " << i;
+
+    // Can't run the test for neutrinos scattered off nuclear targets
+    // because of intranuclear rescattering effects and the presence, in the event
+    // record, of a charged nuclear remnant pseudo-particle whose charge is not stored.
+    // To check charge conservation in the primary interaction, use a sample generated
+    // for a free nucleon targets.
+    GHepParticle * nucltgt = event.TargetNucleus();
+    if (nucltgt) {
+      LOG("gvldtest", pINFO)
+           << "Event in nuclear target - Skipping test...";
+      continue;
+    }
+
+    double Q_init  = 0;
+    double Q_fin   = 0; 
+
+    GHepParticle * p = 0;
+    TIter event_iter(&event);
+    while ( (p = dynamic_cast<GHepParticle *>(event_iter.Next())) ) {
+
+      GHepStatus_t ist  = p->Status();
+
+      if(ist == kIStInitialState) 
+      {
+         Q_init  += p->Charge();
+       }
+       if(ist == kIStStableFinalState)
+       {
+         Q_fin  += p->Charge();
+       }
+    }//p
+
+    double epsilon = 1E-3; 
+    bool ok = TMath::Abs(Q_init - Q_fin)  < epsilon;
+    if(!ok) {
+       LOG("gvldtest", pERROR) 
+         << " ** Charge non-conservation in event: " << i 
+         << "\n"
+         << event;
+       gErrLog << i << endl;    
+       nerr++;
+    }
+
+  }//i
+
+  if(nerr == 0) {
+    gErrLog << "none" << endl;    
   }
-
-  return 0;
 }
 //____________________________________________________________________________
 void GetCommandLineArgs(int argc, char ** argv)
@@ -322,18 +284,8 @@ void GetCommandLineArgs(int argc, char ** argv)
   LOG("gvldtest", pNOTICE) << "*** Parsing command line arguments";
 
   CmdLnArgParser parser(argc,argv);
-
-  // number of events:
-  if( parser.OptionExists('n') ) {
-    LOG("gvldtest", pINFO) << "Reading number of events to analyze";
-    gOptNEvt = parser.ArgAsInt('n');
-  } else {
-    LOG("gvldtest", pINFO)
-        << "Unspecified number of events to analyze - Use all";
-    gOptNEvt = -1;
-  }
   
-  // get GENIE event sample
+  // get input GENIE event sample
   if( parser.OptionExists('f') ) {
     LOG("gvldtest", pINFO) << "Reading event sample filename";
     gOptInpFilename = parser.ArgAsString('f');
@@ -343,6 +295,45 @@ void GetCommandLineArgs(int argc, char ** argv)
     PrintSyntax();
     exit(1);
   }
+
+  // get output error log
+  if( parser.OptionExists('o') ) {
+    LOG("gvldtest", pINFO) << "Reading err log file name";
+    gOptOutFilename = parser.ArgAsString('o');
+  } 
+
+  // number of events
+  if ( parser.OptionExists('n') ) {
+    LOG("gevdump", pINFO) << "Reading number of events to analyze";
+    string nev =  parser.ArgAsString('n');
+    if (nev.find(",") != string::npos) {
+      vector<long> vecn = parser.ArgAsLongTokens('n',",");
+      if(vecn.size()!=2) {
+         LOG("gevdump", pFATAL) << "Invalid syntax";
+         PrintSyntax();
+         gAbortingInErr = true;
+         exit(1);
+      }
+      // read a range of events
+      gOptNEvtL = vecn[0];
+      gOptNEvtH = vecn[1];
+    } else {
+      // read single event
+      gOptNEvtL = parser.ArgAsLong('n');
+      gOptNEvtH = gOptNEvtL;
+    }
+  } else {
+    LOG("gevdump", pINFO)
+      << "Unspecified number of events to analyze - Use all";
+    gOptNEvtL = -1;
+    gOptNEvtH = -1;
+  }
+
+  // checks
+  gOptCheckEnergyMomentumConservation =
+     parser.OptionExists("check-energy-momentum-conservation");
+  gOptCheckChargeConservation =
+     parser.OptionExists("check-charge-conservation");
 }
 //____________________________________________________________________________
 void PrintSyntax(void)
