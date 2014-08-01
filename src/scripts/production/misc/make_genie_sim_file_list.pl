c48df344a1e012a579ec8a663513b3b810944545
candreop <costas.andreopoulos@stfc.ac.uk>
2014-08-01 14:36:51 +0000

    add option to have multiple input file directories per model
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/trunk@4149 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/scripts/production/misc/make_genie_sim_file_list.pl
+++ src/scripts/production/misc/make_genie_sim_file_list.pl
@@ -4,10 +4,11 @@
 # Syntax:
 # % perl make_genie_sim_file_list.pl <inputs>
 #
-# where inputs is a list of file_path,model_name pairs
+# where inputs is a list of "file_paths,model_name" pairs.
+# Multiple ':'-separated file paths can be specified.
 #
 # Example:
-# %perl make_genie_sim_file_list.pl /some/path/,model_name /another/path,another_model_name ...
+# %perl make_genie_sim_file_list.pl /some/path/,model_name /another/path1:/another/path2,another_model_name ...
 #
 # Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
 # STFC, Rutherford Appleton Lab
@@ -18,33 +19,38 @@
 my %data = ();
 
 foreach (@ARGV) {
-  my ($file_path, $model_name) = split(',', $_);
-  $data{$model_name} .= $file_path;
+  my ($file_paths, $model_name) = split(',', $_);
+  $data{$model_name} .= $file_paths;
 }
 
 print "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?> \n\n";
 print "<genie_simulation_outputs> \n";
 
-while( my ($model_name, $file_path) = each %data )
+while( my ($model_name, $file_paths) = each %data )
 {
-  #print "$model_name --> $file_path \n";
+  #print "$model_name --> $file_paths \n";
   print "\n\n";
   print "\t <model name=\"$model_name\"> \n";
-  opendir my $dir, $file_path or die "Can not open directory: $!";
-  my @files = grep { !/^\./ } readdir $dir;
-  closedir $dir;
-  foreach(@files) {
-      if(/.ghep.root$/) {
-          print "\t\t <evt_file format=\"ghep\"> $_ </evt_file> \n";
-      }
-      elsif(/.gst.root$/) {
-          print "\t\t <evt_file format=\"gst\"> $_ </evt_file> \n";
-      } 
-      else {
-          print "\t\t <xsec_file> $_ </xsec_file> \n";
-      }
-  }
 
+  @file_path_array = split(':',$file_paths);
+  foreach(@file_path_array) {
+    $dirname = $_;
+    opendir my $dir, $dirname or die "Can not open directory: $!";
+    my @files = grep { !/^\./ } readdir $dir;
+    closedir $dir;
+    foreach(@files) {
+      $filename = $_;
+      if($filename =~ m/.ghep.root$/) {
+          print "\t\t <evt_file format=\"ghep\"> $dirname/$filename </evt_file> \n";
+      }
+      elsif($filename =~ m/.gst.root$/) {
+          print "\t\t <evt_file format=\"gst\"> $dirname/$filename </evt_file> \n";
+      } 
+      elsif($filename =~ m/.root$/) {
+          print "\t\t <xsec_file> $dirname/$filename </xsec_file> \n";
+      }
+    }
+  }
   print "\t </model> \n\n";
 }
 
