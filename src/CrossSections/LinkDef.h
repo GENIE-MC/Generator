c275009f4370988254ea9b4ddf89a9d738f3c17e
gnperdue <perdue@fnal.gov>
2015-01-14 20:42:19 +0000

    Add ASKXSec, update LinkDef for ASK and new GSL funcs
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/branches/R-2_9_0@4406 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/CrossSections/LinkDef.h
+++ src/CrossSections/LinkDef.h
@@ -14,6 +14,7 @@
 #pragma link C++ class genie::DISXSec;
 #pragma link C++ class genie::IMDXSec;
 #pragma link C++ class genie::COHXSec;
+#pragma link C++ class genie::ASKXSec;
 #pragma link C++ class genie::NuElectronXSec;
 
 // Wrappers for GSL/MathMore lib
@@ -25,6 +26,12 @@
 #pragma link C++ class genie::utils::gsl::wrap::d2XSec_dxdy_Ey;
 #pragma link C++ class genie::utils::gsl::wrap::d2XSec_dWdQ2_EW;
 #pragma link C++ class genie::utils::gsl::wrap::d2XSec_dWdQ2_EQ2;
+#pragma link C++ class genie::utils::gsl::wrap::d5Xsec_dEldOmegaldOmegapi;
+#pragma link C++ class genie::utils::gsl::wrap::d4Xsec_dEldThetaldOmegapi;
+#pragma link C++ class genie::utils::gsl::wrap::d3Xsec_dOmegaldThetapi;
+#pragma link C++ class genie::utils::gsl::wrap::d3Xsec_dTldTkdCosThetal;
+#pragma link C++ class genie::utils::gsl::wrap::dXSec_dElep_AR;
+#pragma link C++ class genie::utils::gsl::wrap::dXSec_Log_Wrapper;
 
 // Depreciated wrappers
 #pragma link C++ class genie::GXSecFunc;
