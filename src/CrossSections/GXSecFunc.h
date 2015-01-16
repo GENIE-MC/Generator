fc67473f529c379a97d54659f9bf1753aa44b23e
gnperdue <perdue@fnal.gov>
2015-01-16 21:57:00 +0000

    Bump copyright in CrossSections
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/branches/R-2_9_0@4458 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/CrossSections/GXSecFunc.h
+++ src/CrossSections/GXSecFunc.h
@@ -2,7 +2,7 @@
 // Exports conrete XSecAlgorithmI class as a GSFunc (GENIE scalar function)
 // for interfacing with the numerical integration code
 //
-// Copyright (c) 2003-2013, GENIE Neutrino MC Generator Collaboration
+// Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
 // For the full text of the license visit http://copyright.genie-mc.org
 //or see $GENIE/LICENSE
 //
