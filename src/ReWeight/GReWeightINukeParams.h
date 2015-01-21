d550f7f4ce8e3996d1adf14333239b451cdf3d50
gnperdue <perdue@fnal.gov>
2015-01-21 18:58:41 +0000

    Bump copyright in ReWeight
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/branches/R-2_9_0@4525 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/ReWeight/GReWeightINukeParams.h
+++ src/ReWeight/GReWeightINukeParams.h
@@ -13,6 +13,9 @@
 
 \created Sep 10, 2009
 
+\cpright  Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
 */
 //____________________________________________________________________________
 
