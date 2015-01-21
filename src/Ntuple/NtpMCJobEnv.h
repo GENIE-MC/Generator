0bdd3718ba1b9bbeb3d89a0ddb314044e89e5547
gnperdue <perdue@fnal.gov>
2015-01-21 17:30:29 +0000

    Bump copyright in Ntuple
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/branches/R-2_9_0@4513 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Ntuple/NtpMCJobEnv.h
+++ src/Ntuple/NtpMCJobEnv.h
@@ -11,6 +11,9 @@
 
 \created September 10, 2006
 
+\cpright  Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
 */
 //____________________________________________________________________________
 
