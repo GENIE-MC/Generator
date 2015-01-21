f176e25753db8da0bff31965c38718d92817a8f1
gnperdue <perdue@fnal.gov>
2015-01-21 18:08:24 +0000

    Bump copyright in Nuclear
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/branches/R-2_9_0@4516 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Nuclear/FermiMomentumTablePool.h
+++ src/Nuclear/FermiMomentumTablePool.h
@@ -10,6 +10,9 @@
 
 \created  August 18, 2005
 
+\cpright  Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
 */
 //____________________________________________________________________________
 
