265e4b30afd164eda7fc24a6adbc93b1803597b9
rhatcher <rhatcher@fnal.gov>
2015-03-24 20:41:03 +0000

    need the .cxx file for the makefile to notice the existence of the header file
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/branches/R-2_9_0@4689 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Utils/XmlParserUtils.cxx
@@ -0,0 +1,28 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+ or see $GENIE/LICENSE
+
+ Author: Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         University of Liverpool & STFC Rutherford Appleton Lab - January 12, 2004
+
+ For documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+
+*/
+//____________________________________________________________________________
+
+#include <sstream>
+
+#include "Utils/XmlParserUtils.h"
+
+using std::ostringstream;
+
+// really only exists so that rootcint will add these to the dictionary
+
+
+//____________________________________________________________________________
+
+
