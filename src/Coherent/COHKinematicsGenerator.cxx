1c593109fb5f982bdfa737619e53c515e7f5091d
gnperdue <perdue@fnal.gov>
2015-07-16 20:04:30 +0000

    Better kinematic bounds checking for FM BS COHPI
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@5378 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Coherent/COHKinematicsGenerator.cxx
+++ src/Coherent/COHKinematicsGenerator.cxx
@@ -822,7 +822,8 @@ double COHKinematicsGenerator::MaxXSec_BergerSehgalFM(const Interaction * in) co
     } else {
       yr = kps.YLim();
     }
-    if ((yr.max < 0) || (yr.max < yr.min)) { // forbidden kinematics
+    if ((yr.max < 0) || (yr.max < yr.min) || 
+        (yr.max > 1) || (yr.min < 0)) { // forbidden kinematics
       continue;
     }
     const double logymin  = TMath::Log10(yr.min);
