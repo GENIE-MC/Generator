b8d53e21def82a662957126663a5e7e6107cc497
gnperdue <perdue@fnal.gov>
2015-07-30 16:21:49 +0000

    When computing the threshold for coherent pion events, check to see if the event is charged or neutral current and then use the appropriate pion mass in the calculation.
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/generator/trunk@5428 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Interaction/KPhaseSpace.cxx
+++ src/Interaction/KPhaseSpace.cxx
@@ -98,8 +98,9 @@ double KPhaseSpace::Threshold(void) const
 
   if (pi.IsCoherent()) {
     int tgtpdgc = tgt.Pdg(); // nuclear target PDG code (10LZZZAAAI)
+    double mpi  = pi.IsWeakCC() ? kPionMass : kPi0Mass;
     double MA   = PDGLibrary::Instance()->Find(tgtpdgc)->Mass();
-    double m    = ml + kPionMass;
+    double m    = ml + mpi;
     double m2   = TMath::Power(m,2);
     double Ethr = m + 0.5*m2/MA;
     return TMath::Max(0.,Ethr);
