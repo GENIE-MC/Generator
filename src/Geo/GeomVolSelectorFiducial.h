734e5da438ef82260e599dae01660700b4cc583c
gnperdue <perdue@fnal.gov>
2016-01-15 17:24:19 +0000

    Even more copyright updates in trunk
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@5711 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Geo/GeomVolSelectorFiducial.h
+++ src/Geo/GeomVolSelectorFiducial.h
@@ -18,7 +18,7 @@
 
 \created  July 14, 2010
 
-\cpright  Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
+\cpright  Copyright (c) 2003-2016, GENIE Neutrino MC Generator Collaboration
           For the full text of the license visit http://copyright.genie-mc.org
           or see $GENIE/LICENSE
 
