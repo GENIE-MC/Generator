d64d032d711d77630f9bc3ee06023efe86b27725
gnperdue <perdue@fnal.gov>
2016-01-15 17:22:16 +0000

    More trunky copyright updates
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@5709 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/EVGCore/InteractionGeneratorMap.h
+++ src/EVGCore/InteractionGeneratorMap.h
@@ -13,7 +13,7 @@
 
 \created January 23, 2006
 
-\cpright Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
+\cpright Copyright (c) 2003-2016, GENIE Neutrino MC Generator Collaboration
          For the full text of the license visit http://copyright.genie-mc.org
          or see $GENIE/LICENSE
 */
