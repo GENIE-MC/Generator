0b4cbf12399115d305abbbdf4a3935eb6bc1ca2b
gnperdue <perdue@fnal.gov>
2016-01-15 17:27:21 +0000

    Finish off the copyright bumps in trunk
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@5713 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/SingleKaon/SKKinematicsGenerator.h
+++ src/SingleKaon/SKKinematicsGenerator.h
@@ -12,7 +12,7 @@
 
 \created  October 03, 2014
 
-\cpright  Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
+\cpright  Copyright (c) 2003-2016, GENIE Neutrino MC Generator Collaboration
           For the full text of the license visit http://copyright.genie-mc.org
           or see $GENIE/LICENSE
 */
--- src/Utils/AppInit.h
+++ src/Utils/AppInit.h
@@ -12,7 +12,7 @@
 
 \created    January 31, 2013
 
-\cpright    Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
+\cpright    Copyright (c) 2003-2016, GENIE Neutrino MC Generator Collaboration
             For the full text of the license visit http://copyright.genie-mc.org
             or see $GENIE/LICENSE
 */
