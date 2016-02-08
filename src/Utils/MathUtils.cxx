4b092b5fcca188a435c12e03fb4cb954b9087bfc
candreop <costas.andreopoulos@stfc.ac.uk>
2016-02-08 17:35:30 +0000

    add simple utilities to use Cholesky's method
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/generator/trunk@5733 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Utils/MathUtils.cxx
+++ src/Utils/MathUtils.cxx
@@ -5,12 +5,13 @@
  or see $GENIE/LICENSE
 
  Author: Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
-         University of Liverpool & STFC Rutherford Appleton Lab - May 06, 2004
+         University of Liverpool & STFC Rutherford Appleton Lab 
 
  For documentation see the corresponding header file.
 
  Important revisions after version 2.0.0 :
-
+ @ Feb 8, 2016 - CA
+   Added Cholesky's method functions
 */
 //____________________________________________________________________________
 
@@ -19,8 +20,193 @@
 #include <TMath.h>
 
 #include "Messenger/Messenger.h"
+#include "Numerical/RandomGen.h"
 #include "Utils/MathUtils.h"
 
+//____________________________________________________________________________
+TMatrixD genie::utils::math::CholeskyDecomposition(const TMatrixD& cov_matrix)
+{
+// Perform a Cholesky decomposition of the input covariance matrix and
+// return the lower triangular matrix
+//
+   const double epsilon = 1E-12;
+
+   int ncols = cov_matrix.GetNcols();
+   int nrows = cov_matrix.GetNrows();
+
+   assert(ncols==nrows);
+
+   int n = nrows;
+
+   TMatrixD L(n, n);
+
+   for (int i = 0; i < n; ++i) {
+
+     // calculate the diagonal term first
+     L(i,i) = cov_matrix(i,i);
+     for (int k = 0; k < i; ++k) {
+        double tmp = L(k,i);
+        L(i,i) -= tmp*tmp;
+     }//k
+
+     if(L(i,i) <= 0) {
+       if(fabs(L(i,i)) < epsilon){
+         L(i,i)=epsilon;
+         LOG("Cholesky", pINFO) 
+           << "Changed element (" << i << ", " << i << ") to " << L(i,i);
+       }
+       else{
+         LOG("Cholesky", pERROR) 
+            << "Decomposed covariance matrix not positive-definite";
+         LOG("Cholesky", pERROR) 
+            << "L(" << i << "," << i << ") = " << L(i,i);
+         exit(1);
+       }
+     }
+     L(i,i) = TMath::Sqrt(L(i,i));
+     // then the off-diagonal terms
+     for (int j = i+1; j < n; ++j) {
+        L(i,j) = cov_matrix(i,j);
+        for (int k = 0; k < i; ++k) {
+           L(i,j) -= L(k,i)*L(k,j);
+        }
+        L(i,j) /= L(i,i);
+     }//j
+  }//i
+
+  // create the transpose of L
+  TMatrixD LT(TMatrixD::kTransposed,L);
+
+  return LT;
+}
+//____________________________________________________________________________
+TVectorD  genie::utils::math::CholeskyGenerateCorrelatedParams (
+    const TMatrixD& cholesky_triangular, TVectorD& mean_params) 
+{
+// Generate a vector of correlated params
+
+  int ncols = cholesky_triangular.GetNcols();
+  int nrows = cholesky_triangular.GetNrows();
+  int npars = mean_params.GetNrows();
+
+  if(ncols != nrows) {
+    LOG("Cholesky", pERROR) 
+        << "Mismatch between number of columns (" << ncols 
+        << ") & rows (" << nrows << ")";
+    exit(1);
+  }
+  if(npars != nrows) {
+    LOG("Cholesky", pERROR) 
+        << "Mismatch between number of parameters (" << npars
+        << ") & array size (" << nrows << ")";
+    exit(1);
+  }
+
+  int n = nrows;
+
+  // create a vector of unit Gaussian variables
+  // and multiply by Lt to introduce the appropriate correlations
+  TVectorD g(n);
+  for (int k = 0; k < n; ++k) {
+    g(k) = RandomGen::Instance()->RndNum().Gaus();
+  }
+  g *= cholesky_triangular;
+
+  // add the mean value offsets and store the results
+  TVectorD correlated_params(n);
+  for (int i = 0; i < n; ++i) {
+     double v = mean_params[i];
+     v += g(i);
+     correlated_params[i] = v;
+  }
+
+  return correlated_params;
+}
+//____________________________________________________________________________
+TVectorD  genie::utils::math::CholeskyGenerateCorrelatedParams (
+ const TMatrixD& cholesky_triangular, TVectorD& mean_params, TVectorD& g_uncorrelated) 
+{
+// Generate a vector of correlated params
+
+  int ncols = cholesky_triangular.GetNcols();
+  int nrows = cholesky_triangular.GetNrows();
+  int npars = mean_params.GetNrows();
+  int nunco = g_uncorrelated.GetNrows();
+
+  if(ncols != nrows) {
+    LOG("Cholesky", pERROR) 
+        << "Mismatch between number of columns (" << ncols 
+        << ") & rows (" << nrows << ")";
+    exit(1);
+  }
+  if(npars != nrows) {
+    LOG("Cholesky", pERROR) 
+        << "Mismatch between number of parameters (" << npars
+        << ") & array size (" << nrows << ")";
+    exit(1);
+  }
+  if(nunco != nrows) {
+    LOG("Cholesky", pERROR) 
+        << "Mismatch between size of uncorrelated parameter vector (" << nunco
+        << ") & array size (" << nrows << ")";
+    exit(1);
+  }
+
+  int n = nrows;
+
+  // create a vector of unit Gaussian variables
+  // and multiply by Lt to introduce the appropriate correlations
+  g_uncorrelated *= cholesky_triangular;
+
+  // add the mean value offsets and store the results
+  TVectorD correlated_params(n);
+  for (int i = 0; i < n; ++i) {
+     double v = mean_params[i];
+     v += g_uncorrelated(i);
+     correlated_params[i] = v;
+  }
+
+  return correlated_params;
+}
+//____________________________________________________________________________
+TVectorD  genie::utils::math::CholeskyGenerateCorrelatedParamVariations (
+    const TMatrixD& cholesky_triangular) 
+{
+  int ncols = cholesky_triangular.GetNcols();
+  int nrows = cholesky_triangular.GetNrows();
+
+  assert(ncols==nrows);
+
+  int n = nrows;
+
+  // create a vector of unit Gaussian variables
+  // and multiply by Lt to introduce the appropriate correlations
+  TVectorD g(n);
+  for (int k = 0; k < n; ++k) {
+    g(k) = RandomGen::Instance()->RndNum().Gaus();
+  }
+  g *= cholesky_triangular;
+
+  return g;
+}
+//____________________________________________________________________________
+TVectorD  genie::utils::math::CholeskyCalculateCorrelatedParamVariations (
+    const TMatrixD& cholesky_triangular, TVectorD & g_uncorrelated) 
+{
+  int ncols = cholesky_triangular.GetNcols();
+  int nrows = cholesky_triangular.GetNrows();
+  int npars = g_uncorrelated.GetNrows();
+
+  assert(ncols==nrows);
+  assert(npars==nrows);
+
+  // create a vector of unit Gaussian variables
+  // and multiply by Lt to introduce the appropriate correlations
+  TVectorD g(g_uncorrelated);
+  g *= cholesky_triangular;
+
+  return g;
+}
 //____________________________________________________________________________
 double genie::utils::math::KahanSummation(double x[], unsigned int n)
 {
