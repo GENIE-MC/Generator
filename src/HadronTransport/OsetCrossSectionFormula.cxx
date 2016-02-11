2dac472a9a7929e18ef90fe85cd849ed9efe03a6
goran <goran>
2016-02-11 17:00:10 +0000

    OsetCrossSection*: updated comments
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@5744 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/HadronTransport/OsetCrossSectionFormula.cxx
+++ src/HadronTransport/OsetCrossSectionFormula.cxx
@@ -11,40 +11,37 @@ const double OsetCrossSectionFormula :: normFactor = 197.327 * 197.327 * 10.0;
 
 const double OsetCrossSectionFormula :: chargedPionMass  = 139.57018; // MeV
 const double OsetCrossSectionFormula :: neutralPionMass  = 134.97666; // MeV
-const double OsetCrossSectionFormula :: chargedPionMass2 = chargedPionMass *
-                                                            chargedPionMass;
-const double OsetCrossSectionFormula :: neutralPionMass2 = neutralPionMass *
-                                                            neutralPionMass;
+const double OsetCrossSectionFormula :: chargedPionMass2 = chargedPionMass * chargedPionMass;
+const double OsetCrossSectionFormula :: neutralPionMass2 = neutralPionMass * neutralPionMass;
                                                             
 const double OsetCrossSectionFormula :: protonMass   = 938.272013; // MeV
 const double OsetCrossSectionFormula :: neutronMass  = 939.565346; // MeV
-const double OsetCrossSectionFormula :: nucleonMass  = (protonMass +
-                                                         neutronMass) / 2.0;
-const double OsetCrossSectionFormula :: nucleonMass2 = nucleonMass *
-                                                        nucleonMass;
+const double OsetCrossSectionFormula :: nucleonMass  = (protonMass + neutronMass) / 2.0;
+const double OsetCrossSectionFormula :: nucleonMass2 = nucleonMass * nucleonMass;
 
 const double OsetCrossSectionFormula :: deltaMass = 1232.0; // MeV
 
 // s-wave parametrization (see sec. 3.1)
 
-const double OsetCrossSectionFormula :: coefSigma[3] = {-0.01334, 0.06889,
-                                                        0.19753};
+const double OsetCrossSectionFormula :: coefSigma[3] = {-0.01334, 0.06889, 0.19753};
 const double OsetCrossSectionFormula :: coefB[3] = {-0.01866, 0.06602, 0.21972};
-const double OsetCrossSectionFormula :: coefD[3] = {-0.08229, 0.37062,
-                                                    -0.03130};
+const double OsetCrossSectionFormula :: coefD[3] = {-0.08229, 0.37062,-0.03130};
 const double OsetCrossSectionFormula :: ImB0 = 0.035;
 
-// delta parametrization coefficients (NuclPhys A468 (1987) 631-652)
+//! delta parametrization coefficients (NuclPhys A468 (1987) 631-652)
 const double OsetCrossSectionFormula ::  coefCQ[3] = { -5.19, 15.35,   2.06};
+//! delta parametrization coefficients (NuclPhys A468 (1987) 631-652)
 const double OsetCrossSectionFormula :: coefCA2[3] = {  1.06, -6.64,  22.66};
+//! delta parametrization coefficients (NuclPhys A468 (1987) 631-652)
 const double OsetCrossSectionFormula :: coefCA3[3] = {-13.46, 46.17, -20.34};
-
-const double OsetCrossSectionFormula :: coefAlpha[3] = { 0.382, -1.322, 1.466};
-const double OsetCrossSectionFormula :: coefBeta[3] = {-0.038,  0.204, 0.613};
+//! delta parametrization coefficients (NuclPhys A468 (1987) 631-652)
+const double OsetCrossSectionFormula :: coefAlpha[3] = {0.382, -1.322, 1.466};
+//! delta parametrization coefficients (NuclPhys A468 (1987) 631-652)
+const double OsetCrossSectionFormula :: coefBeta[3]  = {-0.038,  0.204, 0.613};
 
 using namespace osetUtils;
 
-// set nuclear density and Fermi momentum / energy
+//! @f$ p_{F} = \left(\frac{3}{2}\pi\rho\right)^{1/3} @f$
 void OsetCrossSectionFormula :: setNucleus (const double &density)
 {
   static const double constFactor = 3.0 / 2.0 * M_PI * M_PI;
@@ -53,7 +50,12 @@ void OsetCrossSectionFormula :: setNucleus (const double &density)
   fermiMomentum  = pow (constFactor * nuclearDensity, 1.0 / 3.0) * 197.327;
   fermiEnergy    = sqrt (fermiMomentum*fermiMomentum + nucleonMass2);
 }
-// do kinematics
+
+/*! <ul>
+ *  <li> set pointer to proper mass (charged vs neutral)
+ *  <li> calculate energy, momentum (in LAB and CMS) and invariant mass
+ *  </ul>
+ */ 
 void OsetCrossSectionFormula :: setKinematics (const double &pionTk,
                                                 const bool &isPi0)
 {
@@ -79,7 +81,13 @@ void OsetCrossSectionFormula :: setKinematics (const double &pionTk,
   momentumCMS  = pionMomentum * nucleonMass / invariantMass;
   momentumCMS2 = momentumCMS * momentumCMS;
 }
-// calculate delta width / propagator / self-energy
+
+/*! <ul>
+ *  <li> calculale Delta half width (in nuclear matter)
+ *  <li> calculalte Delta self energy
+ *  <li> calculalte Delta propagator
+ *  </ul>
+ */ 
 void OsetCrossSectionFormula :: setDelta ()
 {
   static const double constFactor = 1.0 / 12.0 / M_PI;
@@ -98,7 +106,13 @@ void OsetCrossSectionFormula :: setDelta ()
 
   deltaPropagator2 = 1.0 / (ReDelta * ReDelta + ImDelta * ImDelta);
 }
-// calculate cross sections
+
+/*! <ul>
+ *  <li> calculalte p- and s-wave absorption cross section
+ *  <li> calculalte total qel (el+cex) p- and s-wave cross section
+ *  <li> calculalte fraction of cex in total qel (based on isospin relation)
+ *  </ul>
+ */
 void OsetCrossSectionFormula :: setCrossSections ()
 {
 
@@ -109,10 +123,10 @@ void OsetCrossSectionFormula :: setCrossSections ()
   // ----- ABSORPTION ----- //
 
   // constant factor for p-wave absorption cross section
-  static const double pAbsorptionFactor = 4.0 / 9.0;
+  static const double pAborptionFactor = 4.0 / 9.0;
 
   // absorption p-wave cross section (see eq. 2.24 and eq. 2.21)
-  const double pXsecAbsorption = pAbsorptionFactor * pXsecCommon *
+  const double pXsecAbsorption = pAborptionFactor * pXsecCommon *
                                  selfEnergyAbsorption;
 
   // constant factor for s-wave absorption cross section 
@@ -169,8 +183,9 @@ void OsetCrossSectionFormula :: setCrossSections ()
   for (unsigned int i = 0; i < nChannels; i++)
     cexCrossSections[i] = pCexFactor[i] * pXsecTotalQel +
                           sCexFactor[i] * sXsecTotalQel;
-}                                  
-// delta width reduction in nuclear medium
+}
+                                  
+//! related to Pauli blocking, see sec. 2.3
 double OsetCrossSectionFormula :: deltaReduction () const
 {
   // assuming nucleon at rest 
@@ -187,7 +202,8 @@ double OsetCrossSectionFormula :: deltaReduction () const
   
   return (mu0*mu0*mu0 + mu0 + 2) / 4.0;
 }
-// calculate delta self energy (eq. 2.21)
+
+//! based on eq. 2.21
 void OsetCrossSectionFormula :: setSelfEnergy ()
 {
   const double x = pionKineticEnergy / *pionMass;
@@ -215,7 +231,13 @@ void OsetCrossSectionFormula :: setSelfEnergy ()
                     pow (densityFraction, alpha);
 }
 
-// set up nucleus, kinematics, Delta (width, propagator) and cross sections
+/*! <ul>
+ *  <li> set up nucleus
+ *  <li> set up kinematics
+ *  <li> set up Delta (width, propagator)
+ *  <li> calculate cross sections
+ *  </ul> 
+ */
 void OsetCrossSectionFormula :: setupOset (const double &density,
                                            const double &pionTk,
                                            const int &pionPDG,
--- src/HadronTransport/OsetCrossSectionFormula.h
+++ src/HadronTransport/OsetCrossSectionFormula.h
@@ -1,6 +1,10 @@
-/*
- * Author: T. Golan
- * Based on: E. Oset et al., Nucl. Phys. A484 (1988) 557-592
+/**
+ * @brief Formula-based implementation of Oset model
+ * 
+ * @author Tomasz Golan
+ * @date 2015
+ * @warning Applicable for pion with Tk < 350 MeV
+ * @remarks Based on E. Oset et al., Nucl. Phys. A484 (1988) 557-592
  * 
 */
 
@@ -14,75 +18,73 @@ class OsetCrossSectionFormula : public OsetCrossSection
 {
   public:
 
-  // use to set up Oset class (assign pion Tk, nuclear density etc)
+  //! use to set up Oset class (assign pion Tk, nuclear density etc)
   void setupOset (const double &density, const double &pionTk,
                   const int &pionPDG, const double &protonFraction);
 
   private:
 
   // constants 
-  static const double couplingConstant; // f*^2
-  static const double chargedPionMass;  // charged pion mass in MeV
-  static const double neutralPionMass;  // charged pion mass in MeV
-  static const double chargedPionMass2; // charged pion mass squared
-  static const double neutralPionMass2; // charged pion mass squared
-  static const double protonMass;       // proton mass in MeV
-  static const double neutronMass;      // neutron mass in MeV
-  static const double nucleonMass;      // average nucleon mass
-  static const double nucleonMass2;     // average nucleon mass squared
-  static const double deltaMass;        // delta mass in MeV
-  static const double normalDensity;    // normal nuclear density [fm-3]
-  static const double normFactor;       // MeV^-2 -> mb
+  static const double couplingConstant; //!< f*^2
+  static const double chargedPionMass;  //!< charged pion mass in MeV
+  static const double neutralPionMass;  //!< charged pion mass in MeV
+  static const double chargedPionMass2; //!< charged pion mass squared
+  static const double neutralPionMass2; //!< charged pion mass squared
+  static const double protonMass;       //!< proton mass in MeV
+  static const double neutronMass;      //!< neutron mass in MeV
+  static const double nucleonMass;      //!< average nucleon mass
+  static const double nucleonMass2;     //!< average nucleon mass squared
+  static const double deltaMass;        //!< delta mass in MeV
+  static const double normalDensity;    //!< normal nuclear density [fm-3]
+  static const double normFactor;       //!< MeV^-2 -> mb
 
-  // s-wave parametrization
-  static const double coefSigma[nChannels]; // see. 3.7
-  static const double coefB[nChannels];     // see. 3.8
-  static const double coefD[nChannels];     // see. 3.8
-  static const double ImB0;                 // see. 3.12
+  static const double coefSigma[nChannels]; //!< s-wave parametrization eq. 3.7
+  static const double coefB[nChannels];     //!< s-wave parametrization eq. 3.8
+  static const double coefD[nChannels];     //!< s-wave parametrization eq. 3.8
+  static const double ImB0;                 //!< s-wave parametrization eq. 3.12
 
-  // delta parametrization coefficients (NuclPhys A468 (1987) 631-652)
-  static const double    coefCQ[nChannels]; // quasi-elastic term
-  static const double   coefCA2[nChannels]; // two-body absorption
-  static const double   coefCA3[nChannels]; // three-body absorption
-  static const double coefAlpha[nChannels]; // see 2.21
-  static const double  coefBeta[nChannels]; // see 2.21
+  static const double    coefCQ[nChannels]; //!< quasi-elastic term (eq. 2.21)
+  static const double   coefCA2[nChannels]; //!< two-body absorption (eq. 2.21)
+  static const double   coefCA3[nChannels]; //!< three-body absorption (eq. 2.21)
+  static const double coefAlpha[nChannels]; //!< alpha (eq. 2.21)
+  static const double  coefBeta[nChannels]; //!< beta (eq. 2.21)
 
   // kinematics variables
-  const double *pionMass;      // pointer to charged / neutral pion mass
-  const double *pionMass2;     // pointer to pion mass ^2
-  double pionMomentum;         // pion momentum in MeV
-  double pionEnergy;           // pion total energy in MeV
-  double momentumCMS;          // momentum in CMS in MeV
-  double momentumCMS2;         // momentum in CMS squared
-  double invariantMass;        // inv mass = sqrt(s mandelstam) in MeV
+  const double *pionMass;  //!< pointer to charged / neutral pion mass
+  const double *pionMass2; //!< pointer to pion mass ^2
+  double pionMomentum;     //!< pion momentum in MeV
+  double pionEnergy;       //!< pion total energy in MeV
+  double momentumCMS;      //!< momentum in CMS in MeV
+  double momentumCMS2;     //!< momentum in CMS squared
+  double invariantMass;    //!< inv mass = sqrt(s mandelstam) in MeV
 
   // delta variables
-  double reducedHalfWidth;     // reduced delta half width in MeV
-  double selfEnergyTotal;      // total delta self energy in MeV
-  double selfEnergyAbsorption; // abs part of delta self energy in MeV
-  double deltaPropagator2;     // |delta propagator|^2 in MeV-2
+  double reducedHalfWidth;     //!< reduced delta half width in MeV
+  double selfEnergyTotal;      //!< total delta self energy in MeV
+  double selfEnergyAbsorption; //!< abs part of delta self energy in MeV
+  double deltaPropagator2;     //!< |delta propagator|^2 in MeV-2
 
   // nucleus variables
-  double fermiMomentum;  // in MeV      
-  double fermiEnergy;    // in MeV
+  double fermiMomentum;  //!< Fermi momentum in MeV      
+  double fermiEnergy;    //!< Fermi energy in MeV
 
   // cross sections
-  double couplingFactor;       // (coupling constant / pion mass)^2
+  double couplingFactor; //!< (coupling constant / pion mass)^2
 
-  // set nuclear density and Fermi momentum / energy
+  //! set nuclear density and Fermi momentum / energy
   void setNucleus    (const double &density);
-  // do kinematics
+  //! do kinematics
   void setKinematics (const double &pionTk, const bool &isPi0);
-  // calculate delta width / propagator / self-energy
+  //! set up Delta
   void setDelta ();
   
-  // calculate delta self energy (eq. 2.21)
+  //! calculate delta self energy 
   void setSelfEnergy ();
 
-  // delta width reduction in nuclear medium
+  //! calculalte delta width reduction in nuclear medium
   double deltaReduction () const;
 
-  // set up cross sections for all channels
+  //! calculalte cross sections for each channel
   void setCrossSections ();
 };
 
