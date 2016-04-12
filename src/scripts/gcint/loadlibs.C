65f7662f724f1bb16bc8c44dfc60863505d6835b
sdennis <s.r.dennis@liverpool.ac.uk>
2016-04-12 13:57:52 +0000

    Make LoadLibs() read the libraries it wants from genie-config rather than being manually maintained
    git-svn-id: file:///home/mroda/Software/Genie/SVN/PhabGlobal/generator/trunk@6064 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/scripts/gcint/loadlibs.C
+++ src/scripts/gcint/loadlibs.C
@@ -20,50 +20,30 @@
   //
   // GENIE libs
   //
-
-  // framework and utilities
-  gSystem->Load("libGMessenger");
-  gSystem->Load("libGRegistry");
-  gSystem->Load("libGAlgorithm");
-  gSystem->Load("libGInteraction");
-  gSystem->Load("libGHEP");
-  gSystem->Load("libGBase");
-  gSystem->Load("libGNumerical");
-  gSystem->Load("libGUtils");
-  gSystem->Load("libGPDG");
-  gSystem->Load("libGBaryonResonance");
-  gSystem->Load("libGEVGCore");
-  gSystem->Load("libGEVGDrivers");
-  gSystem->Load("libGNtuple");
-  gSystem->Load("libGGeo");
-  gSystem->Load("libGFluxDrivers");
-
-  // physics
-  gSystem->Load("libGPDF");
-  gSystem->Load("libGElFF");
-  gSystem->Load("libGDecay");
-  gSystem->Load("libGFragmentation");
-  gSystem->Load("libGNuclear");
-  gSystem->Load("libGLlewellynSmith");
-  gSystem->Load("libGCrossSections");	
-  gSystem->Load("libGCharm");
-  gSystem->Load("libGElas");
-  gSystem->Load("libGGiBUU");
-  gSystem->Load("libGReinSehgal");
-  gSystem->Load("libGQPM");
-  gSystem->Load("libGBodekYang");
-  gSystem->Load("libGEVGModules");
-  gSystem->Load("libGQEL");
-  gSystem->Load("libGRES");
-  gSystem->Load("libGDIS");
-  gSystem->Load("libGCoh");
-  gSystem->Load("libGDfrc");
-  gSystem->Load("libGMEC");
-  gSystem->Load("libGMECTensor");
-  gSystem->Load("libGNuE");
-  gSystem->Load("libGNuGamma");
-//gSystem->Load("libGVLE");
-//gSystem->Load("libGVHE");
-  gSystem->Load("libGHadronTransp");
+  
+  // Read them from genie-config
+  TString command = TString::Format("genie-config --libs");
+  FILE * f = gSystem->OpenPipe(command.Data(),"r");
+  
+  TPRegexp re("-l([\\d\\w]*)");
+  while (true) {
+    TString line;
+    if (!line.Gets(f)) {break;}
+    TObjArray * tokens = line.Tokenize(" ");
+    for (int i = 0 ; i < tokens->GetEntries() ; i++) {
+      TObjString * token_os = static_cast<TObjString*>(tokens->At(i));
+      if (not token_os) {continue;}
+      TObjArray * matches = re.MatchS(token_os->GetString());
+      if (matches->GetEntries()!=2) { continue; }
+      TObjString * libname_os = static_cast<TObjString*>(matches->At(1));
+      if (!libname_os) {continue;}
+      TString full_libname = "lib"+libname_os->GetString();
+      //~ cerr<<full_libname<<endl;
+      gSystem->Load(full_libname.Data());
+      delete matches;
+    }
+    delete tokens;
+  }
+  gSystem->ClosePipe(f);
 }
 
