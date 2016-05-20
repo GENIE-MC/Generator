e92c2ed8a51aa0d5cc9dbb809195491bf1ee6025
dytman <dytman@pitt.edu>
2016-05-20 15:59:55 +0000

    new Nieves QE code
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@6180 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/LlewellynSmith/NievesQELException.cxx
@@ -0,0 +1,71 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2013, GENIE Neutrino MC Generator Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+ or see $GENIE/LICENSE
+
+ Author: Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         STFC, Rutherford Appleton Laboratory
+
+         Steve Dytman <dytman \at pitt.edu>
+	 Univ. of Pittsburgh      
+
+	 Joe Johnston <jpj13 \at pitt.edu>
+	 Univ. of Pittsburgh
+
+\created June 2015
+
+ For the class documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+
+*/
+//____________________________________________________________________________
+
+#include "LlewellynSmith/NievesQELException.h"
+#include "Messenger/Messenger.h"
+
+using std::endl;
+using namespace genie::exceptions;
+
+//___________________________________________________________________________
+namespace genie {
+ namespace exceptions {
+  ostream & operator<< (ostream& stream, const NievesQELException & exc)
+  {
+   exc.Print(stream);
+   return stream;
+  }
+ }
+}
+//___________________________________________________________________________
+NievesQELException::NievesQELException()
+{
+  this->Init();
+}
+//___________________________________________________________________________
+NievesQELException::NievesQELException(const NievesQELException & exc)
+{
+  this->Copy(exc);
+}
+//___________________________________________________________________________
+NievesQELException::~NievesQELException()
+{
+
+}
+//___________________________________________________________________________
+void NievesQELException::Init(void)
+{
+  fReason = "";
+}
+//___________________________________________________________________________
+void NievesQELException::Copy(const NievesQELException & exc)
+{
+  fReason = exc.fReason;
+}
+//___________________________________________________________________________
+void NievesQELException::Print(ostream & stream) const
+{
+  stream << "**EXCEPTION Reason: " << this->ShowReason() << endl;
+}
+//___________________________________________________________________________
---
+++ src/LlewellynSmith/NievesQELException.h
@@ -0,0 +1,66 @@
+//____________________________________________________________________________
+/*!
+
+\class   genie::exceptions::NievesQELException
+
+\brief   An exception thrown by NievesQELCCPXSec for kinematics problems.
+         When failure occurs, set xsec = 0.
+
+\author  Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         STFC, Rutherford Appleton Laboratory
+
+         Steve Dytman <dytman \at pitt.edu>
+	 Univ. of Pittsburgh         
+	 
+	 Joe Johnston <jpj13 \at pitt.edu>
+	 Univ. of Pittsburgh
+
+\created June 2015
+
+\cpright Copyright (c) 2003-2013, GENIE Neutrino MC Generator Collaboration
+         For the full text of the license visit http://copyright.genie-mc.org
+         or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _NIEVES_QEL_EXCEPTION_H_
+#define _NIEVES_QEL_EXCEPTION_H_
+
+#include <string>
+#include <ostream>
+
+#include <TMath.h>
+
+using std::string;
+using std::ostream;
+
+namespace genie {
+namespace exceptions {
+
+class NievesQELException {
+
+public :
+  NievesQELException();
+  NievesQELException(const NievesQELException & exception);
+ ~NievesQELException();
+
+  void SetReason(string reason) { fReason = reason; }
+
+  string ShowReason(void) const { return fReason; }
+
+  void Init  (void);
+  void Copy  (const NievesQELException & exception);
+  void Print (ostream & stream) const;
+
+  friend ostream & operator << (
+             ostream & stream, const NievesQELException & exception);
+
+private:
+
+  string fReason;
+};
+
+}      // exceptions namespace
+}      // genie namespace
+
+#endif // _NIEVES_QEL_EXCEPTION_H_
