3d59e871952a422a2ad0b61aa378799cf221407c
jwolcott <jwolcott@FNAL.GOV>
2016-07-15 15:08:58 +0000

    Use dedicated exception for exceptions inside Interaction/ to avoid circular dependency
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@6352 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Interaction/InteractionException.cxx
@@ -0,0 +1,49 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::exceptions::InteractionException
+
+\brief    Exception used inside Interaction classes.
+
+\author   Jeremy Wolcott <jwolcott \at fnal.gov>
+          Tufts University
+
+\created  July 15, 2016
+
+\cpright  Copyright (c) 2003-2016, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+
+#include <iostream>
+
+#include "InteractionException.h"
+
+namespace genie
+{
+  namespace exceptions
+  {
+
+    InteractionException::InteractionException ()
+      : fReason("")
+    {}
+
+    InteractionException::InteractionException (const std::string& reason)
+      : fReason(reason)
+    {}
+
+    void InteractionException::Print (std::ostream& stream) const
+    {
+      stream << "**EXCEPTION Reason: " << this->ShowReason() << std::endl;
+    }
+
+  } /* namespace exceptions */
+} /* namespace genie */
+
+std::ostream & operator<< (std::ostream& stream, const genie::exceptions::InteractionException & exc)
+{
+ exc.Print(stream);
+ return stream;
+}
---
+++ src/Interaction/InteractionException.h
@@ -0,0 +1,53 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::exceptions::InteractionException
+
+\brief    Exception used inside Interaction classes.
+
+\author   Jeremy Wolcott <jwolcott \at fnal.gov>
+          Tufts University
+
+\created  July 15, 2016
+
+\cpright  Copyright (c) 2003-2016, GENIE Neutrino MC Generator Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef INTERACTIONEXCEPTION_H_
+#define INTERACTIONEXCEPTION_H_
+
+#include <exception>
+#include <iostream>
+#include <string>
+
+namespace genie
+{
+  namespace exceptions
+  {
+    class InteractionException : public std::exception
+    {
+      public:
+        InteractionException ();
+        InteractionException (const std::string & reason);
+
+        void                Print       (std::ostream & stream) const;
+        const std::string & ShowReason  ()                      const { return fReason;     }
+
+        // from std::exception
+        const char * what  () const throw()  { return this->fReason.c_str(); };
+
+        friend std::ostream & operator << (std::ostream & stream, const InteractionException & exception);
+
+      private:
+        std::string fReason;
+    }; /* class InteractionException */
+
+  } /* namespace exceptions */
+} /* namespace genie */
+
+std::ostream & operator<< (std::ostream& stream, const genie::exceptions::InteractionException & exc);
+
+#endif /* INTERACTIONEXCEPTION_H_ */
