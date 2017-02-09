6cd550dc86f685871e9ed0daaf1f55145fe032d0
gnperdue <perdue@fnal.gov>
2017-02-09 19:41:01 +0000

    bump copyright
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@6861 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/QEL/QELEventGenerator.h
+++ src/QEL/QELEventGenerator.h
@@ -11,7 +11,7 @@
 
 \created  August 04, 2014
 
-\cpright  Copyright (c) 2003-2013, GENIE Neutrino MC Generator Collaboration
+\cpright  Copyright (c) 2003-2017, GENIE Neutrino MC Generator Collaboration
           For the full text of the license visit http://copyright.genie-mc.org
           or see $GENIE/LICENSE
 */
