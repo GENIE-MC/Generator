54816f8d45a2def70b67d4dbd52c4223a1fc46a2
gnperdue <perdue@fnal.gov>
2017-02-09 21:20:40 +0000

    bump copyright to 2017
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/branches/R-2_12_4@6867 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/MEC/EmpiricalMECPXSec2015.h
+++ src/MEC/EmpiricalMECPXSec2015.h
@@ -14,7 +14,7 @@
 
 \created  May 05, 2009
 
-\cpright  Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
+\cpright  Copyright (c) 2003-2017, GENIE Neutrino MC Generator Collaboration
           For the full text of the license visit http://copyright.genie-mc.org
           or see $GENIE/LICENSE
 */
--- src/NeutronOsc/NeutronOscUtils.h
+++ src/NeutronOsc/NeutronOscUtils.h
@@ -10,7 +10,7 @@
 
 \created  November 03, 2011
 
-\cpright  Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
+\cpright  Copyright (c) 2003-2017, GENIE Neutrino MC Generator Collaboration
           For the full text of the license visit http://copyright.genie-mc.org
           or see $GENIE/LICENSE
 */
