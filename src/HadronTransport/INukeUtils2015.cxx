70b44df4e821bd3766c3c3a14a06294805700862
dytman <dytman@pitt.edu>
2017-10-06 15:20:09 +0000

    no BE at low energy
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@7160 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/HadronTransport/INukeUtils2015.cxx
+++ src/HadronTransport/INukeUtils2015.cxx
@@ -1,6 +1,6 @@
 //____________________________________________________________________________
 /*
- Copyright (c) 2003-2017, GENIE Neutrino MC Generator Collaboration
+ Copyright (c) 2003-2015, GENIE Neutrino MC Generator Collaboration
  For the full text of the license visit http://copyright.genie-mc.org
  or see $GENIE/LICENSE
 
@@ -651,7 +651,7 @@ bool genie::utils::intranuke2015::TwoBodyCollision(
 
   // Kinematic variables
   
-  double M3, M4; // rest energies, in GeV
+  double M1, M2, M3, M4; // rest energies, in GeV
   double E3L, P3L, E4L, P4L;
   TVector3 tP1L, tPtot, tbeta, tbetadir, tTrans, tVect;
   TVector3 tP1zCM, tP2zCM, tP3L, tP4L;
@@ -666,6 +666,8 @@ bool genie::utils::intranuke2015::TwoBodyCollision(
   Target target(ev->TargetNucleus()->Pdg());
 
   // get mass for particles
+  M1 = pLib->Find(pcode)->Mass();
+  M2 = pLib->Find(tcode)->Mass();
   M3 = pLib->Find(scode)->Mass();
   M4 = pLib->Find(s2code)->Mass();
 
@@ -675,8 +677,14 @@ bool genie::utils::intranuke2015::TwoBodyCollision(
 
   // binding energy
   double bindE = 0.025; // empirical choice, might need to be improved
-  //double bindE = 0.0;
-
+ LOG("TwoBodyCollision",pINFO)
+	<< "pcode = " << pcode << "  " 
+	<< "t4P1L.E, M " << "   " << t4P1L.E()<< "   "<< M1;
+ // avoid problems at low kinetic energy
+ if((pcode==2112||pcode==2212)&&(t4P1L.E()-M1)<.1) bindE = 0.0;
+ if((pcode==211||pcode==-211||pcode==111)&&(t4P1L.E()-M1)<.05) bindE = 0.0;
+ if((pcode==321)&&(t4P1L.E()-M1)<.1) bindE = 0.0;
+ LOG("TwoBodyCollision",pINFO)   << "BE = " << bindE;
   // carry out scattering
   TLorentzVector t4P3L, t4P4L;
   if (!TwoBodyKinematics(M3,M4,t4P1L,t4P2L,t4P3L,t4P4L,C3CM,RemnP4,bindE))
