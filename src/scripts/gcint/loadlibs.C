186550fc728390c9aede55d72a51bc0eb8d5ce70
sdennis <s.r.dennis@liverpool.ac.uk>
2017-12-07 12:29:08 +0000

    small refactor of loadlibs to move th code that automatically generates a list of libraries to load based off the config --libs command
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@7202 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/scripts/gcint/loadlibs.C
+++ src/scripts/gcint/loadlibs.C
@@ -1,3 +1,35 @@
+Long_t load_libs_from_command(const char * list_libs_command, bool verbose = false)
+{ // Takes a linker-style list of libs (eg "-lmygreatlib -lmylessgoodlib" )
+  // and loads each using gSystem::Load() 
+  // Returns 0 on success, 1 on failure
+  Long_t status = 0;
+  FILE * f = gSystem->OpenPipe(list_libs_command,"r");
+  
+  TPRegexp re("-l([\\d\\w]*)");
+  while (true) {
+    TString line;
+    if (!line.Gets(f)) {break;}
+    TObjArray * tokens = line.Tokenize(" ");
+    for (int i = 0 ; i < tokens->GetEntries() ; i++) {
+      TObjString * token_os = static_cast<TObjString*>(tokens->At(i));
+      if (!token_os) {continue;}
+      TObjArray * matches = re.MatchS(token_os->GetString());
+      if (matches->GetEntries()!=2) { continue; }
+      TObjString * libname_os = static_cast<TObjString*>(matches->At(1));
+      if (!libname_os) {continue;}
+      TString full_libname = "lib"+libname_os->GetString();
+      Long_t this_status = gSystem->Load(full_libname.Data());
+      if (verbose) std::cout<<full_libname<<":\t"<<this_status<<std::endl;
+      status = (status || this_status);
+      delete matches;
+    }
+    delete tokens;
+  }
+  gSystem->ClosePipe(f);
+  return status;
+}
+  
+int loadlibs()
 {
   TString libs0 = gSystem->GetDynamicPath();
   TString libs  = libs0 + ":/usr/local/lib64:/usr/lib64:/lib64:"+
@@ -21,37 +53,13 @@
   // GSL
   gSystem->Load("libgslcblas");
   gSystem->Load("libgsl");
+  //~ load_libs_from_command("gsl-config --libs"); // not guaranteed to be in the right order
 
   //
   // GENIE libs
   //
   
-  Long_t status = 0;
-  
-  // Read them from genie-config
-  TString command = TString::Format("genie-config --libs");
-  FILE * f = gSystem->OpenPipe(command.Data(),"r");
-  
-  TPRegexp re("-l([\\d\\w]*)");
-  while (true) {
-    TString line;
-    if (!line.Gets(f)) {break;}
-    TObjArray * tokens = line.Tokenize(" ");
-    for (int i = 0 ; i < tokens->GetEntries() ; i++) {
-      TObjString * token_os = static_cast<TObjString*>(tokens->At(i));
-      if (!token_os) {continue;}
-      TObjArray * matches = re.MatchS(token_os->GetString());
-      if (matches->GetEntries()!=2) { continue; }
-      TObjString * libname_os = static_cast<TObjString*>(matches->At(1));
-      if (!libname_os) {continue;}
-      TString full_libname = "lib"+libname_os->GetString();
-      //~ cerr<<full_libname<<endl;
-      status = status || gSystem->Load(full_libname.Data());
-      delete matches;
-    }
-    delete tokens;
-  }
-  gSystem->ClosePipe(f);
+  Long_t status = load_libs_from_command("genie-config --libs");
   
   if (status) {
     std::cout<<"Failed to load GENIE libraries"<<std::endl;
