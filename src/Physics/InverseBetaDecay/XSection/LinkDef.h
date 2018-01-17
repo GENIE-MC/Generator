bdf3cfc44baa43dd301f8e29259282766b362475
candreop <costas.andreopoulos@stfc.ac.uk>
2018-01-17 14:22:38 +0000

    cont'd source code re-structuring
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/devel/branches/v3config2@7238 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Physics/InverseBetaDecay/XSection/LinkDef.h
@@ -0,0 +1,17 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace genie;
+
+#pragma link C++ class genie::IBDInteractionListGenerator+;
+#pragma link C++ class genie::IBDKinematicsGenerator+;
+#pragma link C++ class genie::IBDHadronicSystemGenerator+;
+#pragma link C++ class genie::IBDPrimaryLeptonGenerator+;
+#pragma link C++ class genie::StrumiaVissaniIBDPXSec+;
+#pragma link C++ class genie::KLVOxygenIBDPXSec+;
+#pragma link C++ class genie::IBDXSecMap+;
+
+#endif
