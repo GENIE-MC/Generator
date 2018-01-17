f59f2323b213b76162717683dd94b9eab2903321
mroda <mroda@liverpool.ac.uk>
2018-01-17 14:23:08 +0000

    Filling DIS subdir
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/devel/branches/v3config2@7239 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Physics/DeepInelastic/XSection/LinkDef.h
+++ src/Physics/DeepInelastic/XSection/LinkDef.h
@@ -11,4 +11,7 @@
 #pragma link C++ class genie::QPMDISStrucFunc;
 #pragma link C++ class genie::QPMDISPXSec;
 
+#pragma link C++ class genie::BYPDF;
+#pragma link C++ class genie::BYStrucFunc;
+
 #endif
