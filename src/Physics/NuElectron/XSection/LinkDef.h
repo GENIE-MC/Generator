10d489315af1f114948510952c2312bb02f7e38f
candreop <costas.andreopoulos@stfc.ac.uk>
2018-01-17 14:31:06 +0000

    cont'd code re-structuring
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/devel/branches/v3config2@7242 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Physics/NuElectron/XSection/LinkDef.h
+++ src/Physics/NuElectron/XSection/LinkDef.h
@@ -9,9 +9,5 @@
 #pragma link C++ class genie::IMDAnnihilationPXSec;
 #pragma link C++ class genie::BardinIMDRadCorPXSec;
 #pragma link C++ class genie::NuElectronPXSec;
-#pragma link C++ class genie::NuEInteractionListGenerator;
-#pragma link C++ class genie::NuEKinematicsGenerator;
-#pragma link C++ class genie::NuEPrimaryLeptonGenerator;
-#pragma link C++ class genie::NuETargetRemnantGenerator;
 
 #endif
