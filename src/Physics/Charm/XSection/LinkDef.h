3726c168d999e47f74830f1b23ff3eeb70b726ce
mroda <mroda@liverpool.ac.uk>
2018-01-17 14:32:09 +0000

    Filling Charm subdir
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/devel/branches/v3config2@7243 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Physics/Charm/XSection/LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace genie;
+
+#pragma link C++ class genie::AivazisCharmPXSecLO;
+#pragma link C++ class genie::KovalenkoQELCharmPXSec;
+#pragma link C++ class genie::SlowRsclCharmDISPXSecLO;
+
+#endif
