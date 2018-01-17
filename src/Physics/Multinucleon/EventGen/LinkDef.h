a0bd8652fe215aa5ae8388439a86e1d6429abcbd
mroda <mroda@liverpool.ac.uk>
2018-01-17 13:29:38 +0000

    Filling Multinucleon subdir
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/devel/branches/v3config2@7236 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Physics/Multinucleon/EventGen/LinkDef.h
+++ src/Physics/Multinucleon/EventGen/LinkDef.h
@@ -7,12 +7,7 @@
 #pragma link C++ namespace genie;
 #pragma link C++ namespace genie::utils::mec;
 
-#pragma link C++ class genie::MECHadronTensor;
-#pragma link C++ class genie::NievesSimoVacasMECPXSec2016;
-#pragma link C++ class genie::EmpiricalMECPXSec2015;
-
 #pragma link C++ class genie::MECInteractionListGenerator;
 #pragma link C++ class genie::MECGenerator;
-#pragma link C++ class genie::MECXSec;
 
 #endif
