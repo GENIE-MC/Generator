4a083924dd129ec3a9796261f90bac6c021e2969
mroda <mroda@liverpool.ac.uk>
2018-01-19 14:17:45 +0000

    Restore Ntuple
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/devel/branches/v3config2@7365 cc9776de-3512-45ca-aafc-e2d9ed43c22c

---
+++ src/Framework/Ntuple/LinkDef.h
@@ -0,0 +1,18 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ namespace genie;
+
+#pragma link C++ class genie::NtpMCTreeHeader;
+#pragma link C++ class genie::NtpMCDTime;
+#pragma link C++ class genie::NtpMCJobEnv;
+#pragma link C++ class genie::NtpMCJobConfig;
+#pragma link C++ class genie::NtpMCRecHeader;
+#pragma link C++ class genie::NtpMCRecordI;
+#pragma link C++ class genie::NtpMCEventRecord;
+#pragma link C++ class genie::NtpWriter;
+
+#endif
