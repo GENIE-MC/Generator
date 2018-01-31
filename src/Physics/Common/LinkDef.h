4dc4574561f26f6e819044c3181df7bc4e320ba0
candreop <costas.andreopoulos@stfc.ac.uk>
2018-01-31 09:53:56 +0000

    Fix Error: link requested for unknown class genie::FermiMover LinkDef.h:9: Error: link requested for unknown class genie::PauliBlocker LinkDef.h:10: Error: link requested for unknown class genie::NucBindEnergyAggregator LinkDef.h:12: Error: link requested for unknown class genie::HadronTransporter LinkDef.h:15: Error: link requested for unknown class genie::UnstableParticleDecayer LinkDef.h:16:
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/trunk@7404 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/Physics/Common/LinkDef.h
+++ src/Physics/Common/LinkDef.h
@@ -6,14 +6,14 @@
 
 #pragma link C++ namespace genie;
 
-#pragma link C++ class genie::FermiMover;
-#pragma link C++ class genie::PauliBlocker;
-#pragma link C++ class genie::NucDeExcitationSim;
-#pragma link C++ class genie::NucBindEnergyAggregator;
+//#pragma link C++ class genie::FermiMover;
+//#pragma link C++ class genie::PauliBlocker;
+//#pragma link C++ class genie::NucDeExcitationSim;
+//#pragma link C++ class genie::NucBindEnergyAggregator;
 #pragma link C++ class genie::InitialStateAppender;
 #pragma link C++ class genie::VertexGenerator;
-#pragma link C++ class genie::HadronTransporter;
-#pragma link C++ class genie::UnstableParticleDecayer;
+//#pragma link C++ class genie::HadronTransporter;
+//#pragma link C++ class genie::UnstableParticleDecayer;
 #pragma link C++ class genie::PrimaryLeptonGenerator;
 #pragma link C++ class genie::HadronicSystemGenerator;
 #pragma link C++ class genie::KineGeneratorWithCache;
