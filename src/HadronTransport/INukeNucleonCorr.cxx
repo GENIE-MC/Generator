d154450a9de6ea722745f330f5510c683086c0cb
gnperdue <perdue@fnal.gov>
2018-02-06 16:14:58 +0000

    back-port a patch changing a gSystem call to std::getenv
    git-svn-id: file:///home/mroda/Software/Genie/NewGlobal/generator/branches/R-2_12_8@7417 cc9776de-3512-45ca-aafc-e2d9ed43c22c

--- src/HadronTransport/INukeNucleonCorr.cxx
+++ src/HadronTransport/INukeNucleonCorr.cxx
@@ -26,7 +26,7 @@ INukeNucleonCorr* INukeNucleonCorr::fInstance = NULL; // initialize instance wit
 const int NColumns  = 18;
 const int NRows = 205;
        
-string genie_dir = string(gSystem->Getenv("GENIE"));
+string genie_dir(std::getenv("GENIE"));  // = string(gSystem->Getenv("GENIE"));
 //string directory = genie_dir + string("/data/evgen/nncorr/");
 string dir = genie_dir + string("/data/evgen/nncorr/");
 string infile;
