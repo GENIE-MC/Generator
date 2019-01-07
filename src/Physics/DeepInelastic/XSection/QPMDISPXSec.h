e9bd0ea26abc126d0dce6e7f1a8650634d98f39d
Marco Roda <marco.roda.88@gmail.com>
2019-01-07 15:10:42 +0000

    Cleaned the old DIS model so that it's the pure one

--- src/Physics/DeepInelastic/XSection/QPMDISPXSec.h
+++ src/Physics/DeepInelastic/XSection/QPMDISPXSec.h
@@ -50,7 +50,6 @@ public:
 
 private:
   void   LoadConfig                  (void);
-  double DISRESJoinSuppressionFactor (const Interaction * in) const;
 
   mutable DISStructureFunc fDISSF;
   bool                     fInInitPhase;
@@ -61,9 +60,7 @@ private:
 
   const XSecAlgorithmI * fCharmProdModel;
 
-  bool   fUsingDisResJoin;  ///< use a DIS/RES joining scheme?
   bool   fUseCache;         ///< cache reduction factors used in joining scheme
-  double fWcut;             ///< apply DIS/RES joining scheme < Wcut
   double fScale;            ///< cross section scaling factor
   double fSin48w;           ///< sin^4(Weingberg angle)
 };
