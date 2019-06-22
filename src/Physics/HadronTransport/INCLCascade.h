140b869a80246e5a090522877ad8da199fa23d65
narimarc <narymarc@yahoo.com>
2019-06-22 13:36:45 +0300

    fix bugs

--- src/Physics/HadronTransport/INCLCascade.h
+++ src/Physics/HadronTransport/INCLCascade.h
@@ -21,59 +21,52 @@ class TLorentzVector;
 class TVector3;
 //string kDefOptEvFilePrefix="gntp.inuke";
 
-namespace genie{
+//using namespace G4INCL;
 
- class GHepParticle;
- class INukeHadroData;
- class PDGCodeList;
- class HINCLCascade;
- class INCLCascade: public EventRecordVisitorI{
+namespace genie {
 
+  class GHepParticle;
+  class INukeHadroData;
+  class PDGCodeList;
+  class HINCLCascade;
+
+  class INCLCascade: public EventRecordVisitorI {
 
   public :
-  INCLCascade();
-  INCLCascade(string name);
-  INCLCascade(string name, string config);
-  ~INCLCascade();
-  int INCLcascade(int arg, char * test[],GHepRecord * event_rec)const ;
-//  int INCLtopdgcode(int A, int Z)const;
-  int pdgcpiontoA(int pdgc)const;
-  int pdgcpiontoZ(int pdgc)const;
-    //void INCLprint() const;
+    INCLCascade();
+    INCLCascade(string name);
+    INCLCascade(string name, string config);
+    ~INCLCascade();
+
+    int doCascade(int nflags, const char * flags[], GHepRecord * event_rec) const;
+
+    int pdgcpiontoA(int pdgc) const;
+    int pdgcpiontoZ(int pdgc) const;
+
     // implement the EventRecordVisitorI interface
 
-  void Configure (const Registry & config);
-  void Configure (string param_set);
+    void Configure (const Registry & config);
+    void Configure (string param_set);
 
-  virtual void ProcessEventRecord(GHepRecord * event_rec) const;
-
-
- // int INCLpartycleSpecietoPDGCODE(G4INCL::Config *theConfig)const;
- // int INCLtopdgcode(int A, int Z)const;
- // G4INCL::ParticleType toINCLparticletype(int pdgc)const;
-  //GHepParticle *INCLtoGenieParticle(G4INCL::EventInfo result, int nP, GHepStatus_t    ist, int mom1, int mom2)const;
-
-    //virtual void ProcessEventRecordI(int arg, char *test[]) const;
-
-    // override the Algorithm::Configure methods to load configuration
-    // data to protected data members
+    virtual void ProcessEventRecord(GHepRecord * event_rec) const;
 
 
+  protected:
+    bool CanRescatter(const GHepParticle * p) const;
+    bool IsInNucleus(const GHepParticle * p) const;
+    void TransportHadrons(GHepRecord * evrec) const;
+    bool NeedsRescattering(const GHepParticle * p) const;
+    virtual void LoadConfig (void) = 0;
 
-protected:
- bool CanRescatter(const GHepParticle * p) const;
- bool IsInNucleus(const GHepParticle * p) const;
- void TransportHadrons(GHepRecord * evrec) const;
- bool NeedsRescattering(const GHepParticle * p) const;
-virtual void LoadConfig (void)=0;
-   mutable int            fRemnA;         ///< remnant nucleus A
-   mutable int            fRemnZ;         ///< remnant nucleus Z
-   mutable double         fTrackingRadius;
-   mutable TLorentzVector fRemnP4;        ///< P4 of remnant system
-   mutable GEvGenMode_t   fGMode;
-   double       fR0;           ///< effective nuclear size param
-   double       fNR;
- };
+    mutable int            fRemnA;         ///< remnant nucleus A
+    mutable int            fRemnZ;         ///< remnant nucleus Z
+    mutable double         fTrackingRadius;
+    mutable TLorentzVector fRemnP4;        ///< P4 of remnant system
+    mutable GEvGenMode_t   fGMode;
+    double       fR0;           ///< effective nuclear size param
+    double       fNR;
+  };
+
 }
 
 #endif
