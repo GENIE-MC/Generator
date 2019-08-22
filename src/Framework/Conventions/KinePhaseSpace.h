40c67d87ad23152a8dbc4989619faefd6bc70c1d
Kathryn Sutton <ksutton@geniegpvm01.fnal.gov>
2019-08-22 09:49:07 -0500

    typo fix

--- src/Framework/Conventions/KinePhaseSpace.h
+++ src/Framework/Conventions/KinePhaseSpace.h
@@ -113,7 +113,7 @@ public:
       case(kPSlogQ2logyfE): return "<{Q2,y}|E>";      break;
       case(kPSTlctl)      : return "<{Tl,cos(theta_l)}|E>";     break;
       case(kPSElOlOpifE)  : return "<{El,Omega_l,Omega_pi}|E>"; break;
-      case(kPSEgTlOgfE)   : return "<{E_gamma,Theta_l,Omega_gamma}|E>"; break;
+      case(kPSEgTlOgfE)   : return "<{Egamma,Theta_l,Omega_gamma}|E>"; break;
       case(kPSElOlTpifE)  : return "<{El,Omega_l,Theta_pi}|E>"; break;
       case(kPSTkTlctl)    : return "<{Tk,Tl,cos(theta_l)}|E>";  break;
       case(kPSQ2vfE)      : return "<{Q2,v}|E>"; break;
