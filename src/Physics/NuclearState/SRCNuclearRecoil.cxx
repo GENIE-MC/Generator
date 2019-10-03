a8d01a8616447a035f4641f394c06cc0ec60e0a9
afropapp13 <afropapp@yahoo.gr>
2019-10-03 13:21:35 -0500

    refactorization of FermiMover to accomodate the possibility of an SRC recoil nucleon

---
+++ src/Physics/NuclearState/SRCNuclearRecoil.cxx
@@ -0,0 +1,200 @@
+///____________________________________________________________________________
+/*
+ Copyright (c) 2003-2019, The GENIE Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+ or see $GENIE/LICENSE
+
+ Author: Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
+         University of Liverpool & STFC Rutherford Appleton Lab - October 08, 2004
+
+ For the class documentation see the corresponding header file.
+
+ Important revisions after version 2.0.0 :
+ @ Feb 07, 2009 - CA
+   Added option to simulate the recoil nucleon due to short range corelation.
+   The recoil nucleon is added in case that the selected hit nucleon has a
+   momentum selected from the NN corelation tail. The 2 nucleons are put
+   back-to-back. For the time-being using hard-coded relative fractions for
+   the nn, pp, np pairs.
+   The code for adding the recoil nuclear target at the GHEP record was moved
+   into this processing step.
+
+ @ Mar 18, 2016- Joe Johnston (SD)
+   Call GenerateNucleon() with a target and a radius, so the local Fermi
+   gas model can access the radius.
+   Added a check to see if a local Fermi gas model is being used. If so,
+   use a local Fermi gas model when deciding whether to eject a recoil nucleon.
+*/
+//____________________________________________________________________________
+
+#include <cstdlib>
+
+#include <TLorentzVector.h>
+#include <TVector3.h>
+#include <TParticlePDG.h>
+#include <TMath.h>
+
+#include "Framework/Algorithm/AlgFactory.h"
+#include "Framework/Algorithm/AlgConfigPool.h"
+#include "Framework/Conventions/Constants.h"
+#include "Framework/Conventions/Units.h"
+#include "Physics/NuclearState/SRCNuclearRecoil.h"
+
+#include "Physics/NuclearState/NuclearModel.h"
+#include "Physics/NuclearState/NuclearModelI.h"
+#include "Framework/EventGen/EVGThreadException.h"
+#include "Framework/GHEP/GHepRecord.h"
+#include "Framework/GHEP/GHepParticle.h"
+#include "Framework/GHEP/GHepStatus.h"
+#include "Framework/GHEP/GHepFlags.h"
+#include "Framework/Interaction/Interaction.h"
+#include "Framework/Messenger/Messenger.h"
+#include "Physics/NuclearState/FermiMomentumTablePool.h"
+#include "Physics/NuclearState/FermiMomentumTable.h"
+#include "Framework/Numerical/RandomGen.h"
+#include "Framework/ParticleData/PDGLibrary.h"
+#include "Framework/ParticleData/PDGUtils.h"
+#include "Framework/ParticleData/PDGCodes.h"
+#include "Framework/Utils/KineUtils.h"
+#include "Physics/NuclearState/NuclearUtils.h"
+
+using namespace genie;
+using namespace genie::constants;
+
+//___________________________________________________________________________
+SRCNuclearRecoil::SRCNuclearRecoil() :
+EventRecordVisitorI("genie::SRCNuclearRecoil")
+{
+
+}
+//___________________________________________________________________________
+SRCNuclearRecoil::SRCNuclearRecoil(string config) :
+EventRecordVisitorI("genie::SRCNuclearRecoil", config)
+{
+
+}
+//___________________________________________________________________________
+SRCNuclearRecoil::~SRCNuclearRecoil()
+{
+
+}
+//___________________________________________________________________________
+void SRCNuclearRecoil::ProcessEventRecord(GHepRecord * evrec) const
+{
+
+  Interaction *  interaction = evrec       -> Summary();
+  InitialState * init_state  = interaction -> InitStatePtr();
+  Target *       tgt         = init_state  -> TgtPtr();
+
+  // do nothing for non-nuclear targets
+  if(!tgt->IsNucleus()) return;
+
+  // access the hit nucleon and target nucleus at the GHEP record
+  GHepParticle * nucleon = evrec->HitNucleon();
+  GHepParticle * nucleus = evrec->TargetNucleus();
+  assert(nucleon);
+  assert(nucleus);
+
+  TVector3 p3 = fNuclModel->Momentum3();
+  double pF2 = p3.Mag2(); // (fermi momentum)^2
+
+  // Set this to either a proton or neutron to eject a secondary particle
+  int eject_nucleon_pdg = this->SRCRecoilPDG(nucleon, nucleus, tgt, pF2);
+
+  if (eject_nucleon_pdg != 0) {
+
+    LOG("SRCNuclearRecoil", pINFO) << "Adding a recoil nucleon due to short range corelation";
+
+    GHepStatus_t status = kIStHadronInTheNucleus;
+    int imom = evrec->TargetNucleusPosition();
+
+    //-- Has opposite momentum from the struck nucleon
+    double vx = nucleon->Vx();
+    double vy = nucleon->Vy();
+    double vz = nucleon->Vz();
+    double px = -1.* nucleon->Px();
+    double py = -1.* nucleon->Py();
+    double pz = -1.* nucleon->Pz();
+    double M    = PDGLibrary::Instance()->Find(eject_nucleon_pdg)->Mass();
+    double E    = TMath::Sqrt(px*px+py*py+pz*pz+M*M);
+
+    evrec->AddParticle(
+        eject_nucleon_pdg, status, imom, -1, -1, -1, px, py, pz, E, vx, vy, vz, 0);
+
+  }
+
+}
+//___________________________________________________________________________
+int SRCNuclearRecoil::SRCRecoilPDG(GHepParticle * nucleon, GHepParticle * nucleus, Target* tgt, double pF2) const {
+
+      int eject_nucleon_pdg = 0;
+
+      const int nucleus_pdgc = nucleus->Pdg();
+      const int nucleon_pdgc = nucleon->Pdg();
+
+      // Calculate the Fermi momentum, using a local Fermi gas if the
+      // nuclear model is LocalFGM, and RFG otherwise
+      double kF;
+      if(fNuclModel->ModelType(*tgt) == kNucmLocalFermiGas){
+	assert(pdg::IsProton(nucleon_pdgc) || pdg::IsNeutron(nucleon_pdgc));
+	int A = tgt->A();
+	bool is_p = pdg::IsProton(nucleon_pdgc);
+	double numNuc = (is_p) ? (double)tgt->Z():(double)tgt->N();
+	double radius = nucleon->X4()->Vect().Mag();
+	double hbarc = kLightSpeed*kPlankConstant/genie::units::fermi;
+	kF= TMath::Power(3*kPi2*numNuc*
+		  genie::utils::nuclear::Density(radius,A),1.0/3.0) *hbarc;
+      }else{
+        kF = fKFTable->FindClosestKF(nucleus_pdgc, nucleon_pdgc);
+      }
+      if (TMath::Sqrt(pF2) > kF) {
+
+        double Pp = (nucleon->Pdg() == kPdgProton) ? fPPPairPercentage : fPNPairPercentage;
+        RandomGen * rnd = RandomGen::Instance();
+        double prob = rnd->RndGen().Rndm();
+        eject_nucleon_pdg = (prob > Pp) ? kPdgNeutron : kPdgProton;
+
+      }
+
+      return eject_nucleon_pdg;
+}
+//___________________________________________________________________________
+void SRCNuclearRecoil::Configure(const Registry & config)
+{
+  Algorithm::Configure(config);
+  this->LoadConfig();
+}
+//____________________________________________________________________________
+void SRCNuclearRecoil::Configure(string config)
+{
+  Algorithm::Configure(config);
+  this->LoadConfig();
+}
+//____________________________________________________________________________
+void SRCNuclearRecoil::LoadConfig(void)
+{
+  RgKey nuclkey = "NuclearModel";
+  fNuclModel = 0;
+  fNuclModel = dynamic_cast<const NuclearModelI *> (this->SubAlg(nuclkey));
+  assert(fNuclModel);
+
+  this->GetParamDef("PNPairPercentage",       fPNPairPercentage,    0.95);
+
+  if (fPNPairPercentage < 0. || fPNPairPercentage > 1.) { 
+
+	LOG("SRCNuclearRecoil", pFATAL)
+	<< "PNPairPercentage either less than 0 or greater than 1: Exiting" ;
+
+	exit(78); 
+  }
+
+  fPPPairPercentage = 1. - fPNPairPercentage;
+
+  // Get the Fermi momentum table for relativistic Fermi gas
+  GetParam( "FermiMomentumTable", fKFTableName ) ;
+  fKFTable = 0;
+  FermiMomentumTablePool * kftp = FermiMomentumTablePool::Instance();
+  fKFTable = kftp->GetTable(fKFTableName);
+  assert(fKFTable);
+}
+//____________________________________________________________________________
