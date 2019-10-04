b3e4e40df052368482b10d35324712fbd0d36044
afropapp13 <afropapp@yahoo.gr>
2019-10-04 14:30:22 -0500

    removing and editting comments for the new SRCNuclearRecoil module

--- src/Physics/NuclearState/SRCNuclearRecoil.h
+++ src/Physics/NuclearState/SRCNuclearRecoil.h
@@ -3,7 +3,6 @@
 
 \class    genie::SRCNuclearRecoil
 
-<<<<<<< 25269eb370b20cd2d9b07b10212aa503c67430cd
 \brief       Created this new module that controls the addition of the recoil nucleon in the event record 
              and extracts its kinematics
 
@@ -11,16 +10,6 @@
           Massachusetts Institute of Technology - October 04, 2019
 
 \created  October 04, 2019
-=======
-\brief    It visits the event record & computes a Fermi motion momentum for
-          initial state nucleons bound in nuclei.
-          Is a concrete implementation of the EventRecordVisitorI interface.
-
-\author   Costas Andreopoulos <costas.andreopoulos \at stfc.ac.uk>
-          University of Liverpool & STFC Rutherford Appleton Lab
-
-\created  October 08, 2004
->>>>>>> refactorization of FermiMover to accomodate the possibility of an SRC recoil nucleon
 
 \cpright  Copyright (c) 2003-2019, The GENIE Collaboration
           For the full text of the license visit http://copyright.genie-mc.org
@@ -59,24 +48,8 @@ private:
 
   int SRCRecoilPDG(GHepParticle * nucleon, GHepParticle * nucleus, Target* tgt, double pF2) const; // determine the PDG code of the SRC pair
 
-<<<<<<< 25269eb370b20cd2d9b07b10212aa503c67430cd
   void LoadConfig (void);
 
-=======
-//  void Emit2ndNucleonFromSRC   (GHepRecord * evrec,
-//                                const int eject_nucleon_pdg) const;
-//                                ///^ emit a 2nd nucleon due to short range corellations
-
-//  void KickHitNucleon          (GHepRecord * evrec) const; ///< give hit nucleon a momentum
-
-//  void AddTargetNucleusRemnant (GHepRecord * evrec) const; ///< add a recoiled nucleus remnant
-
-  void LoadConfig (void);
-
-//  bool  fKeepNuclOnMassShell;          ///< keep hit bound nucleon on the mass shell?
-//  bool  fSRCRecoilNucleon;             ///< simulate recoil nucleon due to short range corellation?
-
->>>>>>> refactorization of FermiMover to accomodate the possibility of an SRC recoil nucleon
   const NuclearModelI *  fNuclModel;   ///< nuclear model
 
   double fPPPairPercentage;
