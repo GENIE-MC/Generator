6dc08b65c4c85f5c0f440a5e8a524c628ea4009c
Marco Roda <marco.roda.88@gmail.com>
2019-11-05 16:59:10 +0100

    Draft of the test done

--- src/contrib/delta_decay/plots.C
+++ src/contrib/delta_decay/plots.C
@@ -2,9 +2,19 @@
 #include "TFile.h"
 
 #include "Framework/Ntuple/NtpMCTreeHeader.h" 
+#include "Framework/GHEP/EventRecord.h" 
+#include "Framework/ParticleData/BaryonResUtils.h"
+
+#include "Framework/ParticleData/PDGUtils.h"
+
+using namespace genie ;
 
 
-int make_plots( TString file_name ) {
+int FindDelta( const EventRecord & ) ;
+
+
+
+int make_plots( TString file_name, TString flag ) {
 
   
   TFile in_file( file_name );
@@ -14,6 +24,13 @@ int make_plots( TString file_name ) {
   // Get the GENIE GHEP tree and set its branch address
   TTree * tree = dynamic_cast<TTree*> ( file.Get("gtree") );
   
+  TH1D h_theta( "h_theta", flag + " #theta distribution", 100, 0., TMath::Pi() ) ;
+  TH1D h_phi  ( "h_phi",   flag + " #varphi distribution", 100, 0., TMath::Pi() ) ;
+  
+  TH1D h_tp( "h_theta_phi", flag + " (#theta, #varphi) distribution", 
+	     50, 0., TMath::Pi(),
+	     50, 0., TMath::Pi() ) ;
+
   NtpMCEventRecord * mcrec = 0;
   tree->SetBranchAddress( "gmcrec", &mcrec);
 
@@ -23,13 +40,99 @@ int make_plots( TString file_name ) {
     tree->GetEntry(i);
     // print-out the event
     
-    EventRecord & event = *(mcrec->event);
+    EventRecord & event = *( mcrec->event );
     
+    const Interaction & inter = *( event.Summary() ) ;
+
+    const ProcessInfo & proc_info = inter.ProcInfo() ;
+
+    if ( proc_info.IsResonant() ) { 
+
+      const XclsTag & xcl_tag = inter.ExclTag() ;
+
+      Resonance_t res = xcl_tag.Resonance() ;
+
+      if ( utils::res::IsDelta( res )  ) {
+
+	int delta_pos = FindDelta( event ) ;
+
+	if ( delta_pos < 0 ) { 
+	  cerr << "found delta event with no delta" << std::endl ; 
+	  break ; 
+	}
+
+	GHepParticle * delta = event.Particle( delta_pos ) ; 
+
+	uint n_d = delta -> LastDaughter() - delta -> FirstDaughter() + 1 ; 
+
+	if ( n_d == 2 ) {
+
+	  GHepParticle * d1 = delta -> FirstDaughter() ;
+	  GHepParticle * d2 = delta -> LastDaughter() ;
+
+	  if ( ( pdg::IsPion( d1 -> Pdg() )    || pdg::IsPion( d2 -> Pdg() ) ) && 
+	       ( pdg::IsNucleon( d1 -> Pdg() ) || pdg::IsNucleon( d2 -> Pdg() ) ) ) {
+	    
+	    GHepParticle * pion = pdg::IsPion( d1 -> Pdg() ) ? d1 : d2 ;
+	    //GHepParticle * nucl = pdg::IsNucleon( d1 -> Pdg() ) ? d1 : d2 ;
+	    
+	    const TLorentzVector & delta_mom = *( delta -> P4() ) ;
+	    
+	    TLorentzVector pion_mom = * ( pion -> P4() ) ;
+	    //TLorentzVector nucl_mom = * ( nucl -> P4() ) ;
+	    
+	    TLorentzVector in_lep_p4( * (event -> Probe()-> P4() ) ) ;
+	    
+	    in_lep_p4.Boost( - delta_mom.BoostVector() ) ;
+
+	    TLorentzVector out_lep_p4 = *( event -> FinalStatePrimaryLepton() -> P4() ) ;
+	    out_lep_p4.Boost( - delta_mom.BoostVector() ) ;
+
+	    TLorentzVector q = in_lep_p4 - out_lep_p4 ;
+	    
+	    pion_mom.Boost( - delta_mom.BoostVector() );  // this gives us the pion in the Delta reference frame
+
+	    TVector3 pion_dir = pion.Vect().Unit() ;
+	    TVector3 z_axis = q.Vect().Unit() ;
+
+	    TVector3 y_axis = in_lep_p4.Vect().Cross( out_lep_p4.Vect() ).Unit() ;
+	    TVector3 x_axis = y_axis.Cross(z_axis);
+
+	    TVector3 pion_perp( z_axis.Cross( pion_dir.Cross( z_axis ).Unit() ) ) ;
     
+	    double phi = pion_perp.Angle( x_axis ) ;  // phi
+	    
+	    double theta = pion_dir.Angle( z_axis ) ;
+	    
+	    h_theta.Fill( theta ) ;
+	    h_phi.Fill( phi ) ;
+	    h_theta_phi.Fill( theta, phi ) ;
+
+	  }  // if there is a pion and a nucleon
+
+	} // if the delta has 2 daughters
 
 
+	
+	
+      } // is Delta 
+
+      
+      
+    } // is resonant process
+
+
+    
+      
     mcrec->Clear();
   }
+
+  TFile out_file( "result_" + file_name, "RECREATE" ) ;
+  out_file.cd() ;
+  
+  h_theta.Write() ;
+  h_phi.Write() ;
+  h_theta_phi.Write() ;
 
   return tree -> GetEntries() ;
 
