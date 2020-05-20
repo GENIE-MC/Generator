6126c4a58b6c69d9a6eca185b26da19f165b655b
Christopher Backhouse <bckhouse@fnal.gov>
2020-05-20 15:15:09 +0100

    Update comment.

--- src/Tools/EvtLib/EvtLibInteractionListGenerator.cxx
+++ src/Tools/EvtLib/EvtLibInteractionListGenerator.cxx
@@ -32,10 +32,7 @@ InteractionList * EvtLibInteractionListGenerator::CreateInteractionList(
 {
   InteractionList * intlist = new InteractionList;
 
-  // REVIEW these are all these arbitrary choices, but seems to be required to
-  // be able to succesfully pick a spline. Perhaps it would be better to add
-  // new enum entries for "unspecified because it came from an external
-  // library"?
+  // Struck nucleon is arbitrary but seems to be required
 
   ProcessInfo proc_info_cc(kScExternalGen, kIntWeakCC);
   Interaction* interaction_cc = new Interaction(init_state, proc_info_cc);
