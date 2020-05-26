a26bcf55aed82607455629c3d228c6b1cb7edb7d
Marco Roda <mroda@liverpool.ac.uk>
2020-05-26 16:31:42 +0100

    Trace[0][1] implemented

--- src/Physics/Coherent/XSection/COHDeltaCurrent.cxx
+++ src/Physics/Coherent/XSection/COHDeltaCurrent.cxx
@@ -103,6 +103,7 @@ GTrace DirTrace( const Interaction * i,
     // this requires Q2
   double C3V   = delta_ff -> C3V( Q2 ) ;
   double C3vNC = delta_ff -> C3VNC( Q2 ) ;
+  double C5aNC = delta_ff -> C5ANC( Q2 ) ;
 
   double mDelta = util::res::Mass( this -> Resonance() ) ; 
   double mDelta2 = pow( mDelta, 2 ); 
@@ -113,22 +114,37 @@ GTrace DirTrace( const Interaction * i,
   double p0 = out_neutrino.E() ; 
   std::array<double, 4> kg = { t_photon.E(), t_photon.X(), t_photon.Y(), t_photon.Z() } ;
   double mn = kNucleonMass ;
+  double mn2 = pow( mn, 2 ) ;
 
 
   GTrace tr ;
 
   tr[0][0] = -(C3v*C3vNC*( mDelta*p0 + mn*(p0 + q[0]))*( 4*mDelta2*q[1]*kg[1] - 4*pow(p0,2)*q[1]*kg[1] + 
-							 pow(q[1],3) * kg[1] + q[1] * q[3] * q[3] * kg[1] +
-							 q[1] * q[1] * kg[1] * kg[1] - q[3] * q[3] * kg[1] * kg[1] - 
-							 q[3] * q[3] * kg[2] * kg[2] + 
-							 q[0] * q[0] * ( q[1] * q[1] - 3 * q[1] * kg[1] + 
-									 q[3] * (2 * q[3] - 3 * kg[3] ) ) +
-							 q[3] * ( 4*mDelta2 - 4*pow(p0,2) + q[1] * q[1] + 
-								  q[3] * q[3] + 2 * q[1] * kg[1] ) * kg[3] - 
-							 8 * p0 * q[0] * ( q[1] * kg[1] + q[3] * kg[3] ) 
+							 pow(q[1],3)*kg[1] + q[1]*q[3]*q[3]*kg[1] +
+							 q[1]*q[1]*kg[1]*kg[1] - q[3]*q[3]*kg[1]*kg[1] - 
+							 q[3]*q[3]*kg[2]*kg[2] + 
+							 q[0]*q[0]*( q[1]*q[1] - 3*q[1]*kg[1] + q[3]*( 2*q[3] - 3*kg[3] ) ) +
+							 q[3]*( 4*mDelta2 - 4*pow(p0,2) + q[1]*q[1] + q[3]*q[3] + 2*q[1]*kg[1] )*kg[3] - 
+							 8*p0*q[0]*( q[1]*kg[1] + q[3]*kg[3] ) 
 							 ) ) / (3.*mDelta2*pow(mn,3) );
-
-
+  
+  tr[0][1] = ( std::complex( 0., 1. )*C3v*C5aNC*mDelta*( mDelta*p0 + mn*( p0 + q[0] ) )*q[3]*kg[2]*( 8*mn2 - 
+												     2*q[0]*( p0 + q[0] ) + 
+												     q[1]*( q[1] + kg[1] ) + q[3]*( q[3] + kg[3]) ) -
+	       C3v*C3vNC*mn*( 16 *mDelta2 * mn * q[0] * ( p0 + q[0] ) * kg[1] + 
+			      4*pow(mDelta,3)*( 4*p0*q[0]*kg[1] + q[3]*( -( q[3]*kg[1]) + q[1]*kg[3]) ) -
+			      4*mn*(p0 + q[0])*( 4*pow(p0,2)*q[0]*kg[1] + 8*p0*q[0] * q[0]*kg[1] + 
+						 4*pow( q[0], 3)*kg[1] - q[0]*q[1] * q[1]*kg[1] +
+						 2*p0*q[3]*(-(q[3]*kg[1]) + q[1]*kg[3]) - 
+						 q[0]*q[1]*( 2*kg[1]*kg[1] + kg[2]*kg[2] + kg[3]*(- q[3] + kg[3]) ) -
+						 q[0]*kg[1]*( q[0]*q[0] + q[3]*(2*q[3] + kg[3]))) +
+			      mDelta*( -16*pow(p0,3)*q[0]*kg[1] - q[3]*(q[3]*kg[1] - q[1]*kg[3])*( -3*q[0]*q[0] + q[1]*q[1] + 
+												   2*q[1]*kg[1] + q[3]*( q[3] + 2*kg[3] ) ) -
+				       4*pow(p0,2)*(8*q[0]*q[0]*kg[1] + 3*q[3]*( -( q[3]*kg[1] ) + q[1]*kg[3] ) ) +
+				       4*p0*q[0]*( q[0]*q[0]*( q[1] - 3*kg[1] ) + q[1]*q[1]*kg[1] + q[3]*kg[1]*( 4*q[3] + kg[3] ) + 
+						   q[1]*( kg[1]*kg[1] - 3*q[3]*kg[3] ) ) )
+			      ) ) / (12.*mDelta2*pow(mn,4));
+  
 }
 //____________________________________________________________________________
 GTrace DirTrace( const Interaction * i,
