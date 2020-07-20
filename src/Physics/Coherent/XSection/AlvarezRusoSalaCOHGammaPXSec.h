8f65584b72a14b1e790b84a69f228be3c1830567
Marco Roda <marco.roda.88@gmail.com>
2020-07-20 12:29:51 +0100

    Scheleton for AlvarezRusoSalaCOHGamma cross section

---
+++ src/Physics/Coherent/XSection/AlvarezRusoSalaCOHGammaPXSec.h
@@ -0,0 +1,68 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::AlvarezRusoSalaCOHGammaPXSec
+
+\brief    Implementation of the Alvarez-Ruso Sala coherent gamma production model
+
+          Is a concrete implementation of the XSecAlgorithmI interface.
+
+\ref
+
+\author   Marco Roda
+          University of Liverpool
+
+\created  July, 2020
+
+\cpright  Copyright (c) 2003-2020, The GENIE Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          or see $GENIE/LICENSE
+*/
+//____________________________________________________________________________
+
+#ifndef _ALVAREZ_RUSO_SALA_COH_GAMMA_XSEC_H_
+#define _ALVAREZ_RUSO_SALA_COH_GAMMA_XSEC_H_
+
+#include <vector>
+
+#include "Framework/EventGen/XSecAlgorithmI.h"
+#include "Physics/XSectionIntegration/XSecIntegratorI.h"
+
+#include "Physics/Coherent/XSection/COHFormFactorI.h"
+
+namespace genie {
+
+class AlvarezRusoSalaCOHGammaPXSec : public XSecAlgorithmI {
+
+public:
+  AlvarezRusoSalaCOHGammaPXSec();
+  AlvarezRusoSalaCOHGammaPXSec( string config );
+  virtual ~AlvarezRusoSalaCOHGammaPXSec();
+
+  //-- XSecAlgorithmI interface implementation
+  double XSec            ( const Interaction * i, KinePhaseSpace_t k) const;
+  double Integral        ( const Interaction * i) const;
+  bool   ValidProcess    ( const Interaction * i) const;
+
+  //-- overload the Algorithm::Configure() methods to load private data
+  //   members from configuration options
+  void Configure(const Registry & config);
+  void Configure(string config);
+
+protected:
+  void LoadConfig(void);
+
+private:
+
+  //-- private data members loaded from config Registry or set to defaults
+
+  std::vector<const COHHadronicCurrentI *> fCurrents ;
+  const COHFormFactorI * fFormFactors ;
+
+  const XSecIntegratorI * fXSecIntegrator;
+
+};
+
+}      // genie namespace
+
+#endif  // _ALVAREZ_RUSO_SALA_COH_GAMMA_XSEC_H_
