c00fbfa81ba3ee4e9bc5ee6ce4c7c00e02038c71
jsensenig <jonsensenig17@gmail.com>
2020-10-27 04:20:05 +0100

    Clean up event gen validiation macro.

--- src/contrib/coh_gamma/event_valid.C
+++ src/contrib/coh_gamma/event_valid.C
@@ -25,17 +25,31 @@ using namespace genie ;
 double LepPhi( const TLorentzVector & probe, TLorentzVector lep ) ;
 double GammaPhi( const TLorentzVector & probe, TLorentzVector gamma, TLorentzVector lep ) ;
 bool SelEvent( double theta_l, double theta_g, double phi_g ) ;
-double GetXSection( double Eg ) ;
+void GraphIntegratedCOHGammaXsec( double nevts, TH1 * h_E_g, TFile & file ) ;
+
+/////////////
+// Global constants
+
+double g_theta_l_max = 0.6 ;
+double g_theta_l_min = 0.4 ;
+double g_theta_g_max = 0.6 ;
+double g_theta_g_min = 0.4 ;
+double g_phi_g_max   = 3.0 ;
+double g_phi_g_min   = 2.8 ;
+double g_Ev      = 0.5 ;
+int g_target     = 1000060120 ;
+// COH Gamma xsec config set ("Default" or "Consistent")
+std::string config = "Default" ;
 
 
-void event_test( TString in_file_name  = "../event_gen/gntp.0.ghep.root" , 
+void event_valid( TString in_file_name  = "../event_gen/gntp.0.ghep.root" , 
 		 TString out_file_name = "" ) {
 
   
   if ( out_file_name == "" ) {
     out_file_name = in_file_name ; 
     out_file_name.ReplaceAll( ".ghep.root", 
-			      ".plots.root" ) ;
+			      ".evtplots.root" ) ;
   }
     
   TFile in_file( in_file_name ) ; 
@@ -70,11 +84,9 @@ void event_test( TString in_file_name  = "../event_gen/gntp.0.ghep.root" ,
 				       80, 0., 2. ) ; 
   TH1* h_E_nu = hists["E_nu"] = new TH1D("h_E_nu", "E_{#nu};E_{#nu} [GeV]",
         			       100, 0., 10. ) ; 
-  TH1* h_sigma = hists["sigma"] = new TH1D("h_sigma", "#sigma;E_{#gamma} [GeV]",
-        			       80, 0., 2. ) ; 
 
 
-
+  double nevts = in_tree->GetEntries() ;
   size_t evts = 0;
   size_t sel_evts = 0;
 
@@ -100,21 +112,23 @@ void event_test( TString in_file_name  = "../event_gen/gntp.0.ghep.root" ,
 	TLorentzVector gamma  ( * event.Particle(3) -> P4() ) ;
 	TLorentzVector recoil ( * event.Particle(4) -> P4() ) ;
 
-        evts += 1;
+        double theta_l = lep.Angle( probe.Vect() ) ;
+        double theta_g = gamma.Angle( probe.Vect() ) ;
+        double phi_g = GammaPhi( probe, gamma, lep ) ;
 
-        if ( !SelEvent(lep.Angle( probe.Vect() ), gamma.Angle( probe.Vect() ), GammaPhi( probe, gamma, lep ) ) ) continue;
-
-	h_theta_gamma -> Fill( gamma.Angle( probe.Vect() ) ) ;
-	h_theta_lep   -> Fill( lep.Angle( probe.Vect() ) ) ;
+	h_theta_gamma -> Fill( theta_g ) ;
+	h_theta_lep   -> Fill( theta_l ) ;
 	h_theta_recoil   -> Fill( recoil.Angle( probe.Vect() ) ) ;
 
-	h_phi_gamma -> Fill( GammaPhi( probe, gamma, lep ) ) ;			     
+	h_phi_gamma -> Fill( phi_g ) ;			     
 	h_phi_lep -> Fill( LepPhi( probe, lep ) ) ;			     
 
-
 	h_E_l -> Fill( lep.E() ) ;
-	h_E_g -> Fill( gamma.E() ) ;
 	h_E_nu -> Fill( probe.E() ) ;
+      
+        if ( !SelEvent( theta_l, theta_g, phi_g ) ) {  mcrec->Clear(); continue; }
+
+	h_E_g -> Fill( gamma.E() ) ;
 
         sel_evts += 1;
 	
@@ -129,22 +143,16 @@ void event_test( TString in_file_name  = "../event_gen/gntp.0.ghep.root" ,
   TFile out_file ( out_file_name, "RECREATE" ) ;
   out_file.cd() ;
   
+  GraphIntegratedCOHGammaXsec( nevts, h_E_g, out_file ) ;
+
   for ( auto & h : hists ) {
     h.second -> Write() ;
     delete h.second ;
   }
 
-  std::cout << "Processed " << evts << " and selected " << sel_evts << " events." << std::endl;
- 
-  int steps = 80;
-  double xsec_arr[steps];
-  double eg_arr[steps];
-  double eg = 0;
+  out_file.Close() ;
 
-  for (int i = 0; i < steps; i++  ) { eg += 1; eg_arr[i] = eg/(steps/2); xsec_arr[i] = GetXSection(eg/(steps/2))*(1e3); }
-
-  auto gr = new TGraph( 40, eg_arr, xsec_arr ); gr->SetLineColor(1); gr->SetLineWidth(2);
-  gr -> Write();
+  std::cout << "Processed " << nevts << " and selected " << sel_evts << " events." << std::endl;
 
 }
 
@@ -171,35 +179,45 @@ double GammaPhi( const TLorentzVector & probe, TLorentzVector gamma, TLorentzVec
 
 bool SelEvent( double theta_l, double theta_g, double phi_g ) {
 
-  if ( ( theta_l > 0.0 && theta_l < 0.2 ) &&
-       ( theta_g > 0.4 && theta_g < 0.6 ) &&
-       ( phi_g > 2.8 && phi_g < 3.0 ) ) return true;
+  if ( ( theta_l > g_theta_l_min && theta_l < g_theta_l_max ) &&
+       ( theta_g > g_theta_g_min && theta_g < g_theta_g_max ) &&
+       ( phi_g   > g_phi_g_min && phi_g     < g_phi_g_max ) ) return true;
 
   return false;
   
 }
 
-double GetXSection( double Eg ) {
+void GraphIntegratedCOHGammaXsec( double nevts, TH1 * h_E_g, TFile & file ) {
   
-  // -- request the COH NC Gamma model
+  // -- request the COH NC Gamma mode 
   AlgFactory * algf = AlgFactory::Instance();
-  AlgId id("AlvarezRusoSalaCOHGammaPXSec","Default");
+  AlgId id("AlvarezRusoSalaCOHGammaPXSec",config) ;
   const Algorithm * algXsec = algf->GetAlgorithm(id);
   const XSecAlgorithmI * fXSec = dynamic_cast<const XSecAlgorithmI *>(algXsec);
- 
-  Interaction * in = Interaction::COHNC(1000060120, 14, 22, 2);
+   
+  Interaction * in = Interaction::COHNC(g_target, 14, 22, g_Ev);
+  utils::gsl::d4Xsec_dEgdThetaldThetagdPhig func = utils::gsl::d4Xsec_dEgdThetaldThetagdPhig( fXSec, in ) ;
 
-  ROOT::Math::IBaseFunctionMultiDim * func = new utils::gsl::d4Xsec_dEgdThetaldThetagdPhig( fXSec, in );
+  double theta_l = ( g_theta_l_max + g_theta_l_min ) / 2. ;
+  double theta_g = ( g_theta_g_max + g_theta_g_min ) / 2. ;
+  double phi_g   = ( g_phi_g_max + g_phi_g_min ) / 2. ;
 
-  std::array<double, 4> point = {Eg, 0.1, 0.5, 2.9} ; // { Eg, theta_l, theta_g, phi_g }
+  int nbin = h_E_g -> GetNbinsX() ;
+  double xsec_arr[nbin];
+  double eg_arr[nbin];
 
-  double xsec = (*func)( point.data() ) ;
+  for ( int b = 1; b < nbin; b++ ) {
+    double Eg = h_E_g -> GetXaxis() -> GetBinCenter( b ) ;
+    double Ni = h_E_g -> GetBinContent( b ) ;
+    eg_arr[b-1] = Eg ;
+    std::array<double, 4> point = { Eg, theta_l, theta_g, phi_g } ; 
+    xsec_arr[b-1] = func( point.data() ) * ( Ni / nevts ) ;
+  }
 
-  if ( xsec < 0 ) std::cout << "Bad Xsection! " << xsec << std::endl;
+  auto gr = new TGraph( nbin, eg_arr, xsec_arr ); gr->SetLineColor(1); gr->SetLineWidth(2);
+  gr -> Write();
 
-  //std::cout << "Eg = " << Eg << "  XSec = " << xsec << std::endl;
-
-  return xsec ;
+  return ;
 
 }
 
