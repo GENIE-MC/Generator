70a26a6ae385baf6076211932f5fe93f263c91c1
jsensenig <jonsensenig17@gmail.com>
2020-12-18 16:21:47 +0100

    Update COH Gamma xsec extraction macro to be faster

--- src/contrib/coh_gamma/event_valid.C
+++ src/contrib/coh_gamma/event_valid.C
@@ -1,3 +1,23 @@
+/////////////////////////////////////////////////////////
+//
+// To run use `genie -l event_valid.C`
+//
+//  or `genie -l "event_valid.C( int nbins, TString input_file, TString output_file )"`
+//   - e.g: `genie -l "event_valid.C( 5, \"vmu_12c_1gev.root\", \"test.root\" )"`
+//
+// Parameters:
+//
+//  Input file:  in_file_name (line 76)
+//  Output file: out_file_name (line 77)
+//
+//  Bin number: nbins (line 75)
+//    - bins for theta_l = theta_g = nbins
+//    - bins for phi_g = nbins * 2
+//    - bins for E_g = nbins * 6
+//
+//  J. Sensenig 12/2020
+//
+//////////////////////////////////////////////////////
 
 #include "TString.h" 
 #include "TFile.h"
@@ -25,25 +45,26 @@ using namespace genie ;
 
 void SetValues( TTree * in_tree ) ;
 void event_proc( TTree * in_tree, TFile & ofile ) ;
-void event_proc_plot( TTree * in_tree, TFile & ofile ) ;
 double LepPhi( const TLorentzVector & probe, TLorentzVector lep ) ;
 double GammaPhi( const TLorentzVector & probe, TLorentzVector gamma, TLorentzVector lep ) ;
-void GetLimits( std::pair<double,double> &theta_l, std::pair<double,double> &theta_g, std::pair<double,double> &phi_g ) ;
-void GraphIntegratedCOHGammaXsec( double nevts, double xsec, TH1 * h_E_g, TFile & file ) ;
+void GetLimits( std::pair<double,double> &theta_l ) ;
+void ComputeCOHGammXSec( double xsec, TH3 * h_Eg_thetag_phig );
+void GraphIntegratedCOHGammaXsec( int nbin, double nevts, double *xsec_i_arr, double *xsec_arr, double *eg_arr, double *x_err, double *y_err );
 
 /////////////////////
 // Global constants
 
 int g_nbins ;
 double g_theta_l_width ;
-double g_theta_g_width ;
-double g_phi_g_width ;
 double g_theta_l ;
 double g_theta_g ;
+double g_theta_g_max ;
 double g_phi_g ;
+double g_phi_g_max ;
 double g_phi_g_min ;
 double g_Ev ;
 double g_Eg_max ;
+double g_nevts;
 int g_nu ;
 int g_target ;
 
@@ -52,9 +73,8 @@ std::string config = "Default" ;
 
 
 void event_valid( int nbins  = 5, //use 10 bins
-                  bool loop  = true,
-                  TString in_file_name  = "../event_gen/40ar/numu/1/gntp.0.ghep.root" , 
-                  TString out_file_name = ""
+                  TString in_file_name  = "../12c/vmu/0.5/vmu_on_1000060120_COHGAMMA_0.5GeV.gntp.root" , 
+                  TString out_file_name = "test_line_fast_out_05gev.root"
                 ) {
 
   g_nbins = nbins ;
@@ -76,38 +96,23 @@ void event_valid( int nbins  = 5, //use 10 bins
   TFile out_file ( out_file_name, "RECREATE" ) ;
   out_file.cd() ;
     
-  NtpMCTreeHeader * header = dynamic_cast<NtpMCTreeHeader*> (in_file.Get("header"));
-
   // Get the GENIE GHEP tree and set its branch address
   TTree * in_tree = dynamic_cast<TTree*> (in_file.Get("gtree"));
 
+  std::cout << "Extracting and setting values" << std::endl;
+
+  // Get the data ranges
   SetValues( in_tree ) ;
   
-  if ( loop ) { // Loop over the fixed angles, only xsec plot
+  int loop_cnt = 0;
 
-    for ( int  i = 0; i < g_nbins; i++ ) { // theta_l
-      for ( int  i = 0; i < g_nbins; i++ ) { // theta_g
-        for ( int j = 0; j < g_nbins*2; j++ ) { // phi
-          std::cout << " theta_l = " << g_theta_l 
-                    << " theta_g = " << g_theta_g 
-                    << " phi_g = "   << g_phi_g << std::endl;
-          event_proc( in_tree, out_file ) ;
-          g_phi_g += g_phi_g_width ;
-        }
-        g_phi_g = g_phi_g_min + ( g_phi_g_width / 2. ) ;
-        g_theta_g += g_theta_g_width ;
-      }
-      g_theta_g = g_theta_g_width ;
-      g_theta_l += g_theta_l_width ;
-    }
+  for ( int  i = 0; i < g_nbins; i++ ) { // theta_l
 
-  } else { // Single angles, make plots in addition to xsec plot
+    loop_cnt += 1;
+    std::cout << "Loop " << loop_cnt << "/" << g_nbins << " with theta_l = " << g_theta_l << std::endl;
 
-    g_theta_l = g_theta_g = 0.4; g_phi_g = 2.7 ;
-    std::cout << " theta_l = " << g_theta_l 
-              << " theta_g = " << g_theta_g 
-              << " phi_g = "   << g_phi_g << std::endl;
-    event_proc_plot( in_tree, out_file ) ; 
+    event_proc( in_tree, out_file ) ;
+    g_theta_l += g_theta_l_width ;
 
   }
 
@@ -128,21 +133,24 @@ void SetValues( TTree * in_tree ) {
 
   mcrec->Clear();
 
-  double Eg_max = 0. ;
+  double Eg_max      = 0. ;
   double theta_l_max = 0. ;
   double theta_g_max = 0. ;
-  double phi_g_max = 0. ;
-  double phi_g_min = 0. ;
+  double phi_g_max   = 0. ;
+  double phi_g_min   = 0. ;
 
   // Get the max/min limits from the data
   for(Long64_t i=0; i < in_tree->GetEntries(); i++) {
+
     in_tree->GetEntry(i);
     EventRecord & event = *(mcrec->event);
         
     const Interaction & inter = *( event.Summary() ) ;
     const ProcessInfo & proc_info = inter.ProcInfo() ;
+
     if ( proc_info.IsCoherentProduction() ) {
       const XclsTag & xcl_tag = inter.ExclTag() ;
+
       if ( xcl_tag.NSingleGammas() == 1 ) {
         TLorentzVector probe  ( * event.Probe() -> P4() ) ;
         TLorentzVector lep    ( * event.FinalStatePrimaryLepton() -> P4() ) ;
@@ -152,11 +160,13 @@ void SetValues( TTree * in_tree ) {
         double theta_l = lep.Angle( probe.Vect() ) ;
         double theta_g = gamma.Angle( probe.Vect() ) ;
         double phi_g = GammaPhi( probe, gamma, lep ) ;
-        if ( Eg > Eg_max ) Eg_max = Eg ;
+
+        if ( Eg > Eg_max )           Eg_max = Eg ;
         if ( theta_l > theta_l_max ) theta_l_max = theta_l ;
         if ( theta_g > theta_g_max ) theta_g_max = theta_g ;
-        if ( phi_g > phi_g_max ) phi_g_max = phi_g ;
-        if ( phi_g < phi_g_min ) phi_g_min = phi_g ;
+        if ( phi_g > phi_g_max )     phi_g_max = phi_g ;
+        if ( phi_g < phi_g_min )     phi_g_min = phi_g ;
+
       }
     }
     mcrec->Clear();
@@ -177,133 +187,30 @@ void SetValues( TTree * in_tree ) {
             << " phi_g min = "   << phi_g_min << std::endl;
   
   g_theta_l_width = theta_l_max / g_nbins ;
-  g_theta_g_width = theta_g_max / g_nbins ;
-  g_phi_g_width   = phi_g_max / ( g_nbins*2 ) ;
-  g_theta_l = g_theta_l_width ;
-  g_theta_g = g_theta_g_width ;
-  g_phi_g   = phi_g_min + g_phi_g_width ;
-  g_Eg_max  = Eg_max ;
+  g_theta_l       = g_theta_l_width ;
+  g_theta_g_max   = theta_g_max;
+  g_phi_g_max     = phi_g_max;
+  g_phi_g_min     = phi_g_min;
+  g_Eg_max        = Eg_max ;
 
 }
 
-void event_proc_plot( TTree * in_tree, TFile & ofile ) {
-  
-  std::pair<double,double> theta_l_lim ;
-  std::pair<double,double> theta_g_lim ;
-  std::pair<double,double> phi_g_lim ;
-  GetLimits( theta_l_lim, theta_g_lim, phi_g_lim ) ;
-
-  NtpMCEventRecord * mcrec = 0;
-  in_tree->SetBranchAddress("gmcrec", & mcrec);
-  
-  std::map<std::string, TH1*> hists ;
-
-  // plots we need
-  TH1* h_theta_gamma = hists["theta_gamma"] = new TH1D("h_theta_gamma", "#theta_{#gamma};#theta_{#gamma} [rad]",
-        						80, 0., TMath::Pi() ) ; 
-  TH1* h_theta_lep = hists["theta_lep"] = new TH1D("h_theta_lep", "#theta_{l};#theta_{l} [rad]",
-        					   80, 0., TMath::Pi() ) ; 
-  TH1* h_theta_recoil = hists["theta_nuc"] = new TH1D("h_theta_recoil", "#theta_{recoil};#theta_{recoil} [rad]",
-        					   80, 0., TMath::Pi() ) ; 
-                                                                                                                 
-  TH1* h_phi_gamma = hists["phi_gamma"] = new TH1D("h_phi_gamma", "#phi_{#gamma};#phi_{#gamma} [rad]",
-        						100, -TMath::Pi(), TMath::Pi() ) ; 
-  TH1* h_phi_lep = hists["phi_lep"] = new TH1D("h_phi_lep", "#phi_{l};#phi_{#phi} [rad]",
-        						100, -TMath::Pi(), TMath::Pi() ) ; 
-                                                                                                                 
-  TH1* h_E_l = hists["E_l"] = new TH1D("h_E_l", "E_{l};E_{l} [GeV]", 80, 0., 2. ) ; 
-                                                                                                                 
-  TH1* h_E_nu = hists["E_nu"] = new TH1D("h_E_nu", "E_{#nu};E_{#nu} [GeV]", 100, 0., 10. ) ; 
-
-  TH1* h_E_g = hists["E_g"] = new TH1D("h_E_g", "E_{#gamma};E_{#gamma} [GeV]", 30, 0., g_Eg_max ) ; 
-  h_E_g -> Sumw2( true ) ;
-
-  double nevts = in_tree->GetEntries() ;
-  size_t evts = 0;
-  size_t sel_evts = 0;
-  double xsec ;
-
-  // Event loop
-  for(Long64_t i=0; i < in_tree->GetEntries(); i++) {
- 
-    in_tree->GetEntry(i);
-    
-    EventRecord & event = *(mcrec->event);
-    
-    const Interaction & inter = *( event.Summary() ) ;
-    
-    const ProcessInfo & proc_info = inter.ProcInfo() ;
-    
-    if ( proc_info.IsCoherentProduction() ) {
-    
-      const XclsTag & xcl_tag = inter.ExclTag() ;
-
-      if ( xcl_tag.NSingleGammas() == 1 ) {
-	
-	TLorentzVector probe  ( * event.Probe() -> P4() ) ;
-	TLorentzVector lep    ( * event.FinalStatePrimaryLepton() -> P4() ) ;
-	TLorentzVector gamma  ( * event.Particle(3) -> P4() ) ;
-	TLorentzVector recoil ( * event.Particle(4) -> P4() ) ;
-
-        double theta_l = lep.Angle( probe.Vect() ) ;
-        double theta_g = gamma.Angle( probe.Vect() ) ;
-        double phi_g = GammaPhi( probe, gamma, lep ) ;
-
-	  h_theta_gamma -> Fill( theta_g ) ;
-	  h_theta_lep   -> Fill( theta_l ) ;
-   	  h_theta_recoil   -> Fill( recoil.Angle( probe.Vect() ) ) ;
-
-	  h_phi_gamma -> Fill( phi_g ) ;			     
-	  h_phi_lep -> Fill( LepPhi( probe, lep ) ) ;			     
-
-	  h_E_l -> Fill( lep.E() ) ;
-	  h_E_nu -> Fill( probe.E() ) ;
-      
-          if ( ( theta_l > theta_l_lim.first && theta_l < theta_l_lim.second ) &&
-             ( theta_g > theta_g_lim.first && theta_g < theta_g_lim.second ) &&
-             ( phi_g   > phi_g_lim.first   && phi_g   < phi_g_lim.second ) ) {  mcrec->Clear(); continue; }
-
-         h_E_g -> Fill( gamma.E() ) ;
-
-        xsec = event.XSec()/(1.e-38 * units::cm2) ;
-        sel_evts += 1;
-	
-      } // single gamma
-
-    } // coherent production
-
-      mcrec->Clear() ;
-  } // event loop
-
-  if ( sel_evts > 0 ) GraphIntegratedCOHGammaXsec( nevts, xsec, h_E_g, ofile ) ;
-
-  std::cout << "Processed " << nevts << " and selected " << sel_evts << " events." << std::endl;
-
-  for ( auto & h : hists ) {
-    h.second -> Write() ;
-    delete h.second ;
-  }
-
-}
 
 
 void event_proc( TTree * in_tree, TFile & ofile ) {
 
   std::pair<double,double> theta_l_lim ; 
-  std::pair<double,double> theta_g_lim ;
-  std::pair<double,double> phi_g_lim ;
 
-  GetLimits( theta_l_lim, theta_g_lim, phi_g_lim ) ;
+  GetLimits( theta_l_lim ) ;
 
   NtpMCEventRecord * mcrec = 0;
   in_tree->SetBranchAddress("gmcrec", & mcrec);
 
-  TH1* h_E_g = new TH1D("h_E_g", "E_{#gamma};E_{#gamma} [GeV]", 30, 0., g_Eg_max ) ;
-  h_E_g -> Sumw2( true ) ;
+  TH3* h_Eg_thetag_phig = new TH3D("h_Eg_thetag_phig", "E_{#gamma} #theta_{#gamma} #phi_{#gamma};E_{#gamma} [GeV];#theta_{#gamma} [rad];#phi_{#gamma} [rad]",
+                                    (g_nbins*6), 0., g_Eg_max, g_nbins, 0, g_theta_g_max, (g_nbins*2), g_phi_g_min, g_phi_g_max ) ;
 
-  double nevts = in_tree->GetEntries() ;
+  g_nevts = in_tree->GetEntries() ;
   size_t evts = 0;
-  size_t sel_evts = 0;
   double xsec ;
 
   // Event loop
@@ -332,14 +239,11 @@ void event_proc( TTree * in_tree, TFile & ofile ) {
         double theta_g = gamma.Angle( probe.Vect() ) ;
         double phi_g = GammaPhi( probe, gamma, lep ) ;
 
-        if ( ( theta_l < theta_l_lim.first || theta_l > theta_l_lim.second ) ||
-           ( theta_g < theta_g_lim.first   || theta_g > theta_g_lim.second ) ||
-           ( phi_g   < phi_g_lim.first     || phi_g   > phi_g_lim.second ) ) {  mcrec->Clear(); continue; }
-
-         h_E_g -> Fill( gamma.E() ) ;
+        if ( theta_l < theta_l_lim.first || theta_l > theta_l_lim.second )  {  mcrec->Clear(); continue; }
+        
+        h_Eg_thetag_phig -> Fill( gamma.E(), theta_g, phi_g ) ;
 
         xsec = event.XSec()/(1.e-38 * units::cm2) ;
-        sel_evts += 1;
 
       } // single gamma
 
@@ -348,11 +252,13 @@ void event_proc( TTree * in_tree, TFile & ofile ) {
       mcrec->Clear() ;
   } // event loop
 
-  if ( sel_evts > 0 ) GraphIntegratedCOHGammaXsec( nevts, xsec, h_E_g, ofile ) ;
+  ComputeCOHGammXSec( xsec, h_Eg_thetag_phig ) ;
 
-  std::cout << "Processed " << nevts << " and selected " << sel_evts << " events." << std::endl;
+  h_Eg_thetag_phig -> Write();
 
-  delete h_E_g ;
+  std::cout << "Processed " << g_nevts << " events." << std::endl;
+
+  delete h_Eg_thetag_phig ;
 
   return ;
   
@@ -377,48 +283,85 @@ double GammaPhi( const TLorentzVector & probe, TLorentzVector gamma, TLorentzVec
 }
 
 
-void GetLimits( std::pair<double,double> &theta_l, std::pair<double,double> &theta_g, std::pair<double,double> &phi_g ) {
+void GetLimits( std::pair<double,double> &theta_l ) {
  
   theta_l = std::make_pair( g_theta_l - (g_theta_l_width/2.), g_theta_l + (g_theta_l_width/2.) ) ;
-  theta_g = std::make_pair( g_theta_g - (g_theta_g_width/2.), g_theta_g + (g_theta_g_width/2.) ) ;
-  phi_g   = std::make_pair( g_phi_g - (g_phi_g_width/2.), g_phi_g + (g_phi_g_width/2.) ) ;
 
 }
 
-void GraphIntegratedCOHGammaXsec( double nevts, double xsec, TH1 * h_E_g, TFile & file ) {
-  
-  // -- request the COH NC Gamma mode 
+
+void ComputeCOHGammXSec( double xsec, TH3 * h_Eg_thetag_phig ) {
+
+  // -- request the COH NC Gamma mode
   AlgFactory * algf = AlgFactory::Instance();
   AlgId id("AlvarezRusoSalaCOHGammaPXSec",config) ;
   const Algorithm * algXsec = algf->GetAlgorithm(id);
   const XSecAlgorithmI * fXSec = dynamic_cast<const XSecAlgorithmI *>(algXsec);
-   
+
   Interaction * in = Interaction::COHNC(g_target, g_nu, 22, g_Ev);
   utils::gsl::d4Xsec_dEgdThetaldThetagdPhig func = utils::gsl::d4Xsec_dEgdThetaldThetagdPhig( fXSec, in ) ;
 
-  int nbin = h_E_g -> GetNbinsX() ;
-  double xsec_i_arr[nbin];
-  double xsec_arr[nbin];
-  double eg_arr[nbin];
-  double x_err[nbin];
-  double y_err[nbin];
+  int nbins_Eg     = h_Eg_thetag_phig -> GetNbinsX(); // X = E_g
+  int nbins_thetag = h_Eg_thetag_phig -> GetNbinsY(); // Y = theta_g
+  int nbins_phig   = h_Eg_thetag_phig -> GetNbinsZ(); // Z = phi_g
 
-  for ( int b = 0; b < nbin; b++ ) {
-    double Eg = h_E_g -> GetXaxis() -> GetBinCenter( b ) ;
-    double Ni = h_E_g -> GetBinContent( b ) ;
-    double Eg_width = h_E_g -> GetBinWidth( b ) ;
-    
-    // Calculate xsec from events
-    xsec_i_arr[b] =  (1.e3) * xsec * ( Ni / nevts ) / ( Eg_width * g_theta_l_width * g_theta_g_width * g_phi_g_width ) ;
-    x_err[b] = 0. ; y_err[b] = ( xsec_i_arr[b] / Ni ) * h_E_g -> GetBinError( b ) ; 
+  std::cout << "Processing Eg bins " << nbins_Eg << " theta_g bins " <<  nbins_thetag << " phi_g bins " <<  nbins_phig << std::endl;
 
-    // Get the xsec from the functor
-    std::array<double, 4> point = { Eg, g_theta_l, g_theta_g, g_phi_g } ; 
-    xsec_arr[b] = (1.e3) * func( point.data() ) ;
+  // Loop over bins on each axis ignoring the under/over flow bins
+  for ( int b_theta = 1; b_theta < (nbins_thetag+1); b_theta++ ) { // theta_g
+    for ( int b_phi = 1; b_phi < (nbins_phig+1); b_phi++ ) {     // phi_g
 
-    eg_arr[b] = Eg ;
-  }
+      double theta_g       = h_Eg_thetag_phig -> GetYaxis() -> GetBinCenter( b_theta );
+      double theta_g_width = h_Eg_thetag_phig -> GetYaxis() -> GetBinWidth( b_theta );
+      double phi_g         = h_Eg_thetag_phig -> GetZaxis() -> GetBinCenter( b_phi );
+      double phi_g_width   = h_Eg_thetag_phig -> GetZaxis() -> GetBinWidth( b_phi );
 
+      double xsec_i_arr[nbins_Eg];
+      double xsec_arr[nbins_Eg];
+      double eg_arr[nbins_Eg];
+      double x_err[nbins_Eg];
+      double y_err[nbins_Eg];
+
+      // Project out Eg so we can get the bin error
+      TH1D * h_E_g = h_Eg_thetag_phig -> ProjectionX( "h_E_g", b_theta, b_theta, b_phi, b_phi, "e" );
+
+      double cnt = 0;
+
+      for ( int b = 1; b < (nbins_Eg+1); b++ ) { // E_g
+
+        double Eg       = h_Eg_thetag_phig -> GetXaxis() -> GetBinCenter( b );
+        double Eg_width = h_Eg_thetag_phig -> GetXaxis() -> GetBinWidth( b );
+        double Ni       = h_Eg_thetag_phig -> GetBinContent( b, b_theta, b_phi );
+
+        // Calculate xsec from events
+        xsec_i_arr[b-1] = ( 1.e3 ) * xsec * ( Ni / g_nevts ) / ( Eg_width * g_theta_l_width * theta_g_width * phi_g_width );
+
+        x_err[b] = Eg_width / 2.;
+        y_err[b] = ( xsec_i_arr[b-1] / Ni ) * h_E_g -> GetBinError( b );
+
+        // Get the xsec from the functor at given 4d point
+        std::array<double, 4> point = {Eg, g_theta_l, theta_g, phi_g};
+        xsec_arr[b-1] = ( 1.e3 ) * func( point.data() ) ;
+
+        eg_arr[b-1] = Eg;
+
+        cnt += Ni;
+
+      } // E_g
+
+      if ( cnt < 1 ) continue; // Skip plotting if no events in bin
+
+      g_theta_g = theta_g; g_phi_g = phi_g;
+      GraphIntegratedCOHGammaXsec( nbins_Eg, cnt, xsec_i_arr, xsec_arr, eg_arr, x_err, y_err );
+
+    } // phi_g
+  }   // theta_g
+
+}
+
+
+void GraphIntegratedCOHGammaXsec( int nbin, double nevts, double *xsec_i_arr, double *xsec_arr, double *eg_arr, double *x_err, double *y_err ) {
+  
   auto evt_xsec = new TGraphErrors( nbin, eg_arr, xsec_i_arr, x_err, y_err); 
   evt_xsec -> SetLineColor(1); evt_xsec -> SetLineWidth(1); evt_xsec -> SetMarkerStyle(8); evt_xsec -> SetMarkerSize(0.5);
 
@@ -431,10 +374,10 @@ void GraphIntegratedCOHGammaXsec( double nevts, double xsec, TH1 * h_E_g, TFile 
   stringstream description ;
   
   description << g_Ev << " GeV #"    << tprobe -> GetTitle() << " on " << ttgt -> GetTitle() ;
-  description << " #theta_{l}="      << g_theta_l*TMath::RadToDeg()      << "#circ" ;
-  description << " #theta_{#gamma}=" << TMath::RadToDeg()*g_theta_l << "#circ" ;
-  description << " #phi_{#gamma}="   << TMath::RadToDeg()*g_phi_g     << "#circ" ;
-  description << " ("  << h_E_g -> GetEntries() << "/" << nevts << " evts)" ;
+  description << " #theta_{l}="      << (int)( TMath::RadToDeg()*g_theta_l ) << "#circ" ;
+  description << " #theta_{#gamma}=" << (int)( TMath::RadToDeg()*g_theta_g ) << "#circ" ;
+  description << " #phi_{#gamma}="   << (int)( TMath::RadToDeg()*g_phi_g )     << "#circ" ;
+  description << " ("  << nevts << "/" << g_nevts << " evts)" ;
   description << ";E_{#gamma} [GeV];#frac{d^{4}#sigma}{dE_{#gamma}d#theta_{l}d#Omega_{#gamma}} [10^{-41} #frac{cm^{2}}{GeV}]" ;
 
   std::string evt_title = "Event #sigma: " + description.str() ;
@@ -445,9 +388,9 @@ void GraphIntegratedCOHGammaXsec( double nevts, double xsec, TH1 * h_E_g, TFile 
   func_xsec -> SetTitle( func_title.c_str() );
 
   stringstream mg_descrip ;
-  mg_descrip << "xsec_angles_" << (int)(g_theta_l*TMath::RadToDeg()) 
-                        << "_" << (int)(g_theta_g*TMath::RadToDeg()) 
-                        << "_" << (int)(g_phi_g*TMath::RadToDeg()) ;
+  mg_descrip << "xsec_angles_" << (int)( g_theta_l*TMath::RadToDeg() ) 
+                        << "_" << (int)( g_theta_g*TMath::RadToDeg() ) 
+                        << "_" << (int)( g_phi_g*TMath::RadToDeg() ) ;
   std::string mg_name = mg_descrip.str() ;
 
   auto mg = new TMultiGraph();
