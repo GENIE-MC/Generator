ef8cd6a233f037c4dd84d00ebcfb00631dabd146
Marco Roda <marco.roda.88@gmail.com>
2021-01-28 11:59:08 +0000

    added new phase spaces

--- src/Framework/Conventions/KinePhaseSpace.h
+++ src/Framework/Conventions/KinePhaseSpace.h
@@ -38,7 +38,6 @@ typedef enum EKinePhaseSpace {
   kPSlogyfEx,
   kPSxyfE,
   kPSlogxlogyfE,
-  kPSxQ2fE,
   kPSQ2fE,
   kPSQD2fE,
   kPSlogQ2fE,
@@ -69,7 +68,9 @@ typedef enum EKinePhaseSpace {
   kPSTAfE,
   kPSEgTlOgfE,
   kPSDMELEvGen, // Equivalent to kPSQELEvGen for Dark Matter scattering  
-  kPSlog10xlog10Q2fE
+  kPSWQ2ctpphipfE,
+  kPSWQ2ctpfE
+
 } KinePhaseSpace_t;
 
 class KinePhaseSpace
@@ -83,49 +84,49 @@ public:
       case(kPSNull) :
         return "** Undefined kinematic phase space **"; break;
 
-      case(kPSfE)         : return "<|E>";            break;
-      case(kPSxfE)        : return "<{x}|E>";         break;
-      case(kPSlogxfE)     : return "<{logx}|E>";      break;
-      case(kPSxfEy)       : return "<{x}|E,y>";       break;
-      case(kPSlogxfEy)    : return "<{logx}|E,y>";    break;
-      case(kPSyfE)        : return "<{y}|E>";         break;
-      case(kPSlogyfE)     : return "<{logy}|E>";      break;
-      case(kPSyfEx)       : return "<{y}|E,x>";       break;
-      case(kPSlogyfEx)    : return "<{logy}|E,x>";    break;
-      case(kPSlogxlogyfE) : return "<{logx,logy}|E>"; break;
-      case(kPSxyfE)       : return "<{x,y}|E>";       break;
-      case(kPSxQ2fE)      : return "<{x,Q2}|E>";      break;
-      case(kPSQ2fE)       : return "<{Q2}|E>";        break;
-      case(kPSQD2fE)      : return "<{QD2}|E>";       break;
-      case(kPSlogQ2fE)    : return "<{logQ2}|E>";     break;
-      case(kPSQ2fEW)      : return "<{Q2}|E,W>";      break;
-      case(kPSlogQ2fEW)   : return "<{logQ2}|E,W>";   break;
-      case(kPSq2fE)       : return "<{q2}|E>";        break;
-      case(kPSq2fEW)      : return "<{q2}|E,W>";      break;
-      case(kPSWfE)        : return "<{W}|E>";         break;
-      case(kPSWfEQ2)      : return "<{W}|E,Q2>";      break;
-      case(kPSWfEq2)      : return "<{W}|E,q2>";      break;
-      case(kPSWQ2fE)      : return "<{W,Q2}|E>";      break;
-      case(kPSWQD2fE)     : return "<{W,QD2}|E>";     break;
-      case(kPSW2Q2fE)     : return "<{W2,Q2}|E>";     break;
-      case(kPSWlogQ2fE)   : return "<{W,logQ2}|E>";   break;
-      case(kPSW2logQ2fE)  : return "<{W2,logQ2}|E>";  break;
-      case(kPSWq2fE)      : return "<{W,q2}|E>";      break;
-      case(kPSW2q2fE)     : return "<{W2,q2}|E>";     break;
-      case(kPSxytfE)      : return "<{x,y,t}|E>";     break;
-      case(kPSQ2yfE)      : return "<{Q2,y}|E>";      break;
-      case(kPSlogQ2logyfE): return "<{Q2,y}|E>";      break;
-      case(kPSTlctl)      : return "<{Tl,cos(theta_l)}|E>";     break;
-      case(kPSElOlOpifE)  : return "<{El,Omega_l,Omega_pi}|E>"; break;
-      case(kPSEgTlOgfE)   : return "<{Egamma,Theta_l,Omega_gamma}|E>"; break;
-      case(kPSElOlTpifE)  : return "<{El,Omega_l,Theta_pi}|E>"; break;
-      case(kPSTkTlctl)    : return "<{Tk,Tl,cos(theta_l)}|E>";  break;
-      case(kPSQ2vfE)      : return "<{Q2,v}|E>"; break;
+      case(kPSfE)          : return "<|E>";            break;
+      case(kPSxfE)         : return "<{x}|E>";         break;
+      case(kPSlogxfE)      : return "<{logx}|E>";      break;
+      case(kPSxfEy)        : return "<{x}|E,y>";       break;
+      case(kPSlogxfEy)     : return "<{logx}|E,y>";    break;
+      case(kPSyfE)         : return "<{y}|E>";         break;
+      case(kPSlogyfE)      : return "<{logy}|E>";      break;
+      case(kPSyfEx)        : return "<{y}|E,x>";       break;
+      case(kPSlogyfEx)     : return "<{logy}|E,x>";    break;
+      case(kPSlogxlogyfE)  : return "<{logx,logy}|E>"; break;
+      case(kPSxyfE)        : return "<{x,y}|E>";       break;
+      case(kPSQ2fE)        : return "<{Q2}|E>";        break;
+      case(kPSQD2fE)       : return "<{QD2}|E>";       break;
+      case(kPSlogQ2fE)     : return "<{logQ2}|E>";     break;
+      case(kPSQ2fEW)       : return "<{Q2}|E,W>";      break;
+      case(kPSlogQ2fEW)    : return "<{logQ2}|E,W>";   break;
+      case(kPSq2fE)        : return "<{q2}|E>";        break;
+      case(kPSq2fEW)       : return "<{q2}|E,W>";      break;
+      case(kPSWfE)         : return "<{W}|E>";         break;
+      case(kPSWfEQ2)       : return "<{W}|E,Q2>";      break;
+      case(kPSWfEq2)       : return "<{W}|E,q2>";      break;
+      case(kPSWQ2fE)       : return "<{W,Q2}|E>";      break;
+      case(kPSWQD2fE)      : return "<{W,QD2}|E>";     break;
+      case(kPSW2Q2fE)      : return "<{W2,Q2}|E>";     break;
+      case(kPSWlogQ2fE)    : return "<{W,logQ2}|E>";   break;
+      case(kPSW2logQ2fE)   : return "<{W2,logQ2}|E>";  break;
+      case(kPSWq2fE)       : return "<{W,q2}|E>";      break;
+      case(kPSW2q2fE)      : return "<{W2,q2}|E>";     break;
+      case(kPSxytfE)       : return "<{x,y,t}|E>";     break;
+      case(kPSQ2yfE)       : return "<{Q2,y}|E>";      break;
+      case(kPSlogQ2logyfE) : return "<{Q2,y}|E>";      break;
+      case(kPSTlctl)       : return "<{Tl,cos(theta_l)}|E>";     break;
+      case(kPSElOlOpifE)   : return "<{El,Omega_l,Omega_pi}|E>"; break;
+      case(kPSEgTlOgfE)    : return "<{Egamma,Theta_l,Omega_gamma}|E>"; break;
+      case(kPSElOlTpifE)   : return "<{El,Omega_l,Theta_pi}|E>"; break;
+      case(kPSTkTlctl)     : return "<{Tk,Tl,cos(theta_l)}|E>";  break;
+      case(kPSQ2vfE)       : return "<{Q2,v}|E>"; break;
       // TODO: update this string when the appropriate kinematic variables are known
-      case(kPSQELEvGen)   : return "<QELEvGen>"; break;
+      case(kPSQELEvGen)    : return "<QELEvGen>"; break;
       case(kPSDMELEvGen)   : return "<DMELEvGen>"; break;
-      case(kPSTAfE)       : return "<{TA}|E>";   break;
-      case(kPSlog10xlog10Q2fE) : return "<{log10x,log10Q2}|E>"; break;
+      case(kPSTAfE)        : return "<{TA}|E>";   break;
+      case(kPSWQ2ctpphipfE): return "<W, Q2, cost(theta_pion), phi_pion|E>"; break;
+      case(kPSWQ2ctpfE)    : return "<W, Q2, cost(theta_pion)|E>"; break;
     }
     return "** Undefined kinematic phase space **";
   }
