a05a50b66329e959e6e0edd6233088844f9c82a8
Marco Roda <marco.roda.88@gmail.com>
2021-02-02 11:39:46 +0000

    finish importing from QuasiElastic changes

--- src/Physics/QuasiElastic/XSection/LinkDef.h
+++ src/Physics/QuasiElastic/XSection/LinkDef.h
@@ -12,6 +12,7 @@
 #pragma link C++ class genie::BBA03ELFormFactorsModel;
 #pragma link C++ class genie::BBA05ELFormFactorsModel;
 #pragma link C++ class genie::BBA07ELFormFactorsModel;
+#pragma link C++ class genie::GalsterELFormFactorsModel;
 #pragma link C++ class genie::DipoleELFormFactorsModel;
 #pragma link C++ class genie::TransverseEnhancementFFModel;
 
@@ -22,6 +23,9 @@
 #pragma link C++ class genie::LwlynSmithFFCC;
 #pragma link C++ class genie::LwlynSmithFFNC;
 #pragma link C++ class genie::LwlynSmithFF;
+#pragma link C++ class genie::MKFFEM;
+#pragma link C++ class genie::MKFFCC;
+#pragma link C++ class genie::GalsterELFormFactorsModel;
 #pragma link C++ class genie::LwlynSmithFFDeltaS;
 #pragma link C++ class genie::AxialFormFactorModelI;
 #pragma link C++ class genie::AxialFormFactor;
@@ -29,7 +33,6 @@
 #pragma link C++ class genie::ZExpAxialFormFactorModel;
 #pragma link C++ class genie::KuzminNaumov2016AxialFormFactorModel;
 #pragma link C++ class genie::NievesQELCCPXSec;
-#pragma link C++ class genie::SuSAv2QELPXSec;
 #pragma link C++ class genie::SmithMonizQELCCPXSec;
 #pragma link C++ class genie::SmithMonizQELCCXSec;
 #pragma link C++ class genie::SmithMonizUtils;
@@ -38,10 +41,10 @@
 #pragma link C++ class genie::QELFormFactorsModelI;
 
 #pragma link C++ class genie::QELXSec;
-#pragma link C++ class genie::NewQELXSec;
+
 
 
 // Wrappers for GSL/MathMore lib
-#pragma link C++ class genie::utils::gsl::d2Xsec_dQ2dv;
+#pragma link C++ class genie::utils::gsl::d2Xsec_dQ2dv::d2Xsec_dQ2dv
 
 #endif
