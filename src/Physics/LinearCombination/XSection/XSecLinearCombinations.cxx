eeb8c5fcd4f56665268ee3bbb8db36f564f6a094
Julia Tena Vidal j.tena-vidal@liverpool.ac.uk <vidal@mercury008.pp.rl.ac.uk>
2021-04-08 19:01:56 +0100

    adding first draft for new algorithm class

---
+++ src/Physics/LinearCombination/XSection/XSecLinearCombinations.cxx
@@ -0,0 +1,126 @@
+//_________________________________________________________________________
+/*
+ Copyright (c) 2003-2020, The GENIE Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+
+ Original code contributed by J. Schwehr, D. Cherdack, R. Gran
+ Substantial code refactorizations by the core GENIE group.
+*/
+//_________________________________________________________________________
+
+#include "Framework/Algorithm/AlgConfigPool.h"
+#include "Framework/Conventions/Constants.h"
+#include "Framework/Conventions/Units.h"
+#include "Framework/Messenger/Messenger.h"
+#include "Framework/ParticleData/PDGCodes.h"
+#include "Framework/ParticleData/PDGLibrary.h"
+#include "Framework/ParticleData/PDGUtils.h"
+#include "Framework/Utils/KineUtils.h"
+#include "Physics/HadronTensors/NievesMECHadronTensorModel.h"
+#include "Physics/HadronTensors/LabFrameHadronTensorI.h"
+#include "Physics/Multinucleon/XSection/NievesSimoVacasMECPXSec2016.h"
+#include "Physics/Multinucleon/XSection/MECUtils.h"
+#include "Physics/XSectionIntegration/XSecIntegratorI.h"
+
+using namespace genie;
+using namespace genie::constants;
+
+//_________________________________________________________________________
+XSecLinearCombinations::XSecLinearCombinations() :
+XSecLinearCombinations("genie::XSecLinearCombinations")
+{
+
+}
+//_________________________________________________________________________
+XSecLinearCombinations::XSecLinearCombinations(string config) :
+XSecLinearCombinations("genie::XSecLinearCombinations", config)
+{
+
+}
+//_________________________________________________________________________
+XSecLinearCombinations::~XSecLinearCombinations()
+{
+
+}
+//_________________________________________________________________________
+double XSecLinearCombinations::XSec(
+  const Interaction * interaction, KinePhaseSpace_t kps) const
+{
+  double xsec = 0 ; 
+  std::vector<double> vect_xsec ;
+
+  // Store xsec prediction for each entry :
+  for( unsigned int i = 0 ; i < fXSections.size() ; ++ i ) {
+    vect_xsec.push_back( fXSections[i] -> XSec( interaction, kps ) ) ;
+  }
+
+  // Check if we need to normalize the weights: 
+  double total_w = 1 ; 
+  if( fNormalise ) {
+    double sum = 0 ; 
+    for( unsigned int j = 0 ; j < fLinearCoefficients.size() ; ++j ) {
+      sum += fLinearCoefficients[i] ;
+    }
+    total_w = sum ; 
+  }
+
+  for( unsigned int k = 0; k < vect_xsec.size() ; ++k ) {
+    xsec += vect_xsec[k] * fLinearCoefficients[k] ; 
+    xsec *= total_w ; 
+  }  
+
+  return xsec;
+}
+//_________________________________________________________________________
+double XSecLinearCombinations::Integral( const Interaction * interaction ) const
+{
+  // I dont understand what this does yet - figure out before coding ;
+  //    double xsec = fXSecIntegrator->Integrate(this,interaction);
+  //  return xsec;
+
+  return 0 ; 
+}
+//_________________________________________________________________________
+bool XSecLinearCombinations::ValidProcess(
+        const Interaction * interaction) const
+{
+  // CHECK THAT THE XSEC USED CONSIDER A VALID PROCESS
+  
+    return true;
+}
+
+//_________________________________________________________________________
+void XSecLinearCombinations::Configure(const Registry & config)
+{
+    Algorithm::Configure(config);
+    this->LoadConfig();
+}
+//____________________________________________________________________________
+void XSecLinearCombinations::Configure(string config)
+{
+    Algorithm::Configure(config);
+    this->LoadConfig();
+}
+//_________________________________________________________________________
+void XSecLinearCombinations::LoadConfig(void)
+{
+  GetParamVect( "LinearCoefficients", fLinearCoefficients ) ; 
+  GetParamDef( "Normalise", fNormalise, true ) ;
+
+  GetParamVectorKeys( "CrossSection", fXSectionsKeys ) ;
+
+  if( fXSectionsKeys.size() != fLinearCoefficients.size() ) {
+    LOG("XSecLinearCombinations", pFATAL) << "Entries don't match" ;
+    LOG("XSecLinearCombinations", pFATAL) << "Cross Sections size: " << fXSectionsKeys.size() ;
+    LOG("XSecLinearCombinations", pFATAL) << "Linear Coefficients size: " << fLinearCoefficients.size() ;
+  }
+
+  for( unsigned int i = 0 ; i < fXSectionsKeys.size() ; ++i ) {
+    fXSections.push_back( dynamic_cast<const XSecAlgorithmI *> ( this->SubAlg( fXSectionsKeys[i] ) ) );
+    assert( fXSections[i] ) ;
+  }
+
+  fXSecIntegrator = dynamic_cast<const XSecIntegratorI *> ( this->SubAlg("NumericalIntegrationAlg") );
+  assert(fXSecIntegrator);
+
+}
---
+++ src/Physics/LinearCombination/XSection/XSecLinearCombinations.h
@@ -0,0 +1,72 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::XSecLinearCombinations
+
+\brief    Computes the Valencia MEC model differential cross section.
+          Uses precomputed hadon tensor tables.
+          Is a concrete implementation of the XSecAlgorithmI interface.
+
+\author   Code contributed by J. Schwehr, D. Cherdack, R. Gran and described
+          in arXiv:1601.02038 and some of the refereces there-in,
+          in particular PRD 88 (2013) 113007
+
+          Substantial code refactorizations by the core GENIE group.
+
+          Refactored in 2018 by S. Gardiner to use the new hadron tensor
+          framework
+
+\ref      J. Nieves, I. Ruiz Simo, M.J. Vicente Vacas,
+          Inclusive quasi-elastic neutrino reactions, PRC 83 (2011) 045501
+
+\created  Mar 22, 2016
+
+\cpright  Copyright (c) 2003-2020, The GENIE Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+*/
+//____________________________________________________________________________
+
+#ifndef _XSEC_LINEAR_COMBINATIONS_H_
+#define _XSEC_LINEAR_COMBINATIONS_H_
+
+#include "Framework/EventGen/XSecAlgorithmI.h"
+#include "Physics/HadronTensors/HadronTensorModelI.h"
+
+namespace genie {
+
+class XSecIntegratorI;
+
+class XSecLinearCombinations : public XSecAlgorithmI {
+
+public:
+  XSecLinearCombinations();
+  XSecLinearCombinations(string config);
+  virtual ~XSecLinearCombinations();
+
+  // XSecAlgorithmI interface implementation
+  double XSec            (const Interaction * i, KinePhaseSpace_t k) const;
+  double Integral        (const Interaction * i) const;
+  bool   ValidProcess    (const Interaction * i) const;
+
+  // override the Algorithm::Configure methods to load configuration
+  // data to private data members
+  void Configure (const Registry & config);
+  void Configure (string config);
+
+private:
+
+  // Load algorithm configuration
+  void LoadConfig (void);
+
+
+  std::vector<RgKey> fXSectionsKeys ;
+  std::vector<const XSecAlgorithmI*> fXSections ;
+  std::vector<double> fLinearCoefficients ;
+  bool fNormalise ;
+
+  const XSecIntegratorI *  fXSecIntegrator; // Numerical integrator (GSL)
+
+};
+
+}       // genie namespace
+#endif  // _XSEC_LINEAR_COMBINATIONS_H_
