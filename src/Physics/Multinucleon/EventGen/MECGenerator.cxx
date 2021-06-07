1a288a96366f9188d7172add4d5de7954a84cb2a
Julia Tena Vidal <j.tena-vidal@liverpool.ac.uk>
2021-06-07 18:30:59 +0200

    adding truth variables in interacton

--- src/Physics/Multinucleon/EventGen/MECGenerator.cxx
+++ src/Physics/Multinucleon/EventGen/MECGenerator.cxx
@@ -692,11 +692,15 @@ void MECGenerator::SelectNSVLeptonKinematics (GHepRecord * event) const
       Plep = TMath::Sqrt( T * (T + (2.0 * LepMass)));  // ok is sqrt(E2 - m2)
       Q3 = TMath::Sqrt(Plep*Plep + Enu*Enu - 2.0 * Plep * Enu * Costh);
 
+      genie::utils::mec::Getq0q3FromTlCostl(T, Costh, Enu, LepMass, Q0, Q3);
+
       // Don't bother doing hard work if the selected Q3 is greater than Q3Max
       if (Q3 < fQ3Max){
 
           kinematics->SetKV(kKVTl, T);
           kinematics->SetKV(kKVctl, Costh);
+	  kinematics->SetKV( kKVQ0, Q0 ) ; 
+	  kinematics->SetKV( kKVQ3, Q3 ) ; 
 
           // decide whether to accept or reject these kinematics
           // AND set the chosen two-nucleon system
