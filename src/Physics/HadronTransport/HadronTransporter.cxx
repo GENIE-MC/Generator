c867b2f0e33b5bb2ee3ead86e692ac620fd49f27
Julia Tena Vidal <j.tena-vidal@liverpool.ac.uk>
2021-07-30 17:20:03 +0200

    fixing bug

--- src/Physics/HadronTransport/HadronTransporter.cxx
+++ src/Physics/HadronTransport/HadronTransporter.cxx
@@ -139,6 +139,7 @@ void HadronTransporter::Configure(string param_set)
 
   r.Set("HadronTransp-Enable", algos -> GetBool("HadronTransp-Enable") ) ;
   r.Set("HadronTransp-Model",  algos -> GetAlg("HadronTransp-Model")   ) ;
+  r.Set("CascadeReweightAlg",  algos -> GetAlg("CascadeReweightAlg")   ) ;
 
   Algorithm::Configure(r) ;
 
