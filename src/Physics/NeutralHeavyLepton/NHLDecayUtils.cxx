26334540d38c694486d1cda21423b1cbc7e1b7a1
kjplows <kom.plows@gmail.com>
2022-04-13 02:57:37 -0500

    Add direct read from config file to `genie::utils::nhl`

--- src/Physics/NeutralHeavyLepton/NHLDecayUtils.cxx
+++ src/Physics/NeutralHeavyLepton/NHLDecayUtils.cxx
@@ -178,3 +178,98 @@ PDGCodeList genie::utils::nhl::DecayProductList(NHLDecayMode_t nhldm)
   return decay_products;
 }
 //____________________________________________________________________________
+// see e.g. Physics/HadronTensors/TabulatedLabFrameHadronTensor.cxx for this code
+int genie::utils::nhl::GetCfgInt( string file_id, string set_name, string par_name )
+{
+  const genie::Registry * tmpReg = genie::AlgConfigPool::Instance()
+    ->CommonList( file_id, set_name );
+  int param = tmpReg->GetInt( par_name );
+  return param;
+}
+//____________________________________________________________________________
+// see e.g. Tools/Flux/GNuMIFlux.cxx for this code
+std::vector<int> genie::utils::nhl::GetCfgIntVec( string file_id, string set_name, string par_name )
+{
+  // get vector as string first
+  const genie::Registry * tmpReg = genie::AlgConfigPool::Instance()
+    ->CommonList( file_id, set_name );
+  string str = tmpReg->GetString( par_name );
+
+  // turn string into vector<int>
+  // be liberal about separators, users might punctuate for clarity
+  std::vector<string> strtokens = genie::utils::str::Split( str, " ,;:()[]=" );
+  std::vector<int> vect;
+  size_t ntok = strtokens.size();
+
+  for( size_t i=0; i < ntok; ++i ){
+    std::string trimmed = utils::str::TrimSpaces( strtokens[i] );
+    if( " " == trimmed || "" == trimmed ) continue; // skip empty strings
+    int val = strtod( trimmed.c_str(), (char **)NULL );
+    vect.push_back( val );
+  }
+
+  return vect;
+}
+//____________________________________________________________________________
+double genie::utils::nhl::GetCfgDouble( string file_id, string set_name, string par_name )
+{
+  const genie::Registry * tmpReg = genie::AlgConfigPool::Instance()
+    ->CommonList( file_id, set_name );
+  double param = tmpReg->GetDouble( par_name );
+  return param;
+}
+//____________________________________________________________________________
+std::vector<double> genie::utils::nhl::GetCfgDoubleVec( string file_id, string set_name, string par_name )
+{
+  // get vector as string first
+  const genie::Registry * tmpReg = genie::AlgConfigPool::Instance()
+    ->CommonList( file_id, set_name );
+  string str = tmpReg->GetString( par_name );
+
+  // turn string into vector<double>
+  // be liberal about separators, users might punctuate for clarity
+  std::vector<string> strtokens = genie::utils::str::Split( str, " ,;:()[]=" );
+  std::vector<double> vect;
+  size_t ntok = strtokens.size();
+
+  for( size_t i=0; i < ntok; ++i ){
+    std::string trimmed = utils::str::TrimSpaces( strtokens[i] );
+    if( " " == trimmed || "" == trimmed ) continue; // skip empty strings
+    double val = strtod( trimmed.c_str(), (char **)NULL );
+    vect.push_back( val );
+  }
+
+  return vect;
+}
+//____________________________________________________________________________
+bool genie::utils::nhl::GetCfgBool( string file_id, string set_name, string par_name )
+{
+  const genie::Registry * tmpReg = genie::AlgConfigPool::Instance()
+    ->CommonList( file_id, set_name );
+  bool param = tmpReg->GetBool( par_name );
+  return param;
+}
+//____________________________________________________________________________
+std::vector<bool> genie::utils::nhl::GetCfgBoolVec( string file_id, string set_name, string par_name )
+{
+  // get vector as string first
+  const genie::Registry * tmpReg = genie::AlgConfigPool::Instance()
+    ->CommonList( file_id, set_name );
+  string str = tmpReg->GetString( par_name );
+
+  // turn string into vector<bool>
+  // be liberal about separators, users might punctuate for clarity
+  std::vector<string> strtokens = genie::utils::str::Split( str, " ,;:()[]=" );
+  std::vector<bool> vect;
+  size_t ntok = strtokens.size();
+
+  for( size_t i=0; i < ntok; ++i ){
+    std::string trimmed = utils::str::TrimSpaces( strtokens[i] );
+    if( " " == trimmed || "" == trimmed ) continue; // skip empty strings
+    bool val = strtod( trimmed.c_str(), (char **)NULL );
+    vect.push_back( val );
+  }
+
+  return vect;
+}
+//____________________________________________________________________________
--- src/Physics/NeutralHeavyLepton/NHLDecayUtils.h
+++ src/Physics/NeutralHeavyLepton/NHLDecayUtils.h
@@ -22,6 +22,11 @@
 #include "Framework/ParticleData/PDGCodeList.h"
 #include "Physics/NeutralHeavyLepton/NHLDecayMode.h"
 
+// -- for retrieval of parameters from config
+#include "Framework/Algorithm/AlgConfigPool.h"
+#include "Framework/Registry/Registry.h"
+#include "Framework/Utils/StringUtils.h"
+
 using std::string;
 
 namespace genie 
@@ -33,6 +38,14 @@ namespace genie
       string       AsString                (genie::NHL::NHLDecayMode_t nhldm);
       bool         IsKinematicallyAllowed  (genie::NHL::NHLDecayMode_t nhldm, double Mnhl);
       PDGCodeList  DecayProductList        (genie::NHL::NHLDecayMode_t nhldm);
+
+      // for obtaining params, etc, directly from config
+      int                 GetCfgInt        (string file_id, string set_name, string par_name);
+      std::vector<int>    GetCfgIntVec     (string file_id, string set_name, string par_name);
+      double              GetCfgDouble     (string file_id, string set_name, string par_name);
+      std::vector<double> GetCfgDoubleVec  (string file_id, string set_name, string par_name);
+      bool                GetCfgBool       (string file_id, string set_name, string par_name);
+      std::vector<bool>   GetCfgBoolVec    (string file_id, string set_name, string par_name);
 
    } // nhl 
  } // utils 
