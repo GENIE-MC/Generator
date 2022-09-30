b336fe9233ad3324411e60b97300e5fc2c0ccfa3
Julia Tena Vidal <jtenavid@fnal.gov>
2022-09-30 05:05:11 -0500

    adding vN script

---
+++ src/scripts/production/python/xsec_splines/GridUtils.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+import os 
+
+def CreateShellScript ( command , jobs_dir, file_name, grid_system, genie_setup, conf_dir ) :
+    shell_file = jobs_dir+"/"+file_name+".sh"
+    if os.path.exists(shell_file):
+        os.remove(shell_file)
+
+    script = open( shell_file, 'w' ) 
+    script.write("#!/bin/bash \n")
+    if grid_system == 'FNAL':
+        script.write("cd $CONDOR_DIR_INPUT \n")
+    else :
+        script.write("cd "+jobs_dir)
+    script.write("source "+genie_setup+" "+conf_dir+" \n")
+    if grid_system == 'FNAL':
+        script.write("cd $CONDOR_DIR_INPUT \n")
+    script.write(command+"\n")
+    if grid_system == 'FNAL':
+        script.write("ifdh cp -D "+file_name+".xml "+jobs_dir+" \n")
+
+    script.close()
+    return shell_file 
+
+def FNALShellCommands(genie_setup):
+    grid_command_options = "-n --memory=1GB --disk=20GB --expected-lifetime=10h -f "+genie_setup 
+    grid_command_options += " --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC,OFFSITE --lines '+FERMIHTC_AutoRelease=True' "
+    grid_command_options += "--lines '+FERMIHTC_GraceMemory=4096' --lines '+FERMIHTC_GraceLifetime=6000' "
+
+    return grid_command_options 
--- src/scripts/production/python/xsec_splines/vASplineCommands.py
+++ src/scripts/production/python/xsec_splines/vASplineCommands.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 """\
-This script generates the scripts required to run vN splines. In addition it returns
+This script generates the scripts required to run vA splines. In addition it returns
 the commands required to run the scripts in a specific grid. 
 The input variables required by the vNSPlineCommands function are:
 - version : genie version number
@@ -14,7 +14,10 @@ The input variables required by the vNSPlineCommands function are:
 - group : group at the grid
 - softw_topdir : top level dir for softw installations. Default pwd($GENIE_BASEDIR)
 - jobs_topdir : top level dir for job files, default: $PWD
+- freenucsplines : Absolute path to free nuclear spline directory
 - gen_list : comma separated list of event generator list, default all. For electron mode, all corresponds to all EM modes.
+- target-list : comma separated list of targets' PDGs, default De,He4,C12,O16,Ar40,Fe56,Pb207.
+                Note that it needs the PDG code, not chemical name.
 - nu_list : comma separated list of neutrino types. Default all neutrinos. It is possible to use electrons instead.
 - e_max : maxmium energy of the splines in GeV. Default 200 GeV.
 - n_knots : number of knots per spline. Default 100.  
@@ -27,21 +30,15 @@ Author:
 Copyright:
    Copyright (c) 2003-2022, The GENIE Collaboration
    For the full text of the license visit http://copyright.genie-mc.org
-
 """
 import os 
-import GridUtils 
+import GridUtils
 
 # Define Dictionaries
-nucleons_pdg = {
-    'n' : 1000000010,
-    'p' : 1000010010 }
-nucleons_name = {
-    1000000010 : 'n',
-    1000010010 : 'p' } 
 
-nucleons_EW_proc = [ 'none', 'CCRES', 'NCRES', 'CCDIS', 'NCDIS', 'CCDFR', 'NCDFR', 'Fast' ]
-nucleons_EM_proc = [ 'none', 'EMRES', 'EMDIS', 'EMQEL' ]
+nucleons_EW_proc = [ 'none', 'WeakMEC', 'CCCOHPION', 'NCCOHPION', 'Fast' ]
+nucleons_EM_proc = [ 'none', 'EMMEC', 'EMQE', 'EMRES', 'EMDIS' ]
+tgt_pdg = [1000010020, 1000010030, 1000020030, 1000020040, 1000060120, 1000080160, 1000130270, 1000200400, 1000200480, 1000260560, 1000791970, 1000822080, 1000922380 ]
 
 nu_pdg_def = { 've'      :   12,
                'vebar'   :  -12,
@@ -60,11 +57,11 @@ e_pdg_def = { 'e' : 11,
 e_name_def = { 11 : 'e', 
               -11: 'ebar' }
 
-def vNSplineCommands( version='master', conf_dir='', tune='G18_02_02_11b', arch='SL6.x86_64', production='routine_validation', cycle='01', grid_system='FNAL', group='genie', 
+def vASplineCommands( version='master', conf_dir='', tune='G18_02_02_11b', arch='SL6.x86_64', production='routine_validation', cycle='01', grid_system='FNAL', group='genie', 
                       softw_topdir=os.getenv('GENIE_MASTER_DIR'), genie_topdir=os.getenv('GENIE'), jobs_topdir=os.getenv('PWD'), gen_list='all', nu_list='all', 
-                      e_max=200, n_knots=100, with_priority=False, run_one=False ) :
+                      tgt_list = 'all', e_max=200, n_knots=100, with_priority=False, run_one=False ) :
 
-    jobs_dir = jobs_topdir+'/'+version+'-'+production+'_'+cycle+'-xsec_vN'
+    jobs_dir = jobs_topdir+'/'+version+'-'+production+'_'+cycle+'-xsec_vA'
 
     # configure setup 
     if grid_system == 'FNAL' : 
@@ -90,7 +87,7 @@ def vNSplineCommands( version='master', conf_dir='', tune='G18_02_02_11b', arch=
             req_nu_list.append(key)
         for key in e_pdg_def : 
             req_e_list.append(key)
-
+        
     req_EW_list = []
     req_EM_list = []
     if ( gen_list != 'all' ) :
@@ -104,13 +101,11 @@ def vNSplineCommands( version='master', conf_dir='', tune='G18_02_02_11b', arch=
         req_EW_list = nucleons_EW_proc 
         req_EM_list = nucleons_EM_proc
 
-    if len(req_EW_list) : 
-        print( 'EW processes =' )
-        for i in range(len(req_EW_list)): print(req_EW_list[i]); 
- 
-    if len(req_EM_list) : 
-        print( 'EM processes =' )
-        for i in range(len(req_EM_list)): print(req_EM_list[i]); 
+    req_tgt_list = []
+    if ( tgt_list != 'all' ) :
+        req_tgt_list = tgt_list.split(',')
+    else : 
+        req_tgt_list = tgt_pdg 
 
     # Make directory
     if not os.path.exists(jobs_dir) : 
@@ -118,7 +113,6 @@ def vNSplineCommands( version='master', conf_dir='', tune='G18_02_02_11b', arch=
 
     # grid options 
     command_list = []
-
     grid_command_options = ''
     if grid_system == 'FNAL' :
         GridUtils.FNALShellCommands(genie_setup)
@@ -127,27 +121,19 @@ def vNSplineCommands( version='master', conf_dir='', tune='G18_02_02_11b', arch=
     grid_sub_cmd = []     
     shell_file_list = []
     for nu in req_nu_list : 
-        for target in nucleons_pdg : 
+        for target in req_tgt_list : 
             for process in req_EW_list :
                 if process == 'none' : continue 
-                if process == 'CCQE' : 
-                    if target == 'n' and nu_pdg_def[nu] < 0 : continue  
-                    if target == 'p' and nu_pdg_def[nu] > 0 : continue 
-                if process == 'CCDFR' or process == 'NCDFR' :
-                    if target == 'n' : continue 
-                if process == 'FastOn' : 
-                    event_gen_list = process+target 
-                else :
-                    event_gen_list = process 
+                event_gen_list = process 
                 
                 # Job definition
-                job_name = nu+'_on_'+target+'_'+process
+                job_name = nu+'_on_'+str(target)+'_'+process
                 if grid_system == 'FNAL' : 
                     filename_template = job_name
                 else :
                     filename_template = jobs_dir + '/' + job_name
 
-                gmkspl_cmd = "gmkspl -p "+str(nu_pdg_def[nu])+ " -t "+ str(nucleons_pdg[target]) + " -n "+ str(n_knots) + " -e "+ str(e_max) + " --tune " + tune 
+                gmkspl_cmd = "gmkspl -p "+str(nu_pdg_def[nu])+ " -t "+ str(target) + " -n "+ str(n_knots) + " -e "+ str(e_max) + " --tune " + tune 
                 gmkspl_cmd += " -o "+ filename_template+".xml --event-generator-list " + event_gen_list   
                 
                 shell_file = GridUtils.CreateShellScript ( gmkspl_cmd , jobs_dir, filename_template, grid_system, genie_setup, conf_dir ) 
@@ -158,19 +144,19 @@ def vNSplineCommands( version='master', conf_dir='', tune='G18_02_02_11b', arch=
     # Create electron spline commands:
     
     for e in req_e_list : 
-        for target in nucleons_pdg : 
+        for target in req_tgt_list : 
             for process in req_EM_list :
                 if process == 'none' : continue 
                 event_gen_list = process 
                 
                 # Job definition
-                job_name = e+'_on_'+target+'_'+process
+                job_name = e+'_on_'+str(target)+'_'+process
                 if grid_system == 'FNAL' : 
                     output_spline = job_name
                 else :
                     output_spline = jobs_dir + '/' + job_name
                 
-                gmkspl_cmd = "gmkspl -p "+str(e_pdg_def[e])+ " -t "+ str(nucleons_pdg[target]) + " -n "+ str(n_knots) + " -e "+ str(e_max) + " --tune " + tune 
+                gmkspl_cmd = "gmkspl -p "+str(e_pdg_def[e])+ " -t "+ str(target) + " -n "+ str(n_knots) + " -e "+ str(e_max) + " --tune " + tune 
                 gmkspl_cmd += " -o "+ filename_template+".xml --event-generator-list " + event_gen_list   
                 
                 shell_file = GridUtils.CreateShellScript ( gmkspl_cmd , jobs_dir, filename_template, grid_system, genie_setup, conf_dir ) 
@@ -178,7 +164,7 @@ def vNSplineCommands( version='master', conf_dir='', tune='G18_02_02_11b', arch=
                     command_list.append( "jobsub_submit "+grid_command_options+ " file://"+shell_file )
 
 
-    ## Add command list to dictionary; Key is 0 => free nucleon spline calculation
+    ## Add command list to dictionary; Key is 2 => nuclei spline calculation
     command_dict = {}
-    command_dict[0] = command_list ; 
+    command_dict[2] = command_list ; 
     return command_dict ; 
