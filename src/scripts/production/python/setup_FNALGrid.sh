0b316d3ce845d9e7d2534bb00f07cf3fd5cbc46b
Julia Tena Vidal <jtenavid@fnal.gov>
2022-10-04 02:59:10 -0500

    adding max number of copy attempts

--- src/scripts/production/python/setup_FNALGrid.sh
+++ src/scripts/production/python/setup_FNALGrid.sh
@@ -28,6 +28,8 @@ export PATH=$GENIE/bin:$GENIE_REWEIGHT/bin:$PATH
 export LD_LIBRARY_PATH=$GENIE_LIB:$GENIE_REWEIGHT/lib:$LD_LIBRARY_PATH 
 unset GENIEBASE 
 
+export IFDH_CP_MAXRETRIES=2
+
 cd $GENIE 
 
 GENIE_VERSION=master
@@ -54,8 +56,6 @@ if [ ! -z "$2" ] ; then
 else
   unset GALGCONF
 fi
-
-#./configure --enable-gsl --enable-rwght --with-optimiz-level=O2 --with-log4cpp-inc=${LOG4CPP_INC} --with-log4cpp-lib=${LOG4CPP_LIB} --with-libxml2-inc=${LIBXML2_INC} --with-libxml2-lib=${LIBXML2_LIB} --with-lhapdf5-lib=${LHAPDF_LIB} --with-lhapdf5-inc=${LHAPDF_INC} --with-pythia6-lib=${PYTHIA_LIB} 
 
 ./configure \
 --enable-rwght \
