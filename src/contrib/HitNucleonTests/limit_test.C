b1afd378f5d768fd6a30794746df85888c8d47a7
mroda <mroda@liverpool.ac.uk>
2022-10-12 14:02:20 +0100

    Test draft

---
+++ src/contrib/HitNucleonTests/limit_test.C
@@ -0,0 +1,19 @@
+
+
+
+void limit_test {
+
+  using namespace genie;
+
+  // neutrino case on shell
+  ROOT::Math::PxPyPzEVector probe( 0., 0., 2., 2. );
+
+  double proton_mass = PDGLibrary::Instance()->Find(kPdgProton)->Mass();
+
+  ROOT::Math::PxPyPzMVector nucleon( 0.1., 0.2, 0.1, proton_mass );
+  
+  std::cout << probe ;
+  std::cout << nucleon ;
+  
+
+}
