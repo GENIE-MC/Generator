ae41642b77b8b8c4b941c1c13ae1d37e0ad2e5a8
mroda <mroda@liverpool.ac.uk>
2022-10-12 16:29:29 +0100

    Complete test for W limit

--- src/contrib/HitNucleonTests/limit_test.C
+++ src/contrib/HitNucleonTests/limit_test.C
@@ -1,14 +1,25 @@
+#include <iostream>
 
 #include "Framework/ParticleData/PDGLibrary.h"
 #include "Framework/ParticleData/PDGCodes.h"
 
 #include "Framework/Utils/KineUtils.h"
 
+#include "Framework/Utils/Range1.h"
 
 #include "Math/Vector4D.h"
 #include "Math/Boost.h" 
 
-void limit_test() {
+
+ostream & operator << ( ostream & out, const genie::Range1D_t r ) {
+  
+  out << '[' << r.min << ", " << r.max << ']' ;
+  return out ;
+}
+
+
+
+void Wlimit( ) {
 
   using namespace genie;
 
@@ -31,7 +42,7 @@ void limit_test() {
 
   auto new_range = utils::kinematics::InelWLim(sqrt_s, mu_mass);
 
-  std::cout << "New: " << new_range.min << " - " << new_range.max << std::endl;
+  std::cout << "New: " << new_range << std::endl;
 
   // old range
 
@@ -46,7 +57,116 @@ void limit_test() {
 
   auto old_range =  utils::kinematics::InelWLim(old_probe.E(), proton_mass, mu_mass);  
 
-  std::cout << "Old: " << old_range.min << " - " << old_range.max << std::endl;
+  std::cout << "Old: " << old_range << std::endl;
   
+
+  // neutrino case off shell nucleon
+
+  nucleon.SetPxPyPzE( 0.1, 0.2, 0.1, proton_mass - 0.1 );
+  std::cout << "Off shell target: " << nucleon << std::endl ;
+
+  total = probe + nucleon ;
+  sqrt_s = total.M() ;
+
+  std::cout << sqrt_s << std::endl;
+  new_range = utils::kinematics::InelWLim(sqrt_s, mu_mass);
+
+  std::cout << "New: " << new_range << std::endl;
+
+  boost = nucleon.BoostToCM();
+  t.SetComponents(boost);
+  old_probe = t(probe);
+
+  old_range =  utils::kinematics::InelWLim(old_probe.E(), proton_mass, mu_mass);
+
+  std::cout << "Old: " << old_range << std::endl;
+
+  old_range =  utils::kinematics::InelWLim(old_probe.E(), nucleon.M(), mu_mass);
+  std::cout << "Old with correction: " << old_range << std::endl;
+
+
+  // neutrino case very off shell (beta > 1)
+
+  nucleon.SetPxPyPzE( 0.2, 0.8, 0.1, proton_mass - 0.2 );
+  std::cout << "very Off shell target: " << nucleon << std::endl ;
+
+  std::cout << "Beta: " << nucleon.Beta() << std::endl;
+
+  total = probe + nucleon ;
+ 
+  sqrt_s = total.M() ;
+
+  std::cout << sqrt_s << std::endl;
+  new_range = utils::kinematics::InelWLim(sqrt_s, mu_mass);
+
+  std::cout << "New: " << new_range << std::endl;
+
+}
+
+
+void Q2limit( ) {
+
+  using namespace genie;
+
+  // neutrino case on shell
+  ROOT::Math::PxPyPzEVector probe( 0., 0., 2., 2. );
+
+  double proton_mass = PDGLibrary::Instance()->Find(kPdgProton)->Mass();
+
+  ROOT::Math::PxPyPzMVector nucleon( 0.1, 0.2, 0.1, proton_mass );
+  
+  std::cout << probe.E() << std::endl ;
+  std::cout << nucleon.E() << std::endl ;
+  
+  auto total = probe + nucleon ;
+  auto sqrt_s = total.M() ;
+
+  std::cout << sqrt_s << std::endl;
+
+  double mu_mass =  PDGLibrary::Instance()->Find(kPdgMuon)->Mass();
+
+  auto new_range = utils::kinematics::InelWLim(sqrt_s, mu_mass);
+
+  std::cout << "New: " << new_range << std::endl;
+
+  // old range
+
+  auto boost = nucleon.BoostToCM();
+  
+  ROOT::Math::Boost t(boost);
+  
+  auto old_probe = t(probe);
+
+  std::cout << old_probe << std::endl;
+
+
+  auto old_range =  utils::kinematics::InelWLim(old_probe.E(), proton_mass, mu_mass);  
+
+  std::cout << "Old: " << old_range << std::endl;
+  
+
+  // neutrino case off shell nucleon
+
+  nucleon.SetPxPyPzE( 0.1, 0.2, 0.1, proton_mass - 0.1 );
+  std::cout << "Off shell target: " << nucleon << std::endl ;
+
+  total = probe + nucleon ;
+  sqrt_s = total.M() ;
+
+  std::cout << sqrt_s << std::endl;
+  new_range = utils::kinematics::InelWLim(sqrt_s, mu_mass);
+
+  std::cout << "New: " << new_range << std::endl;
+
+  boost = nucleon.BoostToCM();
+  t.SetComponents(boost);
+  old_probe = t(probe);
+
+  old_range =  utils::kinematics::InelWLim(old_probe.E(), proton_mass, mu_mass);
+
+  std::cout << "Old: " << old_range << std::endl;
+
+  old_range =  utils::kinematics::InelWLim(old_probe.E(), nucleon.M(), mu_mass);
+  std::cout << "Old with correction: " << old_range << std::endl;
 
 }
