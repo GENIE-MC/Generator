d471aba48d361c7c8ced862886b432dd3c981e35
Ishaan Vohra <108065195+Ishaan-vohra@users.noreply.github.com>
2022-11-25 18:17:26 +0000

    Indentation for pip_distribution.C

--- src/contrib/ivohra/Data_Analysis/pip_distribution.C
+++ src/contrib/ivohra/Data_Analysis/pip_distribution.C
@@ -1,7 +1,7 @@
 //____________________________________________________________________________
 /*
-\brief    Plot a normalized 1D histogram depicting the distribution of Pi+ 
-          produced in each event
+\brief    Plots a normalized 1D histogram depicting the distribution of final
+          state Pi+ produced in each event.
 \author   Ishaan Vohra <ivohra@exeter.edu / ishaanklv@gmail.com>
           Phillips Exeter Academy
 \created  August 16, 2022
@@ -10,28 +10,23 @@
 
 #include <iostream>
 #include <string>
-
 #include "GHepParticle.h"
 #include "ScatteringType.h"
 #include "ProcessInfo.h"
 #include "GHepParticle.h"
 #include "TObjArray.h"
-
-#include "TString.h" 
+#include "TString.h"
 #include "TFile.h"
 #include "TTree.h"
 #include "TH1.h"
 #include "TAxis.h"
-
-#include "NtpMCTreeHeader.h" 
-#include "NtpMCEventRecord.h" 
-#include "EventRecord.h" 
+#include "NtpMCTreeHeader.h"
+#include "NtpMCEventRecord.h"
+#include "EventRecord.h"
 #include "BaryonResUtils.h"
-
-#include "GHepParticle.h"       
+#include "GHepParticle.h"
 #include "PDGUtils.h"
 #include "GHepStatus.h"
-
 #include "PDGCodes.h"
 
 using namespace genie;
@@ -39,69 +34,66 @@ using namespace genie;
 void pip_distribution(string filename = "/hepstore/ivohra/miniboone1.ghep.root")
 {
 
-// Open the GHEP ROOT file
+  // Open the GHEP ROOT file
 
   TFile infile(filename.c_str());
 
-
   // Get the tree header
 
-
-  NtpMCTreeHeader * header =
-    dynamic_cast<NtpMCTreeHeader*> (infile.Get("header"));
+  NtpMCTreeHeader *header =
+      dynamic_cast<NtpMCTreeHeader *>(infile.Get("header"));
 
   // Get the GENIE GHEP tree and set its branch address
 
-  TTree * tree = dynamic_cast<TTree*> (infile.Get("gtree"));
-  NtpMCEventRecord * mcrec = 0;
+  TTree *tree = dynamic_cast<TTree *>(infile.Get("gtree"));
+  NtpMCEventRecord *mcrec = 0;
   tree->SetBranchAddress("gmcrec", &mcrec);
 
-  // Make histogram
+  // Make the histogram
 
-auto myHist = new TH1D("h1","Distribution of Number of π+ Produced per Event;Count Per Event;Probability",6,-0.5,5.5);
+  auto myHist = new TH1D("h1", "Distribution of Number of π+ Produced per Event;Count Per Event;Probability", 6, -0.5, 5.5);
 
   // Event loop
 
-  for(Long64_t i=0; i < (tree->GetEntries()); i++){
+  for (Long64_t i = 0; i < (tree->GetEntries()); i++)
+  {
     tree->GetEntry(i);
 
- EventRecord & event = *(mcrec->event);
-
-   TObjArrayIter iter(&event);
-   GHepParticle * p = 0;
+    EventRecord &event = *(mcrec->event);
+    TObjArrayIter iter(&event);
+    GHepParticle *p = 0;
 
     int counter = 0;
 
-   // Particle loop
+    // Particle loop
 
-   while((p = dynamic_cast<GHepParticle *>(iter.Next()))) { 
-
-      int pdgc   = p->Pdg();
+    while ((p = dynamic_cast<GHepParticle *>(iter.Next())))
+    {
+      int pdgc = p->Pdg();
       int status = p->Status();
 
-      if(status == 1 && pdgc == 211){
-
-      counter++;
-
-        } 
+      if (status == 1 && pdgc == 211)
+      {
+        counter++;
+      }
     }
+
+    // Fill the bins
 
     myHist->Fill((counter));
     mcrec->Clear();
   }
 
-//normalization
+  // Normalization and stat box removal
 
- TH1*normh = (TH1D*)(myHist->Clone("normh"));
-   normh->Scale(1./normh->GetEntries());
-   normh->SetStats(false);
+  TH1 *normh = (TH1D *)(myHist->Clone("normh"));
+  normh->Scale(1. / normh->GetEntries());
+  normh->SetStats(false);
 
+  // Create the output file
 
-TFile *outfile = new TFile("pip_distribution.root","RECREATE"); 
-outfile->cd();
-normh->Write();   
-outfile->Close();
-
+  TFile *outfile = new TFile("pip_distribution.root", "RECREATE");
+  outfile->cd();
+  normh->Write();
+  outfile->Close();
 }
-
-
