c90db1434ae2760a5c867ebd6963b284ab4180a1
Ishaan Vohra <108065195+Ishaan-vohra@users.noreply.github.com>
2022-11-25 18:20:15 +0000

    Indentation for transverse_p.C

--- src/contrib/ivohra/Kinematic_Variables/transverse_p.C
+++ src/contrib/ivohra/Kinematic_Variables/transverse_p.C
@@ -1,17 +1,18 @@
 //____________________________________________________________________________
 /*
-\brief    Plot a 1D histogram depicting the distribution of the total final 
-          state transverse momentum of each event
+\brief    Plots a 1D histogram depicting the distribution of the total final
+          state transverse momentum of each event.
 \author   Ishaan Vohra <ivohra@exeter.edu / ishaanklv@gmail.com>
           Phillips Exeter Academy
 \created  August 16, 2022
 */
 //____________________________________________________________________________
 
-
 #include <iostream>
 #include <string>
-
+#include <iomanip>
+#include <map>
+#include <random>
 #include "TFile.h"
 #include "TTree.h"
 #include "TTreeReader.h"
@@ -19,30 +20,20 @@
 #include "TTreeReaderArray.h"
 #include "TH1D.h"
 #include "TH2.h"
-#include <iomanip>
-#include <map>
-#include <random>
-
-
 #include "GHepParticle.h"
 #include "ScatteringType.h"
 #include "ProcessInfo.h"
 #include "TObjArray.h"
 #include "PDGLibrary.h"
-
-#include "TString.h" 
-
-#include "NtpMCTreeHeader.h" 
-#include "NtpMCEventRecord.h" 
-#include "EventRecord.h" 
+#include "TString.h"
+#include "NtpMCTreeHeader.h"
+#include "NtpMCEventRecord.h"
+#include "EventRecord.h"
 #include "BaryonResUtils.h"
-     
 #include "PDGUtils.h"
 #include "GHepStatus.h"
-
 #include "PDGCodes.h"
 #include "TDatabasePDG.h"
-
 #include "TVector3.h"
 
 using namespace genie;
@@ -50,74 +41,70 @@ using namespace genie;
 void transverse_p(string filename = "/hepstore/ivohra/miniboone1.ghep.root")
 {
 
-// Open the GHEP/ROOT file
+  // Open the GHEP/ROOT file
 
   TFile infile(filename.c_str());
 
-
   // Get the tree header
 
-
-  NtpMCTreeHeader * header =
-    dynamic_cast<NtpMCTreeHeader*> (infile.Get("header"));
+  NtpMCTreeHeader *header =
+      dynamic_cast<NtpMCTreeHeader *>(infile.Get("header"));
 
   // Get the GENIE GHEP tree and set its branch address
 
-  TTree * tree = dynamic_cast<TTree*> (infile.Get("gtree"));
-  NtpMCEventRecord * mcrec = 0;
+  TTree *tree = dynamic_cast<TTree *>(infile.Get("gtree"));
+  NtpMCEventRecord *mcrec = 0;
   tree->SetBranchAddress("gmcrec", &mcrec);
 
-  //Make histogram
+  // Make the histogram
 
-auto myHist = new TH1D("h1","Total Transverse Momentum;Momentum (GeV/c);Probability",100,0,1);
+  auto myHist = new TH1D("h1", "Total Transverse Momentum;Momentum (GeV/c);Probability", 100, 0, 1);
 
   // Event loop
 
-  for(Long64_t i=0; i < (tree->GetEntries()); i++){
+  for (Long64_t i = 0; i < (tree->GetEntries()); i++)
+  {
     tree->GetEntry(i);
 
+    EventRecord &event = *(mcrec->event);
+    TObjArrayIter iter(&event);
+    GHepParticle *p = 0;
 
- EventRecord & event = *(mcrec->event);
+    double Ptotx = 0;
+    double Ptoty = 0;
 
-   TObjArrayIter iter(&event);
-   GHepParticle * p = 0;
+    // Particle loop
 
-    double Ptotx = 0; 
-    double Ptoty = 0; 
+    while ((p = dynamic_cast<GHepParticle *>(iter.Next())))
+    {
+      int myStatus = p->Status();
 
-   // Particle loop
-
-   while((p = dynamic_cast<GHepParticle *>(iter.Next()))) { 
-
-    int myStatus = p->Status();
-
-      if(myStatus == 1){ 
-
+      if (myStatus == 1)
+      {
         double myPx = p->Px();
         double myPy = p->Py();
-
         Ptotx += myPx;
         Ptoty += myPy;
-
-
-            } 
-        }
-
-    TVector3 Ptotvec (Ptotx, Ptoty,0); 
-
-    myHist->Fill(Ptotvec.Mag());
-
-    mcrec->Clear();
-
+      }
     }
 
-TH1*normh = (TH1D*)(myHist->Clone("normh"));
-normh->Scale(1./normh->GetEntries());
-normh->SetStats(false);
+    // Fill the bins
 
-TFile *outfile = new TFile("transverse_p.root","RECREATE"); 
-outfile->cd();
-normh->Write();   
-outfile->Close();
+    TVector3 Ptotvec(Ptotx, Ptoty, 0);
+    myHist->Fill(Ptotvec.Mag());
+    mcrec->Clear();
+  }
 
+  // Normalization and stat box removal
+
+  TH1 *normh = (TH1D *)(myHist->Clone("normh"));
+  normh->Scale(1. / normh->GetEntries());
+  normh->SetStats(false);
+
+  // Create the output file
+
+  TFile *outfile = new TFile("transverse_p.root", "RECREATE");
+  outfile->cd();
+  normh->Write();
+  outfile->Close();
 }
