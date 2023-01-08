352dec5c029006b8c3c21c20c13c07d78584c242
Steven Gardiner <gardiner@fnal.gov>
2023-01-07 22:29:51 -0600

    Add a new class derived from NtpWriterI called HepMC3NtpWriter. This class produces HepMC3-format files using GHepRecord objects as input

---
+++ src/Framework/Ntuple/HepMC3NtpWriter.h
@@ -0,0 +1,69 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::HepMC3NtpWriter
+
+\brief    Writes GENIE events to a HepMC3-format output file
+
+\author   Steven Gardiner <gardiner \at fnal.gov>
+          Fermi National Accelerator Laboratory
+
+\created  January 7, 2023
+
+\cpright  Copyright (c) 2003-2022, The GENIE Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+*/
+//____________________________________________________________________________
+
+#ifndef _HEPMC3_NTP_WRITER_H_
+#define _HEPMC3_NTP_WRITER_H_
+
+#include "Framework/Conventions/GBuild.h"
+#ifdef __GENIE_HEPMC3_INTERFACE_ENABLED__
+
+#include <memory>
+
+#include "Framework/Ntuple/NtpWriterI.h"
+
+// Forward-declare needed HepMC3 classes here
+namespace HepMC3 {
+  class GenEvent;
+  class GenRunInfo;
+  class WriterAscii;
+}
+
+namespace genie {
+
+class EventRecord;
+class HepMC3Converter;
+
+class HepMC3NtpWriter : public NtpWriterI {
+
+public:
+
+  HepMC3NtpWriter();
+  virtual ~HepMC3NtpWriter();
+
+  ///< initialize the ntuple writer
+  virtual void Initialize() override;
+
+  ///< add event
+  virtual void AddEventRecord( int ievent, const EventRecord* ev_rec ) override;
+
+  ///< save the event tree
+  virtual void Save() override;
+
+protected:
+
+  virtual void SetDefaultFilename(
+    const std::string& filename_prefix = "gntp") override;
+
+  std::shared_ptr< genie::HepMC3Converter > fConverter;
+  std::shared_ptr< HepMC3::WriterAscii > fWriter;
+
+};
+
+} // genie namespace
+
+#endif // __GENIE_HEPMC3_INTERFACE_ENABLED__
+#endif // _HEPMC3_NTP_WRITER_H_
