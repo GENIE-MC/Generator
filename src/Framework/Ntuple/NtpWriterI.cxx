5dba34a354b6a4cf9ca80275fa715270d0aae302
Steven Gardiner <gardiner@fnal.gov>
2023-01-07 21:24:11 -0600

    Refactor the NtpWriter class to be a derived class of a new abstract interface NtpWriterI. This is a step towards supporting arbitrary event output formats, including those which are not ROOT-based

---
+++ src/Framework/Ntuple/NtpWriterI.cxx
@@ -0,0 +1,35 @@
+//____________________________________________________________________________
+/*
+ Copyright (c) 2003-2022, The GENIE Collaboration
+ For the full text of the license visit http://copyright.genie-mc.org
+
+ Costas Andreopoulos <constantinos.andreopoulos \at cern.ch>
+ University of Liverpool & STFC Rutherford Appleton Laboratory
+
+ Steven Gardiner <gardiner \at fnal.gov>
+ Fermi National Accelerator Laboratory
+*/
+//____________________________________________________________________________
+
+#include <string>
+
+#include "Framework/Ntuple/NtpWriterI.h"
+
+using namespace genie;
+
+//____________________________________________________________________________
+NtpWriterI::~NtpWriterI()
+{
+
+}
+//____________________________________________________________________________
+void NtpWriterI::CustomizeFilename(const std::string& filename)
+{
+ fOutFilename = filename;
+}
+//____________________________________________________________________________
+void NtpWriterI::CustomizeFilenamePrefix(const std::string& prefix)
+{
+  this->SetDefaultFilename(prefix);
+}
+//____________________________________________________________________________
