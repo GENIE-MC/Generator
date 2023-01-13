c42655ade852146c535fea550e7415830ffa507a
mroda <mroda@liverpool.ac.uk>
2023-01-13 17:21:40 +0000

    Correction in the solution

--- src/contrib/HitNucleonTests/threshold_test.C
+++ src/contrib/HitNucleonTests/threshold_test.C
@@ -16,7 +16,7 @@ using namespace genie;
 using namespace std;
 
 
-double find_threshold( const Interaction & i, double prec = 0.00001, double max = 10. ) {
+double find_threshold( const Interaction & i, double prec = 0.000001, double max = 10. ) {
 
   double min = 0. ;
 
@@ -58,6 +58,9 @@ double new_threshold( const Interaction & i ) {
   double p_T_p = i.InitState().Tgt().HitNucP4().Pz();
 
   if ( E_T > abs(p_T_p) ) {
+
+    cout << "Simple case" << endl;
+    
     auto a2 = alpha*alpha;
     auto Et2 = E_T*E_T;
     auto ptp2 = p_T_p*p_T_p;
@@ -83,8 +86,13 @@ double new_threshold( const Interaction & i ) {
   }
   else {
 
+    cout << "Weird case, alpha = " << alpha << endl;
+
     if ( p_T_p < 0 ) {
-      if ( alpha < 2*m_p*E_T ) return m_p ;
+      if ( alpha < 2*m_p*E_T ) {
+	cout << "No intersection" << endl;
+	return m_p ;
+      }
       else {
 	auto a2 = alpha*alpha;
 	auto Et2 = E_T*E_T;
@@ -92,7 +100,7 @@ double new_threshold( const Interaction & i ) {
 	auto mp2 = m_p*m_p;
 
 	auto delta = a2*Et2 - 4*(Et2-ptp2)*(ptp2*mp2+a2/4);
-	auto threshold = 0.5*(alpha*E_T + sqrt( delta ))/(Et2 - ptp2);
+	auto threshold = 0.5*(alpha*E_T - sqrt( delta ))/(Et2 - ptp2);
 	return threshold ;
       }
     }
@@ -104,7 +112,21 @@ double new_threshold( const Interaction & i ) {
 }
 
 
+void print_initial_conditions( TLorentzVector tgt ) {
 
+  cout << "Target: ( " << tgt.X() << ", " << tgt.Y() << ", " << tgt.Z() << ", " << tgt.E() << " ) with beta: " << tgt.Beta() << endl ;
+
+}
+
+
+void compare_threshold( double old_th, double new_th ) {
+
+  cout << "Old threshold: " << old_th << " GeV" << endl;
+  cout << "New threshold: " << new_th << " GeV" << endl;
+  cout << "Relative difference:  " << (new_th - old_th)/old_th*100 << " %" << endl;
+
+
+}
 
 
 
@@ -113,28 +135,96 @@ void threshold_test() {
 
   unique_ptr<Interaction> i_p {Interaction::QELCC( 1000180400, kPdgNeutron, kPdgNuMu, 2. )} ;
 
-  // auto th = i_p -> PhaseSpace().Threshold();
-
-  // cout << th << endl;
+  cout << "--------- Neutrino on Target on shell, forward nucleon ------------" << endl;
 
   TLorentzVector on_shell_target;
-  on_shell_target.SetXYZM( 0.1, -0.2, 0.25, PDGLibrary::Instance()->Find(kPdgNeutron)->Mass() - 0.2 );
-    
+  on_shell_target.SetXYZM( 0.1, -0.2, 0.25, PDGLibrary::Instance()->Find(kPdgNeutron)->Mass() );
   i_p -> InitStatePtr() -> TgtPtr() -> SetHitNucP4( on_shell_target ) ;
 
-  i_p -> InitState().Tgt().HitNucP4().Print();
+  print_initial_conditions( on_shell_target );
+  
+  auto old_th = find_threshold(*i_p);
+  auto new_th = new_threshold(*i_p); 
 
-  cout << *i_p << endl;
+  compare_threshold( old_th, new_th );
+  
 
-  auto th = i_p -> PhaseSpace().Threshold();
+  cout << "--------- Neutrino on Target on shell, backward nucleon ------------" << endl;
 
-  cout << th << endl;
+  on_shell_target.SetXYZM( 0.1, -0.2, -0.25, PDGLibrary::Instance()->Find(kPdgNeutron)->Mass() );
+  i_p -> InitStatePtr() -> TgtPtr() -> SetHitNucP4( on_shell_target ) ;
 
-  auto lab_th = find_threshold(*i_p);
+  print_initial_conditions( on_shell_target );
+  
+  old_th = find_threshold(*i_p);
+  new_th = new_threshold(*i_p); 
 
-  cout << lab_th << endl ;
+  compare_threshold( old_th, new_th );
 
-  auto new_th = new_threshold(*i_p);
-  cout << new_th << endl ;
+ 
+  cout << "--------- Neutrino on Target off shell, small beta, forward nucleon ------------" << endl;
+  TLorentzVector off_shell_target;
+  off_shell_target.SetXYZM( 0.1, -0.2, 0.25, PDGLibrary::Instance()->Find(kPdgNeutron)->Mass() - 0.2 );
+  i_p -> InitStatePtr() -> TgtPtr() -> SetHitNucP4( off_shell_target ) ;
+
+  print_initial_conditions( off_shell_target );
+  
+  old_th = find_threshold(*i_p);
+  new_th = new_threshold(*i_p); 
+
+  compare_threshold( old_th, new_th );
+
+  cout << "--------- Neutrino on Target off shell, small beta, backward nucleon ------------" << endl;
+  off_shell_target.SetXYZM( 0.1, -0.2, -0.25, PDGLibrary::Instance()->Find(kPdgNeutron)->Mass() - 0.2 );
+  i_p -> InitStatePtr() -> TgtPtr() -> SetHitNucP4( off_shell_target ) ;
+
+  print_initial_conditions( off_shell_target );
+  
+  old_th = find_threshold(*i_p);
+  new_th = new_threshold(*i_p); 
+
+  compare_threshold( old_th, new_th );
+  
+  cout << endl << "--------- Neutrino on Target off shell, beta > 1, forward nucleon --------" << endl;
+  off_shell_target.SetXYZT( 0.2, 0.8, 0.1, PDGLibrary::Instance()->Find(kPdgNeutron)->Mass() - 0.25 );
+  i_p -> InitStatePtr() -> TgtPtr() -> SetHitNucP4( off_shell_target ) ;
+
+  print_initial_conditions( off_shell_target );
+
+  new_th = new_threshold(*i_p); 
+
+  cout << "New threshold: " << new_th << " GeV" << endl;
+
+  cout << endl << "--------- Neutrino on Target off shell, beta > 1, backward nucleon --------" << endl;
+  off_shell_target.SetXYZT( 0.2, 0.8, -0.1, PDGLibrary::Instance()->Find(kPdgNeutron)->Mass() - 0.25 );
+  i_p -> InitStatePtr() -> TgtPtr() -> SetHitNucP4( off_shell_target ) ;
+
+  print_initial_conditions( off_shell_target );
+
+  new_th = new_threshold(*i_p); 
+
+  cout << "New threshold: " << new_th << " GeV" << endl;
+
+  cout << endl << "--------- Neutrino on Target off shell, beta > 1, forward nucleon --------" << endl;
+  off_shell_target.SetXYZT( 0.2, 0.1, 0.8, PDGLibrary::Instance()->Find(kPdgNeutron)->Mass() - 0.25 );
+  i_p -> InitStatePtr() -> TgtPtr() -> SetHitNucP4( off_shell_target ) ;
+
+  print_initial_conditions( off_shell_target );
+
+  new_th = new_threshold(*i_p); 
+
+  cout << "New threshold: " << new_th << " GeV" << endl;
+
+  cout << endl << "--------- Neutrino on Target off shell, beta > 1, backward nucleon --------" << endl;
+  off_shell_target.SetXYZT( 0.2, 0.1, -0.8, PDGLibrary::Instance()->Find(kPdgNeutron)->Mass() - 0.25 );
+  i_p -> InitStatePtr() -> TgtPtr() -> SetHitNucP4( off_shell_target ) ;
+
+  print_initial_conditions( off_shell_target );
+
+  new_th = new_threshold(*i_p); 
+
+  cout << "New threshold: " << new_th << " GeV" << endl;
+
+
 
 }
