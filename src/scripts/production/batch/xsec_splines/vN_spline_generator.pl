07aeb138b02d49765bd06ff7a5ccb0d2e01373f2
Marco Roda <marco.roda.88@gmail.com>
2023-01-25 10:31:43 +0100

    Updates vN script

--- src/scripts/production/batch/xsec_splines/vN_spline_generator.pl
+++ src/scripts/production/batch/xsec_splines/vN_spline_generator.pl
@@ -288,7 +288,9 @@ foreach $nu ( @nu_list ) {
  	       print SLURM "#SBATCH -p $queue \n"; 
  	       print SLURM "#SBATCH -o $filename_template.slurmout.log \n";
  	       print SLURM "#SBATCH -e $filename_template.slurmerr.log \n";
- 	       print SLURM "#SBATCH -t 8:0:0 \n";
+ 	       print SLURM "#SBATCH -t 4:0 \n";
+         print SLURM "#SBATCH -n 1 \n";
+         print SLURM "#SBATCH --mem=2500 \n";
  	       print SLURM "#SBATCH -L sps \n" if ($batch_system eq 'LyonSlurm');
 	       print SLURM "#SBATCH --priority -1 \n" if ( $priority ) ; 
 	       print SLURM "source $shell_script \n";
