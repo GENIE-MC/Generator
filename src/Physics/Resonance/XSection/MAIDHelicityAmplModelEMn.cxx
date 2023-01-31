174dc7f4f3a96c37d8626ccdde63cbfd38e8bc3b
jtenavidal <jtenavidal@tauex.tau.ac.il>
2023-01-31 16:30:59 +0100

    adding implementation for some resonance states

--- src/Physics/Resonance/XSection/MAIDHelicityAmplModelEMn.cxx
+++ src/Physics/Resonance/XSection/MAIDHelicityAmplModelEMn.cxx
@@ -37,94 +37,143 @@ MAIDHelicityAmplModelEMn::~MAIDHelicityAmplModelEMn()
 //____________________________________________________________________________
 const MAIDHelicityAmpl & MAIDHelicityAmplModelEMn::Compute( const Interaction * interaction ) const {
 
-  Resonance_t res = interaction->ExclTag().Resonance();
+  const InitialState & init_state = interaction -> InitState();
+  const ProcessInfo &  proc_info  = interaction -> ProcInfo();
+  const Target & target = init_state.Tgt();
+  const Resonance_t res = interaction->ExclTag().Resonance();
+  
+  //Get kinematical parameters
+  const Kinematics & kinematics = interaction -> Kine();
+  double W  = kinematics.W();
+  double q2 = kinematics.q2();
+
+  //Compute auxiliary & kinematical factors
+  double Mnuc   = target.HitNucMass();
+  double W2     = TMath::Power(W,    2);
+  double Mnuc2  = TMath::Power(Mnuc, 2);
+
+  double MR  = utils::res::Mass(res);
+  double kR = (MR*MR - Mnuc2)/(2.*MR);  //LAR choice
+                                        //k(W=MR,Q2=0) Q2=0 real photon
+  double kgcm0 = (W2 - Mnuc2)/(2.*MR); // kW at equation 5
+  double egcm = (W2+q2-Mnuc2)/(2.*MR); //photon energy at the W center of mass frame
+  double qcm = TMath::Sqrt(egcm*egcm-q2); //photon momentum k in Equation 3
+
+  double tau = -q2/(4.*Mnuc2);  
 
   switch(res) {
 
    case (kP33_1232) :
    {
-
+     double Fq = 1./TMath::Power(1-q2/0.71,2)*qcm/kgcm0;
+     double AM = 300.*(1. - 0.01*q2)*TMath::Exp(0.23*q2)*Fq;
+     double AE = -6.37 * (1. + 0.021*q2)*TMath::Exp(0.16*q2)*Fq;
+     double AS = -12.40 * (1. - 0.12*q2) / (1. + 4.9*tau)*qcm/kR*TMath::Exp(0.23*q2)*Fq;
+     fAmpl.fA12 = -(3.*AE+AM)/2./1000.;
+     fAmpl.fA32 = TMath::Sqrt(3.)/2.*(AE-AM)/1000.;
+     fAmpl.fS12 = TMath::Sqrt(2.)*AS/1000.;
      break;
    }
    case (kS11_1535) :
    {
+     fAmpl.fA12 = -51.*(1.-4.75*q2)*exp(1.69*q2)/1000.;
+     fAmpl.fS12  =  28.5*(1.-0.36*q2)*exp(1.55*q2)/1000.;
      break;
    }
    case (kD13_1520) :
    {
+     fAmpl.fA12 = -77.*(1.+0.53*q2)*exp(1.55*q2)/1000.;
+     fAmpl.fA32 = -154.*(1.-0.58*q2)*exp(1.75*q2)/1000.;
+     fAmpl.fS12 =  13.6*(1.-15.7*q2)*exp(1.57*q2)/1000.;
      break;
    }
    case (kS11_1650) :
    {
+     fAmpl.fA12 = 9.*(1.-0.13*q2)*exp(1.55*q2)/1000.;
+     fAmpl.fA32 = 0. ;
+     fAmpl.fS12 = 10.1*(1.+0.50*q2)*exp(1.55*q2)/1000.;
      break;
    }
    case (kD13_1700) :
    {
-    
+     // Not implemented
      break;
    }
    case (kD15_1675) :
    {
-    
+     fAmpl.fA12 = -62.*(1.-0.01*q2)*exp(+2.00*q2)/1000.;
+     fAmpl.fA32 = -84.*(1.-0.01*q2)*exp(+2.00*q2)/1000.;
+     fAmpl.fS12 = 0.;
      break;
    }
    case (kS31_1620) :
    {
-    
+     fAmpl.fA12 =  66.*(1.-1.86*q2)*exp(+2.5*q2)/1000.;
+     fAmpl.fA32 = 0. ;
+     fAmpl.fS12 = 16.2*(1.-2.83*q2)*exp(+2.0*q2)/1000.;
      break;
    }
    case (kD33_1700) :
    {
-    
+     fAmpl.fA12 = 226.*(1.-1.91*q2)*exp(+1.77*q2)/1000.;
+     fAmpl.fA32 = 210.*(1.-1.97*q2)*exp(+2.2*q2)/1000.;
+     fAmpl.fS12 = 0.;
      break;
    }
    case (kP11_1440) :
    {
+     fAmpl.fA12 = 54.1*(1.-0.95*q2)*exp(+1.77*q2)/1000.;
+     fAmpl.fS12 = -41.5*(1.-2.98*q2)*exp(+1.55*q2)/1000.;
+     fAmpl.fA32 = 0. ;
      break;
    }
    case (kP33_1600) :
    {
-
+     // Not implemented 
      break;
    }
    case (kP13_1720) :
    {
-
+     fAmpl.fA12 = -3*(1.-12.7*q2)*exp(+1.55*q2)/1000.;
+     fAmpl.fA32 = -31*(1.-4.99*q2)*exp(+1.55*q2)/1000.;
+     fAmpl.fS12 = 0.;
      break;
    }
    case (kF15_1680) :
    {
-
+     fAmpl.fA12 = 28*(1.-0.*q2)*exp(+1.2*q2)/1000.;
+     fAmpl.fA32 = -38*(1.-4.09*q2)*exp(+1.75*q2)/1000.;
+     fAmpl.fS12 = 0.;
      break;
    }
    case (kP31_1910) :
    {
-
+     // Not implemented
      break;
    }
    case (kP33_1920) :
    {
-
+     // Not implemented
      break;
    }
    case (kF35_1905) :
    {
-
+     // Not implemented
      break;
    }
    case (kF37_1950) :
    {
-
+     // Not implemented
      break;
    }
    case (kP11_1710) :
    {
-
+     // Not implemented
      break;
    }
    case (kF17_1970) :
    {
-
+     // Not implemented
      break;
    }
    default:
