bcd30e6ad971083fa6d8ffddc428de56186ecf24
jtenavidal <jtenavidal@tauex.tau.ac.il>
2023-03-14 22:30:08 +0100

    adding Histograms for A12 S12 and A32 form factors

--- src/Apps/gMAIDValidation.cxx
+++ src/Apps/gMAIDValidation.cxx
@@ -3,7 +3,9 @@
 
 \program gMAIDValidation
 
-\brief   Generates MAID Form Factors for all fitted resonances
+\brief   Generates plots MAID Form Factors for all fitted resonances
+         Transverse and Longitudinal cross sections as a function of W are also 
+	 reported 
 
 \syntax  gMAIDValidation [-o output_name.root]
 
@@ -13,7 +15,7 @@
 
 \author  Julia Tena Vidal <jtenavidal \at tauex.tau.ac.il>
 Tel Aviv University
-
+1;95;0c
 \created 13 Mar 2023
 
 \cpright Copyright (c) 2023-2033, The GENIE Collaboration
@@ -37,22 +39,57 @@ Tel Aviv University
 #include <TLatex.h>
 #include <TPaletteAxis.h>
 
+#include "Framework/ParticleData/BaryonResonance.h"
+#include "Framework/ParticleData/BaryonResUtils.h"
+#include "Framework/ParticleData/PDGUtils.h"
+#include "Framework/ParticleData/PDGCodes.h"
+#include "Framework/Utils/StringUtils.h"
+#include "Framework/Utils/SystemUtils.h"
+#include "Framework/Utils/KineUtils.h"
 #include "Framework/Algorithm/AlgFactory.h"
 #include "Framework/Messenger/Messenger.h"
 #include "Physics/PartonDistributions/PDF.h"
 #include "Framework/Utils/CmdLnArgParser.h"
-#include "Framework/Utils/StringUtils.h"
 #include "Framework/Utils/Style.h"
+#include "Framework/Utils/RunOpt.h"
+#include <map>
+#include "Framework/Algorithm/AlgFactory.h"
+#include "Physics/Resonance/XSection/RESVectFormFactorsI.h"
+#include "Physics/Resonance/XSection/RESVectFormFactorsI.h"
 
 using namespace std;
 using namespace genie;
 using namespace genie::utils;
 
 // globals
-string        gOptOutFile = "pdf_comp"; // -o argument
+string  gOptOutFile = "maid_validation.root"; // -o argument
+TFile * goutput_file ; 
+
+map<Resonance_t,TGraph*> gA12_p ;
+map<Resonance_t,TGraph*> gA32_p ;
+map<Resonance_t,TGraph*> gS12_p ;
+map<Resonance_t,TGraph*> gA12_n ;
+map<Resonance_t,TGraph*> gA32_n ;
+map<Resonance_t,TGraph*> gS12_n ;
+ 
+TCanvas * gC = 0;
+
+RESVectFormFactorsI * vffmodel_p = 0 ; 
+RESVectFormFactorsI * vffmodel_n = 0 ; 
+
+// number of resonances and GENIE resonance IDs
+const int kNRes = 18 ;  
+Resonance_t kResId[kNRes] = {
+  kP33_1232, kS11_1535, kD13_1520, kS11_1650,
+  kD13_1700, kD15_1675, kS31_1620, kD33_1700,
+  kP11_1440, kP33_1600, kP13_1720, kF15_1680,
+  kP31_1910, kP33_1920, kF35_1905, kF37_1950,
+  kP11_1710, kF17_1970 
+};
 
 // function prototypes
 void GetCommandLineArgs (int argc, char ** argv);
+void InitializeMembers(void) ; 
 void MakePlots     (void);
 
 //___________________________________________________________________
@@ -61,6 +98,7 @@ int main(int argc, char ** argv)
   utils::style::SetDefaultStyle();
 
   GetCommandLineArgs (argc,argv);   // Get command line arguments
+  InitializeMembers() ; 
   MakePlots();   // Produce all output plots and fill output n-tuple
 
   return 0;
@@ -68,6 +106,78 @@ int main(int argc, char ** argv)
 //_________________________________________________________________________________
 void MakePlots (void)
 {
+  unsigned int n_bins = 100 ; 
+  unsigned int max_Q2 = 4 ; 
+  unsigned int min_Q2 = 0 ;
+  double Q2_width = ( max_Q2 - min_Q2 ) / n_bins ; 
+  vector<double> Q2_binning ; 
+  vector<double> A12_p , A12_n, A32_p, A32_n, S12_p, S12_n ; 
+  int lepton_pdg = kPdgElectron ; 
+
+  // Proton case : 
+  Interaction * inter_p = Interaction::RESEM(1000010010, kPdgProton, lepton_pdg);   
+  Interaction * inter_n = Interaction::RESEM(1000010010, kPdgNeutron, lepton_pdg);   
+  double W = 0 ; 
+  double Q2 = min_Q2 ; 
+
+  for ( int ires = 0 ; ires < kNRes ; ++ires ) { 
+    Resonance_t ResID = kResId[ires] ; 
+    inter_p->ExclTagPtr()->SetResonance(ResID) ; 
+    inter_n->ExclTagPtr()->SetResonance(ResID) ; 
+    
+    for( unsigned int i = 0 ; i < n_bins + 1 ; ++i ) { 
+      Q2 = min_Q2 + i * Q2_width ; 
+      inter_p->KinePtr()->SetQ2(Q2) ;
+      inter_n->KinePtr()->SetQ2(Q2) ;
+      RESVectFFAmplitude vffampl_p = vffmodel_p->Compute(*inter_p);
+      A12_p.push_back( vffampl_p.AmplA12() ) ;
+      A32_p.push_back( vffampl_p.AmplA32() ) ;
+      S12_p.push_back( vffampl_p.AmplS12() ) ;
+      std::cout << " RES ID " << ResID << " A12_p = " << vffampl_p.AmplA12() << std::endl;
+      RESVectFFAmplitude vffampl_n = vffmodel_n->Compute(*inter_n);
+      A12_n.push_back( vffampl_n.AmplA12() ) ;
+      A32_n.push_back( vffampl_n.AmplA32() ) ;
+      S12_n.push_back( vffampl_n.AmplS12() ) ;
+      Q2_binning.push_back( Q2 ) ; 
+    }
+    // Create TGraph 
+    gA12_p[ResID] = new TGraph( Q2_binning.size(), &Q2_binning[0], &A12_p[0] ) ;
+    gA32_p[ResID] = new TGraph( Q2_binning.size(), &Q2_binning[0], &A32_p[0] ) ;
+    gS12_p[ResID] = new TGraph( Q2_binning.size(), &Q2_binning[0], &S12_p[0] ) ;
+
+    gA12_n[ResID] = new TGraph( Q2_binning.size(), &Q2_binning[0], &A12_n[0] ) ;
+    gA32_n[ResID] = new TGraph( Q2_binning.size(), &Q2_binning[0], &A32_n[0] ) ;
+    gS12_n[ResID] = new TGraph( Q2_binning.size(), &Q2_binning[0], &S12_n[0] ) ;
+
+    gA12_p[ResID]->Write() ; 
+    gA32_p[ResID]->Write() ; 
+    gS12_p[ResID]->Write() ; 
+
+    gA12_n[ResID]->Write() ; 
+    gA32_n[ResID]->Write() ; 
+    gS12_n[ResID]->Write() ; 
+    
+    // Clean vectors
+    A12_p.clear() ; 
+    A32_p.clear() ; 
+    S12_p.clear() ; 
+    A12_n.clear() ; 
+    A32_n.clear() ; 
+    S12_n.clear() ; 
+    
+  }
+
+
+}
+//_________________________________________________________________________________
+void InitializeMembers(void) {
+
+  AlgFactory * algf = AlgFactory::Instance();
+
+  vffmodel_p = dynamic_cast<RESVectFormFactorsI*> ( algf->AdoptAlgorithm("genie::MAIDRESVectFormFactorsEMp","Default") );
+  vffmodel_n = dynamic_cast<RESVectFormFactorsI*> ( algf->AdoptAlgorithm("genie::MAIDRESVectFormFactorsEMn","Default") );
+
+  goutput_file = TFile::Open(gOptOutFile.c_str(), "RECREATE") ; 
 
 }
 //_________________________________________________________________________________
