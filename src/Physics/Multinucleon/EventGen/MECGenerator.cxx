21499922ca412bb426e69a732cbc8258d7cbec21
Stephen Dolan <55530774+sjdolan@users.noreply.github.com>
2023-05-20 08:56:54 -0500

    Added a few comments to MECGenerator.cxx
    Added comments to make it clear what we're testing for when we sanity check fQ3Max

--- src/Physics/Multinucleon/EventGen/MECGenerator.cxx
+++ src/Physics/Multinucleon/EventGen/MECGenerator.cxx
@@ -628,7 +628,10 @@ void MECGenerator::SelectNSVLeptonKinematics (GHepRecord * event) const
   // This way if someone reuses this code, they are not tripped up by it.
   TMax = Enu - LepMass;
 
-  // Warn if fQ3Max value is suspect: 
+  // Warn if fQ3Max value is suspect 
+  // (i.e. above the Valencia model's rage of validity)
+  // This is just to warn users who have swapped out a MEC model
+  // without remembering to change fQ3Max.
   if(fQ3Max>1.21){
     LOG("MEC", pWARN)
         << "fQ3 max is larger than expected for Valencia MEC: "
@@ -895,7 +898,10 @@ void MECGenerator::SelectSuSALeptonKinematics(GHepRecord* event) const
   // This way if someone reuses this code, they are not tripped up by it.
   TMax = Enu - LepMass;
 
-  // Warn if fQ3Max value is suspect: 
+  // Warn if fQ3Max value is suspect 
+  // (i.e. below the SuSA model's rage of validity)
+  // This is just to warn users who have swapped out a MEC model
+  // without remembering to change fQ3Max.
   if(fQ3Max<1.99){
     LOG("MEC", pWARN)
         << "fQ3 max is smaller than expected for SuSAv2 MEC: "
