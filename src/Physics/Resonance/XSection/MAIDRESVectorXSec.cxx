56654411f5cb3ffa2ffa91a286da6af98b0c6c07
jtenavidal <jtenavidal@tauex.tau.ac.il>
2023-08-28 14:13:21 +0200

    reorganizing xsec calculation

--- src/Physics/Resonance/XSection/MAIDRESVectorXSec.cxx
+++ src/Physics/Resonance/XSection/MAIDRESVectorXSec.cxx
@@ -81,22 +81,49 @@ double MAIDRESVectorXSec::XSec( const Interaction * interaction, KinePhaseSpace_
     }
   }
 
-  // Get the input baryon resonance
-  Resonance_t resonance = interaction->ExclTag().Resonance();
-  string      resname   = utils::res::AsString(resonance);
-  
-  // Get the electron, hit nucleon & current
-  int  nucpdgc   = target.HitNucPdg();
-  bool is_p      = pdg::IsProton  (nucpdgc);
   bool is_EM     = proc_info.IsEM();
-
   if( !is_EM ) return 0 ;
 
+  double Mnuc = target.HitNucMass() ;
+  double Mnuc2 = TMath::Power(Mnuc,2) ;
+  double W2 = TMath::Power(W,2);
+  double k = 0.5 * ( W2 - Mnuc2) / Mnuc ;
+  double v = k - 0.5 * q2/Mnuc ;
+  double v2 = TMath::Power( v, 2 ) ;
+  double Q2 = v2 - q2 ; 
+  double E = init_state.ProbeE(kRfHitNucRest) ;
+  double Eprime = E - v ; 
+  TLorentzVector ElectronMom = *init_state.GetProbeP4(kRfLab) ;
+  TLorentzVector OutElectronMom = kinematics.FSLeptonP4() ; 
+  double theta = OutElectronMom.Theta();
+  double q3Vect2 = pow( ( ElectronMom - OutElectronMom ).P(),2) ; 
+
+  double epsilon = 1 / ( 1 + 2 * ( q3Vect2 / Q2 ) * TMath::Power( tan( theta ) * 0.5, 2 ) ) ;      
+  double Gamma = ( kAem * 0.5 / pow(kPi,2) ) * ( Eprime / E ) * ( k / Q2 ) / ( 1 - epsilon ) ; 
+  double xsecT = XSecT(interaction);
+  double xsecL = XSecL(interaction);
+  
+  double xsec = Gamma * ( xsecT + epsilon * xsecL ) ; 
+  xsec = TMath::Max(0.,xsec) ; 
+
+  xsec *= fRESScaling ; 
+
+  return xsec ; 
+}
+//____________________________________________________________________________
+double MAIDRESVectorXSec::XSecT( const Interaction * interaction ) const
+{  
+
+  // Get the electron, hit nucleon & current
+  const InitialState & init_state = interaction -> InitState();
+  const Target & target = init_state.Tgt();
+  int  nucpdgc   = target.HitNucPdg();
+  bool is_p      = pdg::IsProton  (nucpdgc);
+
   RESVectFormFactorsI * vffmodel = 0;
-  if(is_EM) {
-    if (is_p) { vffmodel = fVFFEMp;}
-    else      { vffmodel = fVFFEMn;}
-  }
+  if (is_p) { vffmodel = fVFFEMp;}
+  else      { vffmodel = fVFFEMn;}
+  
   if( ! vffmodel ) return 0 ; 
 
   RESVectFFAmplitude vffampl = vffmodel->Compute(*interaction);
@@ -107,117 +134,96 @@ double MAIDRESVectorXSec::XSec( const Interaction * interaction, KinePhaseSpace_
 #endif
   
   TLorentzVector ElectronMom = *init_state.GetProbeP4(kRfLab) ;  
-  TLorentzVector OutElectronMom = kinematics.FSLeptonP4() ; 
+
+  double Ampl2A12 = vffampl.Ampl2A12() ; 
+  double Ampl2A32 = vffampl.Ampl2A32() ; 
+
+  double factor = GetXSecFactors(interaction);
+  double xsecT = factor * ( Ampl2A12 + Ampl2A32 ) ; 
+  return xsecT ; 
+}
+//____________________________________________________________________________
+double MAIDRESVectorXSec::XSecL( const Interaction * interaction ) const
+{
+
+  // Get the electron, hit nucleon & current
+  const InitialState & init_state = interaction -> InitState();
+  const Target & target = init_state.Tgt();
+  int  nucpdgc   = target.HitNucPdg();
+  bool is_p      = pdg::IsProton  (nucpdgc);
+
+  RESVectFormFactorsI * vffmodel = 0;
+  if (is_p) { vffmodel = fVFFEMp;}
+  else      { vffmodel = fVFFEMn;}
+  
+  if( ! vffmodel ) return 0 ; 
+
+  RESVectFFAmplitude vffampl = vffmodel->Compute(*interaction);
+
+#ifdef __GENIE_LOW_LEVEL_MESG_ENABLED__
+  LOG("RSHAmpl", pDEBUG)
+     << "Helicity Amplitudes for RES = " << resname << " : " << vffampl;
+#endif
+
+  double Ampl2S12 = vffampl.Ampl2S12() ; 
 
   double Mnuc = target.HitNucMass() ;
   double Mnuc2 = TMath::Power(Mnuc,2) ; 
+
+  Resonance_t resonance = interaction->ExclTag().Resonance();
+  double MR = utils::res::Mass(resonance) ;
+  double MR2 = TMath::Power(MR,2);
+
+  const Kinematics & kinematics = interaction -> Kine(); 
+  double W  = kinematics.W();
+  double W2 = TMath::Power(W,2);
+  double k = 0.5 * ( W2 - Mnuc2) / Mnuc ; 
+  double q2 = kinematics.q2();
+  double v = k - 0.5 * q2/Mnuc ;
+  double v2 = TMath::Power( v, 2 ) ;
+  double Q2 = v2 - q2 ; 
+
+  TLorentzVector ElectronMom = *init_state.GetProbeP4(kRfLab) ;
+  TLorentzVector OutElectronMom = kinematics.FSLeptonP4() ; 
+  double q3Vect2 = pow( ( ElectronMom - OutElectronMom ).P(),2) ;  
+
+  double factor = GetXSecFactors(interaction);
+  double xsecL = 2 * factor * Q2 * MR2 * Ampl2S12 / ( Mnuc2 * q3Vect2 ) ; 
+  return xsecL;
+}
+//____________________________________________________________________________
+double MAIDRESVectorXSec::GetXSecFactors( const Interaction * interaction ) const
+{
+  const InitialState & init_state = interaction -> InitState();
+  const Target & target = init_state.Tgt();
+  const Kinematics & kinematics = interaction -> Kine(); 
+  double Mnuc = target.HitNucMass() ;
+  double Mnuc2 = TMath::Power(Mnuc,2) ; 
+  double W  = kinematics.W();
   double W2 = TMath::Power(W,2) ;
   double k = 0.5 * ( W2 - Mnuc2) / Mnuc ;
+
+  Resonance_t resonance = interaction->ExclTag().Resonance();
   double MR = utils::res::Mass(resonance) ;
   double MR2 = TMath::Power(MR,2);
   double kres = 0.5 * ( W2 - MR2 ) / MR ; 
+  double q2 = kinematics.q2();  
   double v = k - 0.5 * q2/Mnuc ;
   double v2 = TMath::Power( v, 2 ) ;
   double Q2 = v2 - q2 ; 
   double E = init_state.ProbeE(kRfHitNucRest) ;
   double Eprime = E - v ; 
+  TLorentzVector ElectronMom = *init_state.GetProbeP4(kRfLab) ;
+  TLorentzVector OutElectronMom = kinematics.FSLeptonP4() ; 
   double theta = OutElectronMom.Theta();
   double q3Vect2 = pow( ( ElectronMom - OutElectronMom ).P(),2) ; 
   double epsilon = 1 / ( 1 + 2 * ( q3Vect2 / Q2 ) * TMath::Power( tan( theta ) * 0.5, 2 ) ) ;     
  
   double Gamma = ( kAem * 0.5 / pow(kPi,2) ) * ( Eprime / E ) * ( k / Q2 ) / ( 1 - epsilon ) ; 
   double delta = MR * Gamma / ( ( pow( W2 - MR2, 2) + MR*pow(Gamma,2) ) * kPi ) ;  
-  double Ampl2A12 = vffampl.Ampl2A12() ; 
-  double Ampl2A32 = vffampl.Ampl2A32() ; 
-  double Ampl2S12 = vffampl.Ampl2S12() ; 
 
-  double xsecT = 2 * Mnuc * kPi * ( kres / k ) * ( Ampl2A12 + Ampl2A32 ) * delta ; 
-  double xsecL = 4 * kPi * Mnuc * kres * Q2 * MR2 * Ampl2S12 * delta / ( Mnuc2 * k * q3Vect2 ) ; 
-  
-  double xsec = Gamma * ( xsecT + epsilon * xsecL ) ; 
-  xsec = TMath::Max(0.,xsec) ; 
-
-  // The algorithm computes d2xsec/dEdQ2
-  // Check whether variable transformation is needed
-  if( kps!=kPSQ2fE ) { 
-    double J = utils::kinematics::Jacobian(interaction,kPSQ2fE,kps);
-#ifdef __GENIE_LOW_LEVEL_MESG_ENABLED_
-    LOG("MAIDRESVectorXSec", pDEBUG) << " Jacobian transformation to: " 
-					  << KinePhaseSpace::AsString(kps) << ", J = " << J ; 
-#endif 
-    xsec *= J ; 
-  }
-
-  // If requested, return the free nucleon xsec even for input nuclear tgt
-  if( interaction->TestBit(kIAssumeFreeNucleon) ) return xsec;
-
-  int Z = target.Z() ; 
-  int N = target.N() ; 
-  int A = target.A() ; 
-  // Take into account the number of nucleons/tgt
-  int NNucl = (pdg::IsProton(nucpdgc)) ? Z : N ;
-  xsec *= NNucl;
-
-  if (fUsePauliBlocking && A != 1)
-    {
-      // Calculation of Pauli blocking according references:
-      //
-      //     [1] S.L. Adler,  S. Nussinov,  and  E.A.  Paschos,  "Nuclear
-      //         charge exchange corrections to leptonic pion  production
-      //         in  the (3,3) resonance  region,"  Phys. Rev. D 9 (1974)
-      //         2125-2143 [Erratum Phys. Rev. D 10 (1974) 1669].
-      //     [2] J.Y. Yu, "Neutrino interactions and  nuclear  effects in
-      //         oscillation experiments and the  nonperturbative disper-
-      //         sive  sector in strong (quasi-)abelian  fields,"  Ph. D.
-      //         Thesis, Dortmund U., Dortmund, 2002 (unpublished).
-      //     [3] E.A. Paschos, J.Y. Yu,  and  M. Sakuda,  "Neutrino  pro-
-      //         duction  of  resonances,"  Phys. Rev. D 69 (2004) 014013
-      //         [arXiv: hep-ph/0308130].
-
-      double P_Fermi = 0.0;
-
-      // Maximum value of Fermi momentum of target nucleon (GeV)
-      if (A<6 || !fUseRFGParametrization)
-	{
-	  // Look up the Fermi momentum for this target
-	  FermiMomentumTablePool * kftp = FermiMomentumTablePool::Instance();
-	  const FermiMomentumTable * kft = kftp->GetTable(fKFTable);
-	  P_Fermi = kft->FindClosestKF(pdg::IonPdgCode(A, Z), nucpdgc);
-	}
-      else {
-        // Define the Fermi momentum for this target
-        P_Fermi = utils::nuclear::FermiMomentumForIsoscalarNucleonParametrization(target);
-        // Correct the Fermi momentum for the struck nucleon
-        if(is_p) { P_Fermi *= TMath::Power( 2.*Z/A, 1./3); }
-        else     { P_Fermi *= TMath::Power( 2.*N/A, 1./3); }
-      }
-
-      double FactorPauli_RES = 1.0;
-
-      double k0 = 0., q = 0., q0 = 0.;
-
-      if (P_Fermi > 0.)
-	{
-	  k0 = (W2-Mnuc2-Q2)/(2*W);
-	  k = TMath::Sqrt(k0*k0+Q2);                  // previous value of k is overridden
-	  q0 = (W2-Mnuc2+kPionMass2)/(2*W);
-	  q = TMath::Sqrt(q0*q0-kPionMass2);
-	}
-
-      if (2*P_Fermi < k-q)
-        FactorPauli_RES = 1.0;
-      if (2*P_Fermi >= k+q)
-        FactorPauli_RES = ((3*k*k+q*q)/(2*P_Fermi)-(5*TMath::Power(k,4)+TMath::Power(q,4)+10*k*k*q*q)/(40*TMath::Power(P_Fermi,3)))/(2*k);
-      if (2*P_Fermi >= k-q && 2*P_Fermi <= k+q)
-        FactorPauli_RES = ((q+k)*(q+k)-4*P_Fermi*P_Fermi/5-TMath::Power(k-q, 3)/(2*P_Fermi)+TMath::Power(k-q, 5)/(40*TMath::Power(P_Fermi, 3)))/(4*q*k);
-
-      xsec *= FactorPauli_RES;
-    }
-
-  xsec *= fRESScaling ; 
-
-  return xsec ; 
-
+  double factor = 2 * kPi * Mnuc * ( kres / k ) * delta ; 
+  return factor;
 }
 //____________________________________________________________________________
 double MAIDRESVectorXSec::Integral(const Interaction * interaction) const
