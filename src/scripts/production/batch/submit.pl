edd2b27202a2322c3432d649f54139d997a7c401
Costas Andreopoulos <constantinos.andreopoulos@cern.ch>
2025-07-03 20:44:49 +0100

    modernize old production scripts and adapt for the Liverpool Slurm system

--- src/scripts/production/batch/submit.pl
+++ src/scripts/production/batch/submit.pl
@@ -1,26 +1,28 @@
-#-------------------------------------------------------------------------------------------
-# A script to submit a single job so as to avoid the manual creation of batch script files.
+#--------------------------------------------------------------------------------------------------
+# A helper script to submit a single job with all commands specified directly on the command line,
+# avoiding the need to manually create a separate batch script for one-off or ad hoc tasks.
 #
 # Syntax:
 #    % perl submit.pl <options>
 #
 # Options:
-#    --cmd              : Command(s) to execute
-#    --version          : GENIE version number
-#   [--job-name]        : default: tmp-(random_number)
-#   [--arch]            : <SL4.x86_32, SL5.x86_64, SL6.x86_64, ...>, default: SL6.x86_64
-#   [--production]      : default: routine_validation
-#   [--cycle]           : default: 01
-#   [--batch-system]    : <PBS, LSF, none>, default: PBS
-#   [--queue]           : default: prod
-#   [--softw-topdir]    : top level dir for softw installations, default: /opt/ppd/t2k/softw/GENIE/
-#   [--jobs-topdir]     : top level dir for job files, default: /opt/ppd/t2k/softw/scratch/GENIE/
+#    --cmd               : Command(s) to execute
+#    --gen-version       : GENIE generator version number
+#   [--job-name]         : default: tmp-(random_number)
+#   [--arch]             : <el7.x86_64, ...>, default: el7.x86_64
+#   [--production]       : default: routine_validation
+#   [--cycle]            : default: 01
+#   [--batch-system]     : <Slurm, PBS, LSF>, default: Slurm
+#   [--queue]            : default: compute
+#   [--time-limit]       : default: 10:00:00
+#   [--softw-topdir]     : top level dir for softw installations, default: /opt/ppd/t2k/softw/GENIE/
+#   [--job-topdir]       : top level dir for job files, default: /opt/ppd/t2k/softw/scratch/GENIE/
 #
 # Example:
 #   % perl submit.pl 
 #     --cmd 'gevgen -p 14 -t 1000260560 -e 1 --cross-sections /some/path/xsec.xml \
 #     --seed 1989298 --r 100; mv gntp.100.ghep.root /some/other/path/' \
-#     --version v2.8.0 \
+#     --gen-version v3.6.0 \
 #     --job-name numuFe100
 #
 # Author:
@@ -30,7 +32,7 @@
 # Copyright:
 #   Copyright (c) 2003-2025, The GENIE Collaboration
 #   For the full text of the license visit http://copyright.genie-mc.org
-#-------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 
 #!/usr/bin/perl
 
@@ -39,52 +41,72 @@ use File::Path;
 $iarg=0;
 foreach (@ARGV) {
   if($_ eq '--cmd')           { $cmd             = $ARGV[$iarg+1]; }
-  if($_ eq '--version')       { $genie_version   = $ARGV[$iarg+1]; }
-  if($_ eq '--job-name')      { $name            = $ARGV[$iarg+1]; }
+  if($_ eq '--gen-version')   { $gen_version     = $ARGV[$iarg+1]; }
+  if($_ eq '--job-name')      { $job_name        = $ARGV[$iarg+1]; }
   if($_ eq '--arch')          { $arch            = $ARGV[$iarg+1]; }
   if($_ eq '--production')    { $production      = $ARGV[$iarg+1]; }
   if($_ eq '--cycle')         { $cycle           = $ARGV[$iarg+1]; }
   if($_ eq '--batch-system')  { $batch_system    = $ARGV[$iarg+1]; }
   if($_ eq '--queue')         { $queue           = $ARGV[$iarg+1]; }
+  if($_ eq '--time-limit')    { $time_limit      = $ARGV[$iarg+1]; }
   if($_ eq '--softw-topdir')  { $softw_topdir    = $ARGV[$iarg+1]; }
-  if($_ eq '--jobs-topdir')   { $jobs_topdir     = $ARGV[$iarg+1]; }
+  if($_ eq '--job-topdir')    { $job_topdir      = $ARGV[$iarg+1]; }
   $iarg++;
 }
 die("** Aborting [Undefined GENIE command. Use the --cmd option]")
 unless defined $cmd;
-die("** Aborting [Undefined GENIE version. Use the --version option]")
-unless defined $genie_version;
+die("** Aborting [Undefined GENIE version. Use the --gen-version option]")
+unless defined $gen_version;
 
 $random_num = int (rand(999999999));
 
-$name           = "tmp-$random_num"             unless defined $name;
-$arch           = "SL6.x86_64"                  unless defined $arch;
-$production     = "routine_validation"          unless defined $production;
-$cycle          = "01"                          unless defined $cycle;
-$batch_system   = "PBS"                         unless defined $batch_system;
-$queue          = "prod"                        unless defined $queue;
-$softw_topdir   = "/opt/ppd/t2k/softw/GENIE"    unless defined $softw_topdir;
-$jobs_topdir    = "/opt/ppd/t2k/scratch/GENIE/" unless defined $jobs_topdir;
-$genie_setup    = "$softw_topdir/generator/builds/$arch/$genie_version-setup";
-$job_dir        = "$jobs_topdir/$genie_version-$production\_$cycle-$name";
+$arch              = "el7.x86_64"                            unless defined $arch;
+$production        = "routine_validation"                    unless defined $production;
+$cycle             = "01"                                    unless defined $cycle;
+$batch_system      = "Slurm"                                 unless defined $batch_system;
+$queue             = "compute"                               unless defined $queue;
+$time_limit        = "10:00:00"                              unless defined $time_limit;
+$softw_topdir      = "/user/costasa/projects/GENIE/softw/"   unless defined $softw_topdir;
+$job_topdir        = "/scratch/costasa/GENIE/"               unless defined $job_topdir;
+$job_name          = "tmp-$random_num"                       unless defined $job_name;
+$job_dir           = "$job_topdir/$gen_version-$production\_$cycle-$job_name";
+$gen_setup_script  = "$softw_topdir/generator/builds/$arch/$gen_version-setup.sh";
+$filename_basepath = "$job_dir/$jobname";
 
 # make the job directory
-#
 mkpath ($job_dir, {verbose => 1, mode=>0777});
 
-
 # submit job
-#
+# -----------------------
+
+# Slurm case
+if($batch_system eq 'Slurm') {
+	$batch_script = "$filename_basepath.sh";
+        open(SLURM, ">$batch_script") or die("Can not create the SLURM batch script");
+        print SLURM "#!/bin/bash \n";
+        print SLURM "#SBATCH-p $queue \n";
+        print SLURM "#SBATCH-J $job_name \n";
+        print SLURM "#SBATCH-N 1 \n";
+        print SLURM "#SBATCH-c 1 \n";
+        print SLURM "#SBATCH-o $filename_basepath.slurmout.log \n";
+        print SLURM "#SBATCH-e $filename_basepath.slurmerr.log \n";
+        print SLURM "#SBATCH-t $time_limit \n";
+        print SLURM "source $gen_setup_script \n";
+        print SLURM "cd $job_dir \n";
+        print SLURM "$gmkspl_cmd \n";
+        close(SLURM);
+        `sbatch $batch_script`;
+} #slurm
 
 # PBS case
 if($batch_system eq 'PBS') {
-        $batch_script = "$job_dir/$name.pbs";
+        $batch_script = "$filename_basepath.pbs";
         open(PBS, ">$batch_script") or die("Can not create the PBS batch script");
         print PBS "#!/bin/bash \n";
-        print PBS "#PBS -N $name \n";
-        print PBS "#PBS -o $job_dir/$name.pbsout.log \n";
-        print PBS "#PBS -e $job_dir/$name.pbserr.log \n";
-        print PBS "source $genie_setup \n";
+        print PBS "#PBS -N $job_name \n";
+        print PBS "#PBS -o $filename_basepath.pbsout.log \n";
+        print PBS "#PBS -e $filename_basepath.pbserr.log \n";
+        print PBS "source $gen_setup_script \n";
         print PBS "cd $job_dir \n";
         print PBS "$cmd \n";
         close(PBS);
@@ -93,14 +115,14 @@ if($batch_system eq 'PBS') {
 
 # LSF case
 if($batch_system eq 'LSF') {
-        $batch_script = "$job_dir/$name.sh";
+        $batch_script = "$filename_basepath.sh";
         open(LSF, ">$batch_script") or die("Can not create the LSF batch script");
         print LSF "#!/bin/bash \n";
-        print PBS "#BSUB-j $name \n";
+        print PBS "#BSUB-j $job_name \n";
         print LSF "#BSUB-q $queue \n";
-        print LSF "#BSUB-o $job_dir/$name.lsfout.log \n";
-        print LSF "#BSUB-e $job_dir/$name.lsferr.log \n";
-        print LSF "source $genie_setup \n";
+        print LSF "#BSUB-o $filename_basepath.lsfout.log \n";
+        print LSF "#BSUB-e $filename_basepath.lsferr.log \n";
+        print LSF "source $gen_setup_script \n";
         print LSF "cd $job_dir \n";
         print LSF "$cmd \n";
         `bsub < $batch_script`;
---
+++ src/scripts/production/batch/submit_xsec_calc_jobs_eA.pl
@@ -0,0 +1,214 @@
+#!/usr/bin/perl
+
+#----------------------------------------------------------------------------------------------------------------
+# Submit jobs for calculating the specified GENIE nuclear cross-section splines.
+# The GENIE gspladd utility can be used to merge the job outputs.
+#
+# Syntax:
+#   shell% perl submit_xsec_calc_jobs_eA.pl <options>
+#
+# Options:
+#    --gen-version       : GENIE generator version number
+#    --tune              : GENIE physics tune
+#    --spline-list       : Text file listing the nuclear cross-section splines to be generated.
+#   [--input-splines]    : Input free-nucleon splines that can speed-up the calculation of nuclear splines
+#   [--arch]             : <el7.x86_64, ...>, default: el7.x86_64
+#   [--production]       : default: routine_validation
+#   [--cycle]            : default: 01
+#   [--use-valgrind]     : default: off
+#   [--batch-system]     : <Slurm, PBS, LSF, none>, default: Slurm
+#   [--queue]            : default: compute
+#   [--time-limit]       : default: 10:00:00
+#   [--softw-topdir]     : top level dir for softw installations, default: /user/costasa/projects/GENIE/softw/
+#   [--jobs-topdir]      : top level dir for job files, default: /scratch/costasa/GENIE/
+#
+# Examples:
+#   % perl submit_xsec_calc_jobs_eA.pl --gen-version v3.06.00 \
+#                  --tune G18_10a_02_11b --spline-list ./xsec_splines/hermes.list 
+#
+# Author:
+#   Costas Andreopoulos <c.andreopoulos \st cern.ch>
+#   University of Liverpool
+#
+# Copyright:
+#   Copyright (c) 2003-2025, The GENIE Collaboration
+#   For the full text of the license visit http://copyright.genie-mc.org
+#----------------------------------------------------------------------------------------------------------------
+
+use File::Path;
+use File::Basename;
+
+# inputs
+#  
+$iarg=0;
+foreach (@ARGV) {
+  if($_ eq '--gen-version')    { $gen_version   = $ARGV[$iarg+1]; }
+  if($_ eq '--tune')           { $tune          = $ARGV[$iarg+1]; }
+  if($_ eq '--spline-list')    { $spline_list   = $ARGV[$iarg+1]; }
+  if($_ eq '--input-splines')  { $input_splines = $ARGV[$iarg+1]; }
+  if($_ eq '--arch')           { $arch          = $ARGV[$iarg+1]; }
+  if($_ eq '--production')     { $production    = $ARGV[$iarg+1]; }
+  if($_ eq '--cycle')          { $cycle         = $ARGV[$iarg+1]; }
+  if($_ eq '--use-valgrind')   { $use_valgrind  = $ARGV[$iarg+1]; }
+  if($_ eq '--batch-system')   { $batch_system  = $ARGV[$iarg+1]; }
+  if($_ eq '--queue')          { $queue         = $ARGV[$iarg+1]; }
+  if($_ eq '--time-limit')     { $time_limit    = $ARGV[$iarg+1]; }
+  if($_ eq '--softw-topdir')   { $softw_topdir  = $ARGV[$iarg+1]; }
+  if($_ eq '--jobs-topdir')    { $jobs_topdir   = $ARGV[$iarg+1]; }
+  $iarg++;
+}
+die("** Aborting [Undefined spline list. Use the --spline-list option. See examples in ?]")
+unless defined $spline_list;
+die("** Aborting [Undefined GENIE Generator version. Use the --gen-version option]")
+unless defined $gen_version;
+die("** Aborting [Undefined GENIE physics tune. Use the --tune option]")
+unless defined $tune;
+
+$use_valgrind      = 0                                       unless defined $use_valgrind;
+$arch              = "el7.x86_64"                            unless defined $arch;
+$production        = "routine_validation"                    unless defined $production;
+$cycle             = "01"                                    unless defined $cycle;
+$batch_system      = "Slurm"                                 unless defined $batch_system;
+$queue             = "compute"                               unless defined $queue;
+$time_limit        = "10:00:00"                              unless defined $time_limit;
+$softw_topdir      = "/user/costasa/projects/GENIE/softw/"   unless defined $softw_topdir;
+$jobs_topdir       = "/scratch/costasa/GENIE/"               unless defined $jobs_topdir;
+$input_splines     = ""                                      unless defined $input_splines;
+$gen_setup_script  = "$softw_topdir/generator/builds/$arch/$gen_version-setup.sh";
+$splines_name      = basename($spline_list,".list");
+$jobs_dir          = "$jobs_topdir/$gen_version-$tune-$production\_$cycle-xsec\_eA\_$splines_name";
+
+$nknots  =  0;
+$emax    =  0;
+@probes  = ();
+@targets = ();
+
+open (REQUIRED_SPLINES, $spline_list);
+$i=0;
+while(<REQUIRED_SPLINES>)
+{
+   chomp;
+   # skip comment lines starting with # and empty lines that don't contain any number
+   if( substr($_,0,1) ne '#' && m/[0-9]/ ) 
+   { 
+      s/ //g;  # rm empty spaces from $_
+      #print "line = $_\n" ;
+      $value = "";
+      $idx = index($_,"#");
+      if($idx == -1) { $value = $_; }
+      else           { $value = substr($_,0,$idx); }
+
+      if    ($i==0) { $emax   = $value; }
+      elsif ($i==1) { $nknots = $value; }
+      else {
+          if($value ==  11 || $value == -11) 
+          {
+             push(@probes, $value);
+          }
+          else 
+          {
+             push(@targets, $value);
+          }
+      }
+      $i++;
+   }
+}
+close(REQUIRED_SPLINES);
+
+# make the jobs directory
+#
+mkpath ($jobs_dir, {verbose => 1, mode=>0777});
+
+$probe_codes = "";
+$i=0;
+foreach (@probes) 
+{
+    s/ //g;  # rm empty spaces from $_
+    if($i == 0) { $probe_codes = $_; }
+    else        { $probe_codes = $probe_codes . ",$_"; }
+    $i++;
+}
+
+#
+# loop over nuclear targets & submit jobs
+#
+foreach(@targets) 
+{
+    s/ //g;  # rm empty spaces from $_
+    $tgt_code = $_;
+    $jobname  = "eAxscalc-$tgt_code";
+    $filename_basepath = "$jobs_dir/$jobname";
+
+    $grep_pipe   = "grep -B 100 -A 30 -i \"warn\\|error\\|fatal\"";
+    $gmkspl_opt  = "-p $probe_codes -t $_ -n $nknots -e $emax --tune $tune --event-generator-list EM";
+    $gmkspl_opt  = "$gmkspl_opt --output-cross-sections gxspl_$tgt_code.xml";
+    $gmkspl_opt  = "$gmkspl_opt --input-cross-sections $input_splines" if -e $input_splines;
+   #$gmkspl_cmd  = "gmkspl $gmkspl_opt | $grep_pipe &> $filename_basepath.mkspl.log";
+    $gmkspl_cmd  = "gmkspl $gmkspl_opt &> $filename_basepath.mkspl.log";
+
+    print "@@ exec: $gmkspl_cmd \n";
+
+    # Submit jobs
+    # -------------------------------------------------------------             
+
+    # Slurm case
+    if($batch_system eq 'Slurm') {
+        ##my $time_lim = `sinfo -h -p batch -o %l`;
+        ##my ($days, $hours, $remainder) = $time_lim =~ /([0]+)-([0-9]+):(.*)/;
+        ##my $newhours = $days * 24 + $hours;
+        ##my $new_time_lim = "$newhours:$remainder";
+        ##$time_limit = $new_time_lim lt $time_limit ? $new_time_lim : $time_limit;
+	$batch_script = "$filename_basepath.sh";
+	open(SLURM, ">$batch_script") or die("Can not create the SLURM batch script");
+	print SLURM "#!/bin/bash \n";
+        print SLURM "#SBATCH-p $queue \n";
+        print SLURM "#SBATCH-J $jobname \n";
+        print SLURM "#SBATCH-N 1 \n";   
+        print SLURM "#SBATCH-c 1 \n";   
+        print SLURM "#SBATCH-o $filename_basepath.slurmout.log \n";
+        print SLURM "#SBATCH-e $filename_basepath.slurmerr.log \n";
+        print SLURM "#SBATCH-t $time_limit \n";
+	print SLURM "source $gen_setup_script \n";
+	print SLURM "cd $jobs_dir \n";
+	print SLURM "$gmkspl_cmd \n";
+        close(SLURM);
+	`sbatch --job-name=$jobname $batch_script`;
+    } #slurm
+
+    # PBS case
+    if($batch_system eq 'PBS') {
+	$batch_script = "$filename_basepath.pbs";
+	open(PBS, ">$batch_script") or die("Can not create the PBS batch script");
+	print PBS "#!/bin/bash \n";
+        print PBS "#PBS -N $jobname \n";
+        print PBS "#PBS -o $filename_basepath.pbsout.log \n";
+        print PBS "#PBS -e $filename_basepath.pbserr.log \n";
+	print PBS "source $gen_setup_script \n";
+	print PBS "cd $jobs_dir \n";
+	print PBS "$gmkspl_cmd \n";
+        close(PBS);
+        $job_submission_command = "qsub";
+        `$job_submission_command -q $queue $batch_script`;
+    } #PBS
+
+    # LSF case
+    if($batch_system eq 'LSF') {
+	$batch_script = "$filename_basepath.sh";
+	open(LSF, ">$batch_script") or die("Can not create the LSF batch script");
+	print LSF "#!/bin/bash \n";
+        print LSF "#BSUB-j $jobname \n";
+        print LSF "#BSUB-o $filename_basepath.lsfout.log \n";
+        print LSF "#BSUB-e $filename_basepath.lsferr.log \n";
+	print LSF "source $gen_setup_script \n";
+	print LSF "cd $jobs_dir \n";
+	print LSF "$gmkspl_cmd \n";
+        close(LSF);
+	`bsub < $batch_script`;
+    } #LSF
+    
+    # run interactively
+    if($batch_system eq 'none') {
+        system("source $gen_setup_script; cd $jobs_dir; $gmkspl_cmd");
+    }
+}
+
