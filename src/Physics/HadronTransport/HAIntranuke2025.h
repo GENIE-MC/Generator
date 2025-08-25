b87e7a53714a66ce7fe05d4401b3223cbc599c1b
Mohamed Ismail <mismail@uboonebuild03.fnal.gov>
2025-08-25 14:46:06 -0500

    building hA2025  model

---
+++ src/Physics/HadronTransport/HAIntranuke2025.h
@@ -0,0 +1,87 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::Intranuke
+
+\brief    The INTRANUKE intranuclear hadron transport MC.
+          Is a concrete implementation of the EventRecordVisitorI interface.
+
+\ref      R.Merenyi et al., Phys.Rev.D45 (1992)
+          R.D.Ransome, Nucl.Phys.B 139 (2005)
+
+          Current INTRANUKE development is led by S.Dytman and H.Gallagher.
+          The original INTRANUKE cascade MC was developed (in fortran) for the
+          NeuGEN MC by R.Edgecock, G.F.Pearce, W.A.Mann, R.Merenyi and others.
+
+\author   Steve Dytman <dytman+@pitt.edu>, Pittsburgh University
+          Aaron Meyer <asm58@pitt.edu>, Pittsburgh University
+	  Alex Bell, Pittsburgh University
+          Hugh Gallagher <gallag@minos.phy.tufts.edu>, Tufts University
+          Costas Andreopoulos <c.andreopoulos \at cern.ch> STFC, Rutherford Lab
+
+\created  September 20, 2005
+
+\cpright  Copyright (c) 2003-2024, The GENIE Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+          
+*/
+//____________________________________________________________________________
+
+#ifndef _HA_INTRANUKE_2025_H_
+#define _HA_INTRANUKE_2025_H_
+
+#include <TGenPhaseSpace.h>
+
+#include "Physics/NuclearState/NuclearModelI.h"
+#include "Framework/Algorithm/AlgFactory.h"
+#include "Framework/EventGen/EventRecordVisitorI.h"
+#include "Physics/HadronTransport/INukeMode.h"
+#include "Physics/HadronTransport/INukeHadroFates2025.h"
+#include "Physics/HadronTransport/Intranuke2025.h"
+
+class TLorentzVector;
+class TVector3;
+
+namespace genie {
+
+class GHepParticle;
+class INukeHadroData2025;
+class PDGCodeList;
+
+class HAIntranuke2025 : public Intranuke2025 {
+
+friend class IntranukeTester;
+
+public :
+  HAIntranuke2025();
+  HAIntranuke2025(string config);
+ ~HAIntranuke2025();
+
+  void ProcessEventRecord(GHepRecord * event_rec) const;
+
+  virtual string GetINukeMode() const {return "hA2025";};
+  virtual string GetGenINukeMode() const {return "hA";};
+
+private:
+
+  void LoadConfig (void);
+
+  void  SimulateHadronicFinalState           (GHepRecord* ev, GHepParticle* p) const;
+  void  SimulateHadronicFinalStateKinematics (GHepRecord* ev, GHepParticle* p) const;
+
+  INukeFateHA_t HadronFateHA     (const GHepParticle* p) const;
+  //INukeFateHA_t HadronFateOset   (void) const;
+  void          Inelastic        (GHepRecord* ev, GHepParticle* p, INukeFateHA_t fate) const;
+  void          ElasHA           (GHepRecord* ev, GHepParticle* p, INukeFateHA_t fate) const;
+  void          InelasticHA      (GHepRecord* ev, GHepParticle* p, INukeFateHA_t fate) const;
+  double        PiBounce         (void) const;
+  double        PnBounce         (void) const;
+  int           HandleCompoundNucleus(GHepRecord* ev, GHepParticle* p, int mom) const;           
+
+  mutable int nuclA;     ///< value of A for the target nucleus in hA mode
+  mutable unsigned int fNumIterations;
+};
+
+}      // genie namespace
+
+#endif // _HA_INTRANUKE_2025_H_
---
+++ src/Physics/HadronTransport/INukeHadroFates2025.h
@@ -0,0 +1,123 @@
+//____________________________________________________________________________
+/*!
+
+\class    genie::INukeHadroFates
+
+\brief    An enumeration of possible hadron "fates" taken into account by the
+          INTRANUKE hadron transport MC.
+
+\author   Steve Dytman <dytman+@pitt.edu>, Pittsburgh Univ.
+          Hugh Gallagher <gallag@minos.phy.tufts.edu>, Tufts Univ.
+          Costas Andreopoulos <c.andreopoulos \at cern.ch>, Rutherford Lab.
+
+\created  November 1, 2005
+
+\cpright  Copyright (c) 2003-2024, The GENIE Collaboration
+          For the full text of the license visit http://copyright.genie-mc.org
+           
+*/
+//____________________________________________________________________________
+
+#ifndef _INTRANUKE_FATES_H_
+#define _INTRANUKE_FATES_H_
+
+#include <string>
+
+using std::string;
+
+namespace genie {
+
+// Fates in INTRANUKE's HN mode
+//
+typedef enum EINukeFateHN_t {
+
+   kIHNFtUndefined = 0, 
+   kIHNFtNoInteraction,
+   kIHNFtCEx,       // cex
+   kIHNFtElas,      // elas
+   kIHNFtInelas,    // inelas
+   kIHNFtAbs,       // abs 
+   kIHNFtCmp         //cmp
+
+} INukeFateHN_t;   
+
+// Fates in INTRANUKE's HA mode
+//
+typedef enum EINukeFateHA_t {
+
+   kIHAFtUndefined = 0,
+   kIHAFtNoInteraction,  // no interaction 
+   kIHAFtCEx,            // cex
+   //   kIHAFtElas,           // elas
+   kIHAFtInelas,         // inelas
+   kIHAFtAbs,            // abs
+   kIHAFtKo, 	         // knock out
+   kIHAFtCmp,            // compound nucleus
+   kIHAFtPiProd,         // pi production
+   kIHAFtInclPip,        // pi production : inclusive pi+
+   kIHAFtInclPim,        // pi production : inclusive pi-
+   kIHAFtInclPi0,        // pi production : inclusive pi0 
+   kIHAFtDCEx            // dcex
+
+} INukeFateHA_t;   
+
+class INukeHadroFates {
+
+public:
+  //__________________________________________________________________________
+  static string AsString(INukeFateHN_t fate) {
+     switch (fate) {
+      case kIHNFtUndefined : return "** Undefined HN-mode fate **"; break;
+      case kIHNFtCEx       : return "HN-mode / cex";    break;
+      case kIHNFtElas      : return "HN-mode / elas";   break;
+      case kIHNFtInelas    : return "HN-mode / inelas"; break;
+      case kIHNFtAbs       : return "HN-mode / abs";    break;
+      case kIHNFtCmp	   : return "HN-mode / compound"; break;
+      case kIHNFtNoInteraction : return "HN-mode / no interaction"; break;
+      default              : break; 
+     }
+     return "** Undefined HN-mode fate **"; 
+  }
+  //__________________________________________________________________________
+  static string AsString(INukeFateHA_t fate) {
+     switch (fate) {
+      case kIHAFtUndefined : return "** Undefined HA-mode fate **"; break;
+      case kIHAFtNoInteraction : return "HA-mode / no interaction"; break;
+      case kIHAFtCEx       : return "HA-mode / cex";            break;
+	//      case kIHAFtElas      : return "HA-mode / elas";           break;
+      case kIHAFtInelas    : return "HA-mode / inelas";         break;
+      case kIHAFtAbs       : return "HA-mode / abs";            break;
+      case kIHAFtKo        : return "HA-mode / knock-out";      break;
+      case kIHAFtCmp       : return "HA-mode / compound";       break;
+      case kIHAFtPiProd    : return "HA-mode / pi-production" ; break;
+      case kIHAFtInclPip   : return "HA-mode / pi-prod incl pi+";   break;
+      case kIHAFtInclPim   : return "HA-mode / pi-prod incl pi-";   break;
+      case kIHAFtInclPi0   : return "HA-mode / pi-prod incl pi0";   break;
+      case kIHAFtDCEx      : return "HA-mode / dcex";           break;
+      default              : break;
+     }
+     return "** Undefined HA-mode fate **"; 
+  }
+  //__________________________________________________________________________
+  static string AsSimpleString(INukeFateHA_t fate) {
+     switch (fate) {
+      case kIHAFtUndefined : return "undefined"; break;
+      case kIHAFtNoInteraction : return "no interaction"; break;
+      case kIHAFtCEx       : return "cex";            break;
+	//      case kIHAFtElas      : return "elas";           break;
+      case kIHAFtInelas    : return "inelas";         break;
+      case kIHAFtAbs       : return "abs";            break;
+      case kIHAFtKo        : return "knock out";      break; 
+      case kIHAFtCmp       : return "compound";       break;
+      case kIHAFtPiProd    : return "pi prod";          break;
+      case kIHAFtDCEx      : return "dcex";             break;
+      default              : break;
+     }
+     return "undefined"; 
+  }
+  //__________________________________________________________________________
+
+};
+
+}      // genie
+#endif // _INTRANUKE_FATES_H_
