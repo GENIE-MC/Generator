a5a2cc4754841069f89cb3c281dc77b418d3a314
Steven Gardiner <gardiner@fnal.gov>
2025-09-15 15:05:35 +0100

    Remove references to Pythia 6 in LinkDef.h files

--- src/Physics/Hadronization/LinkDef.h
+++ src/Physics/Hadronization/LinkDef.h
@@ -9,17 +9,14 @@
 //#pragma link C++ namespace genie::utils::fragmrec;
 
 #pragma link C++ class genie::PythiaBaseHadro2019;
-#pragma link C++ class genie::Pythia6Hadro2019;
 #pragma link C++ class genie::Pythia8Hadro2019;
 
-#pragma link C++ class genie::LeptoHadPythia6;
 #pragma link C++ class genie::LeptoHadPythia8;
 
 #pragma link C++ class genie::AGKYLowW2019;
 #pragma link C++ class genie::AGKY2019;
 
 #pragma link C++ class genie::AGCharmPythiaBaseHadro2023;
-#pragma link C++ class genie::AGCharmPythia6Hadro2023;
 #pragma link C++ class genie::AGCharmPythia8Hadro2023;
 
 #pragma link C++ class genie::FragmentationFunctionI;
